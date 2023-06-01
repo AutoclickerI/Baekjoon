a32e0d0b08b0e9778ac2ba35c715da409a64758a
AutoclickerI <87906440+AutoclickerI@users.noreply.github.com>
2023-06-01 19:40:26 +0900

    [Platinum V] Title: 큰 수 만들기, Time: 104 ms, Memory: 32424 KB -BaekjoonHub

---
+++ 백준/Platinum/16496. 큰 수 만들기/큰 수 만들기.py
@@ -0,0 +1,9 @@
+import functools
+input()
+l=input().split()
+def sort1(a,b):
+    if a==b:return 0
+    return (a+b>b+a)*2-1
+l=sorted(l,key=functools.cmp_to_key(sort1))
+ans=''.join(l[::-1])
+print(ans if int(ans[0])else 0)
