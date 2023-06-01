f201a3a6d9fa056f2bbc322394ed86e8dfd326cd
AutoclickerI <87906440+AutoclickerI@users.noreply.github.com>
2023-06-01 19:50:06 +0900

    [Silver II] Title: 병사 배치하기, Time: 72 ms, Memory: 32904 KB -BaekjoonHub

---
+++ 백준/Silver/18353. 병사 배치하기/병사 배치하기.py
@@ -0,0 +1,9 @@
+from bisect import*
+n=int(input())
+l=list(map(int,input().split()))[::-1]
+d=[m:=0]
+c=[-2e+9]
+for i in range(n):
+ if c[-1]<l[i]:c+=[l[i]];d+=[m:=len(c)-1]
+ else:d+=[bisect_left(c,l[i])];c[d[-1]]=l[i]
+print(len(l)-m)
