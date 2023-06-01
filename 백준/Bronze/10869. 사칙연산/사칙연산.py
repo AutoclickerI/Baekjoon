aed98624a2bd749e2fc2ccc9420f592426d1c23b
AutoclickerI <87906440+AutoclickerI@users.noreply.github.com>
2023-06-01 18:49:34 +0900

    [Bronze V] Title: 사칙연산, Time: 68 ms, Memory: 30864 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.py
@@ -0,0 +1,6 @@
+list1 = input().split(' ')
+print(int(list1[0])+int(list1[1]))
+print(int(list1[0])-int(list1[1]))
+print(int(list1[0])*int(list1[1]))
+print(int(list1[0])//int(list1[1]))
+print(int(list1[0])%int(list1[1]))
