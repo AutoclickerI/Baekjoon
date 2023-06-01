cae07e19280122f97f901b5ecc90ee164f69fe0a
AutoclickerI <87906440+AutoclickerI@users.noreply.github.com>
2023-06-01 15:39:03 +0900

    [Silver I] Title: 하노이 탑, Time: 592 ms, Memory: 54524 KB -BaekjoonHub

---
+++ 백준/Silver/1914. 하노이 탑/하노이 탑.py
@@ -0,0 +1,27 @@
+def a(n):
+    if n == 1:
+        return ['1 3']
+    return b(n-1) + ['1 3'] + d(n-1)
+def b(n):
+    if n == 1:
+        return ['1 2']
+    return a(n-1) + ['1 2'] + f(n-1)
+def c(n):
+    if n == 1:
+        return ['2 1']
+    return d(n-1) + ['2 1'] + e(n-1)
+def d(n):
+    if n == 1:
+        return ['2 3']
+    return c(n-1) + ['2 3'] + a(n-1)
+def e(n):
+    if n == 1:
+        return ['3 1']
+    return f(n-1) + ['3 1'] + c(n-1)
+def f(n):
+    if n == 1:
+        return ['3 2']
+    return e(n-1) + ['3 2'] + b(n-1)
+n = int(input())
+print(2**n-1)
+if n<21:print(*a(n),sep='\n')
