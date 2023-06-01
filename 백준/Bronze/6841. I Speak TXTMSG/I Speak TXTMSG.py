5cd0fb09915f71f6b56893d833b4f39aaa760642
AutoclickerI <87906440+AutoclickerI@users.noreply.github.com>
2023-06-01 18:24:37 +0900

    [Bronze IV] Title: I Speak TXTMSG, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/6841. I Speak TXTMSG/I Speak TXTMSG.py
@@ -0,0 +1,1 @@
+for i in open(0):i=i[:-1];print({'CU':'see you',':-)':"I’m happy",':-(':"I’m unhappy",';-)':'wink',':-P':'stick out my tongue','(~.~)':'sleepy','TA':'totally awesome','CCC':'Canadian Computing Competition','CUZ':'because','TY':'thank-you','YW':"you’re welcome",'TTYL':'talk to you later'}.get(i,i))
