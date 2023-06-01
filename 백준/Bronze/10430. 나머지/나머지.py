cd42ac075d7e3ddae8bc3e7b0389f394319855ab
AutoclickerI <87906440+AutoclickerI@users.noreply.github.com>
2023-06-01 18:43:39 +0900

    [Bronze V] Title: 나머지, Time: 68 ms, Memory: 30864 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.py
@@ -0,0 +1,5 @@
+a = input().split(' ')
+print((int(a[0])+int(a[1]))%int(a[2]))
+print((int(a[0])%int(a[2])+int(a[1])%int(a[2]))%int(a[2]))
+print((int(a[0])*int(a[1]))%int(a[2]))
+print((int(a[0])%int(a[2])*int(a[1])%int(a[2]))%int(a[2]))
