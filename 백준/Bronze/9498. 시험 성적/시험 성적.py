e31cc9056844b758fe5466abe509dd49f557555d
AutoclickerI <87906440+AutoclickerI@users.noreply.github.com>
2023-06-01 18:34:46 +0900

    [Bronze V] Title: 시험 성적, Time: 72 ms, Memory: 30864 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,14 @@
+a = int(input())
+if a > 89:
+    print('A')
+else:
+    if a > 79:
+        print('B')
+    else:
+        if a > 69:
+            print('C')
+        else:
+            if a > 59:
+                print('D')
+            else:
+                print('F')
