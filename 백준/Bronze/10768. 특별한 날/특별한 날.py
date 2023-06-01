a88f9a4fa5024fafd5c1082a99027b5e23de00ac
AutoclickerI <87906440+AutoclickerI@users.noreply.github.com>
2023-06-01 18:45:21 +0900

    [Bronze IV] Title: 특별한 날, Time: 68 ms, Memory: 30860 KB -BaekjoonHub

---
+++ 백준/Bronze/10768. 특별한 날/특별한 날.py
@@ -0,0 +1,13 @@
+a=int(input())
+b=int(input())
+if a>2:
+    print('After')
+if a<2:
+    print('Before')
+if a==2:
+    if b>18:
+        print('After')
+    if b<18:
+        print('Before')
+    if b==18:
+        print('Special')
