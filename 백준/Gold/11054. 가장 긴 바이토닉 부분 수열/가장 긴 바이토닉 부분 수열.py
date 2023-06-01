6aca3fe2ddb6b770325427f01e96e67a08b4aa35
AutoclickerI <87906440+AutoclickerI@users.noreply.github.com>
2023-06-01 18:59:05 +0900

    [Gold IV] Title: 가장 긴 바이토닉 부분 수열, Time: 192 ms, Memory: 30864 KB -BaekjoonHub

---
+++ 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.py
@@ -0,0 +1,10 @@
+n=int(input())
+a=list(map(int,input().split()))
+h=[1]*n
+for i in range(1,n):
+    try:h[i]=max([h[j] for j in range(i)if a[j]<a[i]])+1
+    except:0
+for i in range(1,n):
+    try:h[i]=max(h[i],max([h[j] for j in range(i)if a[j]>a[i]])+1)
+    except:0
+print(max(h))
