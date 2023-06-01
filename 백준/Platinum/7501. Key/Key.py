7aa58769f66354c9c203556badd26b6b3b4ff0d9
AutoclickerI <87906440+AutoclickerI@users.noreply.github.com>
2023-06-01 18:25:52 +0900

    [Platinum I] Title: Key, Time: 56 ms, Memory: 39612 KB -BaekjoonHub

---
+++ 백준/Platinum/7501. Key/Key.py
@@ -0,0 +1,26 @@
+import sys
+import math
+from random import*
+sys.setrecursionlimit(9**9)
+
+def miller_rabin(n,a):
+    r=0;d=n-1
+    while d%2-1:r+=1;d//=2
+    x=pow(a,d,n)
+    if not 1!=x!=n-1:return 1
+    for i in range(r-1):
+        if(x:=pow(x,2,n))==n-1:return 1
+ 
+def is_prime(n):
+    if n<2:return
+    if n<4:return 1
+    if n%2-1:return
+    for i in[2,3,5,7,11,13,17,19,23,29,31,37,41]:
+        if n==i:return 1
+        if not miller_rabin(n,i):return
+    return 1
+ 
+p,q=map(int,input().split())
+for i in range(p,q+1):
+    if i==9 or is_prime(i):
+        print(i,end=' ')
