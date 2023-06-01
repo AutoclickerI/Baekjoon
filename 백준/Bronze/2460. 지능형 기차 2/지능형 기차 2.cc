598b2aebe22dc5c5b3115ea99f7091e66b8846f9
AutoclickerI <87906440+AutoclickerI@users.noreply.github.com>
2023-06-01 15:49:21 +0900

    [Bronze III] Title: 지능형 기차 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2460. 지능형 기차 2/지능형 기차 2.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a,b,c=0,d=0;
+    for(int i=0;i<10;i++){
+        cin>>a>>b;
+        c+=b-a;
+        if(c>d)
+            d=c;
+    }
+    cout<<d;
+}
