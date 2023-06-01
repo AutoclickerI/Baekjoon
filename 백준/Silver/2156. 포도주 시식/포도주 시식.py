e397b2268ae5bd98c1a10c204df00138444bb4ab
AutoclickerI <87906440+AutoclickerI@users.noreply.github.com>
2023-06-01 15:43:16 +0900

    [Silver I] Title: 포도주 시식, Time: 540 ms, Memory: 33196 KB -BaekjoonHub

---
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.py
@@ -0,0 +1,8 @@
+a=[int(input())for i in range(int(input()))]
+try:
+    b={1:[(a[0],0),(a[0],0)],2:[(a[1],0),(a[0]+a[1],1)]}
+    for i in range(2,len(a)):
+        b[i+1]=[(max(b[i-1][0][0]+a[i],b[i-1][1][0]+a[i],b[i][1][0]),0),(b[i][0][0]+a[i],1)]
+    print(max([b[i+1][j][0] for i in range(len(a))for j in range(2)]))
+except:
+    print(a[0])
