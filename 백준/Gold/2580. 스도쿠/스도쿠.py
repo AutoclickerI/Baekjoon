90a8c8b66a7577e123d71bb1a194e517cd060385
AutoclickerI <87906440+AutoclickerI@users.noreply.github.com>
2023-06-01 15:54:10 +0900

    [Gold IV] Title: 스도쿠, Time: 3472 ms, Memory: 147468 KB -BaekjoonHub

---
+++ 백준/Gold/2580. 스도쿠/스도쿠.py
@@ -0,0 +1,16 @@
+h=range(9)
+a=[input().split()for i in h]
+b=[(i,j)for i in h for j in h if a[i][j]=='0']
+def c(i,j):
+    x=a[i][j]
+    for k in h:
+        if(x==a[i][k]and j!=k)or(x==a[k][j]and i!=k)or(x==a[i//3*3+k//3][j//3*3+k%3]and(i//3*3+k//3!=i or j//3*3+k%3!=j)):return 1
+def f(n):
+    p,q=b[n]
+    if n==len(b):raise
+    for i in h:
+        a[p][q]=str(i+1)
+        if c(p,q)!=1:f(n+1)
+    a[p][q]='0'
+try:f(0)
+except:[print(' '.join(p))for p in a]
