d902e3ad656c231dc84be307685ddac5e19cc65c
AutoclickerI <87906440+AutoclickerI@users.noreply.github.com>
2023-06-01 19:02:50 +0900

    [Platinum V] Title: 피보나치 수의 제곱의 합, Time: 72 ms, Memory: 30864 KB -BaekjoonHub

---
+++ 백준/Platinum/11440. 피보나치 수의 제곱의 합/피보나치 수의 제곱의 합.py
@@ -0,0 +1,12 @@
+p=10**9+7
+y=[[1,1],[1,0]]
+u=[[1]]*2
+h=range(2)
+def z(y,n):
+ if n==1:return y
+ elif n%2:return x(z(y,n-1),y)
+ else:return z(x(y,y),n//2)
+def x(a,b):return [[sum([a[i][k]*b[k][j]for k in h])%p for j in range(len(b[0]))]for i in h]
+def f(n):return 1 if n<3 else x(z(y,n-2),u)[0][0]
+k=int(input())
+print(f(k)*f(k+1)%p)
