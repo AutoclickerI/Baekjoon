576c2b5d09752abca0a0009dacd7ffb03490b737
AutoclickerI <87906440+AutoclickerI@users.noreply.github.com>
2023-06-27 17:19:22 +0900

    [Gold I] Title: Points and Lines, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/13100. Points and Lines/Points and Lines.py
+++ 백준/Gold/13100. Points and Lines/Points and Lines.py
@@ -1,4 +1,3 @@
-from fractions import Fraction
 class BOJ:
     def __init__(self,a,b=None):
         self.type='point'
@@ -17,15 +16,15 @@ class BOJ:
         elif self.type==b.type=='line':
             A,B,C=self.data
             D,E,F=b.data
-            ret.data=(Fraction(B*F-E*C,A*E-B*D),Fraction(C*D-A*F,A*E-B*D))
+            ret.data=((B*F-E*C)/(A*E-B*D),(C*D-A*F)/(A*E-B*D))
         else:
             a=self
             if b.type=='line':a,b=b,a
             A,B,C=a.data
             D,E=b.data
             v1,v2=-2*C-B*E-A*D,-B*D+A*E
-            ret.data=(Fraction(v1*A-v2*B,A*A+B*B),Fraction(v1*B+v2*A,A*A+B*B))
+            ret.data=((v1*A-v2*B)/(A*A+B*B),(v1*B+v2*A)/(A*A+B*B))
         return ret
     def __repr__(self):
-        return f'{float(self.data[0]):.8f} {float(self.data[1]):.8f}'
+        return f'{self.data[0]:.8f} {self.data[1]:.8f}'
 for i in[*open(0)][:-1]:print(eval(i.replace('(','BOJ(')))
