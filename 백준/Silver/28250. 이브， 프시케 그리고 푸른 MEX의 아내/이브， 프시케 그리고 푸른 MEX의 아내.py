540b2c3385803c2b66dfcc41f8661065255edf92
AutoclickerI <87906440+AutoclickerI@users.noreply.github.com>
2023-07-03 14:03:56 +0900

    [Silver II] Title: 이브, 프시케 그리고 푸른 MEX의 아내, Time: 88 ms, Memory: 53144 KB -BaekjoonHub

--- 백준/Silver/28250. 이브， 프시케 그리고 푸른 MEX의 아내/이브， 프시케 그리고 푸른 MEX의 아내.py
+++ 백준/Silver/28250. 이브， 프시케 그리고 푸른 MEX의 아내/이브， 프시케 그리고 푸른 MEX의 아내.py
@@ -1,13 +1,4 @@
-l=[0,0,0]
-input()
-*L,=map(int,input().split())
-ans=0
-for i in L:
-    if i==0:
-        ans+=2*l[1]+l[0]+l[2]
-    elif i==1:
-        ans+=2*l[0]
-    else:
-        ans+=l[0]
-    l[min(i,2)]+=1
-print(ans)
+n,*l=map(int,open(0).read().split())
+f=l.count
+p=f(0)
+print(p*n+p*~p//2+p*f(1))
