7d14c817d5e76c6dad5f53e0c5aab301432e0729
AutoclickerI <87906440+AutoclickerI@users.noreply.github.com>
2023-07-05 22:06:06 +0900

    [Gold I] Title: 낚시왕, Time: 1024 ms, Memory: 33300 KB -BaekjoonHub

--- 백준/Gold/17143. 낚시왕/낚시왕.py
+++ 백준/Gold/17143. 낚시왕/낚시왕.py
@@ -1,33 +1,23 @@
-ans=0
-R,C,M=map(int,input().split())
-board=[[(0,0,0)]*C for _ in[0]*R]
-for _ in[0]*M:
-    r,c,s,d,z=map(int,input().split())
-    board[r-1][c-1]=z,s,d
-for i in range(C):
-    for j in range(R):
-        if board[j][i][0]:
-            ans+=board[j][i][0]
-            board[j][i]=0,0,0
-            break
-    tmp=[[(0,0,0)]*C for _ in[0]*R]
-    for K in range(C):
-        for l in range(R):
-            k=K
-            z,s,d=board[l][k]
-            if d<3:
-                l+=s*(2*d-3)
-                l%=(2*R-2)
-                if~-(0<=l<R):
-                    l=-l+2*R-2
-                    d=3-d
-                tmp[l][k]=max(tmp[l][k],(z,s,d))
-            else:
-                k+=s*(7-2*d)
-                k%=(2*C-2)
-                if~-(0<=k<C):
-                    k=-k+2*C-2
-                    d=7-d
-                tmp[l][k]=max(tmp[l][k],(z,s,d))
-    board=tmp
-print(ans)
+Z=range
+P,*Q=open(0)
+A=0
+R,C,M=map(int,P.split())
+B=[C*[(0,0,0)]for _ in[0]*R]
+for i in Q:r,c,s,d,z=map(int,i.split());B[r-1][c-1]=z,s,d
+for i in Z(C):
+ for j in Z(R):
+  if B[j][i][0]:A+=B[j][i][0];B[j][i]=0,0,0;break
+ T=[C*[(0,0,0)]for _ in[0]*R]
+ for K in Z(C):
+  for l in Z(R):
+   z,s,d=B[l][K]
+   if d<3:
+    l+=s*(2*d-3);l%=(2*R-2)
+    if~-(0<=l<R):l=-l+2*R-2;d=3-d
+    T[l][K]=max(T[l][K],(z,s,d))
+   else:
+    k=(K+s*(7-2*d))%(2*C-2)
+    if~-(0<=k<C):k=-k+2*C-2;d=7-d
+    T[l][k]=max(T[l][k],(z,s,d))
+ B=T
+print(A)
