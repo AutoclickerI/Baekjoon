b8689c52eb72acd9eb742afcce7cb77ed04421f1
AutoclickerI <87906440+AutoclickerI@users.noreply.github.com>
2023-07-07 14:21:48 +0900

    [Bronze III] Title: 과 조사하기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/28289. 과 조사하기/과 조사하기.py
+++ 백준/Bronze/28289. 과 조사하기/과 조사하기.py
@@ -1,7 +1,3 @@
-l=[0,0,0,0]
-for _ in[0]*int(input()):
-    p,q,r=map(int,input().split())
-    q-=q!=1
-    l[3]+=p==1
-    l[q-1]+=p!=1
+l=[0]*4
+for i in[*open(0)][1:]:p,q,r=map(int,i.split());l[3]+=p==1;l[q-1-(q!=1)]+=p!=1
 print(*l)
