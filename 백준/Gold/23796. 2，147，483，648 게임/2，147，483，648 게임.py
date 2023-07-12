8e701d6fb4506bdb0a31b8d938631f6e05774054
AutoclickerI <87906440+AutoclickerI@users.noreply.github.com>
2023-07-12 12:31:27 +0900

    [Gold V] Title: 2,147,483,648 게임, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/23796. 2，147，483，648 게임/2，147，483，648 게임.py
+++ 백준/Gold/23796. 2，147，483，648 게임/2，147，483，648 게임.py
@@ -8,18 +8,14 @@ if s=='L':l=[[*i]for i in zip(*l)]
 for _ in range(8):
     for i in range(1,8):
         for j in range(8):
-            if l[i-1][j]==0!=l[i][j]:
-                l[i-1][j],l[i][j]=l[i][j],0
+            if l[i-1][j]==0!=l[i][j]:l[i-1][j],l[i][j]=l[i][j],0
 for i in range(1,8):
     for j in range(8):
-        if l[i-1][j]==l[i][j]:
-            l[i-1][j]*=2
-            l[i][j]=0
+        if l[i-1][j]==l[i][j]:l[i-1][j]*=2;l[i][j]=0
 for _ in range(8):
     for i in range(1,8):
         for j in range(8):
-            if l[i-1][j]==0!=l[i][j]:
-                l[i-1][j],l[i][j]=l[i][j],0
+            if l[i-1][j]==0!=l[i][j]:l[i-1][j],l[i][j]=l[i][j],0
 if s=='D':l=l[::-1]
 if s=='R':l=[[*i][::-1]for i in zip(*l)][::-1]
 if s=='L':l=[[*i]for i in zip(*l)]
