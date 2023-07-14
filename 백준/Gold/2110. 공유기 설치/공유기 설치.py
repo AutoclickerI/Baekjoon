e2e22f15e53797829a7fa423c38045d1be476cd6
AutoclickerI <87906440+AutoclickerI@users.noreply.github.com>
2023-07-14 12:37:20 +0900

    [Gold IV] Title: 공유기 설치, Time: 608 ms, Memory: 53144 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.py
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.py
@@ -1,17 +1,10 @@
-N,C,*l=map(int,open(0).read().split())
-l.sort()
-s=1
-e=l[-1]+1
-def match():
-    d=l[0]
-    a=1
-    for i in l:
-        if m<=i-d:
-            a+=1
-            d=i
-    return a>=C
-while e-s>1:
-    m=(e+s)//2#새 거리
-    if match():s=m
-    else:e=m
-print(s)
+n,c,*x=map(int,open(l:=0).read().split())
+x.sort()
+h=x[-1]
+while l<=h:
+ m=(l+h)//2;b=1;p=x[0]
+ for q in x:
+  if q-p>=m:b+=1;p=q
+ if b<c:h=m-1
+ else:l=m+1
+print(h)
