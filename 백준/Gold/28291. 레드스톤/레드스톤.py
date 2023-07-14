c4847eb8d8e1fcf5447d4a0549db0466b90f4806
AutoclickerI <87906440+AutoclickerI@users.noreply.github.com>
2023-07-14 10:40:51 +0900

    [Gold V] Title: 레드스톤, Time: 208 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/28291. 레드스톤/레드스톤.py
+++ 백준/Gold/28291. 레드스톤/레드스톤.py
@@ -1,25 +1,18 @@
-redstone_block=0
-redstone_dust=1
-redstone_lamp=2
-W,H=map(int,input().split())
+ck=1
+t=0
+p=2
+i,_,*j=open(0)
+W,H=map(int,i.split())
 board=[H*[(0,0)]for _ in[0]*W]
 lamp=set()
-for _ in[0]*int(input()):
-    p,q,r=eval(input().replace(*' ,'))
-    if p==2:
-        lamp.add((q,r))
-    elif p==1:
-        board[q][r]=(0,1)
-    else:
-        board[q][r]=(16,1)
+for i in j:
+    P,q,r=eval(i[12:].replace(*' ,'))
+    if P==2:lamp.add((q,r))
+    else:board[q][r]=(16*P,1)
 for _ in[0]*15:
-    for i in range(W):
-        for j in range(H):
-            for dx,dy in(-1,0),(0,1),(0,-1),(1,0):
-                if 0<=i+dy<W and 0<=j+dx<H:
-                    p,q=board[i+dy][j+dx]
-                    board[i][j]=max(board[i][j],(p*q-1,board[i][j][1]))
-for i,j in lamp:
-    if board[i][j][0]==0:
-        exit(print('failed'))
+ for i in range(W):
+  for j in range(H):
+   for dx,dy in(-1,0),(0,1),(0,-1),(1,0):
+    if W>i+dy>=0<=j+dx<H:p,q=board[i+dy][j+dx];board[i][j]=max(board[i][j],(p*q-1,board[i][j][1]))
+for i,j in lamp:(0,0)==board[i][j]and exit(print('failed'))
 print('success')
