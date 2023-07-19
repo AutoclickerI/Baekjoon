4ad5c30358b984028c2ca7036aead4942f42bafe
AutoclickerI <87906440+AutoclickerI@users.noreply.github.com>
2023-07-19 12:40:18 +0900

    [Gold III] Title: IQ Test, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/1111. IQ Test/IQ Test.py
+++ 백준/Gold/1111. IQ Test/IQ Test.py
@@ -1,7 +1,8 @@
 p=print
 n,i,*l=map(int,open(0).read().split())
 n>2or exit(p(['A',i][n==2and i==l[0]]))
-a=l[0]-i and(l[1]-l[0])//(l[0]-i)or 0
+a=l[0]-i
+if a:a=(l[1]-l[0])//a
 b=l[0]-i*a
 [x*a+b==y or exit(p('B'))for x,y in zip(l,l[1:])]
 p(a*l[-1]+b)
