38c15ad26bdd7613fc721b2a2afbe7bde21cdb5e
AutoclickerI <87906440+AutoclickerI@users.noreply.github.com>
2023-07-21 12:49:39 +0900

    [Platinum IV] Title: I Am Knowledge, Time: 256 ms, Memory: 53812 KB -BaekjoonHub

---
+++ 백준/Platinum/28068. I Am Knowledge/I Am Knowledge.py
@@ -0,0 +1,21 @@
+asc=[]
+desc=[]
+for i in[*open(0)][1:]:
+    p,q=map(int,i.split())
+    if q<p:
+        desc+=(q,p),
+    else:
+        asc+=(p,q),
+#%%       
+asc.sort()
+desc.sort()
+e=0
+for i,j in asc:
+    e-=i
+    if e<0:exit(print(0))
+    e+=j
+for j,i in desc[::-1]:
+    e-=i
+    if e<0:exit(print(0))
+    e+=j
+print(1)
