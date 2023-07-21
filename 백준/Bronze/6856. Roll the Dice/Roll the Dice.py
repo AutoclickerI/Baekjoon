402cc9f98e809dcc1d2823dafe338843a9df5eaf
AutoclickerI <87906440+AutoclickerI@users.noreply.github.com>
2023-07-21 12:06:20 +0900

    [Bronze III] Title: Roll the Dice, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/6856. Roll the Dice/Roll the Dice.py
+++ 백준/Bronze/6856. Roll the Dice/Roll the Dice.py
@@ -1,3 +1,3 @@
 n,m=map(int,open(0))
-v=sum(i%m+i//m==8for i in range(n*m))
-print('There','airse'[v==1::2],v,'way'+'s'*(v!=1),'to get the sum 10.')
+v=min(9,m)+min(9,n)-9
+print('There','airse'[v==1::2],v*(v>0),'way'+'s'*(v!=1),'to get the sum 10.')
