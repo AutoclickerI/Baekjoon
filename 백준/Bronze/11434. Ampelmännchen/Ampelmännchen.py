9c49f7040a358674f7c8a8bceb3f8e8ba615aeb8
AutoclickerI <87906440+AutoclickerI@users.noreply.github.com>
2023-07-23 17:56:27 +0900

    [Bronze III] Title: Ampelmännchen, Time: 296 ms, Memory: 32468 KB -BaekjoonHub

--- 백준/Bronze/11434. Ampelmännchen/Ampelmännchen.py
+++ 백준/Bronze/11434. Ampelmännchen/Ampelmännchen.py
@@ -1,7 +1,5 @@
-for k in range(int(input())):
-    n,W,E=map(int,input().split())
-    hap=0
-    for _ in[0]*n:
-        p,q,r,s=map(int,input().split())
-        hap+=max(W*p+E*r,W*q+E*s)
-    print(f'Data Set {k+1}:\n{hap}\n')
+_,*l=map(int,open(k:=0).read().split())
+while l:
+    k+=1;n,W,E,*l=l
+    for _ in[H:=0]*n:p,q,r,s,*l=l;H+=max(W*p+E*r,W*q+E*s)
+    print(f'Data Set {k}:\n{H}\n')
