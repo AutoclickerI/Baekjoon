14ac39ab8e816e4b6e9aa7d4a8b21a454cb369ab
AutoclickerI <87906440+AutoclickerI@users.noreply.github.com>
2023-07-25 21:55:20 +0900

    [Ruby V] Title: Hey, Better Bettor, Time: 9076 ms, Memory: 115188 KB -BaekjoonHub

--- 백준/Ruby/8878. Hey， Better Bettor/Hey， Better Bettor.py
+++ 백준/Ruby/8878. Hey， Better Bettor/Hey， Better Bettor.py
@@ -2,6 +2,6 @@ x,p=map(float,input().split())
 a=0.0
 try:
  for W in range(2499):
-  for L in range(1,20529):r=(100-p)/p;K=r**-L;n=(1-K)/(r**W-K);a=max(a,W*n-L*(1-x/100)*(1-n))
+  for L in range(1,20529):r=100/p-1;n=1-r**-L;n/=(r**W+n-1);a=max(a,W*n-L*(1-x/100)*(1-n))
 except:0
 print(a)
