142ed7311ee88b95d679a39e334ae6bd3ac4a6fa
AutoclickerI <87906440+AutoclickerI@users.noreply.github.com>
2023-07-25 22:07:19 +0900

    [Bronze III] Title: Secret Instructions, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/21614. Secret Instructions/Secret Instructions.py
+++ 백준/Bronze/21614. Secret Instructions/Secret Instructions.py
@@ -1,9 +1,5 @@
 while(s:=input())!='99999':
-    if s[:2]=='00':
-        0
-    elif sum(map(int,s[:2]))%2:
-        d='left'
-    else:
-        d='right'
+    if s[:2]=='00':0
+    else:d=['right','left'][sum(map(int,s[:2]))%2]
     print(d,s[2:])
     
