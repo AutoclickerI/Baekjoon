224233baa84088056e62b41b02d75bf1e2910f0e
AutoclickerI <87906440+AutoclickerI@users.noreply.github.com>
2023-07-26 10:09:26 +0900

    [Bronze III] Title: Lobby, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/13063. Lobby/Lobby.py
+++ 백준/Bronze/13063. Lobby/Lobby.py
@@ -1,3 +1,1 @@
-while(i:=input())>'00':
-    p,q,r=map(int,i.split());n=p//2+1-q
-    print(-(q+r+n>p)or max(0,n))
+while(i:=input())>'00':p,q,r=map(int,i.split());n=p//2+1-q;print(-(q+r+n>p)or max(0,n))
