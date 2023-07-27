e4266b83ca97b0dcdb0ebcf84463d885691c96d8
AutoclickerI <87906440+AutoclickerI@users.noreply.github.com>
2023-07-27 18:12:20 +0900

    [Bronze III] Title: Isn’t It Funny How a Bear Likes Honey?, Time: 148 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/5163. Isn’t It Funny How a Bear Likes Honey？/Isn’t It Funny How a Bear Likes Honey？.py
+++ 백준/Bronze/5163. Isn’t It Funny How a Bear Likes Honey？/Isn’t It Funny How a Bear Likes Honey？.py
@@ -1,3 +1,1 @@
-for i in range(int(input())):
-    b,w=map(eval,input().split())
-    print(f'Data Set {i+1}:\n{"YNeos"[sum(4*3.14159265358979323846/3*eval(input())**3for _ in[0]*b)<w*1000::2]}\n')
+for i in range(int(input())):b,w=map(eval,input().split());print(f'Data Set {i+1}:\n{"YNeos"[sum(4*3.14159265358979/3*eval(input())**3for _ in[0]*b)<w*1000::2]}\n')
