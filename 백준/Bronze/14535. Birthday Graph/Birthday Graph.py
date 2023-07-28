2b32608e2dda9ec25b6ed0c56784befda638d94b
AutoclickerI <87906440+AutoclickerI@users.noreply.github.com>
2023-07-28 23:54:03 +0900

    [Bronze III] Title: Birthday Graph, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/14535. Birthday Graph/Birthday Graph.py
@@ -0,0 +1,8 @@
+i=0
+while s:=int(input()):
+    i+=1
+    l=[0]*13
+    for _ in[0]*s:l[int(input()[3:5])]+=1
+    print(f'Case #{i}:')
+    for p,q in enumerate(['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']):
+        print(q+':'+'*'*l[-~p])
