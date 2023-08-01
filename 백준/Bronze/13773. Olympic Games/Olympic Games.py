190c6a2ac3d6c55c085f3f7eb0f88016273d885b
AutoclickerI <87906440+AutoclickerI@users.noreply.github.com>
2023-08-01 17:43:02 +0900

    [Bronze III] Title: Olympic Games, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/13773. Olympic Games/Olympic Games.py
+++ 백준/Bronze/13773. Olympic Games/Olympic Games.py
@@ -1,6 +1,1 @@
-while s:=int(input()):
-    t=s//4
-    if s%4 or t<474:print(s,'No summer games')
-    elif t==479 or t==485 or t==486:print(s,'Games cancelled')
-    elif t>505:print(s,'No city yet chosen')
-    else:print(s,'Summer Olympics')
+for i in[*map(int,open(0))][:-1]:print(i,["No summer games",[["Summer Olympics","No city yet chosen"][i>2020],"Games cancelled"][i in[1916,1940,1944]]][i%4<1<i>1895])
