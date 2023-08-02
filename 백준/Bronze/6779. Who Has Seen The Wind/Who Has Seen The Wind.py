4fe3e5fb3fea41912c6fe06eb8f64fb1de7d16b3
AutoclickerI <87906440+AutoclickerI@users.noreply.github.com>
2023-08-02 14:07:52 +0900

    [Bronze III] Title: Who Has Seen The Wind, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/6779. Who Has Seen The Wind/Who Has Seen The Wind.py
+++ 백준/Bronze/6779. Who Has Seen The Wind/Who Has Seen The Wind.py
@@ -1,3 +1,3 @@
 h,m=map(int,open(0))
 *l,=map(lambda t:max(h*t**3-6*t**4+2*t*t+t,0),range(1,m+1))
-print('The balloon does not touch ground in the given time.'*(0 not in l)or f'The balloon first touches ground at hour: {l.index(0)+1}')
+print('The balloon','does not touch ground in the given time.'*all(l)or f'first touches ground at hour: {l.index(0)+1}')
