ec8a2bed60871ab4902e9cb86df0e464f6bcea24
AutoclickerI <87906440+AutoclickerI@users.noreply.github.com>
2023-08-09 01:02:14 +0900

    [Silver II] Title: 랜선 자르기, Time: 152 ms, Memory: 115908 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
@@ -1,4 +1,4 @@
 v,*o=open(0)
 s=[0,2**31]
-while s[1]-s[0]>1:l=sum(s)//2;s[sum(int(i)//l for i in o)<int(v.split()[1])]=l
+while-~s[0]<s[1]:l=sum(s)//2;s[sum(int(i)//l for i in o)<int(v.split()[1])]=l
 print(s[0])
