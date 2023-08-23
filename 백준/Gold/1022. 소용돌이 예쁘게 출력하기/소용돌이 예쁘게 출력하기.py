e39706bc76d4c536f31cc6f6d0d0754b730332fe
AutoclickerI <87906440+AutoclickerI@users.noreply.github.com>
2023-08-23 23:19:30 +0900

    [Gold III] Title: 소용돌이 예쁘게 출력하기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/1022. 소용돌이 예쁘게 출력하기/소용돌이 예쁘게 출력하기.py
+++ 백준/Gold/1022. 소용돌이 예쁘게 출력하기/소용돌이 예쁘게 출력하기.py
@@ -6,14 +6,14 @@ def get_num(i,j):
         return (i-j)**2-i+j+1 if i>0 else (i-j)**2-i+j+1
     if abs(i)>abs(j):
         if i>0:
-            return get_num(i,-i)-i-j
+            return (2*i)**2-2*i+1-i-j
         else:
-            return get_num(i,i)-i+j
+            return (2*i)**2+1-i+j
     else:
         if j>0:
-            return get_num(-j,j)+i+j
+            return (2*j)**2+2*j+1+i+j
         else:
-            return get_num(j,j)-i+j
+            return (2*j)**2+1-i+j
 l=[[str(get_num(i,j))for i in range(c1,c2+1)]for j in range(r1,r2+1)]
 m=max(max(len(i)for i in j)for j in l)
 l=[[' '*(m-len(i))+i for i in j]for j in l]
