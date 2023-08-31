016badb89927ade8f0ca9aab924e87932dd22362
AutoclickerI <87906440+AutoclickerI@users.noreply.github.com>
2023-09-01 01:38:11 +0900

    [Silver IV] Title: 나이트 투어, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1331. 나이트 투어/나이트 투어.py
+++ 백준/Silver/1331. 나이트 투어/나이트 투어.py
@@ -1,9 +1,2 @@
-l=[0]*36
-s,e=a,b=input()
-l[(ord(a)-65)*6+ord(b)-49]=1
-for _ in l[1:]:
-    c,d=input()
-    l[(ord(c)-65)*6+ord(d)-49]=1
-    if(ord(a)-ord(c))**2+(ord(b)-ord(d))**2!=5:exit(print('Invalid'))
-    a,b=c,d
-print(['Inv','V'][all(l)and((ord(s)-ord(a))**2+(ord(e)-ord(b))**2==5)]+'alid')
+*a,=open(0)
+print(((35<sum({0,1,2}=={*map(lambda x,y:abs(ord(x)-ord(y)),a[i],a[i-1])}for i in range(len({*a}))))*'V'or'Inv')+'alid')
