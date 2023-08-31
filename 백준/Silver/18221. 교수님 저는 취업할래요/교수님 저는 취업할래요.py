9da624963ab7093dfc3729a3d2d509815c468c25
AutoclickerI <87906440+AutoclickerI@users.noreply.github.com>
2023-09-01 01:11:16 +0900

    [Silver V] Title: 교수님 저는 취업할래요, Time: 176 ms, Memory: 49644 KB -BaekjoonHub

--- 백준/Silver/18221. 교수님 저는 취업할래요/교수님 저는 취업할래요.py
+++ 백준/Silver/18221. 교수님 저는 취업할래요/교수님 저는 취업할래요.py
@@ -1,16 +1,7 @@
-n=int(input())
-l=[input().split()for _ in[0]*n]
-for i in range(n):
-    for j in range(n):
-        if l[i][j]=='5':prof=i,j
-        if l[i][j]=='2':sg=i,j
-a,b=min(prof[0],sg[0]),max(prof[0],sg[0])
-c,d=min(prof[1],sg[1]),max(prof[1],sg[1])
-if(a-b)**2+(c-d)**2<25:
-    print(0)
-else:
-    cnt=0
-    for i in range(a,b+1):
-        for j in range(c,d+1):
-            cnt+=l[i][j]=='1'
-    print(+(cnt>2))
+n,*a=map(int,open(0).read().split())
+f=lambda i:divmod(a.index(i),n)
+x,y=f(2);w,z=f(5)
+p,q=sorted([x,w])
+r,s=sorted([y,z])
+c=sum(a[i*n+r:i*n-~s].count(1)for i in range(p,q+1))
+print(+(((x-w)**2+(y-z)**2>24)*c>2))
