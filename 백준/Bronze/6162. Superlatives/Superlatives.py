912cf15bbc0c6fc4eeeb0cf62aebd69675c1e35e
AutoclickerI <87906440+AutoclickerI@users.noreply.github.com>
2023-09-02 01:46:18 +0900

    [Bronze III] Title: Superlatives, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/6162. Superlatives/Superlatives.py
+++ 백준/Bronze/6162. Superlatives/Superlatives.py
@@ -1,7 +1,8 @@
-for i in range(int(input())):
-    p,q=map(int,input().split())
+for i in[*open(t:=0)][1:]:
+    t+=1
+    p,q=map(int,i.split())
     p-=1
-    print(f'Data Set {i+1}:')
+    print(f'Data Set {t}:')
     if p<q:
         print(end='no ')
     else:
