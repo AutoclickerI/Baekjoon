fdb272db90d0b934b239629ba8cf8b2b972e9b92
AutoclickerI <87906440+AutoclickerI@users.noreply.github.com>
2023-09-03 02:02:25 +0900

    [Bronze III] Title: 割り勘, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/22380. 割り勘/割り勘.py
+++ 백준/Bronze/22380. 割り勘/割り勘.py
@@ -1,1 +1,2 @@
-while(s:=input())>'00':p,q=map(int,s.split());print(sum(min(i,q//p)for i in map(int,input().split())))
+*l,_=open(0)
+while l:p,q=map(int,l.pop(0).split());print(sum(min(i,q//p)for i in map(int,l.pop(0).split())))
