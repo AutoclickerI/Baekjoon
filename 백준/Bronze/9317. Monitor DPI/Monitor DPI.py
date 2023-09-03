a458167b7596145016a19f2472db04fcbcc20751
AutoclickerI <87906440+AutoclickerI@users.noreply.github.com>
2023-09-03 20:05:18 +0900

    [Bronze III] Title: Monitor DPI, Time: 92 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/9317. Monitor DPI/Monitor DPI.py
+++ 백준/Bronze/9317. Monitor DPI/Monitor DPI.py
@@ -1,3 +1,3 @@
-while(s:=input())>'00':
-    p,q,r=map(eval,s.split())
+for i in[*open(0)][:-1]:
+    p,q,r=map(eval,i.split())
     print(f'Horizontal DPI: {q*337**.5/16/p:.2f}\nVertical DPI: {r*337**.5/9/p:.2f}')
