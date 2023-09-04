e0d110f14cba1187a1afec5384e289b017b53552
AutoclickerI <87906440+AutoclickerI@users.noreply.github.com>
2023-09-04 21:42:12 +0900

    [Bronze III] Title: When Do We Finish?, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/5074. When Do We Finish？/When Do We Finish？.py
+++ 백준/Bronze/5074. When Do We Finish？/When Do We Finish？.py
@@ -1,4 +1,1 @@
-for i in[*open(0)][:-1]:
-    p,q,r,s=map(int,i.replace(*': ').split())
-    t,h=divmod((p+r)*60+q+s,1440)
-    print(f'{h//60:02}:{h%60:02}'+f' +{t}'*min(t,1))
+for i in[*open(0)][:-1]:a,b,c,d=map(int,i.replace(*': ').split());t=(a+c)*60+b+d;print('%02d:%02d'%(t//60%24,t%60),f'+{t//1440}'*(t>1439))
