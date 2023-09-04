ee0baf93942d52cf9b67f004e9cd4335c28ec65b
AutoclickerI <87906440+AutoclickerI@users.noreply.github.com>
2023-09-04 22:49:55 +0900

    [Bronze III] Title: More Dice, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/9288. More Dice/More Dice.py
+++ 백준/Bronze/9288. More Dice/More Dice.py
@@ -1,6 +1,1 @@
-for i in range(int(input())):
-    print(f'Case {i+1}:')
-    n=int(input())
-    for i in range(1,n//2+1):
-        if n-i<7:
-            print(f'({i},{n-i})')
+for i in[*open(c:=0)][1:]:c+=1;print(f'Case {c}:',*[f'({p},{int(i)-p})'for p in range(1,7)if p<=int(i)-p<7])
