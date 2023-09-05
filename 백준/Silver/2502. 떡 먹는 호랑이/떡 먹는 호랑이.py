1bc86297686111ddb590e8e1933df581966d93d4
AutoclickerI <87906440+AutoclickerI@users.noreply.github.com>
2023-09-05 19:36:14 +0900

    [Silver I] Title: 떡 먹는 호랑이, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2502. 떡 먹는 호랑이/떡 먹는 호랑이.py
+++ 백준/Silver/2502. 떡 먹는 호랑이/떡 먹는 호랑이.py
@@ -1,5 +1,5 @@
-D,K=map(int,input().split())
-a,b=(1,0),(0,1)
-for _ in[0]*~-D:a,b=b,(a[0]+b[0],a[1]+b[1])
-for i in range(1,10**6):
-    if(K-a[0]*i)%a[1]==0:exit(print(i,(K-a[0]*i)//a[1],sep='\n'))
+d,k=map(int,input().split())
+x=-1
+for a in[y:=1]*d:x,y=y,x+y
+while(b:=k-a*x)%y:a+=1
+print(a,"\n",b//y)
