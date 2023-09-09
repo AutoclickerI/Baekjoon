8168a709c42b9e6cae15b98dde8f0565a6cbca52
AutoclickerI <87906440+AutoclickerI@users.noreply.github.com>
2023-09-09 10:17:04 +0900

    [Gold V] Title: Acowdemia I, Time: 124 ms, Memory: 42172 KB -BaekjoonHub

--- 백준/Gold/21820. Acowdemia I/Acowdemia I.py
+++ 백준/Gold/21820. Acowdemia I/Acowdemia I.py
@@ -1,16 +1,14 @@
 N,L=map(int,input().split())
 l=sorted(map(int,input().split()))
-from heapq import*
-hq=[l.pop()]
-added=[]
-hval=1
-m=1
-while l and ((hq[0]>=hval-1 if hq else 1) or L) and (hval<=added[0]if added else 1):
-    hval+=1
-    heappush(hq,l.pop())
-    while L and hq and hq[0]<hval:
-        heappush(added,heappop(hq)+1)
-        L-=1
-    if (hq[0]>=hval if hq else 1) and (hval<=added[0]if added else 1):
-        m=len(hq)+len(added)
-print(m)
+hindex=0
+for i,j in zip(range(N),l[::-1]):
+    if i<j:hindex=i+1
+s=min(hindex+1,N)
+while L and s:
+    l[-s]+=1
+    s-=1
+    L-=1
+hindex=0
+for i,j in zip(range(N),sorted(l)[::-1]):
+    if i<j:hindex=i+1
+print(hindex)
