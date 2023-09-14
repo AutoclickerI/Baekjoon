469236664d622a6dde734a5205ca79ea6e39ece5
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2023-09-14 11:31:33 +0900

    [Bronze II] Title: Palindroom, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/29829. Palindroom/Palindroom.py
+++ 백준/Bronze/29829. Palindroom/Palindroom.py
@@ -1,6 +1,3 @@
 *l,=map(int,input().split())
-if sum(i>j for i,j in zip(l,l[::-1]))>1:
-    print('EI')
-else:
-    print('JAH')
-    print(*l[:len(l)//2]+l[:-~len(l)//2][::-1])
+if sum(i>j for i,j in zip(l,l[::-1]))>1:print('EI')
+else:print('JAH',*l[:len(l)//2]+l[~-len(l)//2::-1])
