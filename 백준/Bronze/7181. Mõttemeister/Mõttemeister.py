6688eed25bbb0ff352b1a70ab8771a68ce7eecf9
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2023-09-17 19:32:47 +0900

    [Bronze III] Title: Mõttemeister, Time: 64 ms, Memory: 32276 KB -BaekjoonHub

--- 백준/Bronze/7181. Mõttemeister/Mõttemeister.py
+++ 백준/Bronze/7181. Mõttemeister/Mõttemeister.py
@@ -1,4 +1,3 @@
-s=input()
-for _ in[0]*int(input()):
-    t=input()
-    print(sum(i in s for i in t),sum(i==j for i,j in zip(s,t)))
+s,_,*l=open(0)
+s=s[:-1]
+for t in l:print(sum(i in s for i in t),sum(i==j for i,j in zip(s,t)))
