d6d31350ca723430506eaedeef36e11214f456a7
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2023-09-17 20:38:51 +0900

    [Bronze II] Title: Tabbtabbande, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/24197. Tabbtabbande/Tabbtabbande.py
+++ 백준/Bronze/24197. Tabbtabbande/Tabbtabbande.py
@@ -1,8 +1,4 @@
-N,_=map(int,input().split())
-*l,=map(int,input().split())
+N,_,*l=map(int,open(a:=0).read().split())
 s=1
-a=0
-for i in l:
-    a+=min((i-s)%N,(s-i)%N)
-    s=i
+for i in l:a+=min((i-s)%N,(s-i)%N);s=i
 print(a)
