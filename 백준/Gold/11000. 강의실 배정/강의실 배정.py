6c030b56785aa5266921400029dda70f203fdfa4
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2023-09-21 17:32:01 +0900

    [Gold V] Title: 강의실 배정, Time: 336 ms, Memory: 87564 KB -BaekjoonHub

--- 백준/Gold/11000. 강의실 배정/강의실 배정.py
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.py
@@ -1,9 +1,6 @@
 from heapq import*
-_,*l=[[*map(int,i.split())]for i in open(0)]
 hq=[-1]
-for p,q in sorted(l):
-    if hq[0]<=p:
-        heapreplace(hq,q)
-    else:
-        heappush(hq,q)
+for p,q in sorted([*map(int,i.split())]for i in[*open(0)][1:]):
+    if hq[0]<=p:heapreplace(hq,q)
+    else:heappush(hq,q)
 print(len(hq))
