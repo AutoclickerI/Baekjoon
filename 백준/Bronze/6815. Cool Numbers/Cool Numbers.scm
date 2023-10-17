ad3e9eefe39d57bff78a673ddf6052d755bd4b60
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2023-10-17 09:52:34 +0900

    [Bronze II] Title: Cool Numbers, Time: 4 ms, Memory: 17508 KB -BaekjoonHub

--- 백준/Bronze/6815. Cool Numbers/Cool Numbers.scm
+++ 백준/Bronze/6815. Cool Numbers/Cool Numbers.scm
@@ -1,5 +1,3 @@
-(define s (read)) 
-(define e (read))
-(define (sum n)
-  (let ((val (expt n 6))) (if (> val e) 0 (+ (or (and (>= val s) 1) 0) (sum (+ n 1))))))
-(print (sum 1))
+(define (sum n s e)
+  (let ((val (expt n 6))) (if (> val e) 0 (+ (or (and (>= val s) 1) 0) (sum (+ n 1) s e)))))
+(print (sum 1 (read) (read)))
