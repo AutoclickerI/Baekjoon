e6f9aecddf2e09efd45bcef9682f602682bf2472
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2023-11-03 20:14:53 +0900

    [Bronze I] Title: Расколбас с Франкенштейном, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/28535. Расколбас с Франкенштейном/Расколбас с Франкенштейном.py
@@ -0,0 +1,10 @@
+a,b,c=input().split()
+for i in range(1):
+            if 'R'in a+c:
+                print('R')
+            elif a==c=='N'and b in'+*':
+                print('N')
+            elif b=='/'or'Q'in a+c:
+                print('Q')
+            else:
+                print('Z')
