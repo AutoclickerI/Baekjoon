1e44faad171d6860648133c3e933737c96017d92
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2023-11-11 15:10:51 +0900

    [Bronze II] Title: <code>apt upgrade</code>, Time: 80 ms, Memory: 42036 KB -BaekjoonHub

--- 백준/Bronze/30489. ＜code＞apt upgrade＜／code＞/＜code＞apt upgrade＜／code＞.py
+++ 백준/Bronze/30489. ＜code＞apt upgrade＜／code＞/＜code＞apt upgrade＜／code＞.py
@@ -1,3 +1,2 @@
-a,b,c=map(int,input().split())
-s=sum(l:=sorted(map(int,input().split())))
-print(sum(l[-b-c:])*100/s)
+n,a,b,*c=map(int,open(0).read().split())
+print(sum(sorted(c)[-a-b:])*100/sum(c))
