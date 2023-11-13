7c4e2f70139216cfcaff0c2a8c34192139cd286c
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2023-11-13 14:38:45 +0900

    [Bronze II] Title: 만취한 상범, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/6359. 만취한 상범/만취한 상범.py
+++ 백준/Bronze/6359. 만취한 상범/만취한 상범.py
@@ -1,8 +1,1 @@
-for n in[*map(int,open(0))][1:]:
-    l=[0]*n
-    for i in range(n):
-        j=i
-        while j<n:
-            l[j]^=1
-            j+=i+1
-    print(sum(l))
+print(*[int(int(i)**.5)for i in open(0)][1:])
