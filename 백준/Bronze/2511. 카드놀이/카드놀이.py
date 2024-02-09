f9dff4856e20c227de33c3ba60f2ff42bef682f8
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-02-09 22:27:17 +0900

    [Bronze II] Title: 카드놀이, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2511. 카드놀이/카드놀이.py
+++ 백준/Bronze/2511. 카드놀이/카드놀이.py
@@ -1,6 +1,6 @@
 x=y=w=0
-for i,j in zip(l:=open(0).read().split(),l[10:]):
+for i,j in zip(input().split(),input().split()):
  if i>j:x+=3;w=1
  elif i<j:y+=3;w=2
  else:x+=1;y+=1
-s="DAB"[w]+"AB";print(x,y,s[(x>y)-(x<y)])
+print(x,y,'DAB'[w]*(x==y)or'AB'[x<y])
