b5b44f41ef2a346c352c15ae2ab272834e0e2f56
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-02-18 10:56:57 +0900

    [Bronze I] Title: 캠핑, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/4796. 캠핑/캠핑.py
+++ 백준/Bronze/4796. 캠핑/캠핑.py
@@ -1,2 +1,2 @@
 c=0
-while(s:=input())>'1':L,P,V=map(int,s.split());c+=1;print(f'Case {c}:',min(V%P,L)+V//P*L)
+for s in[*open(0)][:-1]:L,P,V=map(int,s.split());c+=1;print(f'Case {c}:',min(V%P,L)+V//P*L)
