138529d07e312377f4c476849030e45fdcdd6e87
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-02-24 12:00:48 +0900

    [Silver II] Title: 부분수열의 합, Time: 140 ms, Memory: 143696 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -1,11 +1,4 @@
-N,S=map(int,input().split())
-b=[0]*4000001
-for i in map(int,input().split()):
-    tmp=[0]*4000001
-    tmp[i]+=1
-    for j in range(-2000000,2000001):
-        if abs(i+j)>2000000:
-            continue
-        tmp[i+j]+=b[j]+b[i+j]
-    b=tmp
-print(b[S])
+p,q=open(0)
+l=[0]
+for j in q.split():l+=[i+int(j)for i in l]
+print(l[1:].count(int(p[2:])))
