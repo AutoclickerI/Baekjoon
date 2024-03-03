c450ae76fbebc02f23682ea936f9feb2763e16dd
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-03-03 20:33:04 +0900

    [Bronze II] Title: 트로피 진열, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1668. 트로피 진열/트로피 진열.py
+++ 백준/Bronze/1668. 트로피 진열/트로피 진열.py
@@ -1,11 +1,9 @@
 _,*l=map(int,open(0))
 m=c=0
 for i in l:
-    c+=m<i
-    m=max(m,i)
+    c+=m<(m:=max(m,i))
 print(c)
 m=c=0
 for i in l[::-1]:
-    c+=m<i
-    m=max(m,i)
+    c+=m<(m:=max(m,i))
 print(c)
