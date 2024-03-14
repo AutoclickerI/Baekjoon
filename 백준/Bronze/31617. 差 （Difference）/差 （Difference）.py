eb624049a1d25077f9fac0dc2f8929a3b1666397
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-03-14 18:27:07 +0900

    [Bronze III] Title: 差 (Difference), Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/31617. 差 （Difference）/差 （Difference）.py
+++ 백준/Bronze/31617. 差 （Difference）/差 （Difference）.py
@@ -1,2 +1,2 @@
-(K,),_,(*A,),_,(*B,)=[map(int,i.split())for i in open(0)]
-print(sum(i+K==j for i in A for j in B))
+K,_,A,_,B=[[*map(int,i.split())]for i in open(0)]
+print(sum(j==i+K[0]for i in A for j in B))
