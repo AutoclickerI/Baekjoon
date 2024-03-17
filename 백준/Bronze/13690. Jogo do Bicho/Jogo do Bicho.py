39ef175643fd1e92efe169df544b53288f287b38
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-03-17 13:18:08 +0900

    [Bronze II] Title: Jogo do Bicho, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/13690. Jogo do Bicho/Jogo do Bicho.py
+++ 백준/Bronze/13690. Jogo do Bicho/Jogo do Bicho.py
@@ -1,4 +1,1 @@
-for i in[*open(0)][:-1]:
-    v,n,m,a=*map(eval,i.split()),16
-    for i in range(3):a=[50,500,3000][i]*((n-m)%10**(i+2)<1)or a
-    print('%.2f'%(float(v)*a*(~-n%100//4==~-m%100//4)))
+for i in[*open(0)][:-1]:v,n,m=map(eval,i.split());print(f'{v*max(16,*[50*[1,10,60][i]*((n-m)%10**(i+2)<1)for i in[0,1,2]])*(-n%100//4==-m%100//4):.2f}')
