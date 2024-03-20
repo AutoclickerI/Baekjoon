86878189f09d1ebdcaf0e25b2fca0d8973ff41ee
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-03-20 14:19:07 +0900

    [Bronze II] Title: Pabėgimo kambarys, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/30063. Pabėgimo kambarys/Pabėgimo kambarys.py
+++ 백준/Bronze/30063. Pabėgimo kambarys/Pabėgimo kambarys.py
@@ -1,7 +1,3 @@
 d={'A':2,'R':1,'K':1,'T':1,'S':1}
-input()
-c=1
-for i in input():
-    d[i]=(d.get(i)or 1)-1
-    c+=any(d.values())
+for i in[*open(0)][c:=1]:d[i]=(d.get(i)or 1)-1;c+=any(d.values())
 print(c)
