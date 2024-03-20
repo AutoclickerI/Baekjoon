fe2f93655831be02c085058afcfc1e2d333edf58
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-03-20 14:28:03 +0900

    [Bronze II] Title: Zombie Invasion, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/9433. Zombie Invasion/Zombie Invasion.py
+++ 백준/Bronze/9433. Zombie Invasion/Zombie Invasion.py
@@ -1,4 +1,1 @@
-for i in[*open(0)][1:]:
-    *l,=map(int,i.split())
-    for i in range(19)[::-1]:l[i]+=l[i+1]//2;l[i+1]%=2
-    print(*l)
+exec('*l,=map(int,input().split())\nfor i in range(19)[::-1]:l[i]+=l[i+1]//2;l[i+1]%=2\nprint(*l);'*int(input()))
