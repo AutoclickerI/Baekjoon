ec4f4de40769272bfd19d67b38538f2eafcb222c
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-04-01 21:24:55 +0900

    [Bronze II] Title: Beehives, Time: 56 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/24767. Beehives/Beehives.py
+++ 백준/Bronze/24767. Beehives/Beehives.py
@@ -1,14 +1,5 @@
-while 1:
-    s=input()
-    if s=='0.0 0':break
-    d,N=map(eval,s.split())
-    l=[[*map(eval,input().split())]for _ in[0]*N]
-    a=b=0
-    res=[0]*N
-    for i in range(N-1):
-        for j in range(i+1,N):
-            t=(l[i][0]-l[j][0])**2+(l[i][1]-l[j][1])**2<d*d
-            res[i]|=t
-            res[j]|=t
-    a=sum(res)
-    print(f'{a} sour, {N-a} sweet')
+while s:=input():
+ {*s}=={*'0. '}<exit();d,N=map(eval,s.split());l=[[*map(eval,input().split())]for _ in[0]*N];a=b=0;R=[0]*N
+ for i in range(N-1):
+  for j in range(i+1,N):t=(l[i][0]-l[j][0])**2+(l[i][1]-l[j][1])**2<d*d;R[i]|=t;R[j]|=t
+ print(a:=sum(R),'sour,',N-a,'sweet')
