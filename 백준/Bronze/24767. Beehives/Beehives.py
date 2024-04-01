b47704bd829e6826abafd04438eae245aa32b825
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-04-02 00:59:28 +0900

    [Bronze II] Title: Beehives, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/24767. Beehives/Beehives.py
+++ 백준/Bronze/24767. Beehives/Beehives.py
@@ -1,5 +1,8 @@
 while s:=input():
- {*s}=={*'0. '}<exit();d,N=map(eval,s.split());l=[[*map(eval,input().split())]for _ in[0]*N];a=b=0;R=[0]*N
+ {*s}=={*'0. '}<exit();d,N=map(eval,s.split());l=[input()for _ in[0]*N];a=b=0;R=[0]*N
+ for i in range(N):
+        p,q=map(eval,l[i].split())
+        l[i]=p+q*1j
  for i in range(N-1):
-  for j in range(i+1,N):t=(l[i][0]-l[j][0])**2+(l[i][1]-l[j][1])**2<d*d;R[i]|=t;R[j]|=t
+  for j in range(i+1,N):t=abs(l[i]-l[j])<d;R[i]|=t;R[j]|=t
  print(a:=sum(R),'sour,',N-a,'sweet')
