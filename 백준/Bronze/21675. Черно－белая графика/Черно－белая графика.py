603ddaf6829780f8f3c38d7cbc045e110360ed0e
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-04-02 22:47:37 +0900

    [Bronze II] Title: Черно-белая графика, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/21675. Черно－белая графика/Черно－белая графика.py
+++ 백준/Bronze/21675. Черно－белая графика/Черно－белая графика.py
@@ -1,2 +1,2 @@
 h,*p,s=open(0)
-for i,j in zip(p,p[int(h.split()[1]):]):print(*[s[2*int(x)+int(y)]for x,y in zip(i,j[:-1])],sep='')
+for i,j in zip(p,p[len(p)//2:]):print(*[s[2*int(x)+int(y)]for x,y in zip(i,j[:-1])],sep='')
