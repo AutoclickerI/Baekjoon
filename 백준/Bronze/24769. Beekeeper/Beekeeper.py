1dbaeb385f94ba31300cb92b42f48f68a886ade9
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-04-02 12:27:34 +0900

    [Bronze II] Title: Beekeeper, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/24769. Beekeeper/Beekeeper.py
+++ 백준/Bronze/24769. Beekeeper/Beekeeper.py
@@ -1,1 +1,1 @@
-while s:=int(input()):print(max([input()for _ in[0]*s],key=lambda s:sum(map(s.count,['aa','ee','ii','oo','uu','yy']))))
+while s:=int(input()):print(max([input()for _ in[0]*s],key=lambda s:sum(s.count(i*2)for i in'aeiouy')))
