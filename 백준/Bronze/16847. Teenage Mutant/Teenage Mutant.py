fc43b765c7050d0c0380b504b12a9f0d0a0099cd
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-04-02 18:34:00 +0900

    [Bronze II] Title: Teenage Mutant, Time: 68 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/16847. Teenage Mutant/Teenage Mutant.py
+++ 백준/Bronze/16847. Teenage Mutant/Teenage Mutant.py
@@ -1,7 +1,4 @@
 for i in range(int(input())):
-    n,k=map(int,input().split())
-    m=input()
-    l=[input()for _ in[0]*n]
-    a=0
-    for j in range(k):a+=all(m[j]!=o[j]for o in l)
-    print(f'Data Set {i+1}:\n{a}/{k}\n')
+ n,k=map(int,input().split());m,*l=[input()for _ in[0]*-~n];a=0
+ for j in range(k):a+=all(m[j]!=o[j]for o in l)
+ print(f'Data Set {i+1}:\n{a}/{k}\n')
