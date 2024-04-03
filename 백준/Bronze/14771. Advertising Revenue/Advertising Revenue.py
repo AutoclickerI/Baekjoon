57e174f45fd0cb63f7c1d071c9127736fe569ee9
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-04-03 18:59:54 +0900

    [Bronze II] Title: Advertising Revenue, Time: 208 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/14771. Advertising Revenue/Advertising Revenue.py
+++ 백준/Bronze/14771. Advertising Revenue/Advertising Revenue.py
@@ -1,6 +1,5 @@
-for i in range(int(input())):
-    n,v=map(int,input().split())
-    ans=0
-    ads=[[*map(int,input().split())]for _ in[0]*n]
-    for _ in[0]*v:p,q,r=map(int,input().split());ans+=ads[p-1][1]*(ads[p-1][0]==1or r==1)+ads[q-1][1]*(ads[q-1][0]==1or 1<r)
+I=lambda:map(int,input().split())
+for i in range(*I()):
+    n,v=I();ans=0;ads=[[*I()]for _ in[0]*n]
+    for _ in[0]*v:p,q,r=I();ans+=ads[p-1][1]*(ads[p-1][0]or r==1)+ads[q-1][1]*(ads[q-1][0]or 1<r)
     print(f'Data Set {i+1}:\n{ans}\n')
