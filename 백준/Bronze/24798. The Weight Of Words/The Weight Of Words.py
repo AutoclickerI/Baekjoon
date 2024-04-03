e50a67ddbeffaa31d2bbb0c7f4eecf3ffc211406
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-04-03 14:45:49 +0900

    [Bronze II] Title: The Weight Of Words, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/24798. The Weight Of Words/The Weight Of Words.py
+++ 백준/Bronze/24798. The Weight Of Words/The Weight Of Words.py
@@ -1,7 +1,2 @@
 l,w=map(int,input().split())
-a=' abcdefghijklmnopqrstuvwxyz'
-if 26*l<w or w<l:print('impossible')
-else:
-    d=w//l
-    r=w-d*l
-    print(a[d]*(l-r)+(a[d+1]*r if r else''))
+print(['impossible',''.join(chr(96+w//l+(i<w%l))for i in range(l))][l<=w<=l*26])
