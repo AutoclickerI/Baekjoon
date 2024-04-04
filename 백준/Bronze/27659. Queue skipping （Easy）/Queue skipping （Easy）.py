deea5ac368257018333480c73ae98252c6842cb8
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-04-04 11:26:06 +0900

    [Bronze II] Title: Queue skipping (Easy), Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/27659. Queue skipping （Easy）/Queue skipping （Easy）.py
+++ 백준/Bronze/27659. Queue skipping （Easy）/Queue skipping （Easy）.py
@@ -1,10 +1,5 @@
-for _ in[0]*int(input()):
-    input()
-    n,e=map(int,input().split())
-    *l,=range(n+1)
-    m=0
-    for i in[int(input())for _ in[0]*e][::-1]:
-        if l[i]:m=i;l[i]=0
-    for i in l:
-        if l[i]:m=i
-    print(m)
+t,*a=map(int,open(0).read().split())
+while a:
+ n,e=a[:2];x=a[2:2+e];a=a[2+e:];*q,=range(1,n+1)
+ for p in x:q.remove(p);q=[p]+q
+ print(q[-1])
