99c5443534a6ce0c2883451d21bef62ab160e4f7
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-04-06 14:17:09 +0900

    [Bronze I] Title: 임시 반장 정하기, Time: 812 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1268. 임시 반장 정하기/임시 반장 정하기.py
+++ 백준/Bronze/1268. 임시 반장 정하기/임시 반장 정하기.py
@@ -1,2 +1,2 @@
 l=[[*map(int,i.split())]for i in[*open(0)][1:]]
-print(max(enumerate(l),key=lambda s:(sum(any(j==k for j,k in zip(i,s[1]))for i in l),-s[0]))[0]+1)
+print(min(enumerate(l),key=lambda s:(sum(all(j!=k for j,k in zip(i,s[1]))for i in l),s[0]))[0]+1)
