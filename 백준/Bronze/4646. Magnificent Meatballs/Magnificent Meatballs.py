990c2dc89c19692c8975b664ba667d89288dedd5
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-04-08 11:21:09 +0900

    [Bronze II] Title: Magnificent Meatballs, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/4646. Magnificent Meatballs/Magnificent Meatballs.py
+++ 백준/Bronze/4646. Magnificent Meatballs/Magnificent Meatballs.py
@@ -1,6 +1,6 @@
 z=' stops at position '
 while next(l:=map(int,input().split())):
-    i=0;*a,b=[i:=i+j for j in l]
-    if(b&1^1)*b//2 in a:q=a.index(b//2)+1;q=f'Sam{z}{q} and Ella{z}{q+1}.'
-    else:q='No equal partitioning.'
-    print(q)
+ i=0;*a,b=[i:=i+j for j in l]
+ if(b&1^1)*b//2in a:q=a.index(b//2)+1;q=f'Sam{z}{q} and Ella{z}{q+1}.'
+ else:q='No equal partitioning.'
+ print(q)
