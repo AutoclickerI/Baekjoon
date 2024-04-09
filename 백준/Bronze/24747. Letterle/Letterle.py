369ec2f87a643416bbcd1ca7e8411fad41d198e6
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-04-09 16:21:04 +0900

    [Bronze II] Title: Letterle, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/24747. Letterle/Letterle.py
+++ 백준/Bronze/24747. Letterle/Letterle.py
@@ -1,7 +1,6 @@
 s=input()
+P=print
 for j in range(7):
-    p=input()
-    s==p<exit(print('WINNER'))
-    j==6<exit(print('LOSER'))
-    for i,j in zip(p,s):print(end='G'*(i==j)or'Y'*(i in s)or'X')
-    print()
+ p=input();s==p<exit(P('WINNER'));j>5<exit(P('LOSER'))
+ for i,j in zip(p,s):P(end='G'*(i==j)or'Y'*(i in s)or'X')
+ P()
