0d626f3317eb1e10147f19dd1edcb9a8ab9d8301
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-04-10 15:47:56 +0900

    [Silver V] Title: Context-Free Clock, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/4754. Context－Free Clock/Context－Free Clock.py
@@ -0,0 +1,5 @@
+while 1:
+    t,h,m,s=map(int,input().replace(*': ').split())
+    if t<0:break
+    t=((t*120-12*(m*60+s)+(x:=h*3600+m*60+s))%43200//11+x)%86400
+    print(f"{t//3600:02d}:{t%3600//60:02d}:{t%60:02d}")
