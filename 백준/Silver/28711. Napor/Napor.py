4b3b6b62e643d0678cdd4a03e30a0fd4e6f0990c
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-04-11 13:31:51 +0900

    [Silver V] Title: Napor, Time: 108 ms, Memory: 34728 KB -BaekjoonHub

--- 백준/Silver/28711. Napor/Napor.py
+++ 백준/Silver/28711. Napor/Napor.py
@@ -1,15 +1,2 @@
-ans = []
-for _ in range(int(input())):
-    cur, l = 0, 0
-    for c in input():
-        if c.isdigit():
-            cur = cur * 10 + int(c)
-            l += 1
-        else:
-            if cur or l:
-                ans.append(cur)
-            cur = 0
-            l = 0
-    if cur or l:
-        ans.append(cur)
-print(*sorted(ans))
+import re
+print(*sorted(map(int,re.findall('\d+',str(''.join(open(0))))[1:])))
