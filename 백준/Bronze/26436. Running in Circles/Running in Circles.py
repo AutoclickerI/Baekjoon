b5a0a1830d30c358a92706d6243c8f86afe14e91
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-04-11 12:12:54 +0900

    [Bronze I] Title: Running in Circles, Time: 15028 ms, Memory: 31120 KB, Score: 6 point -BaekjoonHub

--- 백준/Bronze/26436. Running in Circles/Running in Circles.py
+++ 백준/Bronze/26436. Running in Circles/Running in Circles.py
@@ -1,10 +1,16 @@
 for i in range(int(input())):
     L,N=map(int, input().split())
     a=p=0
-    d='C'
+    d = 'C'
     for _ in range(N):
-        D,C=input().split()
-        c,p=divmod(p*((C==d)*2-1)+int(D),L)
-        if c!=-1:d=C
-        a+=c
+        D, C = input().split()
+        D = int(D)
+        
+        if C == d:
+            c, p = divmod(p + D, L)
+        else:
+            c, p = divmod(-p + D, L)
+            if c != -1:
+                d = C
+        a += c
     print(f"Case #{i+1}: {a}")
