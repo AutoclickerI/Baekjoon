6ee670e604b9361a199812fa43607b8abd48d103
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-04-13 15:21:56 +0900

    [Bronze I] Title: Adjusting passwords, Time: 184 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/27682. Adjusting passwords/Adjusting passwords.py
+++ 백준/Bronze/27682. Adjusting passwords/Adjusting passwords.py
@@ -2,10 +2,5 @@ for _ in[0]*int(input()):
     input()
     pw=input()
     s=input()
-    for i in range(len(pw)+1):
-        if pw[:i]==s[:i]:
-            pos=i
-    if pos==len(s):
-        print(pw[pos:]+'*')
-    if pos<len(s):
-        print(min('*'+pw,'<'*(len(s)-pos)+pw[pos:],key=len)+'*')
+    pos=max(i*(pw[:i]==s[:i])for i in range(len(pw)+1))
+    print(min('*'+pw,'<'*(len(s)-pos)+pw[pos:],key=len)+'*')
