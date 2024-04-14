78703a4c0084b4af9e5477a953ecc4d700110189
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-04-14 13:00:04 +0900

    [Bronze I] Title: 타일, Time: 160 ms, Memory: 47604 KB -BaekjoonHub

--- 백준/Bronze/5556. 타일/타일.py
+++ 백준/Bronze/5556. 타일/타일.py
@@ -1,2 +1,2 @@
 N=int(input())
-exec('a,b=map(int,input().split());print(3+min(a,b,N+1-a,N+1-b)%-3);'*int(input()))
+exec('a,b=map(int,input().split());print(3-max(-a,-b,a+~N,b+~N)%3);'*int(input()))
