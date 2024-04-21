76abde630bc4a58c517bfd978975471ca52a1cc9
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-04-21 12:04:41 +0900

    [Bronze I] Title: 귀여운 수~ε٩(๑> ₃ <)۶з, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/17294. 귀여운 수～ε٩（๑＞ ₃ ＜）۶з/귀여운 수～ε٩（๑＞ ₃ ＜）۶з.py
+++ 백준/Bronze/17294. 귀여운 수～ε٩（๑＞ ₃ ＜）۶з/귀여운 수～ε٩（๑＞ ₃ ＜）۶з.py
@@ -1,2 +1,2 @@
-s=input()
-print(['◝(⑅•ᴗ•⑅)◜..°♡ 뀌요미!!','흥칫뿡!! <(￣ ﹌ ￣)>'][len({ord(i)-ord(j)for i,j in zip(s,s[1:])})>1])
+[s]=open(0,'rb')
+print(['◝(⑅•ᴗ•⑅)◜..°♡ 뀌요미!!','흥칫뿡!! <(￣ ﹌ ￣)>'][len({i-j for i,j in zip(s,s[1:])})>2])
