ea57d433a72a77501660621e4743c90654d391ec
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-04-23 17:38:46 +0900

    [Gold V] Title: IP-adresser, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/26902. IP－adresser/IP－adresser.py
+++ 백준/Gold/26902. IP－adresser/IP－adresser.py
@@ -1,15 +1,16 @@
 s=input()
 a=0
+R=range(1,4)
 def check(r):
     if r=='0':
         return 1
     if r[0]=='0':
         return 0
     return int(r)<256
-for i in range(3):
-    for j in range(3):
-        for k in range(3):
-            for l in range(3):
-                if i+j+k+l+4==len(s):
-                    a+=all(map(check,[s[:i+1],s[i+1:i+j+2],s[i+j+2:i+j+k+3],s[i+j+k+3:]]))
+for i in R:
+    for j in R:
+        for k in R:
+            for l in R:
+                if i+j+k+l==len(s):
+                    a+=all(map(check,[s[:i],s[i:i+j],s[i+j:i+j+k],s[i+j+k:]]))
 print(a)
