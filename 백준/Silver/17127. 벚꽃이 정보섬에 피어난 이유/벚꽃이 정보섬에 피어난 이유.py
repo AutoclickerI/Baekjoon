337a6dc0e070db9798d7bdf58158b565b2089081
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-04-24 21:47:23 +0900

    [Silver V] Title: 벚꽃이 정보섬에 피어난 이유, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/17127. 벚꽃이 정보섬에 피어난 이유/벚꽃이 정보섬에 피어난 이유.py
+++ 백준/Silver/17127. 벚꽃이 정보섬에 피어난 이유/벚꽃이 정보섬에 피어난 이유.py
@@ -2,4 +2,4 @@ n,l=open(0)
 n=int(n)
 l=l.split()
 R=range
-print(max(eval('+'.join('*'.join(s)for s in[l[:i],l[i:j],l[j:k],l[k:]]))for i in R(1,n-2)for j in R(i+1,n-1)for k in R(j+1,n)))
+print(max(eval('+'.join('*'.join(s)for s in[l[:k],l[k:j],l[j:i],l[i:]]))for i in R(n)for j in R(i)for k in R(1,j)))
