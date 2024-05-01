bfa4aa043096c70f2b014965e5a2f929ea9ed936
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-05-01 15:22:41 +0900

    [Gold V] Title: Googol String (Large), Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/12075. Googol String （Large）/Googol String （Large）.py
+++ 백준/Gold/12075. Googol String （Large）/Googol String （Large）.py
@@ -1,10 +1,1 @@
-def get(k):
-    if k<3:
-        return 0
-    if 2**k.bit_length()-k==k:
-        return 0
-    return 1^get(2**k.bit_length()-k)
-
-for n in[*open(i:=0)][1:]:
-    i+=1
-    print(f'Case #{i}:',get(int(n)))
+for n in[*map(int,open(i:=0))][1:]:i+=1;print(f'Case #{i}:',n//(n&-n)//2%2)
