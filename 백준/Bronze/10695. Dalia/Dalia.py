9eba8f9984ce90a135fee3a6becf516d8c359d5d
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-05-02 11:05:31 +0900

    [Bronze III] Title: Dalia, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10695. Dalia/Dalia.py
+++ 백준/Bronze/10695. Dalia/Dalia.py
@@ -1,2 +1,1 @@
-j=0
-for i in[*open(0)][1:]:_,a,b,c,d=map(int,i.split());j+=1;print(f'Case {j}:','NYOE S'[(abs(c-a),abs(d-b))in[(1,2),(2,1)]::2])
+for i in[*open(t:=0)][1:]:t+=1;n,a,b,c,d=map(int,i.split());print(f'Case {t}:','YNEOS'[{abs(c-a),abs(d-b)}!={1,2}::2])
