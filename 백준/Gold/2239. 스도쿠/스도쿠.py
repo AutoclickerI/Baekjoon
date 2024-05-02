e64945a20ace74373c199f4d00fb181ed534ef99
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-05-02 18:35:35 +0900

    [Gold IV] Title: 스도쿠, Time: 5412 ms, Memory: 153972 KB -BaekjoonHub

--- 백준/Gold/2239. 스도쿠/스도쿠.py
+++ 백준/Gold/2239. 스도쿠/스도쿠.py
@@ -1,8 +1,7 @@
 def f(k):
  k==len(z)!=[print(*a[i*9:i*9+9],sep='')for i in R(9)]<exit();r,c=z[k];s={*R(1,10)};x,y=r//3*3,c//3*3
  for i in R(9):s-={a[r*9+i],a[i*9+c]}
- for i in R(x,x+3):
-  for j in R(y,y+3):s-={a[i*9+j]}
+ s-={a[i*9+j+y]for i in R(x,x+3)for j in R(3)}
  for n in s:a[t:=r*9+c]=n;f(k+1);a[t]=0
 R=range
 a=[]
