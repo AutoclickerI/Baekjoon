327370a90df1c3b69d8d6d0bf54096201df7429e
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-05-03 13:33:36 +0900

    [Bronze III] Title: Rope Intranet (Small), Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/12571. Rope Intranet （Small）/Rope Intranet （Small）.py
+++ 백준/Bronze/12571. Rope Intranet （Small）/Rope Intranet （Small）.py
@@ -2,4 +2,4 @@ I=lambda:map(int,input().split())
 for i in range(*I()):
  [n]=I();a,b=I()
  if n==2:c,d=I()
- print(f"Case #{i+1}: {+(n==2>0>(a-c)*(b-d))}")
+ print(f'Case #{i+1}:',+(n==2>0>(a-c)*(b-d)))
