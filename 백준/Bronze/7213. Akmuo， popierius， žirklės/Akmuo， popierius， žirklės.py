3ea9727e7852b3122a9d84e198191e0ab20437e3
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-05-03 19:26:46 +0900

    [Bronze I] Title: Akmuo, popierius, žirklės, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/7213. Akmuo， popierius， žirklės/Akmuo， popierius， žirklės.py
@@ -0,0 +1,9 @@
+a1,p1,z1,a2,p2,z2=map(int,open(0).read().split())
+def solve(l,r,d):
+    score=0
+    for i in range(3):
+        cnt=min(l[i],r[(i-d)%3]);l[i]-=cnt;r[(i-d)%3]-=cnt;score+=cnt
+        if l[i]:cnt=min(l[i], r[i]);l[i]-=cnt;r[i]-=cnt
+        if l[i]:cnt=min(l[i],r[(i+d)%3]);l[i]-=cnt;r[(i+d)%3]-=cnt;score-=cnt
+    return score
+print(solve([a1,p1,z1],[a2,p2,z2],1),-solve([a1,p1,z1],[a2,p2,z2],-1))
