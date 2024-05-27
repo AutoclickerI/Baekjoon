497f8902328973d11f77845776aca48eaba02b1e
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-05-27 15:53:09 +0900

    [Silver V] Title: 2021은 무엇이 특별할까?, Time: 100 ms, Memory: 39904 KB -BaekjoonHub

---
+++ 백준/Silver/24039. 2021은 무엇이 특별할까？/2021은 무엇이 특별할까？.py
@@ -0,0 +1,42 @@
+import sys
+import math
+from random import*
+
+def miller_rabin(n,a):
+    r=0;d=n-1
+    while d%2-1:r+=1;d//=2
+    x=pow(a,d,n)
+    if not 1!=x!=n-1:return 1
+    for i in range(r-1):
+        if(x:=pow(x,2,n))==n-1:return 1
+ 
+def is_prime(n):
+    if n<2:return
+    if n<4:return 1
+    if n%2-1:return
+    for i in[2,3,5,7,11,13,17,19,23,29,31,37,41]:
+        if n==i:return 1
+        if not miller_rabin(n,i):return
+    return 1
+ 
+def pollardRho(n):
+    if is_prime(n):return n
+    if n==1:return 1
+    if n%2-1:return 2
+    x=y=randrange(2, n);c=randrange(1, n);d=1
+    f=lambda v:((v*v%n)+c+n)%n
+    while d == 1:
+        x=f(x);y=f(f(y));d=math.gcd(abs(x-y), n)
+        if d==n:return pollardRho(n)
+    if is_prime(d):return d
+    else:return pollardRho(d)
+def check(n):
+    N=n
+    m=1e9
+    while n-1:m=min(m,x:=pollardRho(n));n//=x
+    t=m+1
+    while not is_prime(t):t+=1
+    return m*t==N
+n=int(input())+1
+while not check(n):n+=1
+print(n)
