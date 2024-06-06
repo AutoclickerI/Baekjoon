fcde5e006d56be14404776f960b10f7bd88cfb4d
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-06-06 19:35:11 +0900

    [Gold III] Title: OTPOR, Time: 56 ms, Memory: 35520 KB -BaekjoonHub

--- 백준/Gold/11957. OTPOR/OTPOR.py
+++ 백준/Gold/11957. OTPOR/OTPOR.py
@@ -1,11 +1,6 @@
-class resistance:
-    def __init__(self,R):
-        self.R=R
-    def __or__(self,R):
-        return resistance(1/(1/R.R+1/self.R))
-    def __sub__(self,R):
-        return resistance(R.R+self.R)
-input()
-for i,j in enumerate(input().split(),1):
-    exec(f'R{i}=resistance({j})')
-print(eval(input()).R)
+class r:
+ def __init__(s,R):s.R=R
+ __or__=lambda s,R:r(1/(1/R.R+1/s.R));__sub__=lambda s,R:r(R.R+s.R)
+_,p,q=open(0)
+for i,j in enumerate(p.split(),1):exec(f'R{i}=r({j})')
+print(eval(q).R)
