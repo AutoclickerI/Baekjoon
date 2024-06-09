0bb15f16eaa9b75f3fafdd41dd63df53a1f109c8
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-06-09 15:12:57 +0900

    [Gold III] Title: OTPOR, Time: 48 ms, Memory: 35536 KB -BaekjoonHub

--- 백준/Gold/11957. OTPOR/OTPOR.py
+++ 백준/Gold/11957. OTPOR/OTPOR.py
@@ -1,6 +1,4 @@
-class r:
- def __init__(s,R):s.R=R
- __or__=lambda s,R:r(1/(1/R.R+1/s.R));__sub__=lambda s,R:r(R.R+s.R)
-_,p,q=open(0)
-for i,j in enumerate(p.split(),1):exec(f'R{i}=r({j})')
-print(eval(q).R)
+class R(float):__or__=lambda s,r:R(r*s/(r+s));__sub__=lambda s,r:R(r+s)
+_,p,q=open(i:=0)
+for j in p.split():i+=1;exec(f'R{i}=R({j})')
+print(eval(q))
