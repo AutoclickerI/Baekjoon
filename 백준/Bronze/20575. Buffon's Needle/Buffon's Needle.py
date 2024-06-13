c5b349a356f9fa02926974e8b56ebea4663eaa65
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-06-13 21:15:08 +0900

    [Bronze I] Title: Buffon's Needle, Time: 52 ms, Memory: 32140 KB -BaekjoonHub

--- 백준/Bronze/20575. Buffon's Needle/Buffon's Needle.py
+++ 백준/Bronze/20575. Buffon's Needle/Buffon's Needle.py
@@ -1,10 +1,3 @@
-N=int(input())
-
-c = 0
-
-for _ in[0]*N:
-    x1, y1, x2, y2 = map(eval,input().split())
-    min_x, max_x = sorted([x1, x2])
-    c += min_x//1 < max_x//1
-
-print(2 * N / c)
+N,*l=open(c:=0)
+for i in l:x,*t=i.replace(*'. ').split();c+=x!=t[3]
+print(2*int(N)/c)
