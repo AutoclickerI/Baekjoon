94c41c9cb2b37c926d00f31593e8fbfcb5b51580
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-06-15 18:02:40 +0900

    [Gold V] Title: Cow Line, Time: 1072 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/5963. Cow Line/Cow Line.py
+++ 백준/Gold/5963. Cow Line/Cow Line.py
@@ -1,18 +1,12 @@
 l=[1]
 for i in range(20):l+=l[-1]*-~i,
 N,K=map(int,input().split())
-def get_perm(tot,n,li):
-    if n<1:
-        return li
-    return [li.pop(n//l[tot-1])]+get_perm(tot-1,n%l[tot-1],li)
+p=lambda tot,n,li:(n<1)*li or[li.pop(n//l[tot-1])]+p(tot-1,n%l[tot-1],li)
 
-def get_order(tot,li):
-    if li==[]:
-        return 1
-    return l[tot-1]*sorted(li).index(li[0])+get_order(tot-1,li[1:])
+o=lambda tot,li:+(li==[])or l[tot-1]*sorted(li).index(li[0])+o(tot-1,li[1:])
 
 for _ in[0]*K:
     if'P'<input():
-        print(get_order(N,[*map(int,input().split())]))
+        print(o(N,[*map(int,input().split())]))
     else:
-        print(*get_perm(N,int(input())-1,[*range(1,N+1)]))
+        print(*p(N,int(input())-1,[*range(1,N+1)]))
