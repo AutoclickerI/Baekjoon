b8ebff7f55fa15100aad32b9f15cd2949d001a2f
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-06-19 16:10:44 +0900

    [Gold II] Title: 직사각형 만들기, Time: 856 ms, Memory: 245016 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/30091. 직사각형 만들기/직사각형 만들기.py
+++ 백준/Gold/30091. 직사각형 만들기/직사각형 만들기.py
@@ -1,9 +1,15 @@
-n=int(input())
-l=sorted(map(int,input().split()))
+n,*l=map(int,open(0).read().split())
+c={}
+for i in l:c[i]=c.get(i,0)+1
+l=sorted(c)
 d={}
 m=0
-for i in range(n):
-    for j in range(i+1,n):
-        d[l[i]+l[j]]=d.get(l[i]+l[j],0)+l[i]*l[j]
-        m=max(m,d[l[i]+l[j]])
+for i in range(len(l)):
+    a=l[i]
+    d[a+a]=d.get(a+a,0)+c[a]//2*a*a
+    m=max(m,d[a+a])
+    for j in range(i+1,len(l)):
+        b=l[j]
+        d[a+b]=d.get(a+b,0)+a*b*min(c[a],c[b])
+        m=max(m,d[a+b])
 print(m)
