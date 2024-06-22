1152d3d69528d558666eb802758429e299ed9952
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-06-22 12:59:13 +0900

    [Bronze I] Title: 단어 퍼즐, Time: 684 ms, Memory: 35292 KB -BaekjoonHub

--- 백준/Bronze/9946. 단어 퍼즐/단어 퍼즐.py
+++ 백준/Bronze/9946. 단어 퍼즐/단어 퍼즐.py
@@ -1,2 +1,3 @@
+f=sorted
 *l,_,_=open(i:=0)
-while l:a,b,*l=l;i+=1;print(f'Case {i}:',['different','same'][sorted(a)==sorted(b)])
+while l:a,b,*l=l;i+=1;print(f'Case {i}:',['different','same'][f(a)==f(b)])
