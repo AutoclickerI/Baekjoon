5932cfa5774e253e250761df3c0b74a28d9c3fe0
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-06-23 17:04:09 +0900

    [Gold II] Title: 買い物 2 (Shopping 2), Time: 1700 ms, Memory: 182552 KB, Score: 20 point -BaekjoonHub

---
+++ 백준/Gold/31619. 買い物 2 （Shopping 2）/買い物 2 （Shopping 2）.py
@@ -0,0 +1,14 @@
+import sys
+input=sys.stdin.readline
+N,M,Q=map(int,input().split())
+price=[[*map(int,input().split())]for _ in[0]*N]
+query=[[*map(int,input().split())]for _ in[0]*Q]
+price_sum=[0]
+for p,_ in price:
+    price_sum+=price_sum[-1]+p,
+*N_price,=eval('[[-1,0]],'*-~M)
+for i,[p,m]in enumerate(price):
+    N_price[m]+=[i,N_price[m][-1][1]+p//2],
+from bisect import*
+for m,s,e in query:
+    print(price_sum[e]-price_sum[s-1]-N_price[m][min(bisect_left(N_price[m],[e-1,0]),len(N_price[m])-1)][1]+N_price[m][bisect_left(N_price[m],[s-1,0])-1][1])
