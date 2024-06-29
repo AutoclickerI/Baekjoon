95b3064b85348ac4dd8725217b41c6edf5ef6b42
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-06-29 11:56:15 +0900

    [Bronze II] Title: Check the Check, Time: 6456 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/15406. Check the Check/Check the Check.py
+++ 백준/Bronze/15406. Check the Check/Check the Check.py
@@ -1,2 +1,3 @@
-*l,n=map(eval,[i.replace(*' *')for i in[*open(0)][1::2]])
-print(['PAY','PROTEST'][sum(l)<n])
+s=0
+while'TOTAL'!=input():s+=eval(input().replace(*' *'))
+print(['PAY','PROTEST'][s<int(input())])
