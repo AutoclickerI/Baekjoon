b5153b4655b2a0e907fc01e91f392b058edee131
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-07-07 22:34:22 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.py
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.py
@@ -1,2 +1,2 @@
 I=input
-exec('l=[I().split()[1]for _ in[0]*int(I())];r=1\nfor e in{*l}:r*=l.count(e)+1\nprint(r-1);'*int(I()))
+exec('l=[I().split()[1]for _ in[0]*int(I())];r=1\nfor i in map(l.count,{*l}):r*=i+1\nprint(r-1);'*int(I()))
