28482b53d8b3188fed200a7aa0f46e89f2c22d87
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-07-08 14:03:29 +0900

    [Bronze II] Title: Between the Mountains, Time: 1868 ms, Memory: 31580 KB -BaekjoonHub

--- 백준/Bronze/9558. Between the Mountains/Between the Mountains.py
+++ 백준/Bronze/9558. Between the Mountains/Between the Mountains.py
@@ -1,2 +1,2 @@
-f=lambda:[*map(int,input().split())][1:]
-exec('a,b=f(),f();print(min(abs(x-y)for x in a for y in b));'*int(input()))
+f=lambda:map(int,input().split()[1:])
+exec('*a,=f();print(min(abs(x-y)for x in f()for y in a));'*int(input()))
