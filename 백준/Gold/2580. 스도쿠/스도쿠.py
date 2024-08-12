c00dba688cc0cf1011ed98a9e3f0580285656c64
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-08-12 15:10:59 +0900

    [Gold IV] Title: 스도쿠, Time: 2468 ms, Memory: 132960 KB -BaekjoonHub

--- 백준/Gold/2580. 스도쿠/스도쿠.py
+++ 백준/Gold/2580. 스도쿠/스도쿠.py
@@ -1,9 +1,36 @@
-def f(k):
- k==len(z)<exit(print(*a));r,c=z[k];s={*R(1,10)};x,y=r//3*3,c//3*3
- for i in R(9):s-={a[r*9+i],a[i*9+c]}
- s-={a[i*9+j+y]for i in R(x,x+3)for j in R(3)}
- for n in s:a[t:=r*9+c]=n;f(k+1);a[t]=0
-R=range
-*a,=map(int,open(0).read().split())
-z=[(k//9,k%9)for k in R(81)if a[k]<1]
-f(0)
+board=[list(map(int,input().split()))for _ in range(9)]
+def getyx(n):
+    return n//9,n%9
+
+def check(y,x):
+    for i in range(9):
+        if i!=x and board[y][i]==board[y][x]:
+            return False
+        if i!=y and board[i][x]==board[y][x]:
+            return False
+    for i in range(3):
+        for j in range(3):
+            if (y//3*3+i,x//3*3+j)!=(y,x) and board[y//3*3+i][x//3*3+j]==board[y][x]:
+                return False
+    return True
+def backtracking(n):
+    y,x=getyx(n)
+    while n<81 and board[y][x]:
+        n+=1
+        y,x=getyx(n)
+        
+    if n==81:
+        for line in board:
+            print(*line)
+        raise
+    for i in range(1,10):
+        tmp=board[y][x]
+        board[y][x]=i
+        if check(y,x):
+            backtracking(n+1)
+        board[y][x]=tmp
+
+try:
+    backtracking(0)
+except:
+    pass
