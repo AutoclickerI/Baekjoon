4d3dde5fa2cd12a23d55452c3a9e2bea29ec65ab
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-08-20 09:24:16 +0900

    [Silver IV] Title: 이상한 호텔의 송이, Time: 636 ms, Memory: 32140 KB -BaekjoonHub

--- 백준/Silver/29702. 이상한 호텔의 송이/이상한 호텔의 송이.py
+++ 백준/Silver/29702. 이상한 호텔의 송이/이상한 호텔의 송이.py
@@ -1,3 +1,3 @@
 for i in[*open(0)][1:]:
- b=f'{int(i):b}';n=int(b[1:],2)+1
- for f in range(len(b),0,-1):print(f"{f}{n:018d}");n=-~n//2
+ b=f'{int(i):b}';n=int(b[1:],2);f=len(b);
+ while f:print(f'{f}{n+1:018}');n//=2;f-=1;
