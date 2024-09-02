13b63d1ddf2709d107dce38c4d121dbb1bdc9a38
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-09-02 20:03:41 +0900

    [Bronze V] Title: SUAPC 2024 Winter, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/32154. SUAPC 2024 Winter/SUAPC 2024 Winter.py
+++ 백준/Bronze/32154. SUAPC 2024 Winter/SUAPC 2024 Winter.py
@@ -1,3 +1,1 @@
-z='ACEFGHLM'
-s=[z+'BDJ',*[z+'I']*2,z+'B',*[z]*5,'ABCFGHLM'][int(input())-1]
-print(len(s),*sorted(s))
+print(len(s:='A'+['BCFGH',*[z:='CEFGH']*5,'B'+z,*[z+'I']*2,'BCDEFGHJ'][-int(input())]+'LM'),*s)
