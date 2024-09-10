1b97e2c47fe452c8efbd521d44fa6e004b74a1f1
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-09-10 14:40:42 +0900

    [Silver I] Title: 더 게임 오브 데스, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/32248. 더 게임 오브 데스/더 게임 오브 데스.py
+++ 백준/Silver/32248. 더 게임 오브 데스/더 게임 오브 데스.py
@@ -1,8 +1,8 @@
 N,T,*A=map(int,open(0).read().split())
-c=t=1
-v=[0]*-~N
+c=t=0
+v=[0]*N
 while T:
  T-=1
- if v[c]:T%=t-v[c];v=[0]*-~N
- v[c]=t;c=A[c-1];t+=1
-print(c)
+ if v[c]:T%=t-v[c];v=[0]*N
+ v[c]=t;c=A[c]-1;t+=1
+print(1+c)
