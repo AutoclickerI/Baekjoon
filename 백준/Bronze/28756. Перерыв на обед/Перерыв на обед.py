3d3d4136110a366cbac6218acd4a4548fc627893
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-09-13 15:38:44 +0900

    [Bronze I] Title: Перерыв на обед, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/28756. Перерыв на обед/Перерыв на обед.py
+++ 백준/Bronze/28756. Перерыв на обед/Перерыв на обед.py
@@ -1,2 +1,4 @@
 (x,y,X,Y),_,*l=[map(int,i.split())for i in open(0)]
-print(min(t+((p-x)**2+(q-y)**2)**.5+((p-X)**2+(q-Y)**2)**.5for p,q,t in l))
+s=x+y*1j
+e=X+Y*1j
+print(min(t+abs(s-p-q*1j)+abs(e-p-q*1j)for p,q,t in l))
