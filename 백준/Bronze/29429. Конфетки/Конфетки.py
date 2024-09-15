b57a8b9d8b43ebc290f33b0f7a302769634f8f28
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-09-15 20:58:41 +0900

    [Bronze I] Title: Конфетки, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/29429. Конфетки/Конфетки.py
+++ 백준/Bronze/29429. Конфетки/Конфетки.py
@@ -1,2 +1,2 @@
 a,b=open(0)
-print(*['Bad luck']if len(b)>len(a)or a==b else[0]*(int(a)>int(b))or[1,[i for i in range(len(a))if a[i]<b[i]][0]+1])
+print(*['Bad luck']if len(b)>len(a)*(a!=b)else[0]*(int(a)>int(b))or[1,[i+1for i in range(len(a))if a[i]<b[i]][0]])
