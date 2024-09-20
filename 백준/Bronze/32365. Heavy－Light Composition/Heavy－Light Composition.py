e732d169942ee731482bad49930d184d306fb595
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-09-21 00:09:22 +0900

    [Bronze II] Title: Heavy-Light Composition, Time: 220 ms, Memory: 32140 KB, Score: 15 point -BaekjoonHub

--- 백준/Bronze/32365. Heavy－Light Composition/Heavy－Light Composition.py
+++ 백준/Bronze/32365. Heavy－Light Composition/Heavy－Light Composition.py
@@ -1,5 +1,6 @@
 for i in[*open(0)][1:]:
     d={}
+    i=i[:-1]
     for j in i:d[j]=d.get(j,0)+1
     p=[2>d[j]for j in i[::2]];q=[2>d[j]for j in i[1::2]]
     print('FT'[all(p)and not any(q) or all(q)and not any(p)])
