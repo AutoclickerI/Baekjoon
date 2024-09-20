ebc57491be9bddf82407841363f99d2f31891b9e
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-09-20 18:47:33 +0900

    [Bronze II] Title: Fitness, Time: 28 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/15180. Fitness/Fitness.py
+++ 백준/Bronze/15180. Fitness/Fitness.py
@@ -1,4 +1,4 @@
 n,*l,_=open(0)
 v=[int(n)]
-for a,b,_ in l:v+=1+(v[-1]+int(b)*(-1)**(a<'B')-1)%8,
-print(*v,'reject'*(len(v)<5 or len(v)-len(set(v))))
+for a,b,_ in l:v+=8+(v[-1]+int(b)*(1-2*(a<'B')))%-8,
+print(*v,'reject'*(len(v)<5or len(v)-len(set(v))))
