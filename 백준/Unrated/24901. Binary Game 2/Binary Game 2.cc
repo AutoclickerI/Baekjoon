ac3f59126db7c05ea457c978a56e14308774723e
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-09-21 19:01:18 +0900

    [Unrated] Title: Binary Game 2, Time: 212 ms, Memory: 98416 KB, Score: 222222 point -BaekjoonHub

---
+++ 백준/Unrated/24901. Binary Game 2/Binary Game 2.cc
@@ -0,0 +1,16 @@
+//; # \u000a import java.util.Scanner;public class Main {public static String[] l={"0","1","10","11","100","101","110","111","1000","1001","1010"};public static void main(String[] args){Scanner input = new Scanner(System.in);int n=input.nextInt();for (int i=0;i<=n;i++){System.out.print(l[i]);}}} /*
+#if true /*
+n=gets.to_i;for i in 0...n+1;print ["0","1","10","11","100","101","110","111","1000","1001","1010"][i];end
+=begin
+/* */ // \u000a /*
+#include <iostream>
+#include <string>
+#include <vector>
+int main()
+{int n;std::cin>>n;std::vector<std::string>l={"0","1","10","11","100","101","110","111","1000","1001","1010"};for(int i=0;i<=n;++i){std::cout<<l[i];}}
+// */
+// */ import Foundation;let l=["0","1","10","11","100","101","110","111","1000","1001","1010"];var n = Int(readLine()!)!;for i in 0...n{print(l[i],terminator:"")} // \u000a /*
+#endif
+/*
+=end
+# */
