422f73475819e025b4e5397450baaee902f56174
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-09-22 02:07:50 +0900

    [Bronze I] Title: Account Numbers, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/20350. Account Numbers/Account Numbers.py
+++ 백준/Bronze/20350. Account Numbers/Account Numbers.py
@@ -1,4 +1,2 @@
 s=input()
-s=s[4:]+s[:4]
-for i in range(65,91):s=s.replace(chr(i),str(i-55))
-print('in'*(int(s)%97!=1)+'correct')
+print('in'*(int(''.join(str(int(x,36))for x in s[4:]+s[:4]))%97!=1)+'correct')
