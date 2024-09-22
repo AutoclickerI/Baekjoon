4e8769fef30242a842c14e1709a09dc00a36b10d
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-09-22 23:30:25 +0900

    [Bronze II] Title: Stacked Floating Mountains, Time: 28 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/5238. Stacked Floating Mountains/Stacked Floating Mountains.py
+++ 백준/Bronze/5238. Stacked Floating Mountains/Stacked Floating Mountains.py
@@ -1,1 +1,1 @@
-for i in[*open(0)][1:]:_,*l=map(int,i.split());print('YNEOS'[any(k!=i+j for i,j,k in zip(l,l[1:],l[2:]))::2])
+for i in[*open(0)][1:]:_,*l=map(int,i.split());print('YNEOS'[any(i+j-k for i,j,k in zip(l,l[1:],l[2:]))::2])
