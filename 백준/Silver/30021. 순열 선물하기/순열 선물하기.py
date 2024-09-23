83788a08c4c32d4b0c45233add5c5db0ba4b3a40
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-09-23 19:03:53 +0900

    [Silver II] Title: 순열 선물하기, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/30021. 순열 선물하기/순열 선물하기.py
+++ 백준/Silver/30021. 순열 선물하기/순열 선물하기.py
@@ -1,6 +1,3 @@
-N=int(input())
-if N==2:print('NO')
-else:
-    print('YES')
-    if N==1:print(1)
-    else:print(1,3,2,*range(4,N+1))
+n=int(input())
+f=n!=2
+print('NYOE S'[f::2],*(*'132'*f,*range(4,n+1))[:n])
