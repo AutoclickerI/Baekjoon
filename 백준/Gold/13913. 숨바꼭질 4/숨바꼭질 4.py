fbe33c0bbc7c2f28706b7b1b181efcde901695d1
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-09-24 22:49:42 +0900

    [Gold IV] Title: 숨바꼭질 4, Time: 196 ms, Memory: 170228 KB -BaekjoonHub

--- 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.py
+++ 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.py
@@ -1,19 +1,12 @@
-from collections import deque
 N,K=map(int,input().split())
-l=deque([N])
-v=[-1]*999999
-p=[-1]*999999
-v[N]=0
-while 1:
-    t=l.popleft()
-    if t==K:break
-    for i in t-1,t+1,2*t:
-        if -1<i<999999:
-            if v[i]<0:
-                v[i]=v[t]+1
-                p[i]=t
-                l.append(i)
-print(v[K])
-a=[]
-while 0<=K:a+=K,;K=p[K]
-print(*a[::-1])
+l=[N]
+v=[-1]*7**7
+p=v[:]
+v[N]=I=0
+while l[I]-K:
+ t=l[I];I+=1
+ for i in t-1,t+1,2*t:
+  if 7**7>i>=0>v[i]:v[i]=v[t]+1;p[i]=t;l+=i,
+t,*a=v[K],
+while~K:a+=K,;K=p[K]
+print(t,*a[::-1])
