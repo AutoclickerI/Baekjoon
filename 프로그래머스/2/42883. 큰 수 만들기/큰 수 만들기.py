2bcf0b0cf0d78c3e284b26d47f95e92f326f7d27
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-09-25 17:20:45 +0900

    [level 2] Title: 큰 수 만들기, Time: 85.49 ms, Memory: 14.8 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
@@ -1,7 +1,5 @@
-def solution(number,k):
-    l=[]
-    for i in number:
-        while k and l and l[-1]<i:l.pop();k-=1
-        l+=i
-    while k:l.pop();k-=1
-    return''.join(l)
+def solution(s,k,l=[]):
+ for i in s:
+  while k and l and l[-1]<i:l.pop();k-=1
+  l+=i
+ return''.join(l[:len(l)-k])
