fb107d77142f5cc47069ff1d1715d0c843814995
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-09-26 15:03:00 +0900

    [Silver IV] Title: Домашнее задание, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/19971. Домашнее задание/Домашнее задание.py
+++ 백준/Silver/19971. Домашнее задание/Домашнее задание.py
@@ -2,6 +2,4 @@ f=lambda:[*map(int,input().split())]
 n,m=f()
 t=f()
 b={f()[0]for _ in[0]*m}
-a=0
-for i in range(n):a=max(a,t[i]*(-~i not in b))
-print(sum(t)-a)
+print(sum(t)+min(j*~-(-~i in b)for i,j in enumerate(t)))
