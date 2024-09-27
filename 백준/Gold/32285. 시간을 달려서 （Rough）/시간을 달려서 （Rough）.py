88482a62ee6765fa31e94134608877116f66ae57
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-09-27 18:26:10 +0900

    [Gold III] Title: 시간을 달려서 (Rough), Time: 220 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Gold/32285. 시간을 달려서 （Rough）/시간을 달려서 （Rough）.py
+++ 백준/Gold/32285. 시간을 달려서 （Rough）/시간을 달려서 （Rough）.py
@@ -1,7 +1,4 @@
 G,F=map(int,input().split())
-f=lambda n:n.bit_length()+n.bit_count()-1
-m=f(G)
-while G.bit_length()-1<m:
-    G+=F
-    m=min(m,f(G))
+m=100
+while G.bit_length()-1<m:m=min(m,G.bit_length()+G.bit_count()-1);G+=F
 print(m)
