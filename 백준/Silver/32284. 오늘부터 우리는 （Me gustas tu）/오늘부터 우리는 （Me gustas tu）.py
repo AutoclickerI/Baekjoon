267dbb52abb1b010cfddda7639b25d9d35fcd063
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-09-27 16:59:06 +0900

    [Silver III] Title: 오늘부터 우리는 (Me gustas tu), Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/32284. 오늘부터 우리는 （Me gustas tu）/오늘부터 우리는 （Me gustas tu）.py
+++ 백준/Silver/32284. 오늘부터 우리는 （Me gustas tu）/오늘부터 우리는 （Me gustas tu）.py
@@ -1,8 +1,2 @@
 N,M,a,b=map(int,open(0).read().split())
-t=[''for _ in[0]*N]
-for y in range(N):
-    for x in range(M):
-        if x<b:t[y]+='E'
-        elif x>b:t[y]+='W'
-        else:t[y]+='NS'[y<a]
-for i in t:print(i)
+print(('S'*M+'\n')*a+'E'*b+'W'*(M-b)+('\n'+'N'*M)*(N+~a))
