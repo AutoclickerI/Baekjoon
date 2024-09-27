0997bbf744b72d62dbf11a452aa3efeb7593e19a
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-09-27 20:35:22 +0900

    [Silver IV] Title: Милый общий делитель, Time: 36 ms, Memory: 33240 KB -BaekjoonHub

--- 백준/Silver/19768. Милый общий делитель/Милый общий делитель.py
+++ 백준/Silver/19768. Милый общий делитель/Милый общий делитель.py
@@ -1,6 +1,6 @@
 from math import*
 n=gcd(*map(int,input().split()))
 a=0
-for i in range(1,1+isqrt(n)):
-    if n%i<1:a=max(a,i,n//i,key=lambda x:sum(int(a)for a in str(x)))
+for i in range(1,1+int(n**.5)):
+ if n%i<1:a=max(a,i,n//i,key=lambda x:sum(int(a)for a in str(x)))
 print(a)
