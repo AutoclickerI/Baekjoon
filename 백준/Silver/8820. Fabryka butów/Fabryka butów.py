915b31873d719b22aff4ea379662565036e979c5
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-09-27 14:33:38 +0900

    [Silver V] Title: Fabryka butów, Time: 244 ms, Memory: 46796 KB -BaekjoonHub

--- 백준/Silver/8820. Fabryka butów/Fabryka butów.py
+++ 백준/Silver/8820. Fabryka butów/Fabryka butów.py
@@ -1,5 +1,5 @@
 f=lambda:[*map(int,input().split())]
 for t in[0]*f()[0]:
  N,K=f();p=f();s=sum(p[K:])
- for i in range(K,N):t=max(t,-s*(~i+K));s-=p[i]
+ for i in range(N-K):t=max(t,s*(i+1));s-=p[i+K]
  print(t)
