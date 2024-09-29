ad068ee19c17f7586ee52b287270f3c3f8575c3d
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-09-30 03:13:13 +0900

    [Bronze II] Title: Old Fishin’ Hole, Time: 152 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/6811. Old Fishin’ Hole/Old Fishin’ Hole.py
@@ -0,0 +1,3 @@
+a,b,c,d=map(int,open(0))
+R=range(101)
+print('Number of ways to catch fish:',len([print(i,'Brown Trout,',j,'Northern Pike,',k,'Yellow Pickerel')for i in R for j in R for k in R if 0<i*a+b*j+c*k<=d]))
