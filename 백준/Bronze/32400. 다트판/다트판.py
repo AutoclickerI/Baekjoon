e939fad46294f899ab88a661dd5e9fe4b3c54ee7
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-09-30 00:42:54 +0900

    [Bronze II] Title: 다트판, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/32400. 다트판/다트판.py
+++ 백준/Bronze/32400. 다트판/다트판.py
@@ -1,7 +1,2 @@
-*l,=map(int,input().split())
-i=l.index(20)
-s1=(l[i]+l[(i+1)%20]+l[i-1])*20
-s2=sum(l)*3
-if s1==s2:print('Tie')
-elif s1>s2:print('Alice')
-else:print('Bob')
+*A,=map(int,input().split())
+print('ABloibc e'[A[i:=A.index(20)-1]+A[i-18]<12::2])
