87bba6685f420c3c7f9dfaf8d390fafc25d05a91
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-09-30 02:38:32 +0900

    [Silver IV] Title: 마법의 나침반, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/32372. 마법의 나침반/마법의 나침반.py
+++ 백준/Silver/32372. 마법의 나침반/마법의 나침반.py
@@ -1,31 +1,5 @@
-N,M=map(int,input().split())
-x=y=1
-X=Y=N
-for _ in[0]*M:
-    a,b,d=map(int,input().split())
-    if d==1:
-        X=min(X,a-1)
-        y=Y=b
-    if d==2:
-        X=min(X,a-1)
-        y=max(y,b+1)
-    if d==3:
-        x=X=a
-        y=max(y,b+1)
-    if d==4:
-        x=max(x,a+1)
-        y=max(y,b+1)
-    if d==5:
-        x=max(x,a+1)
-        y=Y=b
-    if d==6:
-        x=max(x,a+1)
-        Y=min(Y,b-1)
-    if d==7:
-        x=X=a
-        Y=min(Y,b-1)
-    if d==8:
-        X=min(X,a-1)
-        Y=min(Y,b-1)
-
-print(x,y)
+(n,m),*l=[map(int,i.split())for i in open(0)]
+a,b=c,d=1,n
+p,q=min,max
+for x,y,k in l:exec("b=p(b,x-1);d=p(d,y-1) a=x;d=p(d,y-1) a=q(a,x+1);d=p(d,y-1) a=q(a,x+1);c=y a=q(a,x+1);c=q(c,y+1) a=x;c=q(c,y+1) b=p(b,x-1);c=q(c,y+1) b=p(b,x-1);c=y".split()[-k])
+print(a,c)
