1896774123ed12eee7f4469590eab48265a6102d
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-10-03 17:39:07 +0900

    [Bronze I] Title: Judy, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/26515. Judy/Judy.py
+++ 백준/Bronze/26515. Judy/Judy.py
@@ -1,4 +1,4 @@
 for i in[*open(0)][1:]:
   r=''
-  for i in map(int,i.split()):r+=chr(i).lower()if 65<=i<=90 or 97<=i<=122else'-'
+  for i in map(int,i.split()):r+=chr(i).lower()if 64<i<91or 96<i<123else'-'
   print(r[1:]+r[0]+'ay')
