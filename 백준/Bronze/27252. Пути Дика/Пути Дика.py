daaf0a48322b0f8cdd51f7126d33f004fe22a9d0
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-10-06 19:34:55 +0900

    [Bronze I] Title: Пути Дика, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/27252. Пути Дика/Пути Дика.py
+++ 백준/Bronze/27252. Пути Дика/Пути Дика.py
@@ -1,5 +1,5 @@
 s=input()
 l=eval("len(s)*['.'],"*50)
 z=h=0
-for i in s:f=i=='(';z-=1-2*f;l[~z+f][h]='\/'[f];h+=1
+for i in s:f=i<')';z-=1-2*f;l[~z+f][h]='\/'[f];h+=1
 for i in l:{'/'}<{*i}!=print(*i,sep='')
