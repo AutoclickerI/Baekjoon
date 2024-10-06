f76b209f0bd423f785d07ea87cf12abfbaff0e98
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-10-06 16:21:50 +0900

    [Silver V] Title: ABB to BA (Easy), Time: 56 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/32292. ABB to BA （Easy）/ABB to BA （Easy）.py
@@ -0,0 +1,18 @@
+def push(stack,c):
+    stack+=c,
+    if stack[-3:]==['A','B','B']:
+        stack.pop()
+        stack.pop()
+        stack.pop()
+        push(stack,'B')
+        stack+='A'
+
+def process(s):
+    stack=[]
+    for i in s:
+        push(stack,i)
+    print(*stack,sep='')
+
+for _ in[0]*int(input()):
+    input()
+    process(input())
