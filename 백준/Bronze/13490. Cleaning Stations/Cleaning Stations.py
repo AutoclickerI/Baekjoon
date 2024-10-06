18645bad26e6cf76c5ab6d7400798f4804ea32ee
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-10-06 19:59:39 +0900

    [Bronze I] Title: Cleaning Stations, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/13490. Cleaning Stations/Cleaning Stations.py
@@ -0,0 +1,6 @@
+f=lambda:map(int,input().split())
+
+for T in range(int(input())):
+    n,_=f();*l,=f();m=max(b:=eval('sum(i*j for i,j in zip(l,f())),'*n));print(f'Data Set {T+1}:')
+    for i in range(n):b[i]==m!=print(i+1)
+    print()
