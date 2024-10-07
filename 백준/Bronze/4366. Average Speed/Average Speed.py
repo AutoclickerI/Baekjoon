6ceaeedf3e2d2ceb88a22c084791090a9e5af2ae
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-10-07 13:36:44 +0900

    [Bronze I] Title: Average Speed, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/4366. Average Speed/Average Speed.py
+++ 백준/Bronze/4366. Average Speed/Average Speed.py
@@ -1,5 +1,2 @@
 S=T=D=0
-for i in open(0):
- x,*l=i.split();h,m,s=map(int,x.split(':'));D-=(T-(T:=h+m/60+s/3600))*S
- if l:S=int(l[0])
- else:print(x,f'{D:.2f} km')
+for i in open(0):x,*l=i.split();h,m,s=map(int,x.split(':'));D-=(T-(T:=h+m/60+s/3600))*S;S=int(l[0])if l else print(x,f'{D:.2f} km')or S
