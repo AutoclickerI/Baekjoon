57e7356075b67f379d926fe99ae7eacec72a42d4
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-10-07 13:40:39 +0900

    [Bronze I] Title: Snakes and Ladders, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/6905. Snakes and Ladders/Snakes and Ladders.py
@@ -0,0 +1,5 @@
+c=1
+while 1:
+ n=int(input());n<1<exit(print('You Quit!'))
+ if c+n<101:c+=n;c={54:19,90:48,99:77,9:34,40:64,67:86}.get(c,c)
+ print('You are now on square',c);99<c<exit(print('You Win!'))
