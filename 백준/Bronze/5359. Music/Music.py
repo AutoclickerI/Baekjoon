068451a9bbb24e07dad47d8deede7dfde383d09c
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-10-08 18:32:28 +0900

    [Bronze I] Title: Music, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/5359. Music/Music.py
+++ 백준/Bronze/5359. Music/Music.py
@@ -1,7 +1,4 @@
 for a in[0]*int(input()):
-    n,m,c=map(int,input().split())
-    *l,=map(int,input().split())
-    for i in range(n+1-m):
-        s=l[i:i+m]
-        a+=max(s)-min(s)<=c
-    print(a)
+ n,m,c=map(int,input().split());*l,=map(int,input().split())
+ for i in range(n+1-m):a+=max(s:=l[i:i+m])-min(s)<=c
+ print(a)
