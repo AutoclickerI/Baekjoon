6d3f015b3e740d03febeb3bf8977aff0c9717076
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-10-08 20:48:12 +0900

    [Bronze I] Title: Happy Trails, Time: 32 ms, Memory: 33240 KB -BaekjoonHub

--- 백준/Bronze/15066. Happy Trails/Happy Trails.py
+++ 백준/Bronze/15066. Happy Trails/Happy Trails.py
@@ -1,2 +1,2 @@
 from math import*
-print(f'{sum(d*sin(a*pi/180)for a,d in[map(int,i.split())for i in open(0)][1:]):.2f}')
+print('%.2f'%sum(d*sin(a*pi/180)for a,d in[map(int,i.split())for i in open(0)][1:]))
