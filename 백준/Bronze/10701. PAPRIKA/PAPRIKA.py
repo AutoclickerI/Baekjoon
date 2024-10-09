71ddd0724fc040e8fedccaecccf2e3fc9bcd3309
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-10-09 17:07:23 +0900

    [Bronze I] Title: PAPRIKA, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10701. PAPRIKA/PAPRIKA.py
+++ 백준/Bronze/10701. PAPRIKA/PAPRIKA.py
@@ -1,7 +1,4 @@
-(N,X),*b=[[*map(int,i.split())]for i in open(0)]
-for i in range(N-1):
-    if b[i][0]>b[i+1][0] and b[i][1]==1>0==b[i+1][1]:b[i][0],b[i+1][0]=b[i+1][0],b[i][0]
-    elif b[i][0]<b[i+1][0] and b[i][1]==0<1==b[i+1][1]:b[i][0],b[i+1][0]=b[i+1][0],b[i][0]
-c=0
-for a,b in b:c+=a>X>0==b;c+=a<=X and b==1
-print(c)
+(n,m),*s=[[*map(int,i.split())]for i in open(0)]
+for i,j in zip(s,s[1:]):
+ if i[1]!=j[1]and(i[1]^(i[0]<j[0])):i[0],j[0]=j[0],i[0]
+print(sum(i[1]^(i[0]>m)for i in s))
