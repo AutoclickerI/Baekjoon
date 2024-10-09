1150ad60ddffc89dcf6e842d15ffaf2fad4edb2b
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-10-10 00:21:23 +0900

    [Silver III] Title: Таинственный ритуал, Time: 256 ms, Memory: 38828 KB -BaekjoonHub

--- 백준/Silver/28804. Таинственный ритуал/Таинственный ритуал.py
+++ 백준/Silver/28804. Таинственный ритуал/Таинственный ритуал.py
@@ -1,10 +1,6 @@
 d=[0,1,1,3,2,1,3,7,4,9]
-n=int(input())
-for _ in[0]*n:
-    s=input()
+for s in[*open(0)][1:]:
     n=0
-    for i in s[::-1]:
-        n+=int(i)
-        n=n//10+d[n%10]
-    while n not in[1,3,7,9]:n=n//10+d[n%10]
+    for i in s[-2::-1]:n+=int(i);n=n//10+d[n%10]
+    while n in[2,4,5,6,8]:n=n//10+d[n%10]
     print(n)
