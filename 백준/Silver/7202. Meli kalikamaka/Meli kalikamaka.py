df7d53455eb4bc9652a3339a59e5df0f84326b68
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-10-09 21:19:31 +0900

    [Silver IV] Title: Meli kalikamaka, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/7202. Meli kalikamaka/Meli kalikamaka.py
+++ 백준/Silver/7202. Meli kalikamaka/Meli kalikamaka.py
@@ -3,6 +3,6 @@ for c in input()+'a':
  match w+(v:=c in'aeiou'):
   case 2:continue
   case 0 if c==p:continue
-  case 0 if p!=' ':a+='a'
+  case 0 if' '<p:a+='a'
  a,p,w=a+{'r':'l','s':'k'}.get(c,c),c,v
 print(a)
