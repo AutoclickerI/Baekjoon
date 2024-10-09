8d1c00926b546535599255df9d4290d6660dc653
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-10-09 19:12:11 +0900

    [Silver IV] Title: Стать сильнее, Time: 160 ms, Memory: 53648 KB -BaekjoonHub

--- 백준/Silver/28615. Стать сильнее/Стать сильнее.py
+++ 백준/Silver/28615. Стать сильнее/Стать сильнее.py
@@ -1,9 +1,3 @@
-n=int(input())
-m,*l=sorted(map(int,input().split()))[::-1]
-from heapq import*
-hq=[-m]
-for i in l:
-    if-hq[0]-1>i:
-        heappop(hq)
-    heappush(hq,-i)
-print(len(hq))
+l=sorted(map(int,[*open(i:=0)][1].split()))
+for a in l:i+=l[i]+1<a
+print(len(l)-i)
