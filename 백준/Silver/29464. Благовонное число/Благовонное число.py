339928e82dd67b65dec97254faa7df8396273f20
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-10-10 12:44:29 +0900

    [Silver III] Title: Благовонное число, Time: 1032 ms, Memory: 111340 KB -BaekjoonHub

--- 백준/Silver/29464. Благовонное число/Благовонное число.py
+++ 백준/Silver/29464. Благовонное число/Благовонное число.py
@@ -1,16 +1,8 @@
-import sys
-sys.setrecursionlimit(10**5)
 n=int(input())
-i=0
-x=0
-while x+(i and 9*10**((i-1)//2))<n:
-    x+=(i and 9*10**((i-1)//2))
-    i+=1
-def recur(i,n,c=1):
-    if i==1:return str(n-1+c)
-    if i==2:return str(n-1+c)*2
-    while 10**((i-1)//2)<n:
-        c+=1
-        n-=10**((i-1)//2)
-    return f'{c}{recur(i-2,n,0)}{c}'
-print(recur(i,n-x))
+s=f=0
+while 9*10**s<n:
+    n-=9*10**s
+    s+=f
+    f^=1
+a=str(n+10**s-1)
+print(a[:len(a)-1+f]+a[::-1])
