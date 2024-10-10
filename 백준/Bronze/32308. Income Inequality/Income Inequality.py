5323c131fb0a27fa57b5f8f55e7563f2f4bb4237
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-10-10 20:48:07 +0900

    [Bronze I] Title: Income Inequality, Time: 1336 ms, Memory: 144340 KB -BaekjoonHub

--- 백준/Bronze/32308. Income Inequality/Income Inequality.py
+++ 백준/Bronze/32308. Income Inequality/Income Inequality.py
@@ -1,8 +1,6 @@
-n=int(input())
-s=sum(l:=sorted(map(int,input().split())))
+n,*l=map(int,open(0).read().split())
+l.sort()
+s=sum(l)
 i=v=m=0
-while l:
-    v+=l.pop()
-    i+=1
-    m=max(m,v/s*100-i/n*100)
-print(m)
+while l:v+=l.pop();i+=1;m=max(m,v/s-i/n)
+print(m*100)
