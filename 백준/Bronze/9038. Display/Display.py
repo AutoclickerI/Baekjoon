d93d2ccf2de4b5c56e3fdc3ea2cd7934376eea27
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-10-11 15:50:36 +0900

    [Bronze I] Title: Display, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/9038. Display/Display.py
+++ 백준/Bronze/9038. Display/Display.py
@@ -1,6 +1,5 @@
-for c in[0]*int(input()):
- w=int(input());a=input().split();r=1
- for b in a:
-  if c+len(b)>w:r+=1;c=len(b)+1
-  else:c+=len(b)+1
- print(r)
+exec('''w=int(input());c=0;r=1
+for b in input().split():
+ if c+len(b)>w:r+=1;c=len(b)+1
+ else:c+=len(b)+1
+print(r);'''*int(input()))
