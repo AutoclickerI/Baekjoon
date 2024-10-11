65df0ec27947c8d620c28f997b4b9f608560455b
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-10-11 17:09:22 +0900

    [Bronze I] Title: The Cost of Moving, Time: 104 ms, Memory: 111072 KB -BaekjoonHub

--- 백준/Bronze/10383. The Cost of Moving/The Cost of Moving.py
+++ 백준/Bronze/10383. The Cost of Moving/The Cost of Moving.py
@@ -1,2 +1,2 @@
 *x,_=open(s:=0).read().split()
-while x:s+=1;R=range(n:=int(x.pop(0)));i=x[:n];x=x[n:];print(f'Site {s}:',sum(abs(sorted(zip(i,R))[j][1]-j)for j in R))
+while x:s+=1;R=range(n:=int(x.pop(0)));i=x[:n];x=x[n:];print(f'Site {s}:',sum(abs(i-j)for(_,i),j in zip(sorted(zip(i,R)),R)))
