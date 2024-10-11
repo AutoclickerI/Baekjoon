0d6dfbac9902c65443c9eafe1092069bcf717b62
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-10-11 15:22:06 +0900

    [Bronze I] Title: Friendly Rooks, Time: 32 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Bronze/24843. Friendly Rooks/Friendly Rooks.py
+++ 백준/Bronze/24843. Friendly Rooks/Friendly Rooks.py
@@ -1,3 +1,2 @@
 n,m,k=map(int,input().split())
-if k>min(n,m):print('Impossible')
-else:print('Possible',*['.'*m]*(n-k),*['.'*i+'*'+'.'*(m+~i)for i in range(k)],sep='\n')
+print(*['Impossible']*(k>min(n,m))or['Possible']+['.'*m]*(n-k)+['.'*i+'*'+'.'*(m+~i)for i in range(k)],sep='\n')
