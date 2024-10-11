30b271d294b3866ef68f723f8eebb4100f10d0b9
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-10-11 18:39:39 +0900

    [Bronze I] Title: Hand Detection, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/6995. Hand Detection/Hand Detection.py
@@ -0,0 +1,4 @@
+n,*l=open(0)
+from itertools import*
+f=lambda g,i,s:['Do not h','H'][len({*g})==i]+'ave a '+s
+for i in l:*x,=map(int,i.split());print('%-23s'%f((v-w for v,w in combinations(x[1::2],2)),10,'spread.'),f(x[::2],5,'rainbow.'))
