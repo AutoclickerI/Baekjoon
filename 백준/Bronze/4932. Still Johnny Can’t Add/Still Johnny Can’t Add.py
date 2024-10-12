901b6f389b0e2a673180fd132e096ac154352f3f
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-10-13 02:47:59 +0900

    [Bronze I] Title: Still Johnny Can’t Add, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/4932. Still Johnny Can’t Add/Still Johnny Can’t Add.py
@@ -0,0 +1,7 @@
+for T in range(int(input())):
+    n=int(input())
+    l=eval('[*map(int, input().split())],'*n)
+    f=0
+    for i in range(n-1):
+        if len({j[i+1]-j[i]for j in l})!=1:f=1
+    print(f'{T+1}.','YNEOS'[f::2])
