a69893d062aecd12ba0815b34c492ee6111d941a
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-10-13 13:41:55 +0900

    [Bronze I] Title: Kaučuk, Time: 36 ms, Memory: 31120 KB, Score: 50 point -BaekjoonHub

--- 백준/Bronze/23687. Kaučuk/Kaučuk.py
+++ 백준/Bronze/23687. Kaučuk/Kaučuk.py
@@ -1,11 +1,12 @@
 a=b=c=0
-for i in[*open(0)][1:]:
-    t,s=i.split()
+for i in[0]*int(input()):
+    t,s=input().split()
     if t == 'section':
         a, b, c = a + 1, 0, 0
         print(a, s)
     elif t == 'subsection':
         b += 1
+        c = 0
         print(f'{a}.{b}', s)
     else:
         c += 1
