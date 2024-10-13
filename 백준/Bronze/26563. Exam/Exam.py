d8d4a51404f3e3a1fb6179bb825bf1fdd4e48d01
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-10-13 14:05:27 +0900

    [Bronze I] Title: Exam, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/26563. Exam/Exam.py
+++ 백준/Bronze/26563. Exam/Exam.py
@@ -1,2 +1,2 @@
 I=input
-for i in range(int(I())):x=int(I());n=len(y:=I());print(min(x,k:=sum(i==j for i,j in zip(y,I())))+n-max(k,x))
+for _ in[0]*int(I()):x=int(I());print(len(y:=I())+min(x,k:=sum(i==j for i,j in zip(y,I())))-max(k,x))
