2185adface6789845fcfede86f9d44c72cb14555
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-10-14 17:49:18 +0900

    [Bronze I] Title: Bus Numbers, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/20743. Bus Numbers/Bus Numbers.py
+++ 백준/Bronze/20743. Bus Numbers/Bus Numbers.py
@@ -1,10 +1,7 @@
 n=int(input())
 d={}
-r={}
+r=[]
 s=range(1,74)
 for i in s:
- for j in s[i:]:
-  t=i**3+j**3
-  if t in d:r[-t]=1
-  d[t]=1
-print(([-i for i in sorted(r)if-i<=n]+['none'])[0])
+ for j in s[i:]:t=i**3+j**3;r+=[-t]*(t in d);d[t]=1
+print(([-i for i in sorted({*r})if-i<=n]+['none'])[0])
