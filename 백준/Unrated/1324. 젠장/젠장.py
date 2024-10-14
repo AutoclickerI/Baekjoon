c1c9a962cd5210e4b6e9cff8e9a5fe73c1d69d91
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-10-14 14:20:21 +0900

    [Unrated] Title: 젠장, Time: 316 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Unrated/1324. 젠장/젠장.py
+++ 백준/Unrated/1324. 젠장/젠장.py
@@ -1,15 +1,10 @@
 N,*l=map(int,open(0).read().split())
-assert len(l)==2*N
-A=l[:N]
-B=l[N:]
 DP=[0]*N
 
-for i in range(N):
-    cnt=0
+for i in l[N:]:
+    c=0
     for j in range(N):
-        if A[i]==B[j]:
-            DP[j]=cnt+1
-        elif A[i]>B[j]:
-            cnt=max(cnt,DP[j])
+        if i==l[j]:DP[j]=c+1
+        elif i>l[j]:c=max(c,DP[j])
 
 print(max(DP))
