4a2c01f3dcdd4ebe3fd29f3ea25e3f6f5c9282ce
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-10-14 12:15:49 +0900

    [Bronze I] Title: Toilet Seat, Time: 28 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/11189. Toilet Seat/Toilet Seat.py
+++ 백준/Bronze/11189. Toilet Seat/Toilet Seat.py
@@ -1,7 +1,5 @@
-s=input()
-if s[0]=='U':print(s[1:].count('D')*2)
-else:print(s[2:].count('D')*2+1)
-
-if s[0]=='U':print(s[2:].count('U')*2+1)
-else:print(s[1:].count('U')*2)
-print(sum(i!=j for i,j in zip(s,s[1:])))
+i,*l=input()
+a=b=c=i
+v=w=x=0
+for s in l:v+=a!=s;v+=s!='U';a='U';w+=b!=s;w+=s!='D';b='D';x+=c!=s;c=s
+print(v,w,x)
