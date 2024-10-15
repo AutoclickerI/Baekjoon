d1a1ac1de3edd2add01519ef4c63fb902a939eb7
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-10-15 14:16:00 +0900

    [Bronze I] Title: Password, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/11293. Password/Password.py
@@ -0,0 +1,4 @@
+r=range
+for T in r(int(input())):
+ k=eval("input().replace(' ',''),"*int(input()));print('Customer',T+1)
+ for q in r(int(input())):l=input().split();a,b,c=map(int,l[:3]);t=k[a-1];print('ceorrrroerc t'[[t[b-1],t[c-1]]!=l[3:]::2])
