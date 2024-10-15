8d057018ac9e11186a50fe9b5475ddaf8587172e
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-10-16 00:06:47 +0900

    [Bronze I] Title: Daily Trips, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/28043. Daily Trips/Daily Trips.py
+++ 백준/Bronze/28043. Daily Trips/Daily Trips.py
@@ -1,14 +1,6 @@
-N, H, W = map(int, input().split())
-for _ in range(N):
-    a, b = input().split()
-    ans = []
-    if a == 'Y' or W == 0:
-        H -= 1
-        W += 1
-        ans+='Y'
-    else:
-        ans+='N'
-
-    if b=='Y'or H==0:W-=1;H+=1;ans+='Y'
-    else:ans+='N'
-    print(*ans)
+s,*l=open(0)
+n,h,w=map(int,s.split())
+for a,_,b,_ in l:
+ if w<1or'N'<a:w+=1;h-=1;a='Y'
+ if h<1or'N'<b:w-=1;h+=1;b='Y'
+ print(a,b)
