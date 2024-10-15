51b0a5845084363de8b326956bd4fbd17eb867d3
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-10-15 14:09:23 +0900

    [Bronze I] Title: Fall 2006: 100 Years of Engineering, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/11018. Fall 2006： 100 Years of Engineering/Fall 2006： 100 Years of Engineering.py
@@ -0,0 +1,5 @@
+for T in range(int(input())):
+    n,m=input().split();k=[[eval(m)]];v=h=0
+    for x,y,z in eval('map(eval,input().split()),'*int(n))[::-1]:k+=[x+k[-1][0],y,z],
+    for m,t,f in k[:0:-1]:a=f/m-9.81;h+=v*t+a*t*t/2;v+=a*t
+    print(f'Data Set {T+1}:\n%.2f\n'%h)
