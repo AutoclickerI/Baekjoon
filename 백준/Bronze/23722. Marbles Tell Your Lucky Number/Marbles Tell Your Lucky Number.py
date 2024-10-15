0c72c5c91f977f235b2e23bc64f3dc779ffcc3de
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-10-15 14:33:01 +0900

    [Bronze I] Title: Marbles Tell Your Lucky Number, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/23722. Marbles Tell Your Lucky Number/Marbles Tell Your Lucky Number.py
@@ -0,0 +1,9 @@
+for s in[*open(0)][:-1]:  
+    *l,=map(int,s.split())
+    while l.count(0)!=3:
+        idx,cnt=0,101
+        for i in range(4):
+            if 0<l[i]<cnt:idx=i;cnt=l[i]
+        for i in range(4):
+            if i!=idx and l[i]:l[i]-=l[idx]
+    print(max(l))
