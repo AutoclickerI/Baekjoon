50d17afcade290dc1a7d33d898af32d540ab7607
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-10-15 23:27:45 +0900

    [Bronze I] Title: Cued In, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/15029. Cued In/Cued In.py
+++ 백준/Bronze/15029. Cued In/Cued In.py
@@ -1,7 +1,2 @@
-import statistics as s
-dic = {'red': 1, 'yellow': 2, 'green': 3, 'brown': 4,'blue': 5, 'pink': 6, 'black': 7}
-res = s.Counter([dic[input()] for _ in range(int(input()))])
-if len(res) == 1 and res[1]:
-    print(1)
-else:
-    print(res[1] * max(res) + sum(k * v for k, v in res.items()))
+t=[' dleouna'.find(i[2])for i in[*open(0)][1:]]
+print(+((s:=t.count(1))==len(t))or sum(t)+max(t)*s)
