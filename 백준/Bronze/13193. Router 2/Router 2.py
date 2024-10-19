2b800902f1061e7b1eeb9cfdc32c70928d8ebd94
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-10-20 02:18:16 +0900

    [Bronze I] Title: Router 2, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/13193. Router 2/Router 2.py
+++ 백준/Bronze/13193. Router 2/Router 2.py
@@ -1,10 +1,9 @@
 N=223
-Mlim=500000
+Mlim=500000 
 Plim=100
 Nlim=500000
 nodes_used=2*N
-div1=3
-div2=3
+div1=div2=5
 
 connections=[]
 
@@ -30,46 +29,15 @@ class cluster:
         self.count=0
     def __len__(self):
         return len(self.l)
+    def getnode(self,need):
+        if Plim<self.count+need:
+            self.reset()
+        return self.node
     def connect(self,f):
         assert len(self)*len(f)<=Plim
-        if Plim<len(self)*(self.count+len(f)):
-            self.reset()
+        connections.append((self.getnode(len(f)),f.getnode(len(self))))
         self.count+=len(f)
-        if Plim<len(f)*(f.count+len(self)):
-            f.reset()
         f.count+=len(self)
-        connections.append((self.node,f.node))
-
-class cluster_2:
-    def __init__(self,l,head):
-        self.l=l
-        self.head=head
-        self.reset(True)
-        self.len=sum(map(len,l))
-    def reset(self,init=False):
-        self.node=getnode()
-        if self.head:
-            for i in self.l:
-                if not init:
-                    i.reset()
-                connections.append((i.node,self.node))
-        else:
-            for i in self.l:
-                if not init:
-                    i.reset()
-                connections.append((self.node,i.node))
-        self.count=0
-    def __len__(self):
-        return self.len
-    def connect(self,f):
-        assert len(self)*len(f)<=Plim
-        if Plim<len(self)*(self.count+len(f)):
-            self.reset()
-        self.count+=len(f)
-        if Plim<len(f)*(f.count+len(self)):
-            f.reset()
-        f.count+=len(self)
-        connections.append((self.node,f.node))
 
 start=[]
 for i in range(1,N+1,div1):
@@ -77,18 +45,10 @@ for i in range(1,N+1,div1):
 
 end=[]
 for i in range(1,N+1,div1):
-    end+=cluster([*range(N+i,N+min(i+div1,N+1))],False),
+    end+=cluster([*range(N+i,N+min(i+div2,N+1))],False),
 
-start_2=[]
-for i in range(0,len(start),div2):
-    start_2+=cluster_2(start[i:i+div2],True),
-
-end_2=[]
-for i in range(0,len(end),div2):
-    end_2+=cluster_2(end[i:i+div2],False),
-
-for i in start_2:
-    for j in end_2:
+for i in start:
+    for j in end:
         i.connect(j)
 assert len(connections)<=Mlim
 print(nodes_used,len(connections))
