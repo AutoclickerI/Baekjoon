97721a5540b8c20914e8ae5b00e1928f6fcd6a37
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-10-19 20:09:01 +0900

    [Bronze I] Title: Letter Frequency, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/25775. Letter Frequency/Letter Frequency.py
@@ -0,0 +1,15 @@
+l=[i[:-1]for i in open(0)][1:]
+m=max(map(len,l))
+def get(n):
+    return[i[n]for i in l if n<len(i)]
+for i in range(m):
+    z=[]
+    s=get(i)
+    n=0
+    for c in range(97,123):
+        if n<s.count(chr(c)):
+            z=[chr(c)]
+            n=s.count(chr(c))
+        elif n==s.count(chr(c)):
+            z+=chr(c),
+    print(f'{i+1}:',*z)
