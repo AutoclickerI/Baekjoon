923bcfb54ffb933b70467e61ff0dcfe8df54a062
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-10-27 23:09:20 +0900

    [Bronze I] Title: Osobna, Time: 32 ms, Memory: 31120 KB, Score: 50 point -BaekjoonHub

--- 백준/Bronze/32532. Osobna/Osobna.py
+++ 백준/Bronze/32532. Osobna/Osobna.py
@@ -1,6 +1,6 @@
 a,b,c=open(0)
 z=c.split('<')
-print(f'''Ime: {z[0]}
-Prezime: {z[2]}
+print(f'''Ime: {z[0].title()}
+Prezime: {z[2].title()}
 Datum rodjenja: {b[4:6]}-{b[2:4]}-{'20'if int(b[:2])<25 else'19'}{b[:2]}
 OIB: {a[15:26]}''')
