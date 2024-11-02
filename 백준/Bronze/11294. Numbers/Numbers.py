6e85678df81d229ef754c5536ce038f5ce481cf7
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-11-02 13:53:27 +0900

    [Bronze I] Title: Numbers, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/11294. Numbers/Numbers.py
+++ 백준/Bronze/11294. Numbers/Numbers.py
@@ -1,3 +1,3 @@
 i=input
-f=lambda n:n and f(n//b)+[str(x:=n%b),chr(55+x)][x>9]or''
+f=lambda n:n and f(n//b)+chr((x:=n%b)+48+7*(x>9))or''
 while(a:=i())>'#':b=int(i());print(a,c:=int(i()),f(c),sep=', ')
