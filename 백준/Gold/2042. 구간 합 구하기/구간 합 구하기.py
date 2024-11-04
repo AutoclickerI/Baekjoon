87982d615f3be6ba238d85c5d5da865d20277023
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-11-04 22:14:19 +0900

    [Gold I] Title: 구간 합 구하기, Time: 1132 ms, Memory: 140996 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.py
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.py
@@ -1,39 +1,44 @@
 import sys
-sys.setrecursionlimit(9**9)
 input=sys.stdin.readline
-p,q,r=map(int,input().split())
-l=[int(input())for _ in[0]*p]
-tree=4*len(l)*[0]
-n=q+r
-def build(s,e,i):
-    if s==e:
-        tree[i]=l[s]
-        return tree[i]
-    m=(s+e)//2
-    tree[i]=build(s,m,2*i)+build(m+1,e,2*i+1)
-    return tree[i]
-build(0,len(l)-1,1)
-def get_sum(s,e,i,f,t):
-    if e<f or t<s:
-        return 0
-    if f<=s and e<=t:
-        return tree[i]
-    m=(s+e)//2
-    return get_sum(s,m,2*i,f,t)+get_sum(m+1,e,2*i+1,f,t)
-def update(s,e,i,w,v):
-    if w<s or w>e:
-        return
-    tree[i]+=v
-    if s-e:
-        m=(s+e)//2
-        update(s,m,2*i,w,v)
-        update(m+1,e,2*i+1,w,v)
 
-while n:
-    p,q,r=map(int,input().split())
-    if p-1:
-        print(get_sum(0,len(l)-1,1,q-1,r-1))
+N,M,K=map(int,input().split())
+l=[int(input())for _ in[0]*N]
+tree_size=N*4
+tree=[0]*tree_size
+
+# [start, end)
+def build(start,end,idx=1):
+    if end-start==1:
+        tree[idx]=l[start]
     else:
-        update(0,len(l)-1,1,q-1,r-l[q-1])
-        l[q-1]=r
-    n-=1
+        val=build(start,(start+end)//2,idx*2)+build((start+end)//2,end,idx*2+1)
+        tree[idx]=val
+    return tree[idx]
+
+# [start, end) is current checkint pos. [left, right) is target pos
+def get_val(start,end,left,right,idx=1):
+    if end<=left or right<=start:
+        return 0
+    if left<=start and end<=right:
+        return tree[idx]
+    return get_val(start,(start+end)//2,left,right,2*idx)+get_val((start+end)//2,end,left,right,2*idx+1)
+
+def update(start,end,target,val,idx=1):
+    if end-start==1:
+        tree[idx]=val
+        return
+    mid=(start+end)//2
+    if start<=target<mid:
+        update(start,mid,target,val,2*idx)
+    else:
+        update(mid,end,target,val,2*idx+1)
+    tree[idx]=tree[2*idx]+tree[2*idx+1]
+
+build(0,N)
+
+for _ in[0]*(M+K):
+    a,b,c=map(int,input().split())
+    if a==1:
+        update(0,N,b-1,c)
+    else:
+        print(get_val(0,N,b-1,c))
