57587bef9ff9178dd7e662c2a587e3a3821a6cae
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-11-09 15:18:10 +0900

    [Gold I] Title: エゴイ展 (EGOI Exhibition), Time: 44 ms, Memory: 31120 KB, Score: 52 point -BaekjoonHub

--- 백준/Gold/24424. エゴイ展 （EGOI Exhibition）/エゴイ展 （EGOI Exhibition）.py
+++ 백준/Gold/24424. エゴイ展 （EGOI Exhibition）/エゴイ展 （EGOI Exhibition）.py
@@ -4,35 +4,32 @@ input=sys.stdin.readline
 N=int(input())
 M=int(input())
 
-stack=[]
-for _ in[0]*N:
-    a,v=map(int,input().split())
-    if stack and stack[-1][0]==a:
-        if stack[-1][1]<v:
-            stack.pop()
-            stack+=(a,v),
-    else:
-        stack+=(a,v),
-l=[]
-for a,v in stack:
-    if not l and v<0:
-        continue
-    if l:
-        tmp=l.pop()
-        while l and tmp[1]<0 and tmp[0]!=v:
-            tmp2=l.pop()
-            if 0<=tmp2[1]:
-                l+=tmp2,
-                break
-            if tmp[1]<tmp2[1]:
-                tmp=tmp2
-        l+=tmp,
-    if 1<len(l) and l[-1][1]<0 and l[-2][0]==a:
-        s=(a,max(l[-2][1]+l[-1][1]+v,l[-2][1],v))
-        l.pop()
-        l.pop()
-        l+=s,
-    else:
-        l+=(a,v),
+tree_size=M*4
+tree=[0]*tree_size
+dp=[0]*M
 
-print(sum(i for _,i in l if 0<i))  
+def update(start,end,target,val,idx=1):
+    if end-start==1:
+        tree[idx]=val
+        dp[start]=val
+        return
+    mid=(start+end)//2
+    if target<mid:
+        update(start,mid,target,val,idx*2)
+    else:
+        update(mid,end,target,val,idx*2+1)
+    tree[idx]=max(tree[idx*2],tree[idx*2+1])
+
+def get_val(start,end,left,right,idx=1):
+    if end<=left or right<=start or start==end:
+        return 0
+    if left<=start and end<=right:
+        return tree[idx]
+    mid=(start+end)//2
+    return max(get_val(start,mid,left,right,idx*2),get_val(mid,end,left,right,idx*2+1))
+
+for _ in range(N):
+    a,v=map(int,input().split())
+    dp_a=dp[a-1]
+    update(0,M,a-1,max(v+max(get_val(0,M,0,a-1),get_val(0,M,a,M)),dp_a))
+print(get_val(0,M,0,M))
