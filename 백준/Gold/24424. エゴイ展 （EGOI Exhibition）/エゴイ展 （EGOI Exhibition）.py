10565307f1ecaf77047ae7bf00220b49d02adfff
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-11-09 15:40:12 +0900

    [Gold I] Title: エゴイ展 (EGOI Exhibition), Time: 324 ms, Memory: 32288 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/24424. エゴイ展 （EGOI Exhibition）/エゴイ展 （EGOI Exhibition）.py
+++ 백준/Gold/24424. エゴイ展 （EGOI Exhibition）/エゴイ展 （EGOI Exhibition）.py
@@ -4,32 +4,14 @@ input=sys.stdin.readline
 N=int(input())
 M=int(input())
 
-tree_size=M*4
-tree=[0]*tree_size
-dp=[0]*M
+l=[(0,0)]*2
 
-def update(start,end,target,val,idx=1):
-    if end-start==1:
-        tree[idx]=val
-        dp[start]=val
-        return
-    mid=(start+end)//2
-    if target<mid:
-        update(start,mid,target,val,idx*2)
-    else:
-        update(mid,end,target,val,idx*2+1)
-    tree[idx]=max(tree[idx*2],tree[idx*2+1])
-
-def get_val(start,end,left,right,idx=1):
-    if end<=left or right<=start or start==end:
-        return 0
-    if left<=start and end<=right:
-        return tree[idx]
-    mid=(start+end)//2
-    return max(get_val(start,mid,left,right,idx*2),get_val(mid,end,left,right,idx*2+1))
-
-for _ in range(N):
+for _ in[0]*N:
     a,v=map(int,input().split())
-    dp_a=dp[a-1]
-    update(0,M,a-1,max(v+max(get_val(0,M,0,a-1),get_val(0,M,a,M)),dp_a))
-print(get_val(0,M,0,M))
+    new=sorted(l+[(v+p,a)for p,q in l if q!=a])
+    l=[new[-1]]
+    for i in new[-2::-1]:
+        if l[0][1]!=i[1]:
+            l+=i,
+            break
+print(max(l[0][0],l[1][0]))
