a87bedf6ce21d17b65ffa6724d15739a79a4e135
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-11-11 13:53:29 +0900

    [Gold I] Title: エゴイ展 (EGOI Exhibition), Time: 444 ms, Memory: 117520 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/24424. エゴイ展 （EGOI Exhibition）/エゴイ展 （EGOI Exhibition）.py
+++ 백준/Gold/24424. エゴイ展 （EGOI Exhibition）/エゴイ展 （EGOI Exhibition）.py
@@ -4,14 +4,33 @@ input=sys.stdin.readline
 N=int(input())
 M=int(input())
 
-l=[(0,0)]*2
+tree_size=M*2
+tree=[0]*tree_size
 
-for _ in[0]*N:
+def update(target,val):
+    target+=M
+    tree[target]=val
+    while 1<target:
+        target//=2
+        tree[target]=max(tree[target*2],tree[target*2+1])
+
+def get_val(left,right):
+    left+=M
+    right+=M
+    ret=0
+    while left<right:
+        if left%2:
+            ret=max(ret,tree[left])
+            left+=1
+        if right%2:
+            right-=1
+            ret=max(ret,tree[right])
+        left//=2
+        right//=2
+    return ret
+
+for _ in range(N):
     a,v=map(int,input().split())
-    new=sorted(l+[(v+p,a)for p,q in l if q!=a])
-    l=[new[-1]]
-    for i in new[-2::-1]:
-        if l[0][1]!=i[1]:
-            l+=i,
-            break
-print(max(l[0][0],l[1][0]))
+    dp_a=get_val(a-1,a)
+    update(a-1,max(v+max(get_val(0,a-1),get_val(a,M)),dp_a))
+print(get_val(0,M))
