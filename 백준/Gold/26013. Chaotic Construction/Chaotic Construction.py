d1994902bfbe5dc6e3b05839fc5ec52dfb4c34c7
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-11-11 14:09:55 +0900

    [Gold III] Title: Chaotic Construction, Time: 248 ms, Memory: 115500 KB -BaekjoonHub

--- 백준/Gold/26013. Chaotic Construction/Chaotic Construction.py
+++ 백준/Gold/26013. Chaotic Construction/Chaotic Construction.py
@@ -2,45 +2,40 @@ import sys
 input=sys.stdin.readline
 
 N,Q=map(int,input().split())
-tree_size=N*4
+tree_size=N*2
 tree=[0]*tree_size
 
-def build(start,end,idx=1):
-    if end-start==1:
-        tree[idx]=l[start]
-        return tree[idx]
-    mid=(start+end)//2
-    tree[idx]=build(start,mid,idx*2)+build(mid,end,idx*2+1)
-    return tree[idx]
+def update(target,val):
+    target+=N
+    tree[target]=val
+    while 1<target:
+        target//=2
+        tree[target]=tree[target*2]+tree[target*2+1]
 
-def update(start,end,target,val,idx=1):
-    if end-start==1:
-        tree[idx]=val
-        return
-    mid=(start+end)//2
-    if target<mid:
-        update(start,mid,target,val,idx*2)
-    else:
-        update(mid,end,target,val,idx*2+1)
-    tree[idx]=tree[idx*2]+tree[idx*2+1]
-
-def get_val(start,end,left,right,idx=1):
-    if right<=start or end<=left:
-        return 0
-    if left<=start and end<=right:
-        return tree[idx]
-    mid=(start+end)//2
-    return get_val(start,mid,left,right,idx*2)+get_val(mid,end,left,right,idx*2+1)
+def get_val(left,right):
+    left+=N
+    right+=N
+    ret=0
+    while left<right:
+        if left%2:
+            ret+=tree[left]
+            left+=1
+        if right%2:
+            right-=1
+            ret+=tree[right]
+        left//=2
+        right//=2
+    return ret
 
 for _ in[0]*Q:
     q,a,*b=input().split()
     if q=='-':
-        update(0,N,int(a)-1,1)
+        update(int(a)-1,1)
     elif q=='+':
-        update(0,N,int(a)-1,0)
+        update(int(a)-1,0)
     else:
         a,b=sorted([int(a),int(b[0])])
-        if get_val(0,N,a-1,b)and get_val(0,N,b-1,N)+get_val(0,N,0,a):
+        if get_val(a-1,b)and get_val(b-1,N)+get_val(0,a):
             print('impossible')
         else:
             print('possible')
