920bd181ee80c5c9be113d705e0ea5e53eecb53f
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-11-14 06:30:20 +0900

    [Bronze II] Title: 좋은 자동차 번호판, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1871. 좋은 자동차 번호판/좋은 자동차 번호판.py
+++ 백준/Bronze/1871. 좋은 자동차 번호판/좋은 자동차 번호판.py
@@ -1,4 +1,5 @@
-for s in[0]*int(input()):
-    i,j=input().split('-')
-    for c in i:s*=26;s+=ord(c)-65
-    print('not '*(100<abs(int(j)-s))+'nice')
+for z in[*open(0)][1:]:
+ s=0
+ i,j=z.split('-')
+ for c in i:s*=26;s+=ord(c)-65
+ print('not '*(100<abs(int(j)-s))+'nice')
