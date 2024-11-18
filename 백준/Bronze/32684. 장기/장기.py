1c34bd87f1da4291f60b4db5ac412264cd138430
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2024-11-18 13:27:19 +0900

    [Bronze IV] Title: 장기, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/32684. 장기/장기.py
+++ 백준/Bronze/32684. 장기/장기.py
@@ -1,2 +1,2 @@
-f=lambda:sum(i*int(j)for i,j in zip([26,14,10,6,6,4],input().split()))
-print('cocjr0208'*(f()>f()+3)or'ekwoo')
+f=lambda:sum(i*int(j)for i,j in zip(b'h8(',input().split()))
+print(['cocjr0208','ekwoo'][f()<f()+12])
