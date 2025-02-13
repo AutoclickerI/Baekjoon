e23c15f59f5d4a077f76fbbbb34dfdb19f2e98e8
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-02-13 14:18:01 +0900

    [Bronze II] Title: X частей, Time: 36 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Bronze/29292. X частей/X частей.py
+++ 백준/Bronze/29292. X частей/X частей.py
@@ -1,6 +1,4 @@
-_,(*n,),(*X,)=[map(int,i.split()[::-1])for i in open(0)]
+_,n,X=[[*map(int,i.split()[::-1])]for i in open(0)]
 a=0
-while X and n:
-    X[-1]-=n.pop()
-    if X[-1]<1:a-=X.pop()
-print(-1 if X else a+sum(n))
+while X and n:X[-1]-=n.pop();a-=X[-1]<1and X.pop()
+print(X and-1or a+sum(n))
