e058615d1f743a52a98dc65581068f1c92d65a5b
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-03-04 23:02:27 +0900

    [Silver V] Title: 코코넛 그 두 번째 이야기, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/6417. 코코넛 그 두 번째 이야기/코코넛 그 두 번째 이야기.py
+++ 백준/Silver/6417. 코코넛 그 두 번째 이야기/코코넛 그 두 번째 이야기.py
@@ -1,1 +1,1 @@
-while~(n:=int(input())):print(n,'coconuts,',['no solution',*[f'{k} people and 1 monkey'for k in range(1,9)if(n+(-k)**k+k)%k**-~k==1]][-1])
+while~(n:=int(input())):print(n,'coconuts,',max(((n+(-k)**k+k)%k**-~k==1)*f'{k} people and 1 monkey'for k in range(1,9))or'no solution')
