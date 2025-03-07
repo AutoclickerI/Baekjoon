3d125a0f52008688c62504564405f1d51898ed34
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-03-07 17:34:18 +0900

    [Silver I] Title: 회전초밥, Time: 344 ms, Memory: 166868 KB -BaekjoonHub

--- 백준/Silver/28107. 회전초밥/회전초밥.py
+++ 백준/Silver/28107. 회전초밥/회전초밥.py
@@ -1,14 +1,10 @@
-N,M=map(int,input().split())
-
-l=[[]for _ in[0]*200001]
-for v in range(N):
-    for i in input().split()[1:]:
-        l[int(i)]+=v,
-
+(N,M),*x,z=[[*map(int,i.split())]for i in open(0)]
+l=[[]for _ in[0]*8**6]
+c=[v:=0]*N
+while v<N:
+ for i in x[v][1:]:l[i]+=v,
+ v+=1
 l=[i[::-1]for i in l]
-cnt=[0]*N
-
-for i in map(int,input().split()):
-    if l[i]:
-        cnt[l[i].pop()]+=1
-print(*cnt)
+for i in z:
+ if l[i]:c[l[i].pop()]+=1
+print(*c)
