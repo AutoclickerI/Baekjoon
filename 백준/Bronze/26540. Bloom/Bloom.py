33f082de59b5ae3f1fb3cf4a0a27f27ec922cb5d
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-03-12 17:36:48 +0900

    [Bronze I] Title: Bloom, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/26540. Bloom/Bloom.py
+++ 백준/Bronze/26540. Bloom/Bloom.py
@@ -1,2 +1,2 @@
 _,*l=[[*map(int,i.split())]for i in open(0)]
-while l:[n]=l.pop(0);v=l[:n];[s],*l=l[n:];print(n-sum(r<0<s!=sum(j)or(s-sum(j))%sum(j[r:])>0for*j,r in v))
+while l:[n]=l.pop(0);v=l[:n];[s],*l=l[n:];print(n-sum(r<0<s<sum(j)or(s-sum(j))%sum(j[r:])>0for*j,r in v))
