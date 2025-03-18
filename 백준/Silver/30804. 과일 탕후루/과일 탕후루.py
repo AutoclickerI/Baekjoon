81cea0fc410d8375c935c0c5ced8b80f9f4e768a
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-03-18 13:56:06 +0900

    [Silver II] Title: 과일 탕후루, Time: 192 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/30804. 과일 탕후루/과일 탕후루.py
+++ 백준/Silver/30804. 과일 탕후루/과일 탕후루.py
@@ -1,18 +1,4 @@
-from collections import deque
-
-d={}
-dq=deque()
-m=c=0
-
-for i in[*open(0)][1][::2]:
-    d[i]=d.get(i,0)+1
-    c+=1
-    while len(d)>2:
-        v=dq.popleft()
-        d[v]-=1
-        if d[v]<1:
-            del d[v]
-        c-=1
-    m=max(m,c)
-    dq+=i,
-print(m)
+l=[-1]*10
+o=i=0
+for v in[*open(0)][1][::2]:l[int(v)]=i;o=max(o,i-sorted(l)[-3]);i+=1
+print(o)
