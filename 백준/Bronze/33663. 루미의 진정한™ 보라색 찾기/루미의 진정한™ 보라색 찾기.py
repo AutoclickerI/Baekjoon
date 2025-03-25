eb62be4350acb2392e33765a84168f6300c04231
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-03-25 13:55:39 +0900

    [Bronze III] Title: 루미의 진정한™ 보라색 찾기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/33663. 루미의 진정한™ 보라색 찾기/루미의 진정한™ 보라색 찾기.py
@@ -0,0 +1,5 @@
+h,H,s,S,v,V,*l=map(int,open(0).read().split())
+i=l.index(x:=max(l))
+m=min(l)
+a=x-m
+print('Lumi will '+'not '*-~-(v<=x<=V>-1<s<=255*a/x<=S>-1<h<=60*(2*i+(l[i-2]-l[i-1])/a)%360<=H)+'like it.')
