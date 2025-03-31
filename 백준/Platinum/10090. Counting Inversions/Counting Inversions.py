02ed30fd44573fd35ceab338c22c1b0946f3563a
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-03-31 14:51:07 +0900

    [Platinum V] Title: Counting Inversions, Time: 4408 ms, Memory: 132864 KB -BaekjoonHub

---
+++ 백준/Platinum/10090. Counting Inversions/Counting Inversions.py
@@ -0,0 +1,25 @@
+cnt=[0]
+
+def mergesort(l):
+    if len(l)==1:
+        return l
+    n=len(l)//2
+    return merge(mergesort(l[:n]),mergesort(l[n:]))
+
+def merge(a,b):
+    arr=[]
+    while a and b:
+        if b[-1]<a[-1]:
+            cnt[0]+=len(b)
+            arr+=a.pop(),
+        else:
+            arr+=b.pop(),
+    while a:
+        arr+=a.pop(),
+    while b:
+        arr+=b.pop(),
+    return arr[::-1]
+
+mergesort([*map(int,[*open(0)][1].split())])
+
+print(*cnt)
