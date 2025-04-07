7844c4dd8423acf6db03f92258315998a9d2dd9c
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-04-07 21:05:17 +0900

    [Diamond V] Title: 라면 사기 (Small), Time: 48 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Diamond/18185. 라면 사기 （Small）/라면 사기 （Small）.py
+++ 백준/Diamond/18185. 라면 사기 （Small）/라면 사기 （Small）.py
@@ -1,4 +1,2 @@
-n,*l=map(int,open(0).read().split())
-p=q=C=0
-for v in l:X=min(p,v);Y=min(q+X,v);v-=Y;C+=2*Y+3*v;p,q=v,X
-print(C)
+p=q=0
+print(-sum((p:=min(q+(q:=min(-p,v:=int(i)))-v,0))-v*2for i in[*open(0)][1].split()))
