f1d46597554a83cd1f802869ba70c508d17635bb
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-04-09 21:12:24 +0900

    [Platinum IV] Title: 잘못 작성한 요세푸스 코드, Time: 76 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Platinum/1215. 잘못 작성한 요세푸스 코드/잘못 작성한 요세푸스 코드.py
+++ 백준/Platinum/1215. 잘못 작성한 요세푸스 코드/잘못 작성한 요세푸스 코드.py
@@ -1,33 +1,5 @@
-N,K=map(int,input().split())
-
-def naive(N,K):
-    r=0
-    
-    i=N
-    while i:
-        r+=K%i
-        i-=1
-    
-    return r
-
-def fast(N,K):
-    r=0
-    
-    if N>K:
-        r=K*(N-K)
-        N=K
-    
-    i=N
-    while 1<i:
-        if K%i<K%(i-1) and K//i==K//(i-1):
-            step=K%(i-1)-K%i
-            p=0-(K%i-i)//(step+1)
-            r+=(K%i*2+(p-1)*step)*p//2
-            i-=p
-        else:
-            r+=K%i
-            i-=1
-    
-    return r
-
-print(fast(N,K))
+n,k=map(int,input().split())
+r=n*k
+s=1
+while k>=s<=n:a=k//s;e=min(k//a,n);r+=a*(s+e)*(s-e-1)//2;s=e+1
+print(r)
