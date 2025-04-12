02eb5b7daf6ed3399e5aac92b7d49990a3590e47
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-04-12 17:34:24 +0900

    [Bronze II] Title: Target Practice, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/5367. Target Practice/Target Practice.py
+++ 백준/Bronze/5367. Target Practice/Target Practice.py
@@ -1,4 +1,4 @@
 N=int(input())-2
 z=s='|'+'-'*N+'|\n'
-for i in range(N):*l,=' '*N;l[i]=l[N-i-1]='*';s+='|'+''.join(l)+'|\n'
+for i in range(N):*l,=' '*N;l[i]=l[~i]='*';s+='|'+''.join(l)+'|\n'
 print(s+z)
