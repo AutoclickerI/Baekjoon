83b9c6ef6a0e0009f4d0666e2808fa752c2d389b
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-04-17 14:34:59 +0900

    [Gold III] Title: LCA, Time: 516 ms, Memory: 450836 KB -BaekjoonHub

---
+++ 백준/Gold/11437. LCA/LCA.py
@@ -0,0 +1,54 @@
+import sys
+sys.setrecursionlimit(2*10**5)
+input=sys.stdin.readline
+
+N=int(input())
+
+edges=[[]for _ in[0]*-~N]
+
+for _ in[0]*~-N:
+    s,e=map(int,input().split())
+    edges[e]+=s,
+    edges[s]+=e,
+
+v=[0]*-~N
+d=[0]*-~N
+p=[N.bit_length()*[0]for _ in[0]*-~N]
+
+def DFS(n,depth):
+    v[n]=1
+    d[n]=depth
+    for e in edges[n]:
+        if v[e]<1:
+            DFS(e,depth+1)
+            p[e][0]=n
+
+DFS(1,1)
+
+for j in range(N.bit_length()-1):
+    for i in range(N+1):
+        p[i][j+1]=p[p[i][j]][j]
+
+def up(n,v):
+    if v==0:
+        return n
+    t=v.bit_length()-1
+    return up(p[n][t],v^1<<t)
+
+def lca(a,b):
+    if d[b]<d[a]:
+        a,b=b,a
+    b=up(b,d[b]-d[a])
+    
+    if a==b:
+        return a
+    
+    for i in range(N.bit_length()-1,-1,-1):
+        if p[a][i]!=p[b][i]:
+            a=p[a][i]
+            b=p[b][i]
+    return p[a][0]
+
+for _ in[0]*int(input()):
+    s,e=map(int,input().split())
+    print(lca(s,e))
