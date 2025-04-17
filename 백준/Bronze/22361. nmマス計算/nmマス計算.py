bb1f6f3719f4d8c29fff6a351c76271893ef3753
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-04-17 14:03:06 +0900

    [Bronze II] Title: nmマス計算, Time: 192 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Bronze/22361. nmマス計算/nmマス計算.py
+++ 백준/Bronze/22361. nmマス計算/nmマス計算.py
@@ -1,2 +1,2 @@
 f=lambda:[*map(int,input().split())]
-while any(l:=f()):w=f();print(*map(''.join(str(i*j)for i in f()for j in w).count,'0123456789'))
+while(l:=f())[0]:w=f();print(*map(''.join(str(i*j)for i in f()for j in w).count,'0123456789'))
