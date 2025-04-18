eed7ccba0e46e05e26712327f9c763415bee00b1
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-04-19 03:57:25 +0900

    [Bronze II] Title: 이건 무슨 진법이지?, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/13877. 이건 무슨 진법이지？/이건 무슨 진법이지？.py
+++ 백준/Bronze/13877. 이건 무슨 진법이지？/이건 무슨 진법이지？.py
@@ -1,3 +1,1 @@
-for _ in[0]*int(input()):
-    c,n=input().split()
-    print(c,'0'*('7'<max(n))or int(n,8),int(n),int(n,16))
+for _ in[0]*int(input()):c,n=input().split();print(c,'0'*('7'<max(n))or int(n,8),int(n),int(n,16))
