ee0ed61e1d91fea11425bf429e01bb7b3b4784c7
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-04-19 23:42:47 +0900

    [Bronze II] Title: Pea Soup and Pancakes, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/17897. Pea Soup and Pancakes/Pea Soup and Pancakes.py
@@ -0,0 +1,11 @@
+d={}
+z=[]
+for _ in[0]*int(input()):
+    n=int(input())
+    z+=input(),
+    d[z[-1]]=eval('input(),'*n)
+l=[]
+for i in z:
+    if{'pea soup','pancakes'}<={*d[i]}:
+        exit(print(i))
+print('Anywhere is fine I guess')
