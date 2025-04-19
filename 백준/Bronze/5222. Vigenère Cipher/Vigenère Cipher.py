b8bd5c5f22b7ddf4c42e78d2ce9b8181496a84f8
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-04-20 00:09:33 +0900

    [Bronze II] Title: Vigenère Cipher, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/5222. Vigenère Cipher/Vigenère Cipher.py
+++ 백준/Bronze/5222. Vigenère Cipher/Vigenère Cipher.py
@@ -1,2 +1,1 @@
-n,*l=open(0,'rb')
-for i in l:v,w=i.split();print('Ciphertext:',''.join(chr((i+j)%26+65)for i,j in zip(v*99,w)))
+for i in[*open(0,'rb')][1:]:v,w=i.split();print('Ciphertext:',''.join(chr((i+j)%26+65)for i,j in zip(v*99,w)))
