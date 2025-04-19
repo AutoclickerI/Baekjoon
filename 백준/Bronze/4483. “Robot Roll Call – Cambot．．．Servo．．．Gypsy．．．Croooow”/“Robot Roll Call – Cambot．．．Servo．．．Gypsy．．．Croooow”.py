88091e48d79dfa49d892f437adcf79001c1b8978
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-04-19 22:51:46 +0900

    [Bronze I] Title: “Robot Roll Call – Cambot...Servo...Gypsy...Croooow”, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/4483. “Robot Roll Call – Cambot．．．Servo．．．Gypsy．．．Croooow”/“Robot Roll Call – Cambot．．．Servo．．．Gypsy．．．Croooow”.py
@@ -0,0 +1,6 @@
+for T in range(1,int(input())+1):
+    s=[input()for _ in[0]*int(input())]
+    v={*sum([input().split()for _ in[0]*int(input())],[])}
+    print(f'Test set {T}:')
+    for i in s:print(i,'is','parbe'[i not in v::2]+'sent')
+    print()
