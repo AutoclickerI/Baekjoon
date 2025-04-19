ece5b666ba96b4c9488ae9759bd72d4619e04070
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-04-19 22:03:43 +0900

    [Bronze I] Title: Integer Square, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/20240. Integer Square/Integer Square.py
+++ 백준/Bronze/20240. Integer Square/Integer Square.py
@@ -1,15 +1,4 @@
 N=int(input())
-
-x=0
-for i in range(100):
-    for j in range(100):
-        if i*i+j*j==N:x=i,j
-
-if x:
-    i,j=x
-    print(0,0)
-    print(i,j)
-    print(-j,i)
-    print(i-j,i+j)
-else:
-    print('Impossible')
+for i in range(99):
+    for j in range(99):i*i+j*j==N<exit(print(0,0,i,j,-j,i,i-j,i+j))
+print('Impossible')
