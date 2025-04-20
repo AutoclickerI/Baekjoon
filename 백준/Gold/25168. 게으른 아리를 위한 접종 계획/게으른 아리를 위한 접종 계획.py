863f77dd5a8770daaa5610eaa8edc9193f136983
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-04-20 15:16:36 +0900

    [Gold III] Title: 게으른 아리를 위한 접종 계획, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/25168. 게으른 아리를 위한 접종 계획/게으른 아리를 위한 접종 계획.py
+++ 백준/Gold/25168. 게으른 아리를 위한 접종 계획/게으른 아리를 위한 접종 계획.py
@@ -1,8 +1,4 @@
-N,M=map(int,input().split())
-
-dp=[0]*-~N
-
-for s,e,m in sorted([*map(int,input().split())]for _ in[0]*M):
-    dp[e]=max(dp[e],dp[s]+m+(6<m))
-
-print(max(dp)+1)
+(N,M),*l=[[*map(int,i.split())]for i in open(0)]
+d=[1]*-~N
+for s,e,m in sorted(l):d[e]=max(d[e],d[s]+m+(6<m))
+print(max(d))
