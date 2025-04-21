39dd7a51f2456663a307d518365b54ce38d7c351
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-04-21 12:17:08 +0900

    [Bronze II] Title: The Robot Plow, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/5989. The Robot Plow/The Robot Plow.py
+++ 백준/Bronze/5989. The Robot Plow/The Robot Plow.py
@@ -1,12 +1,5 @@
-X, Y, I = map(int, input().split())
-field = [[False] * (X + 1) for _ in range(Y + 1)]
-
-for _ in range(I):
-    x1, y1, x2, y2 = map(int, input().split())
-    for y in range(y1, y2 + 1):
-        for x in range(x1, x2 + 1):
-            field[y][x] = True
-
-# True 값 카운트
-plowed_count = sum(row.count(True) for row in field)
-print(plowed_count)
+(N,M,_),*l=[map(int,i.split())for i in open(0)]
+s=N*M*[0]
+for x,y,X,Y in l:
+    for i in range(x-1,X):s[i*M+y-1:i*M+Y]=[1]*(Y-y+1)
+print(sum(s))
