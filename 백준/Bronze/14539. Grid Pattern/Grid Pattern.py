1b6e34309bd9708ce33165e5bfa72ae59e3a53e2
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-04-21 14:23:06 +0900

    [Bronze II] Title: Grid Pattern, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/14539. Grid Pattern/Grid Pattern.py
+++ 백준/Bronze/14539. Grid Pattern/Grid Pattern.py
@@ -1,1 +1,1 @@
-for i in[*open(T:=0)][1:]:T+=1;r,c,w,h=map(int,i.split());a=('+'+'-'*w)*c+'+';print(f'Case #{T}:',a,*[(('|'+w*'*')*c+'| ')*h+a]*r)
+for i in[*open(T:=0)][1:]:T+=1;r,c,w,h=map(int,i.split());print(f'Case #{T}:',a:='+'+('-'*w+'+')*c,*[(('|'+w*'*')*c+'| ')*h+a]*r)
