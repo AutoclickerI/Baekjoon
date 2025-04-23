db37bdd605e3a6b5cd68320147abe0e90b04e9b0
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-04-23 10:17:14 +0900

    [Bronze II] Title: School Colors, Time: 176 ms, Memory: 111420 KB -BaekjoonHub

--- 백준/Bronze/5205. School Colors/School Colors.py
+++ 백준/Bronze/5205. School Colors/School Colors.py
@@ -1,27 +1,8 @@
-import math
-
-def euclidean(c1, c2):
-    return math.sqrt((c1[0]-c2[0])**2 + (c1[1]-c2[1])**2 + (c1[2]-c2[2])**2)
-
-K = int(input())
-for dataset_index in range(1, K+1):
-    n = int(input())
-    colors = [tuple(map(int, input().split())) for _ in range(n)]
-
-    max_contrast = -1
-    best_pairs = []
-
+for T in range(int(input())):
+    print(f'Data Set {T+1}:')
+    n=int(input())
+    l=[[*map(int,input().split())]for _ in[0]*n]
+    mv=max(sum((j-i)**2for i,j in zip(l[i],l[j]))for i in range(n)for j in range(n))
     for i in range(n):
-        for j in range(i+1, n):
-            contrast = euclidean(colors[i], colors[j])
-            if contrast > max_contrast:
-                max_contrast = contrast
-                best_pairs = [(i+1, j+1)]
-            elif contrast == max_contrast:
-                best_pairs.append((i+1, j+1))
-
-    best_pairs.sort()  # lexicographic sort by (i, j)
-
-    print(f"Data Set {dataset_index}:")
-    for a, b in best_pairs:
-        print(f"{a} {b}")
+        for j in range(i,n):
+            sum((j-i)**2for i,j in zip(l[i],l[j]))==mv!=print(i+1,j+1)
