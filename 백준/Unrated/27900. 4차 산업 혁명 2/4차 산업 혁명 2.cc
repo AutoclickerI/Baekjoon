ec2f930ac35082a9b6d44412e2871ab9ce0f0c64
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-04-23 11:20:00 +0900

    [Unrated] Title: 4차 산업 혁명 2, Time: 16 ms, Memory: 71204 KB, Score: 526 point -BaekjoonHub

--- 백준/Unrated/27900. 4차 산업 혁명 2/4차 산업 혁명 2.cc
+++ 백준/Unrated/27900. 4차 산업 혁명 2/4차 산업 혁명 2.cc
@@ -9,22 +9,22 @@ int next_move(std::string state){
         return 4;
     if(state=="0101011a010101")
         return 4;
-    if(state=="0101012a030101") // +8 (2 games are here)
+    if(state=="0101012a030101") // +2
         return 2;
-    if(state=="0102012a070101") // +8 (1 game is here)
+    if(state=="0102012a070101") // +1
         return 1;
-    if(state=="0202032a070101") // +16 (1 game is here)
+    if(state=="0202032a070101") // +1
         return 3;
-    if(state=="02020d2a070101") // +32 (1 game is here)
+    if(state=="02020d2a070101") // +1
         return 3;
-    if(state=="0602152a070101") // +64 (1 game is here)
+    if(state=="0602152a070101") // +1
         return 1;
-    if(state=="0a06152a070101") // +128 (1 game is here)
+    if(state=="0a06152a070101") // +1
         return 3;
-    if(state=="0a06256a070101")
+    if(state=="0a06256a070101") // +1
         return 1;
-    if(state=="01010106010101")
-        return 4;
+    if(state=="120e256a070101")
+        return 2;
     if(state=="0101011a010101")
         return 4;
     if(state=="01010101010101")
