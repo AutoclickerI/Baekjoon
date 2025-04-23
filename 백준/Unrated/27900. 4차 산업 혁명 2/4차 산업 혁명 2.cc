7ae434141255a2844d029c27a4170eb97cbb43d2
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-04-23 10:56:10 +0900

    [Unrated] Title: 4차 산업 혁명 2, Time: 8 ms, Memory: 71204 KB, Score: 56 point -BaekjoonHub

--- 백준/Unrated/27900. 4차 산업 혁명 2/4차 산업 혁명 2.cc
+++ 백준/Unrated/27900. 4차 산업 혁명 2/4차 산업 혁명 2.cc
@@ -9,10 +9,10 @@ int next_move(std::string state){
         return 4;
     if(state=="0101011a010101")
         return 4;
-    if(state=="0101012a030101")
+    if(state=="0101012a030101") // +8 (2 games are here)
         return 2;
-    if(state=="01010106010101")
-        return 4;
+    if(state=="0102012a070101")
+        return 1;
     if(state=="0101011a010101")
         return 4;
     return 0;
