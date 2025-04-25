fae6fed268ce29ca8931e270d6251e59aa340b07
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-04-25 12:46:44 +0900

    [Bronze I] Title: Assigning Partners, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10105. Assigning Partners/Assigning Partners.py
+++ 백준/Bronze/10105. Assigning Partners/Assigning Partners.py
@@ -1,3 +1,2 @@
-d={}
-for i,j in zip(*[i.split()for i in open(0)][1:]):d[i]=j;d[j]=i
-print('gboaodd'[any(i!=d[d[i]]or i==d[i]for i in d)::2])
+d=dict(zip(*[i.split()for i in open(0)][1:]))
+print('bgaodo d'[all(d[d[i]]==i!=d[i]for i in d)::2])
