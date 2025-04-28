06e84edaa42dde3cd6e351201f5462652374f0ca
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-04-28 19:52:36 +0900

    [Bronze I] Title: PUTOVANJE, Time: 112 ms, Memory: 110736 KB -BaekjoonHub

--- 백준/Bronze/11923. PUTOVANJE/PUTOVANJE.py
+++ 백준/Bronze/11923. PUTOVANJE/PUTOVANJE.py
@@ -1,24 +1,3 @@
-def solve():
-    import sys
-    input = sys.stdin.readline
-
-    # Number of fruits and capacity
-    N, C = map(int, input().split())
-    fruits = list(map(int, input().split()))
-    
-    max_count = 0
-    # Try starting from each fruit
-    for start in range(N):
-        total = 0
-        count = 0
-        # Simulate eating from the current starting index
-        for i in range(start, N):
-            if total + fruits[i] <= C:
-                total += fruits[i]
-                count += 1
-        max_count = max(max_count, count)
-    
-    print(max_count)
-
-if __name__ == '__main__':
-    solve()
+n,k,*a=map(int,open(m:=0).read().split())
+for i in range(n):s=0;m=max(m,sum(s<(s:=s+j*(s+j<=k))for j in a[i:]))
+print(m)
