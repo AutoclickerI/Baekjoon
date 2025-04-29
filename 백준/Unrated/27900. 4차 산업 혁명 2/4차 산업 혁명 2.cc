1dfcd9cf2a3f8ab15a04190da03964800ee8bdd5
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-04-29 14:03:09 +0900

    [Unrated] Title: 4차 산업 혁명 2, Time: 4 ms, Memory: 71204 KB, Score: 2838 point -BaekjoonHub

--- 백준/Unrated/27900. 4차 산업 혁명 2/4차 산업 혁명 2.cc
+++ 백준/Unrated/27900. 4차 산업 혁명 2/4차 산업 혁명 2.cc
@@ -3,47 +3,49 @@
 void init(){ }
 
 int next_move(std::string state){
-    if(state=="01010101010101")
-        return 4;
-    if(state=="01010106010101")
-        return 4;
-    if(state=="0101011a010101")
-        return 4;
-    if(state=="0101012a030101") // +2
+    if(state=="0302012a030101") // +1
         return 2;
-    if(state=="0102012a070101") // +1
-        return 1;
-    if(state=="0202032a070101") // +1
-        return 3;
-    if(state=="02020d2a070101") // +1
-        return 3;
-    if(state=="0602152a070101") // +1
-        return 1;
-    if(state=="0a06152a070101") // +1
-        return 3;
-    if(state=="0a06256a070101") // +1
-        return 1;
-    if(state=="120e256a070101") // +1
-        return 2;
-    if(state=="1236256a070101") // +1
-        return 1;
-    if(state=="6236256a070101") // +1
-        return 2;
-    if(state=="6256256a070301") // +1
+    if(state=="6256656a6b1d54") // +1
         return 6;
-    if(state=="6256256a070d01") // +1
-        return 5;
-    if(state=="6256256a1b0d01") // +1
-        return 5;
-    if(state=="6256256a6b0d01") // +1
-        return 7;
-    if(state=="6256656a6b0d02") // +1
+    if(state=="6256656a6b0d34") // +1
         return 7;
     if(state=="6256656a6b0d0c") // +1
         return 7;
-    if(state=="6256656a6b0d34") // +1
+    if(state=="6256656a6b0d02") // +1
         return 7;
-    if(state=="6256656a6b1d54") // +1
+    if(state=="6256256a6b0d01") // +1
+        return 7;
+    if(state=="6256256a1b0d01") // +1
+        return 5;
+    if(state=="6256256a070d01") // +1
+        return 5;
+    if(state=="6256256a070301") // +1
         return 6;
+    if(state=="6236256a070101") // +1
+        return 2;
+    if(state=="1236256a070101") // +1
+        return 1;
+    if(state=="120e256a070101") // +1
+        return 2;
+    if(state=="0a06256a070101") // +1
+        return 1;
+    if(state=="0a06152a070101") // +1
+        return 3;
+    if(state=="0602152a070101") // +1
+        return 1;
+    if(state=="02020d2a070101") // +1
+        return 3;
+    if(state=="0202032a070101") // +1
+        return 3;
+    if(state=="0102012a070101") // +1
+        return 1;
+    if(state=="0101012a030101") // +2
+        return 2;
+    if(state=="0101011a010101")
+        return 4;
+    if(state=="01010106010101")
+        return 4;
+    if(state=="01010101010101")
+        return 4;
     return 0;
 }
