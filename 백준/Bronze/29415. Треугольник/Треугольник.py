0717a061965fb49a1aee987894252b3ee07791d9
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-04-30 10:28:33 +0900

    [Bronze II] Title: Треугольник, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/29415. Треугольник/Треугольник.py
+++ 백준/Bronze/29415. Треугольник/Треугольник.py
@@ -1,8 +1,2 @@
 s=int(input())*2
-c=0
-for x in range(1,s+1):
-    if(y:=s//x)<x:break
-    z=x**2+y**2
-    c+=z**.5%1==0==s%x
-
-print(c)
+print(sum(s%x==0==(x**2+(s/x)**2)**.5%1for x in range(1,s+1))//2)
