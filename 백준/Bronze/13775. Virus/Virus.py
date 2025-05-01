67290b4f87b3ce4a3b21125b847bb4125664230f
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-05-01 22:59:24 +0900

    [Bronze I] Title: Virus, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/13775. Virus/Virus.py
+++ 백준/Bronze/13775. Virus/Virus.py
@@ -1,8 +1,3 @@
 n,a=open(0)
-s=''
 n=int(n)
-for i in a:
- x=ord(i)
- if 96<x<123:s+=chr((x-n+7)%26+97);n=n%25+1
- else:s+=i
-print(s)
+for i in a:x=ord(i);print(end=[i,chr((x-n+7)%26+97)][f:=96<x<123]);n=f+n%25or 25
