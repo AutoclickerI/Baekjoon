0ab2a4dbc391f6b0e80cffbbd7e438d4923af766
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-05-01 11:49:17 +0900

    [Bronze I] Title: Molar mass, Time: 108 ms, Memory: 110900 KB -BaekjoonHub

--- 백준/Bronze/8941. Molar mass/Molar mass.py
+++ 백준/Bronze/8941. Molar mass/Molar mass.py
@@ -1,9 +1,5 @@
-C=12010
-H=1008
-N=14010
-O=16000
-for s in[*open(0)][1:]:
-    v=''
-    for i,j in zip(s,s[1:]):v+=i+'+'*('@'<j)+'*'*('.'<j<':'<'@'<i)
-    print(f'{eval(v)/1000:.3f}')
-        
+C=12.01
+H=1.008
+N=14.01
+O=16
+for s in[*open(0)][1:]:print('%.3f'%eval(''.join(i+'+'*('@'<j)+'*'*('.'<j<':'<i)for i,j in zip(s,s[1:]))))
