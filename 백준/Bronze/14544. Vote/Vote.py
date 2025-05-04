8a1c0b822f9fbb9ec3419e884e23ecd5c64a6e2b
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-05-04 18:17:45 +0900

    [Bronze I] Title: Vote, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/14544. Vote/Vote.py
+++ 백준/Bronze/14544. Vote/Vote.py
@@ -1,7 +1,6 @@
 r=range
 for T in r(int(input())):
-    n,q=map(int,input(f'VOTE {T+1}: ').split())
-    d={input():0 for i in r(n)}
+    n,q=map(int,input(f'VOTE {T+1}: ').split());d={input():0 for i in r(n)}
     for i in r(q):x,y,z=input().split();d[x]+=int(y)
     d=sorted([[d[i],i] for i in d])
     if len(d)==1 or(a:=d[-1])[0]>d[-2][0]:print('THE WINNER IS',*a[::-1])
