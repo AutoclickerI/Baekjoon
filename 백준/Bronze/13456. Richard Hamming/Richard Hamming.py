64947ad36e9958a41c1552be78e00d75fb1b0f71
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-05-04 23:51:13 +0900

    [Bronze III] Title: Richard Hamming, Time: 100 ms, Memory: 110576 KB -BaekjoonHub

--- 백준/Bronze/13456. Richard Hamming/Richard Hamming.py
+++ 백준/Bronze/13456. Richard Hamming/Richard Hamming.py
@@ -1,2 +1,2 @@
-_,*l=open(0).read().split()
-while l:n=int(l.pop(0));print(sum(i!=j for i,j in zip(l[:n],l[n:])));l=l[2*n:]
+I=input
+exec('I();print(sum(map(str.__ne__,I().split(),I().split())));'*int(I()))
