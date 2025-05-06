cd8e688d360b1e7f3c9868d93fe9af9be6eca77d
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-05-06 16:27:28 +0900

    [Bronze II] Title: Suspicious Event, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Bronze/27622. Suspicious Event/Suspicious Event.py
+++ 백준/Bronze/27622. Suspicious Event/Suspicious Event.py
@@ -1,5 +1,20 @@
-d={0}
-for i in[*open(s:=0)][1].split():
-  if'0'>i:s+=1-({i[1:]}<=d);d-={i[1:]}
-  d|={i}
-print(s)
+def count_suspicious(events):
+    logged_in = set()
+    suspicious = 0
+    for a in events:
+        if a > 0:
+            logged_in.add(a)
+        else:
+            x = -a
+            if x not in logged_in:
+                suspicious += 1
+            else:
+                logged_in.remove(x)
+    return suspicious
+
+if __name__ == "__main__":
+    import sys
+    input = sys.stdin.readline
+    n = int(input())
+    events = list(map(int, input().split()))
+    print(count_suspicious(events))
