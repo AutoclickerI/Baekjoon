871af6730dcb65082a0b54cf50907643d4cb35e9
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-05-06 15:41:55 +0900

    [Bronze I] Title: Finaliści, Time: 44 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Bronze/33817. Finaliści/Finaliści.py
+++ 백준/Bronze/33817. Finaliści/Finaliści.py
@@ -1,30 +1,4 @@
-def main():
-    import sys
-    input = sys.stdin.readline
-
-    n = int(input())
-    eligible = []
-    for i in range(1, n+1):
-        s, x = input().split()
-        x = int(x)
-        if s == "TAK":
-            eligible.append((i, x))
-
-    # First 10 finalists: the top 10 by original ranking
-    group_a = [idx for idx, _ in eligible[:10]]
-
-    # Remove those already selected
-    remaining = eligible[10:]
-    group_b = []
-    for idx, x in remaining:
-        if len(group_b) >= 10:
-            break
-        if x < 2:
-            group_b.append(idx)
-
-    finalists = sorted(group_a + group_b)
-    print(*finalists)
-
-
-if __name__ == "__main__":
-    main()
+c=i=0
+for s in[*open(0)]:
+  if'T'<s!=-c>~(int(s[3:])<2)*10:print(i);c+=1
+  i+=1
