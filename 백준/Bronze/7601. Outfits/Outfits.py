08b390e03babf6b7feab73dee70072cde051802b
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-05-11 15:55:30 +0900

    [Bronze II] Title: Outfits, Time: 92 ms, Memory: 109416 KB -BaekjoonHub

--- 백준/Bronze/7601. Outfits/Outfits.py
+++ 백준/Bronze/7601. Outfits/Outfits.py
@@ -1,6 +1,2 @@
-s=1
-n,d,*l=map(int,open(0).read().split())
-while n:
- a,b=eval('l.pop(0)or 1e3,'*2);print('Scenario',s);s+=1
- for i in range(d):c,d,*l=l;print('Day',i+1,['OK','ALERT'][c+(c>=a)==n-d+(d<b)])
- n,d,*l=l
+n,d,*l=map(int,open(s:=0).read().split())
+while n:a,b,*l=l;print('Scenario',s:=s+1);i=0;exec("c,d,*l=l;print('Day',i:=i+1,['OK','ALERT'][c+(c>=a>0)==n-d+1-(d>=b>0)]);"*d);n,d,*l=l
