7553d0994ea491690204b976edd60d10795c5eef
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-05-13 12:15:23 +0900

    [Bronze II] Title: X X glued, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/14547. X X glued/X X glued.py
+++ 백준/Bronze/14547. X X glued/X X glued.py
@@ -1,2 +1,1 @@
-from itertools import*
-while'#'<(s:=input()):print(end=''.join(f'{v} {v} glued and '*(len([*w])>1)for v,w in groupby(s))[:-4])
+while'#'<(s:=input()):print(end=''.join(f'{v} {v} glued and 'for v in{i for i,j in zip(s,s[1:])if i==j})[:-4])
