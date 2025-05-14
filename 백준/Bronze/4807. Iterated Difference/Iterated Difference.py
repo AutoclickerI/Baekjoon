0c41dd6c91c62896c49165848051709064e2a519
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-05-14 10:30:11 +0900

    [Bronze II] Title: Iterated Difference, Time: 480 ms, Memory: 111552 KB -BaekjoonHub

--- 백준/Bronze/4807. Iterated Difference/Iterated Difference.py
+++ 백준/Bronze/4807. Iterated Difference/Iterated Difference.py
@@ -1,4 +1,4 @@
 for i in[*open(T:=0)][1::2]:
- l=[*map(int,i.split())];c=0
- while c<1000>1<len({*l}):c+=1;l=[abs(l[i]-l[i-1])for i in range(len(l))]
+ *l,=map(int,i.split());c=0
+ while c<1e3>1<len({*l}):c+=1;l=[abs(i-j)for i,j in zip(l,(2*l)[1:])]
  T+=1;print(f'Case {T}:',['not attained','%d iterations'%c][c<1000])
