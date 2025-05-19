517baf44fccbc3c762f2ead3dcce106f0f0a97e1
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-05-19 10:35:04 +0900

    [Bronze II] Title: Eligibility, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10410. Eligibility/Eligibility.py
+++ 백준/Bronze/10410. Eligibility/Eligibility.py
@@ -1,2 +1,1 @@
-e='eligible'
-for i in[*open(0)][1:]:a,b,c,d=i.split();print(a,[['coach petitions','in'+e][int(d)>40],e][b>'201'or'1991'<c])
+for i in[*open(0)][1:]:a,b,c,d=i.split();print(a,[e:='eligible',['coach petitions','in'+e][int(d)>40]][b<'201'>'1991'>c])
