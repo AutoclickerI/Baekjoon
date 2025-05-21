a65b673ca6ebc9118a74a49134afc294b8f0d6fa
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-05-21 11:18:30 +0900

    [Gold II] Title: 중앙값 구하기, Time: 972 ms, Memory: 309988 KB -BaekjoonHub

--- 백준/Gold/2696. 중앙값 구하기/중앙값 구하기.py
+++ 백준/Gold/2696. 중앙값 구하기/중앙값 구하기.py
@@ -1,9 +1,5 @@
 from heapq import*
+h=heappush
+p=heappop
 _,*x=map(int,open(0).read().split())
-while x:
-    n=x.pop(0);l=x[:n];x=x[n:];S=[];L=[];a=[]
-    for i in l:
-        heappush(L,i)
-        heappush(S,-heappop(L))
-        if len(L)<len(S):heappush(L,-heappop(S));a+=L[0],
-    print(len(a),*a)
+while x:S=[];L=[];a=[];exec('i^=1;h(L,x.pop(0));h(S,-p(L))\nif i:h(L,-p(S));a+=L[0],\n'*x.pop(i:=0));print(len(a),*a)
