9a37722b8f54e29640a128a7841423d84e300ffe
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-05-22 14:24:20 +0900

    [Bronze II] Title: Blocks, Time: 60 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/4349. Blocks/Blocks.py
+++ 백준/Bronze/4349. Blocks/Blocks.py
@@ -1,9 +1,1 @@
-for i in[*open(0)][1:]:
-    i=int(i)
-    m=2+4*i
-    for w in range(1,i+1):
-        if i%w<1:
-            for h in range(1,i//w+1):
-                if i//w%h<1:
-                    m=min(m,2*w*h+i//w//h*2*(w+h))
-    print(m)
+for i in[*open(0)][1:]:i=int(i);print(2*min(w*h+i//w//h*(w+h)for w in range(1,i+1)for h in range(1,i//w+1)if i%w<1>i//w%h))
