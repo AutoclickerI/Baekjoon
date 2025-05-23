4e59b660d028cf864ed8c4f9867ff2c954dd0a0e
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-05-23 11:43:05 +0900

    [Bronze II] Title: DVDs, Time: 116 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Bronze/5103. DVDs/DVDs.py
+++ 백준/Bronze/5103. DVDs/DVDs.py
@@ -1,4 +1,2 @@
-while'#'<(s:=input()):
-    m,n=map(int,input().split())
-    for i in range(int(input())):o=input();n=max(0,min(m,n-int(o[1:])*(2*('S'<o)-1)))
-    print(s,n)
+I=input
+while'#'<(s:=I()):m,n=map(int,I().split());exec("o=I();n=max(0,min(m,n-int(o[1:])*(2*('S'<o)-1)));"*int(I()));print(s,n)
