0918921237e34772c3c35e4478b4c31a35d9e8de
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-05-24 01:34:28 +0900

    [Bronze II] Title: Jean and Joe’s Clothes, Time: 36 ms, Memory: 33572 KB -BaekjoonHub

--- 백준/Bronze/5100. Jean and Joe’s Clothes/Jean and Joe’s Clothes.py
+++ 백준/Bronze/5100. Jean and Joe’s Clothes/Jean and Joe’s Clothes.py
@@ -1,2 +1,2 @@
 i=1
-while i:a=[0]*5;i=int(input());exec('s=int(input(),36);a[[20<s<23,s>37,0,s==28,s==33,0,0,1].index(1)%5]+=1;'*i);i>0!=print(*a)
+while i:=int(input()):a=[0]*5;exec('s=int(input(),36)-20;a[[0<s<3,s>17,0,s==8,s==13,0,0,1].index(1)%5]+=1;'*i);print(*a)
