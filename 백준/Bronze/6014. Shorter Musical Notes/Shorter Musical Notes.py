25ad9ac6aa3390762b5927100ac0a00a8516fac9
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-05-23 11:53:39 +0900

    [Bronze II] Title: Shorter Musical Notes, Time: 420 ms, Memory: 214340 KB -BaekjoonHub

--- 백준/Bronze/6014. Shorter Musical Notes/Shorter Musical Notes.py
+++ 백준/Bronze/6014. Shorter Musical Notes/Shorter Musical Notes.py
@@ -1,21 +1,4 @@
-# Python 3
-import sys
-from bisect import bisect_right
-
-input = sys.stdin.readline
-N, Q = map(int, input().split())
-B = [int(input()) for _ in range(N)]
-
-# 누적합
-P = [0]
-for b in B:
-    P.append(P[-1] + b)
-
-out = []
-for _ in range(Q):
-    T = int(input())
-    # P[i] > T 인 최소 i
-    idx = bisect_right(P, T)
-    out.append(str(idx))
-
-print("\n".join(out))
+N,Q,*A=map(int,open(i:=0).read().split())
+X=[]
+exec('i+=1;X+=[i]*A.pop(0);'*N)
+for a in A:print(X[a])
