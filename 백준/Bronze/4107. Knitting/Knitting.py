df762b14a23214a3c7b67873114061cfb929b49d
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-05-23 12:05:48 +0900

    [Bronze II] Title: Knitting, Time: 184 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/4107. Knitting/Knitting.py
+++ 백준/Bronze/4107. Knitting/Knitting.py
@@ -1,15 +1,1 @@
-import sys
-
-for line in sys.stdin:
-    N, M, K = map(int, line.split())
-    if N == M == K == 0:
-        break
-    deltas = list(map(int, sys.stdin.readline().split()))
-
-    total = current = N
-    for r in range(2, M + 1):
-        d = deltas[(r - 2) % K]
-        current += d
-        total += current
-
-    print(total)
+while'1'<(s:=input()):n,m,k,*l=map(int,(s+' '+input()).split());print(n*m+sum(l[i%k]*(m+~i)for i in range(m-1)))
