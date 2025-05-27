513deea510a97e0c5f045b99c838e4de62ce42e5
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-05-27 09:52:24 +0100

    [Bronze I] Title: 임스의 잠수맵, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/34002. 임스의 잠수맵/임스의 잠수맵.py
+++ 백준/Bronze/34002. 임스의 잠수맵/임스의 잠수맵.py
@@ -1,5 +1,4 @@
-a,b,c,s,v,l=map(int,open(0).read().split())
+a,b,c,s,v,l=map(int,open(t:=0).read().split())
 x=(250-l)*100
-t=0
 for m,n in(c,v),(b,s):d=min(x,m*30*n);t-=-d//m;x-=d
-print(t*(x<1)or t-x//-a)
+print(t-(0<x)*x//-a)
