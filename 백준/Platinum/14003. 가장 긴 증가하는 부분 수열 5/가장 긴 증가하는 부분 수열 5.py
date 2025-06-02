65c62c4df930b7d627b021471c2858cbc3656944
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-06-02 16:02:09 +0900

    [Platinum V] Title: 가장 긴 증가하는 부분 수열 5, Time: 700 ms, Memory: 270460 KB -BaekjoonHub

--- 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.py
+++ 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.py
@@ -1,13 +1,8 @@
 from bisect import*
-n,*l=map(int,open(0).read().split())
-d=[m:=0]
-c=[-2e+9]
-for i in l:
- if c[-1]<i:d+=[m:=len(c)];c+=[i]
- else:d+=[L:=bisect_left(c,i)];c[L]=i
-print(m)
-a=[]
-while n:
- if d[n]==m:a+=[l[n-1]];m-=1
- n-=1
-print(*a[::-1])
+n,*a=map(int,open(0).read().split())
+d=[]
+r=[]
+for e in a:p=bisect(d,e-1);d[p:p+1]=e,;r+=p,
+m,*t=max(r),
+while n:n-=1;f=r[n]==m;t+=[n]*f;m-=f
+print(len(d),*[a[i]for i in t[::-1]])
