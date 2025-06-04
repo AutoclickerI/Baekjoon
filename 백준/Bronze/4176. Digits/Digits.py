19073a0ac957954e55edabc7b0a5749a9b3f9d51
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-06-04 12:58:57 +0900

    [Bronze II] Title: Digits, Time: 40 ms, Memory: 34368 KB -BaekjoonHub

--- 백준/Bronze/4176. Digits/Digits.py
+++ 백준/Bronze/4176. Digits/Digits.py
@@ -1,10 +1,4 @@
-for i in[*open(0)][:-1]:
-    c=2
-    if i=='1\n':
-        print(1)
-        continue
-    i=str(len(i)-1)
-    while'1'<i:
-        i=str(len(i))
-        c+=1
-    print(c)
+while(s:=input())<'E':
+ r=1
+ while'1'!=s:s=str(len(s));r+=1
+ print(r)
