3e705c2542ffc149d9738d7f8af0905fd164ee41
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-06-04 16:15:15 +0900

    [Bronze II] Title: Crazy Fencing, Time: 36 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Bronze/21617. Crazy Fencing/Crazy Fencing.py
+++ 백준/Bronze/21617. Crazy Fencing/Crazy Fencing.py
@@ -1,2 +1,2 @@
-_,l,x=[[*map(int,i.split())]for i in open(0)]
-print(sum(k*(i+j)for i,j,k in zip(l,l[1:],x))/2)
+n,*l=map(int,open(0).read().split())
+print(sum(k*(i+j)for i,j,k in zip(l,l[1:],l[-n:]))/2)
