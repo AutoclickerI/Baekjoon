22a8dd8f1bfcce1c8565036595510abb45a025ac
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-06-04 15:33:12 +0900

    [Bronze II] Title: NASSA’s Robot, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/9636. NASSA’s Robot/NASSA’s Robot.py
+++ 백준/Bronze/9636. NASSA’s Robot/NASSA’s Robot.py
@@ -1,14 +1,1 @@
-for _ in[0]*int(input()):
-    x=y=p=0
-    for i in input():
-        if i=='?':
-            p+=1
-        if i=='U':
-            y+=1
-        if i=='D':
-            y-=1
-        if i=='L':
-            x-=1
-        if i=='R':
-            x+=1
-    print(x-p,y-p,x+p,y+p)
+for s in[*open(0)][1:]:R,L,U,D,Q=map(s.count,'RLUD?');print(R-L-Q,U-D-Q,R-L+Q,U-D+Q)
