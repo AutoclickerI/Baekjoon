b0c480f05b247af4faeb4378eb0c40f9443fe37d
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-06-04 15:55:11 +0900

    [Bronze II] Title: Escape Route, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/5364. Escape Route/Escape Route.py
+++ 백준/Bronze/5364. Escape Route/Escape Route.py
@@ -1,3 +1,4 @@
-_,[x,y],*l=[[*map(int,i.split())]for i in open(0)]
-p,q=min(l,key=lambda i:(x-i[0])**2+(y-i[1])**2)
-print(x,y,p,q,f'{abs(x-p+(y-q)*1j):.2f}')
+n,[x,y],*l=[[*map(int,i.split())]for i in open(0)]
+f=lambda a:abs(a[0]-x+(a[1]-y)*1j)
+m=min(l,key=f)
+print(x,y,*m,'%.2f'%f(m))
