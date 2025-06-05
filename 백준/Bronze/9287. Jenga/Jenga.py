c0c7f3f73fa3df3fa96f229f174d9d96de415137
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-06-05 20:10:52 +0900

    [Bronze I] Title: Jenga, Time: 60 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/9287. Jenga/Jenga.py
+++ 백준/Bronze/9287. Jenga/Jenga.py
@@ -1,3 +1,1 @@
-for T in range(1,int(input())+1):
-    l=[input()in'10001'for _ in[0]*int(input())]
-    print(f'Case {T}:',['Standing','Fallen'][any(l)])
+for i in range(int(input())):print(f'Case {i+1}:','SFtaalnldeinn g'[any(['00'in input()for _ in[0]*int(input())])::2])
