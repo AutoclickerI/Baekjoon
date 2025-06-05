f919e291ce8e3c1a37811fa940aa857e49bfdf87
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-06-05 20:13:42 +0900

    [Bronze I] Title: Jenga, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/9287. Jenga/Jenga.py
+++ 백준/Bronze/9287. Jenga/Jenga.py
@@ -1,2 +1,2 @@
 _,*l=open(T:=0)
-while l:T+=1;print(f'Case {T}:','SFtaalnldeinn g'[any(['00'in l.pop(0)for _ in[0]*int(l.pop(0))])::2])
+while l:T+=1;print(f'Case {T}:','SFtaalnldeinn g'[max('00'in l.pop(0)for _ in[0]*int(l.pop(0)))::2])
