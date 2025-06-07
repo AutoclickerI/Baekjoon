7d95975df7da46b7b4207549bdde20faddfbd502
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-06-07 17:27:03 +0900

    [Bronze I] Title: Claustrophobic Cows, Time: 564 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/6003. Claustrophobic Cows/Claustrophobic Cows.py
+++ 백준/Bronze/6003. Claustrophobic Cows/Claustrophobic Cows.py
@@ -1,7 +1,7 @@
 _,*l=[eval(i.replace(' ','+1j*'))for i in open(0)]
 m=1e9
-for i in range(len(l)):
-    for j in range(i+1,len(l)):
-        v=abs(l[i]-l[j])
-        if v<m:m=v;a=i+1,j+1
+for i in range(z:=len(l)):
+ for j in range(i+1,z):
+  v=abs(l[i]-l[j])
+  if v<m:m=v;a=i+1,j+1
 print(*a)
