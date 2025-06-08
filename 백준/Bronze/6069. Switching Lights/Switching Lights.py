dc6c6172606ee241e156afa15adb60ebd8b6e1ef
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-06-08 19:49:20 +0900

    [Bronze I] Title: Switching Lights, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/6069. Switching Lights/Switching Lights.py
+++ 백준/Bronze/6069. Switching Lights/Switching Lights.py
@@ -1,3 +1,1 @@
-[n,m],*z=[map(int,i.split())for i in open(0)]
-v=0
-for q,s,e in z:q and print((2**e-2**~-s&v).bit_count());v^=(2**e-2**~-s)*(1-q)
+for i in[*open(v:=0)][1:]:q,s,e=map(int,i.split());v^=(z:=2**e-2**~-s)*(1-q);q>0!=print((z&v).bit_count())
