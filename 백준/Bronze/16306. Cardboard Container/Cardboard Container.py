106a46dfded16f40577c509ea5c8ace3b6b539df
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-06-08 19:37:53 +0900

    [Bronze I] Title: Cardboard Container, Time: 112 ms, Memory: 110760 KB -BaekjoonHub

--- 백준/Bronze/16306. Cardboard Container/Cardboard Container.py
+++ 백준/Bronze/16306. Cardboard Container/Cardboard Container.py
@@ -1,10 +1,3 @@
 n=int(input())
-r=2*n+1
-i=1
-while i**3<=n:
-    j=i
-    while i*j*j<=n:
-        if n%(i*j)<1:r=min(r,i*j+(i+j)*n//i//j)
-        j+=1
-    i+=1
-print(2*r)
+R=range(1,999)
+print(min(z+n//z*(i+j)for i in R for j in R if n%(z:=i*j)<1)*2)
