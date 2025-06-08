926e73e0893ffd947eeceaf469a129f06297b828
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-06-08 13:57:33 +0900

    [Bronze I] Title: Too Much Water, Time: 112 ms, Memory: 111264 KB -BaekjoonHub

--- 백준/Bronze/6696. Too Much Water/Too Much Water.py
+++ 백준/Bronze/6696. Too Much Water/Too Much Water.py
@@ -1,10 +1,1 @@
-import math
-import sys
-
-for line in sys.stdin:
-    x, y = map(float, line.split())
-    if x == 0 and y == 0:
-        break
-    d2 = x*x + y*y
-    hours = math.ceil(math.pi * d2 / 100.0)
-    print(f"The property will be flooded in hour {hours}.")
+for i in[*open(0)][:-1]:x,y=map(eval,i.split());print('The property will be flooded in hour %d.'%(1+(x*x+y*y)//31.831))
