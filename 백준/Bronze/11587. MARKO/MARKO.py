915d2fd45b390f642a3e97bcc649a96d45e131d1
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-06-09 22:53:20 +0900

    [Bronze II] Title: MARKO, Time: 156 ms, Memory: 111244 KB -BaekjoonHub

--- 백준/Bronze/11587. MARKO/MARKO.py
+++ 백준/Bronze/11587. MARKO/MARKO.py
@@ -1,26 +1,2 @@
-d={}
-def to_str(s):
-    r=''
-    for i in s:
-        if'v'<i:
-            r+='9'
-        elif's'<i:
-            r+='8'
-        elif'o'<i:
-            r+='7'
-        elif'l'<i:
-            r+='6'
-        elif'i'<i:
-            r+='5'
-        elif'f'<i:
-            r+='4'
-        elif'c'<i:
-            r+='3'
-        else:
-            r+='2'
-    return r
-for _ in[0]*int(input()):
-    s=input()
-    v=to_str(s)
-    d[v]=d.get(v,0)+1
-print(d.get(input(),0))    
+_,*W,s=open(0)
+print(sum(all(i in'abc def ghi jkl mno pqrs tuv wxyz'.split()[int(j)-2]for i,j in zip(w,s[:-1]))for w in W))
