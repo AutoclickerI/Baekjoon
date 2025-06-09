d4fc4912668798db820488516dafc81ea9d68956
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-06-09 23:12:26 +0900

    [Bronze I] Title: Magic Squares, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10177. Magic Squares/Magic Squares.py
+++ 백준/Bronze/10177. Magic Squares/Magic Squares.py
@@ -1,9 +1,4 @@
 for _ in[0]*int(input()):
-    N=int(input())
-    l=[[*map(int,input().split())]for _ in[0]*N]
-    a=b=0
+    N=int(input());l=[[*map(int,input().split())]for _ in[0]*N];a=b=0
     for i in range(N):a+=l[i][i];b+=l[~i][i]
-    if len({a,b,*[sum(i)for i in(*l,*zip(*l))]})<2:
-        print('Magic square of size',N)
-    else:
-        print('Not a magic square')
+    print(['Not a magic square',f'Magic square of size {N}'][len({a,b,*[sum(i)for i in(*l,*zip(*l))]})<2])
