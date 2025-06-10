ebb2ce3ada79ec797222aef370518576f2659cf5
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-06-10 15:14:37 +0900

    [Bronze I] Title: 벌점 (DemeritPoints), Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Bronze/16130. 벌점 （DemeritPoints）/벌점 （DemeritPoints）.py
+++ 백준/Bronze/16130. 벌점 （DemeritPoints）/벌점 （DemeritPoints）.py
@@ -1,9 +1,9 @@
 for *x,_ in[*open(0)][1:]:
-    b=s=p=i=S=0
+    b=s=p=i=0;S=''
     for d in x:
         s+=int(d,36)
         if s//10!=p:
             p=s//10
             if p<4:b+=p
-            else:S=p
-    print(f"{b}{[['(weapon)','(09)'][S>4],''][S<1]}")
+            else:S=['(weapon)','(09)'][p>4]
+    print(f'{b}'+S)
