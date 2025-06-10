74dc316ff9b730a62de76d1bd5a349d1341e49ff
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-06-10 12:03:41 +0900

    [Gold IV] Title: 호텔, Time: 688 ms, Memory: 229284 KB -BaekjoonHub

--- 백준/Gold/1106. 호텔/호텔.py
+++ 백준/Gold/1106. 호텔/호텔.py
@@ -1,5 +1,4 @@
 l=[0]+[1e9]*1100
-[C,_],*z=[map(int,i.split())for i in open(0)]
-for c,v in z:
- for i in range(1100):l[i]=min(l[i-v]+c,l[i])
+C,_,*z=map(int,open(0).read().split())
+while z:c,v,*z=z;i=0;exec('l[i]=min(l[i-v]+c,l[i]);i+=1;'*1100)
 print(min(l[C:]))
