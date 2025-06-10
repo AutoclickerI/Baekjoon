77c546231c994d607255ffc61e3951a553011b89
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-06-10 22:16:23 +0900

    [Bronze I] Title: TicTacToe, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/13227. TicTacToe/TicTacToe.py
+++ 백준/Bronze/13227. TicTacToe/TicTacToe.py
@@ -1,3 +1,2 @@
 s=open(0).read()
-a,b,c=s.split()
-print('NYOE S'[any({*i}<{*'OX'}for i in(a,b,c,*zip(a,b,c),s[0::5],s[8::-3]))::2])
+print('NYOE S'[any(s[m-i:m-~i:i]in'OOO XXX'for m,i in zip(b'	',b''))::2])
