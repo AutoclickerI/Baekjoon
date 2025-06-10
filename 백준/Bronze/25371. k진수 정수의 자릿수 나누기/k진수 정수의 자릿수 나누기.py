b84a39eb128462edbb6fe96e3729bf38a159cc5c
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-06-10 12:12:47 +0900

    [Bronze I] Title: k진수 정수의 자릿수 나누기, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/25371. k진수 정수의 자릿수 나누기/k진수 정수의 자릿수 나누기.py
+++ 백준/Bronze/25371. k진수 정수의 자릿수 나누기/k진수 정수의 자릿수 나누기.py
@@ -1,7 +1,5 @@
 n,k=map(int,input().split())
-t=''
-while n:t=str(n%k)+t;n//=k
-t=sum(map(int,t.replace(*'0 ').split()))
-n=''
-while t:n=str(t%k)+n;t//=k
-print(n)
+def f(n,k,t=''):
+ while n:t=str(n%k)+t;n//=k
+ return t
+print(f(eval(f(n,k).replace(*'0+')+'+0'),k))
