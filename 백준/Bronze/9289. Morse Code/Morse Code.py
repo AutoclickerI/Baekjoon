3d5b30cdc693d9e388b30a889e0dcf055c014213
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-06-11 17:37:35 +0900

    [Bronze I] Title: Morse Code, Time: 88 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/9289. Morse Code/Morse Code.py
+++ 백준/Bronze/9289. Morse Code/Morse Code.py
@@ -1,2 +1,1 @@
-l=b'5HJ<2B>@4G=D76?FM:839A;IKL'
-for T in range(int(input())):print(f'Case {T+1}:',''.join(chr(65+l.index(48+int('1'+i.translate({46:48, 45:49}),2)))for i in input().split()))
+for T in range(int(input())):print(f'Case {T+1}:',''.join(chr(65+b'5HJ<2B>@4G=D76?FM:839A;IKL'.find(48+int('1'+i.translate({46:48,45:49}),2)))for i in input().split()))
