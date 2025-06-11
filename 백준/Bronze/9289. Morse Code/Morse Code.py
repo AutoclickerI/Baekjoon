35b138166ce68bcfceeabadcc03b2536d75f48fe
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-06-11 17:34:18 +0900

    [Bronze I] Title: Morse Code, Time: 92 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/9289. Morse Code/Morse Code.py
+++ 백준/Bronze/9289. Morse Code/Morse Code.py
@@ -1,6 +1,6 @@
-l=[5,24,26,12,2,18,14,16,4,23,13,20,7,6,15,22,29,10,8,3,9,17,11,25,27,28]
+l=b'5HJ<2B>@4G=D76?FM:839A;IKL'
 for T in range(int(input())):
     s=''
     for i in input().split():
-        s+=chr(65+l.index(int('1'+i.translate({46:48, 45:49}),2)))
+        s+=chr(65+l.index(48+int('1'+i.translate({46:48, 45:49}),2)))
     print(f'Case {T+1}:',s)
