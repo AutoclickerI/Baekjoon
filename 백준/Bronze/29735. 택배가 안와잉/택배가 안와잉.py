69bf079008a86676b61bbbc37b2c3c2aef3f7bd2
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-06-11 19:07:34 +0900

    [Bronze I] Title: 택배가 안와잉, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Bronze/29735. 택배가 안와잉/택배가 안와잉.py
+++ 백준/Bronze/29735. 택배가 안와잉/택배가 안와잉.py
@@ -1,7 +1,4 @@
 h,m,H,M,N,T=map(int,open(0).read().replace(*': ').split())
-t=(H*60+M-h*60-m-1)//T
-z=N//t
-N%=t
-N+=1
-v=h*60+m+N*T
-print(z,f'{v//60:02}:{v%60:02}')
+t=((H-h)*60+M+~m)//T
+v=m-~(N%t)*T
+print(N//t,f'{h+v//60:02}:{v%60:02}')
