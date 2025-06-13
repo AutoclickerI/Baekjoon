a760854ef2cf105cdbfbf3e7f23e7fc0c1187b43
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-06-13 12:37:15 +0900

    [Platinum I] Title: N! mod P (2), Time: 2712 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Platinum/17467. N！ mod P （2）/N！ mod P （2）.py
+++ 백준/Platinum/17467. N！ mod P （2）/N！ mod P （2）.py
@@ -1,8 +1,4 @@
 N,P=map(int,input().split())
-a=1
-if P<N*2:
-    for i in range(N+1,P):a*=i;a%=P
-    a=pow(-a,-1,P)
-else:
-    for i in range(1,N+1):a*=i;a%=P
-print(a)
+f=P<N*2
+for i in range(*(a:=1,N+1,P)[f:f+2]):a=a*i%P
+print(f*pow(-a,-1,P)or a)
