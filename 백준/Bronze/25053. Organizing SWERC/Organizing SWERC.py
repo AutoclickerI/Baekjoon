b81d154988609a51969f33540941f21dd95a4dbe
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-06-16 13:14:14 +0900

    [Bronze II] Title: Organizing SWERC, Time: 116 ms, Memory: 110812 KB -BaekjoonHub

--- 백준/Bronze/25053. Organizing SWERC/Organizing SWERC.py
+++ 백준/Bronze/25053. Organizing SWERC/Organizing SWERC.py
@@ -1,5 +1,4 @@
 for _ in range(int(input())):
-  N=int(input())
-  MX=[0]*10
-  for _ in range(N):a,b=map(int,input().split());MX[-b]=max(MX[-b],a)
-  print('MOREPROBLEMS'*(min(MX)<1)or sum(MX))
+ N=int(input());MX=[0]*10
+ for _ in[0]*N:a,b=map(int,input().split());MX[-b]=max(MX[-b],a)
+ print('MOREPROBLEMS'*(min(MX)<1)or sum(MX))
