3998c534d63f17c233a2afb8dafa3c00be40898d
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-06-17 12:33:25 +0900

    [Bronze II] Title: Mountain Ranges, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/18041. Mountain Ranges/Mountain Ranges.py
+++ 백준/Bronze/18041. Mountain Ranges/Mountain Ranges.py
@@ -1,8 +1,2 @@
-n,x=map(int,input().split())
-d=[*map(int,input().split())]
-s=m=1
-for i,j in zip(d,d[1:]):
-  if j-i<=x:s+=1
-  else:s=1
-  m=max(m,s)
-print(max(m,s))
+n,x,*l=map(int,open(0).read().split())
+print(max([s:=1]+[s:=s*(j+~i<x)+1for i,j in zip(l,l[1:])]))
