15cd83a49d9038d8a12c45d13d102601c162a888
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-06-17 16:34:23 +0900

    [Gold III] Title: 타율 분석, Time: 92 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Gold/34029. 타율 분석/타율 분석.py
+++ 백준/Gold/34029. 타율 분석/타율 분석.py
@@ -1,5 +1,4 @@
 import math
 A,B=map(int,input()[2:].split())
-t=5*10**(3*B-2)
 a=i=0
-while 1:a+=A**i*(1000-A)**(B-i)*math.comb(B,i);t<=a<exit(print(i));i+=1
+while 1:a+=20*A**i*(1000-A)**(B-i)*math.comb(B,i);1000**B<=a<exit(print(i));i+=1
