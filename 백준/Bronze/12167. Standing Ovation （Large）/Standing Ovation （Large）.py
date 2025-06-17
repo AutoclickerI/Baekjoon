e0c5e96f49e1d968e2fdd818dec4c072dc935374
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-06-17 12:41:21 +0900

    [Bronze II] Title: Standing Ovation (Large), Time: 64 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/12167. Standing Ovation （Large）/Standing Ovation （Large）.py
+++ 백준/Bronze/12167. Standing Ovation （Large）/Standing Ovation （Large）.py
@@ -1,4 +1,4 @@
 for x in open(T:=0).read().split()[2::2]:
- s=a=0;T+=1
- for i,v in enumerate(x):m=max(i-s,0);a+=m;s+=m+int(v)
+ s=a=i=0;T+=1
+ for v in x:m=max(i-s,0);a+=m;s+=m+int(v);i+=1
  print(f'Case #{T}:',a)
