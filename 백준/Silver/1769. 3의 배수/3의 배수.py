2e86bbb8b3d110f8b49287767e1f43e7dafe6a01
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-06-19 00:04:51 +0900

    [Silver V] Title: 3의 배수, Time: 108 ms, Memory: 114496 KB -BaekjoonHub

--- 백준/Silver/1769. 3의 배수/3의 배수.py
+++ 백준/Silver/1769. 3의 배수/3의 배수.py
@@ -1,8 +1,4 @@
-s=input()
+k=input()
 i=0
-while 1<len(s):
-    i+=1
-    s=str(sum(map(int,s)))
-print(i)
-if s in'369':print('YES')
-else:print('NO')
+while k[1:]:k=str(sum(map(int,k)));i+=1
+print(i,'NYOE S'[k in'369'::2])
