62b4485d4c1f63c5cfe4edf8bae010a9dca45b0a
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-06-18 12:54:00 +0900

    [Bronze II] Title: Znak działania, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/8713. Znak działania/Znak działania.py
+++ 백준/Bronze/8713. Znak działania/Znak działania.py
@@ -1,4 +1,4 @@
 a,b=input().split()
-f=lambda x:[f'{x}',f'({x})'][int(x)<0]
+f=lambda x:['%s','(%s)'][int(x)<0]%x
 T={eval(t:=f(a)+i+f(b)):t for i in'+-*'}
 print(['NIE',T[m:=max(T)]+'='+f(m)][len(T)>2])
