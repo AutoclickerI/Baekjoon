f1636c5287f2f814c975d0f8fd0bb71cef0776e6
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-06-19 13:09:17 +0900

    [Bronze III] Title: Triangle, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/26509. Triangle/Triangle.py
+++ 백준/Bronze/26509. Triangle/Triangle.py
@@ -1,2 +1,2 @@
 f=lambda:sorted(map(int,input().split()))
-for i in range(*f()):x,y,z=a=f();b=f();print('NYOE S'[(x*x+y*y==z*z)*(a==b)::2])
+for i in range(*f()):x,y,z=a=f();b=f();print('NYOE S'[x*x+y*y==z*z!=a==b::2])
