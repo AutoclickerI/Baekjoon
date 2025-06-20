e615f60f03ef650b330a8e6cd70db67f9ec37020
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-06-20 11:15:22 +0900

    [Bronze II] Title: Imena, Time: 156 ms, Memory: 112584 KB -BaekjoonHub

--- 백준/Bronze/13939. Imena/Imena.py
+++ 백준/Bronze/13939. Imena/Imena.py
@@ -1,2 +1,3 @@
 import re
-print(*[len(re.findall(r'\b[A-Z][a-z]*\b',s))for s in re.split('[.?!]',[*open(0)][1][:-1])if s])
+input()
+for s in re.split('[.?!]',input()):''<s!=print(len(re.findall(r'\b[A-Z][a-z]*\b',s)))
