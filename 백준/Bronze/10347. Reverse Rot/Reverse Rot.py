2cdbdbd0533ab960ed45add948db5f7d97f66d9c
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-06-20 11:07:23 +0900

    [Bronze II] Title: Reverse Rot, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10347. Reverse Rot/Reverse Rot.py
+++ 백준/Bronze/10347. Reverse Rot/Reverse Rot.py
@@ -1,2 +1,2 @@
-d=[*map(chr,range(65,91)),*'_.']*2
-while'0'<(i:=input()):n,s=i.split();print(''.join(d[d.index(c)+int(n)]for c in s[::-1]))
+d=*map(chr,range(65,91)),*'_.'
+while'0'<(i:=input()):n,s=i.split();print(''.join(d[d.index(c)+int(n)-28]for c in s[::-1]))
