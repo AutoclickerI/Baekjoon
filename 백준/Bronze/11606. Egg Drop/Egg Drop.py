83bd2dc7b629df7d448c3c99ab05f623ab5c34b6
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-06-20 10:51:30 +0900

    [Bronze II] Title: Egg Drop, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/11606. Egg Drop/Egg Drop.py
+++ 백준/Bronze/11606. Egg Drop/Egg Drop.py
@@ -1,8 +1,8 @@
-SAFE=1
-BROKEN=0
-(_,k),*l=[[*map(eval,i.split())]for i in open(0)]
+(_,k),*l=map(str.split,open(0))
 x=1
+k=int(k)
 for f,s in l:
- if s:x=max(x,f)
+ f=int(f)
+ if'C'<s:x=max(x,f)
  else:k=min(k,f)
 print(x+1,k-1)
