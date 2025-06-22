546247068f4f804e5c56d3e199d6a1308a48f38c
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-06-22 16:26:40 +0900

    [Bronze II] Title: Beautiful Music, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/15181. Beautiful Music/Beautiful Music.py
+++ 백준/Bronze/15181. Beautiful Music/Beautiful Music.py
@@ -1,2 +1,2 @@
 t='ABCDEFG'*2
-for s in[*open(0)][:-1]:print('OTuhcaht!  mTuhsaitc  hiusr tbse amuyt iefaurls..'[all(any(j==t[t.find(i)+k]for k in b'')for i,j in zip(s,s[1:-1]))::2])
+while'#'<(s:=input()):print('OTuhcaht!  mTuhsaitc  hiusr tbse amuyt iefaurls..'[all(any(j==t[t.find(i)+k]for k in b'')for i,j in zip(s,s[1:]))::2])
