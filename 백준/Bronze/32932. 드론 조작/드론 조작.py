c5157e4613b6f9efc87e24f316c93cb923fc346a
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-06-23 15:49:32 +0900

    [Bronze I] Title: 드론 조작, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/32932. 드론 조작/드론 조작.py
+++ 백준/Bronze/32932. 드론 조작/드론 조작.py
@@ -1,7 +1,6 @@
-_,*l,s=open(0)
-y=x=0
-for i in s[:-1]:
-    dy,dx=[(-1,0),(0,1),(1,0),(0,-1)]['DRUL'.find(i)]
-    ny,nx=y+dy,x+dx
-    if f'{nx} {ny}\n'not in l:y,x=ny,nx
+_,*l,o=open(0)
+x=y=0
+for i in o[:-1]:
+ q='DUR'.find(i)*2;z=x+q//3,y+q%3-1
+ if'%d %d\n'%z not in l:x,y=z
 print(x,y)
