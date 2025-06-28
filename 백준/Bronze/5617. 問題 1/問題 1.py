5d8638e881d409f8b083ab30b899c4e72a24dd4f
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-06-29 01:46:50 +0900

    [Bronze II] Title: 問題 1, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/5617. 問題 1/問題 1.py
+++ 백준/Bronze/5617. 問題 1/問題 1.py
@@ -1,2 +1,2 @@
 t=a=o=0
-for s in open(0):x,y,z=sorted(map(int,s.split()));x+y<=z<exit(print(t,t-a-o,a,o));t+=1;v=x*x+y*y-z*z;o+=v<0;a+=0<v
+for i in open(0):x,y,z=sorted(map(int,i.split()));x+y<=z<exit(print(t,t-a-o,a,o));t+=1;v=x*x+y*y-z*z;o+=v<0;a+=0<v
