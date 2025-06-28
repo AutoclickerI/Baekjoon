1a681425c8c7506d7626deb09dd28659590e3d91
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-06-29 00:32:03 +0900

    [Bronze II] Title: Vera and ABCDE, Time: 108 ms, Memory: 110736 KB -BaekjoonHub

--- 백준/Bronze/15444. Vera and ABCDE/Vera and ABCDE.py
+++ 백준/Bronze/15444. Vera and ABCDE/Vera and ABCDE.py
@@ -1,4 +1,3 @@
-import re
-l=[re.findall('...',f'{ord(i):b}'.translate({49:42,48:46}))for i in'篭篯礧筯秧']
+l=[[f'{ord(i):b}'.translate({49:42,48:46})[j*3:-~j*3]for j in range(5)]for i in'篭篯礧筯秧']
 input()
 for i in zip(*[l[ord(i)-65]for i in input()]):print(*i,sep='')
