298acbfbddfa338a181ae0acfbd8619163aa3115
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-06-29 00:10:44 +0900

    [Bronze II] Title: Vera and ABCDE, Time: 40 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Bronze/15444. Vera and ABCDE/Vera and ABCDE.py
+++ 백준/Bronze/15444. Vera and ABCDE/Vera and ABCDE.py
@@ -1,5 +1,6 @@
-d='*** *** *** *** *** *.* *.* *.. *.* *.. *** *** *.. *.* *** *.* *.* *.. *.* *.. *.* *** *** *** ***'.split()
+v=[7,5,5,4,5,4]
+d=[7]*4+[*v,7,7,4,5,*v,5]+[7]*4
 l=[]
 input()
 for i in input():l+=d[ord(i)-65::5],
-for i in zip(*l):print(*i,sep='')
+for i in zip(*l):print(''.join(f'{j:03b}'.translate(str.maketrans('10','*.'))for j in i))
