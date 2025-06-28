ac076223b204bbe787e9f21c8b7093b1c490a413
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-06-28 12:09:39 +0900

    [Bronze II] Title: Millennium, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/7015. Millennium/Millennium.py
+++ 백준/Bronze/7015. Millennium/Millennium.py
@@ -1,1 +1,1 @@
-for i in[*open(0)][1:]:y,m,d=map(int,i.split());print(196471-(~-y*195+~-y//3*5-(y%3>0)*~-m//2+~-m*20+d))
+for i in[*open(0)][1:]:y,m,d=map(int,i.split());print(196471-(118*~-y//3*5-(y%3>0)*~-m//2+~-m*20+d))
