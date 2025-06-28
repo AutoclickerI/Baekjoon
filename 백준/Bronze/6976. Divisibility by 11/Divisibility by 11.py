c7647439123e3813c80a776fe46bad1e488d4639
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-06-29 01:40:30 +0900

    [Bronze II] Title: Divisibility by 11, Time: 96 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Bronze/6976. Divisibility by 11/Divisibility by 11.py
+++ 백준/Bronze/6976. Divisibility by 11/Divisibility by 11.py
@@ -1,4 +1,4 @@
 for i in[*open(0)][1:]:
- i=t=int(i);
- while 100<=t:print(t);x=t%10;t//=10;t-=x
+ i=t=int(i)
+ while 99<t:print(t);t=t//10-t%10
  print(t,'The number',i,'is'+' not'*(0<t%11),'divisible by 11.\n')
