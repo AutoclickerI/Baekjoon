143b170e1af0fc39b66c7f3c062efc046fba3fbd
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-06-30 23:39:15 +0900

    [Bronze I] Title: 여름나기, Time: 108 ms, Memory: 112256 KB -BaekjoonHub

--- 백준/Bronze/18130. 여름나기/여름나기.py
+++ 백준/Bronze/18130. 여름나기/여름나기.py
@@ -1,24 +1,3 @@
-def main():
-    import sys
-    input = sys.stdin.readline
-    N, Q = map(int, input().split())
-    
-    best_cost = None
-    best_idx = 0
-    
-    for i in range(1, N+1):
-        P, K, C = map(int, input().split())
-        # Q시간 이전까지 지불해야 하는 횟수
-        M = (Q - 1) // K
-        # 1*C + 2*C + ... + M*C = C * M*(M+1)//2
-        extra = C * M * (M + 1) // 2
-        total = P + extra
-        
-        if best_cost is None or total < best_cost:
-            best_cost = total
-            best_idx = i
-    
-    print(best_idx, best_cost)
-
-if __name__ == "__main__":
-    main()
+(n,q),*l=[map(int,i.split())for i in open(0)]
+v=0
+print(*min((i+(x:=~-q//j)*-~x//2*k,v:=v+1)for i,j,k in l)[::-1])
