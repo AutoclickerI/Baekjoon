80c056979459b7d48447ce1eba304cb1b46d1e76
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-06-30 23:41:53 +0900

    [Bronze I] Title: 여름나기, Time: 108 ms, Memory: 112256 KB -BaekjoonHub

--- 백준/Bronze/18130. 여름나기/여름나기.py
+++ 백준/Bronze/18130. 여름나기/여름나기.py
@@ -1,3 +1,3 @@
-(n,q),*l=[map(int,i.split())for i in open(0)]
+(_,q),*l=[map(int,i.split())for i in open(0)]
 v=0
-print(*min((i+(x:=~-q//j)*-~x//2*k,v:=v+1)for i,j,k in l)[::-1])
+print(*min((i-(x:=q//-j)*~x//2*k,v:=v+1)for i,j,k in l)[::-1])
