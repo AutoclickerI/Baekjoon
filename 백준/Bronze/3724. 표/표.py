6f02be169e78c7cb918e160bee9bb3b7f24f4799
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-07-04 13:32:17 +0900

    [Bronze I] Title: 표, Time: 92 ms, Memory: 37800 KB -BaekjoonHub

--- 백준/Bronze/3724. 표/표.py
+++ 백준/Bronze/3724. 표/표.py
@@ -1,5 +1,5 @@
 _,*l=map(int,open(0).read().split())
 while l:
- M,N,*l=l;z=l[:M*N];l=l[M*N:]
- for i in range(M,M*N):z[i%M]*=z[i]
- print(max((z[i],i+1)for i in range(M))[1])
+ M,N,*l=l
+ for i in range(M,M*N):l[i%M]*=l[i]
+ print(max((l[i],i+1)for i in range(M))[1]);l=l[M*N:]
