2a4a4cac25516247683c3a07396b658b0cb4998c
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-07-04 11:36:38 +0900

    [Bronze I] Title: 생일 맞추기, Time: 348 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/28358. 생일 맞추기/생일 맞추기.py
+++ 백준/Bronze/28358. 생일 맞추기/생일 맞추기.py
@@ -1,7 +1,1 @@
-l=[31,29,31,30,31,30,31,31,30,31,30,31]
-for v in[*open(0)][1:]:
- a=0;s={str(i)for i in range(10)if'0'<v[i*2]}
- for i in range(12):
-  for j in range(l[i]):
-   a+=s-{*f'{i+1}{j+1}'}==s
- print(a)
+for s in[*open(0)][1:]:print(sum(all(s[2*int(i)]<'1'for i in f'{m}{d+1}')for m in range(1,13)for d in range(29+(2!=m)+(m>>3^m&1))))
