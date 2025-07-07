c616d21477ea584f45483e737bdd6c0e5abd26f4
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-07-07 14:31:13 +0900

    [Bronze I] Title: 814 - 1, Time: 32 ms, Memory: 32412 KB, Score: 623 point -BaekjoonHub

--- 백준/Bronze/16972. 814 － 1/814 － 1.py
+++ 백준/Bronze/16972. 814 － 1/814 － 1.py
@@ -2,25 +2,25 @@ N=16280
 
 def trifill(v):
     l=[]
-    f=y=0
-    dy=-int((3**.5/2*v)//-1)
+    f=1
+    y=0
+    dy=int(3**.5/2*v)
     sx=v//2
-    while sx*sx+dy*dy<v*v:
-        dy+=1
     while y<=N:
         if f:
             x=sx
-            while x<N:
+            while x<=N:
                 l+=(x,y),
                 x+=v
         else:
             x=0
-            while x<N:
+            while x<=N:
                 l+=(x,y),
                 x+=v
         y+=dy
+        f^=1
     return l
 
-z=trifill(625)[:813]
+*z,(p,q)=trifill(625)[:814]
 for x,y in z:print(x-8140,y-8140)
-print(-5640,8119)
+print(p-8140,q-8141)
