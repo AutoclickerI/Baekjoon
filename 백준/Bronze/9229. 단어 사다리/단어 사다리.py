f121f8b37ae1d2b348893c96f9a3749322a8a803
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-07-07 14:44:33 +0900

    [Bronze I] Title: 단어 사다리, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/9229. 단어 사다리/단어 사다리.py
+++ 백준/Bronze/9229. 단어 사다리/단어 사다리.py
@@ -1,5 +1,4 @@
 while 1:
  a=[]
  while(i:=input())>'#':a+=i,
- a or exit()
- print(('C','Inc')[any(len(b)!=len(c)or 1!=len({*b}-{*c})for b,c in zip(a,a[1:]))]+'orrect')
+ print(('C','Inc')[any(len(b)!=len(c)or 1!=len({*b}-{*c})for b,c in zip(a or exit(),a[1:]))]+'orrect')
