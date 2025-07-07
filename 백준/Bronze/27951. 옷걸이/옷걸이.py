7cc53bb1b2510b8755516074325e16921bc925b3
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-07-07 12:34:31 +0900

    [Bronze I] Title: 옷걸이, Time: 148 ms, Memory: 119488 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/27951. 옷걸이/옷걸이.py
+++ 백준/Bronze/27951. 옷걸이/옷걸이.py
@@ -1,8 +1,5 @@
-n,*a,u,d=map(int,open(0).read().split())
-for i in range(n):
- x=a[i]
- if x<3:a[i]=' UD'[x];u-=x<2;d-=x>1
-if(u<0)|(d<0):exit(print('NO'))
-for i in range(n):
- if a[i]==3:a[i]='DU'[u>0];u-=u>0
-print('YES\n'+''.join(a))
+_,s,i=open(0)
+U,D=map(int,i.split())
+u,d=map(s.count,'12')
+f=str.replace
+print(['NO','YES\n'+f(f(f(f(f(s,*'3U',U-u),' ',''),*'2D'),*'1U'),*'3D')][u-U<1>d-D])
