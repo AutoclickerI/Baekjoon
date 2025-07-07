3cbe0843090897a14019f003cfe1e5f3fed5d33e
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-07-07 14:17:01 +0900

    [Bronze I] Title: 814 - 1, Time: 32 ms, Memory: 32412 KB, Score: 541 point -BaekjoonHub

--- 백준/Bronze/16972. 814 － 1/814 － 1.py
+++ 백준/Bronze/16972. 814 － 1/814 － 1.py
@@ -1,2 +1,26 @@
-for i in range(-8140,8120,20):print(i,i)
-print(8119,8119)
+N=16280
+
+def trifill(v):
+    l=[]
+    f=y=0
+    dy=-int((3**.5/2*v)//-1)
+    sx=v//2
+    while sx*sx+dy*dy<v*v:
+        dy+=1
+    while y<=N:
+        if f:
+            x=sx
+            while x<N:
+                l+=(x,y),
+                x+=v
+        else:
+            x=0
+            while x<N:
+                l+=(x,y),
+                x+=v
+        y+=dy
+    return l
+
+z=trifill(625)[:813]
+for x,y in z:print(x-8140,y-8140)
+print(-5640,8119)
