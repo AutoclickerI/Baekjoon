991b78e27be820ad1c4f684ebfb793712e115861
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-07-10 12:26:29 +0900

    [Bronze I] Title: 문자 인식, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/3448. 문자 인식/문자 인식.py
+++ 백준/Bronze/3448. 문자 인식/문자 인식.py
@@ -1,4 +1,2 @@
 s=[]
-for*i,_ in[*open(0)][1:]:
- s+=i;x=round(100*(1-s.count('#')/len(s)),1)
- if[]==i:print(f'Efficiency ratio is {[int(x),x][0<x%1]}%.');s=[]
+for*i,_ in[*open(0)][1:]:s+=i;x=round(100*(1-s.count('#')/len(s)),1);i or print(f'Efficiency ratio is {[int(x),x][0<x%1]}%.');s=i and s
