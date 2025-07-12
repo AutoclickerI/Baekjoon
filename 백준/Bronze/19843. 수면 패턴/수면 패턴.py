d28ea42ebf093312a2c1ac184674301ef48b64d4
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-07-12 23:15:51 +0900

    [Bronze I] Title: 수면 패턴, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/19843. 수면 패턴/수면 패턴.py
+++ 백준/Bronze/19843. 수면 패턴/수면 패턴.py
@@ -1,5 +1,5 @@
 f='ouehr'.find
-t,*l=map(str.split,open(0))
-t=int(t[0])
-for a,b,c,d in l:t-=(f(c[1])-f(a[1]))*24+int(d)-int(b)
-print(-(48<t)or max(t,0))
+t,*l=open(0)
+t=int(t[:-3])
+for i in l:a,b,c,d=i.split();t-=(f(c[1])-f(a[1]))*24+int(d)-int(b)
+print(-(48<t)or(0<t)*t)
