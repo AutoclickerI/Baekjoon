756b21845b5bf62ca1595f1ffcc6147d44095c94
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-07-12 22:56:33 +0900

    [Bronze I] Title: 리그, Time: 96 ms, Memory: 111456 KB -BaekjoonHub

--- 백준/Bronze/5544. 리그/리그.py
+++ 백준/Bronze/5544. 리그/리그.py
@@ -1,4 +1,4 @@
-[N],*l=[map(int,i.split())for i in open(0)]
-B=[0]*-~N
-for a,b,c,d in l:z=c==d;B[a]+=(c>d)*3+z;B[b]+=(c<d)*3+z
+N,*l=open(0)
+B=[0]*-~int(N)
+for i in l:a,b,c,d=map(int,i.split());z=c==d;B[a]+=(c>d)*3+z;B[b]+=(c<d)*3+z
 for a in B[1:]:print(1+sum(a<x for x in B))
