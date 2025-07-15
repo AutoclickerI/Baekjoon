c9249a45d949d21dd707780523b26a145910aa70
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-07-15 10:59:14 +0900

    [Silver V] Title: Marslaste kalender, Time: 120 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Silver/29950. Marslaste kalender/Marslaste kalender.py
+++ 백준/Silver/29950. Marslaste kalender/Marslaste kalender.py
@@ -1,6 +1,4 @@
 A,B=map(int,input().split())
-s=0
-M=m=1
-while m<=B:s-=m*~m;B-=m;m+=1
-while M<A:s+=M*~M;A-=M;M+=1
+s=m=1
+while(x:=m<=B)|(y:=m<A):s-=m*~m*(x-y);A-=m*y;B-=m*x;m+=1
 print(s-A*~-A-B*~B>>1)
