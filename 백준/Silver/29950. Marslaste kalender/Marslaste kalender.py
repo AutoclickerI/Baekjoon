d60c290cf8339c3fb72130d02e3ca46ca6de9511
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-07-15 10:52:46 +0900

    [Silver V] Title: Marslaste kalender, Time: 948 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/29950. Marslaste kalender/Marslaste kalender.py
+++ 백준/Silver/29950. Marslaste kalender/Marslaste kalender.py
@@ -1,4 +1,6 @@
-s=lambda x:~x*x//-2
-c=lambda x:~-~x*~x*x//6
-(m,p),(M,P)=map(lambda d:(m:=-int(((1+8*int(d))**.5-1)//-2),int(d)-~-m*m//2),input().split())
-print(s(P)-s(-p)+(s(m)+c(M-1)-c(m))*(m<M))
+A,B=map(int,input().split())
+s=0
+M=m=1
+while m<=B:s-=m*~m;B-=m;m+=1
+while M<A:s+=M*~M;A-=M;M+=1
+print(s-A*~-A-B*~B>>1)
