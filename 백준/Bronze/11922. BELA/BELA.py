941c7bca9209d21a6512acf43bc6b773bab6d8c4
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-07-16 23:22:53 +0900

    [Bronze II] Title: BELA, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/11922. BELA/BELA.py
+++ 백준/Bronze/11922. BELA/BELA.py
@@ -1,27 +1,2 @@
-import sys
-
-def bela_score():
-    input = sys.stdin.readline
-    # 1) N과 우위 슈트 B 읽기
-    N, B = input().split()
-    N = int(N)
-    
-    # 2) 카드별 점수 테이블 정의
-    score_dom = {'A':11, 'K':4, 'Q':3, 'J':20, 'T':10, '9':14, '8':0, '7':0}
-    score_non = {'A':11,'K':4,'Q':3,'J':2,'T':10,'9':0,'8':0,'7':0}
-    
-    total = 0
-    # 3) 4N장 카드 읽어서 합산
-    for _ in range(4 * N):
-        card = input().strip()
-        rank, suit = card[0], card[1]
-        if suit == B:
-            total += score_dom[rank]
-        else:
-            total += score_non[rank]
-    
-    # 4) 결과 출력
-    print(total)
-
-if __name__ == '__main__':
-    bela_score()
+i,*A=open(0)
+print(sum([11,11,4,4,3,3,2,20,10,10,0,14,0,0,0,0][2*'AKQJT987'.find(x)+(y==i[-2])]for x,y,_ in A))
