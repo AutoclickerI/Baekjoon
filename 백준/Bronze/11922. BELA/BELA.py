9ed838bde4e9c7d3c0849e2e084f1ade974e3942
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-07-16 23:35:26 +0900

    [Bronze II] Title: BELA, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/11922. BELA/BELA.py
+++ 백준/Bronze/11922. BELA/BELA.py
@@ -1,2 +1,2 @@
 i,*l=open(0)
-print(sum([11,4,3,2,10,0,0,0][v:='AKQJT987'.find(x)]+(14*(5==v)+18*(3==v))*(y==i[-2])for x,y,_ in l))
+print(sum(b'LLEEDDCUKKAOAA'[2*'AKQJT9'.find(x)+(y==i[-2])]-65for x,y,_ in l))
