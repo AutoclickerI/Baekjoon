b99e2db056e4f388cc9d86c9f9f248e3983471d1
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-07-17 12:52:18 +0900

    [Bronze II] Title: Look and Say, Time: 180 ms, Memory: 113704 KB -BaekjoonHub

--- 백준/Bronze/4841. Look and Say/Look and Say.py
+++ 백준/Bronze/4841. Look and Say/Look and Say.py
@@ -1,8 +1,2 @@
-for s in[*open(0)][1:]:
-    c=0
-    p=s[0]
-    for i in s:
-        if i!=p:print(c,end=p);c=1
-        else:c+=1
-        p=i
-    print()
+n=1
+for i,j in zip(s:=''.join([*open(0)][1:]),s[1:]):i!=j!=print(end=f'{n}'*('.'<i)+i);n=1+n*(i==j)
