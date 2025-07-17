6061bf1524cba0c2ed827ba53ce07dcc90484e02
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-07-17 14:08:44 +0900

    [Bronze II] Title: Tiles of Tetris, NOT!, Time: 32 ms, Memory: 34536 KB -BaekjoonHub

--- 백준/Bronze/4890. Tiles of Tetris， NOT！/Tiles of Tetris， NOT！.py
+++ 백준/Bronze/4890. Tiles of Tetris， NOT！/Tiles of Tetris， NOT！.py
@@ -1,2 +1,2 @@
-import math
-for i in[*open(0)][:-1]:w,h=map(int,i.split());g=math.gcd(w,h);print(w//g*(h//g))
+from math import*
+for i in[*open(0)][:-1]:a,b=map(int,i.split());print(lcm(a,b)//gcd(a,b))
