c3ca2bb98e82a81607f916ed720d03cf1568cc9c
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-07-17 13:40:37 +0900

    [Bronze II] Title: Bookings, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Bronze/7598. Bookings/Bookings.py
+++ 백준/Bronze/7598. Bookings/Bookings.py
@@ -1,4 +1,4 @@
 while'$'<(s:=input()):
  a,b=s.split();b=int(b)
- while(s:=input())<'X':c,d=s.split();d=int(d);b+=d*[-(d<=b),b+d<69][c<'C']
+ while(s:=input())<'X':d=int(s[1:]);b+=d*[-(d<=b),b+d<69][s<'C']
  print(a,b)
