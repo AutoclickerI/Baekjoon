d5fa20e0db96acfe2394e2749e350f652d0dc3e6
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-07-18 15:45:21 +0900

    [Silver III] Title: 오디션, Time: 168 ms, Memory: 126232 KB -BaekjoonHub

--- 백준/Silver/34068. 오디션/오디션.py
+++ 백준/Silver/34068. 오디션/오디션.py
@@ -1,10 +1,7 @@
 N,*l=[int(i.split()[0])for i in open(0)]
 v=[a:=0]*N
 for i in l:v[-i]+=1
-d=[0]*4**9
+d=[0]*6**7
 for i in v:d[i]+=1
-for i in range(4**9):
-    if d[i]:
-        a+=d[i]-1
-        d[i+1]+=d[i]-1
+for i in range(4**9):f=max(d[i]-1,0);a+=f;d[i+1]+=f
 print(a)
