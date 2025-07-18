c82b265aabddb2d79d9b70d1f774635d3059b8f0
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-07-18 14:27:22 +0900

    [Silver IV] Title: 자리 바꾸기, Time: 168 ms, Memory: 42344 KB -BaekjoonHub

---
+++ 백준/Silver/34069. 자리 바꾸기/자리 바꾸기.py
@@ -0,0 +1,11 @@
+(N,M),*l=[[*map(int,i.split())]for i in open(0)]
+if N*M%2:print('No')
+else:
+    print('Yes')
+    if N%2:
+        *l,=zip(*l)
+    l[::2],l[1::2]=l[1::2],l[::2]
+    if N%2:
+        *l,=zip(*l)
+    for i in l:
+        print(*i)
