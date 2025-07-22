8b9c91fcd888b2c40ea992ad9c6b9741cbda3c33
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-07-22 11:55:36 +0900

    [Bronze II] Title: Cold Compress, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/17011. Cold Compress/Cold Compress.py
+++ 백준/Bronze/17011. Cold Compress/Cold Compress.py
@@ -1,9 +1,3 @@
-for i in[*open(0)][1:]:
-    a=[i[0]]
-    x=[]
-    v=0
-    for c in i:
-        f=a[-1]==c
-        if f:v+=1
-        else:a+=c,;x+=v,;v=1
-    for i,j in zip(a,x):print(j,i)
+for s in[*open(0)][1:]:
+ u=0;p=s[0]
+ for c in s:u=+(c!=p!=print(u,p))or-~u;p=c
