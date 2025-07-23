2d6947a3975c970e96ef3e9b72d391e31f2f6b0d
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-07-23 12:19:31 +0900

    [Bronze II] Title: Outer Triangle Sum, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/14542. Outer Triangle Sum/Outer Triangle Sum.py
+++ 백준/Bronze/14542. Outer Triangle Sum/Outer Triangle Sum.py
@@ -1,5 +1,2 @@
-T=0
-while n:=int(input()):
-    T+=1
-    l=eval('input().split(),'*n)
-    print(f'Case #{T}:{sum(int(i[0])+int(i[-1])for i in l)-int(l[0][0])+sum(map(int,l[-1][1:-1]or[0]))}')
+t=0
+while n:=int(input()):t+=1;print(f"Case #{t}:{eval('+[sum(T:=[*map(int,input().split())]),T[0]+(1<len(T)and T[-1])][len(T)<n]'*n)}")
