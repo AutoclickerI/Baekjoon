d7c549043c9c79b58c9f1e61bdf8293ba1162d3d
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-07-23 11:42:19 +0900

    [Silver I] Title: Tidy Numbers (Large), Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/14791. Tidy Numbers （Large）/Tidy Numbers （Large）.py
+++ 백준/Silver/14791. Tidy Numbers （Large）/Tidy Numbers （Large）.py
@@ -1,3 +1,3 @@
 for s in[*open(T:=0)][1:]:
- for x in range(b:=len(s:=s[:-1])-1,0,-1):s=str(int(s)+~int(v:=s[x:])*(v<s[x-1]))
+ for x in range(b:=len(s:=s[:-1])-1):s=str(int(s)+~int(v:=s[b-x:])*(v<s[b+~x]))
  T+=1;print(f'Case #{T}:',s)
