3324118595263bb6c2bced84c2203a50b600e25b
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-07-24 14:12:54 +0900

    [Bronze II] Title: 보물 지도, Time: 124 ms, Memory: 44748 KB -BaekjoonHub

--- 백준/Bronze/29332. 보물 지도/보물 지도.py
+++ 백준/Bronze/29332. 보물 지도/보물 지도.py
@@ -1,4 +1,4 @@
 z=[[],[],[],[]]
 for i in[*open(0)][1:]:x,y,c=i.split();z[ord(c)%5]+=int((y,x)[c in'LR']),
-f=lambda s,e:min(z[s])+~max(z[e])
-print(f(1,2)*f(3,0)if all(z)else'Infinity')
+f=lambda s:min(z[~s])+~max(z[s])
+print(f(2)*f(0)if all(z)else'Infinity')
