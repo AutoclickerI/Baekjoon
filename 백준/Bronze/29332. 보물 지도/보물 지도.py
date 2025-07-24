9f1c9521072504dbe4cd549efbe4dd6d581a4b83
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-07-24 14:11:47 +0900

    [Bronze II] Title: 보물 지도, Time: 144 ms, Memory: 122896 KB -BaekjoonHub

--- 백준/Bronze/29332. 보물 지도/보물 지도.py
+++ 백준/Bronze/29332. 보물 지도/보물 지도.py
@@ -1,5 +1,4 @@
-l,r,u,d=[],[],[],[]
-for i in[*open(0)][1:]:x,y,c=i.split();[u,l,r,d][ord(c)%5]+=int((y,x)[c in'LR']),
-try:v=(min(l)+~max(r))*(min(d)+~max(u))
-except:v='Infinity'
-print(v)
+z=[[],[],[],[]]
+for i in[*open(0)][1:]:x,y,c=i.split();z[ord(c)%5]+=int((y,x)[c in'LR']),
+f=lambda s,e:min(z[s])+~max(z[e])
+print(f(1,2)*f(3,0)if all(z)else'Infinity')
