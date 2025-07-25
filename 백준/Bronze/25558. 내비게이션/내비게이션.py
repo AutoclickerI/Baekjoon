f4b723f44e44e16199057e376b7a5d2cade749da
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-07-25 22:10:57 +0900

    [Bronze II] Title: 내비게이션, Time: 564 ms, Memory: 53536 KB -BaekjoonHub

--- 백준/Bronze/25558. 내비게이션/내비게이션.py
+++ 백준/Bronze/25558. 내비게이션/내비게이션.py
@@ -1,14 +1,4 @@
-N=int(input())
-p,q,r,s,*l=map(int,input().split())
-for _ in[0]*N:
-    v=[(p,q)]
-    for _ in[0]*int(input()):
-        v+=[*map(int,input().split())],
-    v+=(r,s),
-    l+=v,
-def reduce(v):
-    a=0
-    for(i,j),(k,l)in zip(v,v[1:]):
-        a+=abs(i-k)+abs(j-l)
-    return a
-print(min(range(N),key=lambda i:reduce(l[i]))+1)
+_,q,*z=[[*map(int,i.split())]for i in open(0)]
+l=[]
+while z:v=z[0][0]+1;s,e=q[:2];l+=sum(abs(s-(s:=i))+abs(e-(e:=j))for i,j in(*z[1:v],q[2:])),;z=z[v:]
+print(l.index(min(l))+1)
