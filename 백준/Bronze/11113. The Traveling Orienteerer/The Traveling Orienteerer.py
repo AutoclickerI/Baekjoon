91545d1fe1750a7b9bf4390c95750373533d883c
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-07-26 17:48:13 +0900

    [Bronze II] Title: The Traveling Orienteerer, Time: 112 ms, Memory: 111248 KB -BaekjoonHub

--- 백준/Bronze/11113. The Traveling Orienteerer/The Traveling Orienteerer.py
+++ 백준/Bronze/11113. The Traveling Orienteerer/The Traveling Orienteerer.py
@@ -1,44 +1,4 @@
-import sys
-import math
-
-def read_input():
-    input = sys.stdin.read
-    data = input().splitlines()
-    return data
-
-def main():
-    data = read_input()
-    idx = 0
-
-    # number of control points
-    n = int(data[idx])
-    idx += 1
-
-    # read control point coordinates
-    points = []
-    for _ in range(n):
-        x, y = map(float, data[idx].split())
-        points.append((x, y))
-        idx += 1
-
-    # number of routes
-    m = int(data[idx])
-    idx += 1
-
-    for _ in range(m):
-        p = int(data[idx])
-        idx += 1
-        route = list(map(int, data[idx].split()))
-        idx += 1
-
-        total_dist = 0.0
-        for i in range(p - 1):
-            x1, y1 = points[route[i]]
-            x2, y2 = points[route[i + 1]]
-            dist = math.hypot(x2 - x1, y2 - y1)
-            total_dist += dist
-
-        print(round(total_dist))
-
-if __name__ == "__main__":
-    main()
+[n],*l=map(str.split,open(0))
+n=int(n)
+p=[eval(i+'j+'+j)for i,j in l[:n]]
+for i in l[n+2::2]:print(round(sum(abs(p[int(j)]-p[int(k)])for j,k in zip(i,i[1:]))))
