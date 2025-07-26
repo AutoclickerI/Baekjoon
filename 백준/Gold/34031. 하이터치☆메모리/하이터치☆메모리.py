1197186f66873cfc875de8452eedcf2e31ef88f4
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-07-26 18:54:26 +0900

    [Gold IV] Title: 하이터치☆메모리, Time: 128 ms, Memory: 129628 KB -BaekjoonHub

--- 백준/Gold/34031. 하이터치☆메모리/하이터치☆메모리.py
+++ 백준/Gold/34031. 하이터치☆메모리/하이터치☆메모리.py
@@ -1,6 +1,6 @@
 A,B=open(0,'rb')
 d={}
 c=z=l=a=0
-for i in A[:-1]:c-=i%2*2-1;l|=c<0;d[c]=d.get(c,0)+1-l
-for i in B:z-=i%2*2-1;l=min(l,z);a+=(z==l)*d.get(-z,0)
+for i in A[:-1]:c+=i%-2|1;l|=c<0;d[c]=d.get(c,0)+1-l
+for i in B:z+=i%-2|1;l=min(l,z);a+=(z==l)*d.get(-z,0)
 print(a)
