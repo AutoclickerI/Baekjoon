031343794c8e9871e4a8a86170b87aead00541bf
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-07-27 09:24:10 +0900

    [Bronze II] Title: 마라탕후루, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/31923. 마라탕후루/마라탕후루.py
+++ 백준/Bronze/31923. 마라탕후루/마라탕후루.py
@@ -1,7 +1,6 @@
-(n,p,q),A,B=[map(int,i.split())for i in open(0)]
-C=[]
+(n,p,q),A,B,*l=[map(int,i.split())for i in open(0)]
 for a,b in zip(A,B):
  c=0
  while a-b:a+=p;b+=q;c+=1;99<c<exit(print('NO'))
- C+=c,
-print('YES',*C)
+ l+=c,
+print('YES',*l)
