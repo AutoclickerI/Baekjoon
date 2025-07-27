9a14b2f66674eaa6d9bed24d7e0d49b6756509e8
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-07-27 09:42:43 +0900

    [Bronze II] Title: 타자 연습, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/17487. 타자 연습/타자 연습.py
+++ 백준/Bronze/17487. 타자 연습/타자 연습.py
@@ -1,5 +1,5 @@
 l=[0,0]
 c=0
-for i in input():c+=i<']';v=ord(i)%32;l[7<v<17 or v==21]+=' '<i
+for i in input():c+=i<']';v=ord(i)%32;l[7<v<17or v==21]+=' '<i
 while c:c-=1;l[l[1]<l[0]]+=1
 print(*l)
