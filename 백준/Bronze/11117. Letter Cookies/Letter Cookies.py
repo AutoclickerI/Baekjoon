bd4226534dadd80dd819180272068710ed607e67
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-07-28 22:52:31 +0900

    [Bronze II] Title: Letter Cookies, Time: 68 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Bronze/11117. Letter Cookies/Letter Cookies.py
+++ 백준/Bronze/11117. Letter Cookies/Letter Cookies.py
@@ -1,2 +1,5 @@
 _,*l=open(0)
-while l:s,n,*l=l;exec("i=l.pop(0);print('YNEOS'[any(s.count(c)<i.count(c)for c in i)::2]);"*int(n))
+while l:
+ n=int(l[1])
+ for i in l[2:n+2]:print('YNEOS'[any(l[0].count(c)<i.count(c)for c in i)::2])
+ l=l[n+2:]
