8475583dd4b35fdb48ea6f12e9b8a2a1e359798a
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-07-29 19:43:10 +0900

    [Bronze II] Title: Cow Routing, Time: 132 ms, Memory: 111460 KB -BaekjoonHub

--- 백준/Bronze/10675. Cow Routing/Cow Routing.py
+++ 백준/Bronze/10675. Cow Routing/Cow Routing.py
@@ -1,24 +1,9 @@
-def calc(a, b, c, v):
-    try:
-        idx_a = v.index(a)
-        idx_b = v.index(b)
-        return c if idx_a < idx_b else int(1e9)
-    except ValueError:
-        return int(1e9)
-
-def solve():
-    a, b, n = map(int, input().split())
-    ans = int(1e9)
-    
-    for _ in range(n):
-        c,k = map(int, input().split())
-        *rest, = map(int, input().split())
-        if len(rest) < k:
-            rest += list(map(int, input().split()))
-        v = rest[:k]
-        ans = min(ans, calc(a, b, c, v))
-
-    print(-1 if ans == int(1e9) else ans)
-
-if __name__ == "__main__":
-    solve()
+f=lambda:map(int,input().split())
+*A,n=f()
+y=999
+for x in[0]*n:
+ c,r=f()
+ for i in f():
+  x+=i==A[x]
+  if x>1:y=min(y,c);break
+print(-(998<y)or y)
