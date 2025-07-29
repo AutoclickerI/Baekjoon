91f25915dac572c3276af1130056954cf90644ab
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-07-29 19:49:12 +0900

    [Bronze II] Title: 맥주 냉장고, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/3595. 맥주 냉장고/맥주 냉장고.py
+++ 백준/Bronze/3595. 맥주 냉장고/맥주 냉장고.py
@@ -1,2 +1,2 @@
 n=int(input())
-print(*min(min(((i*j+n//j+n//i),i,j,n//i//j)for j in range(1,int((n//i)**0.5)+1)if n//i%j<1)for i in range(1,int(n**0.5)+1)if n%i<1)[1:])
+print(*min(min(((i*j+n//j+n//i),i,j,n//i//j)for j in range(1,int((n//i)**.5)+1)if n//i%j<1)for i in range(1,int(n**.5)+1)if n%i<1)[1:])
