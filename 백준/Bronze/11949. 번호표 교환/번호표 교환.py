e43fa2a542fc364e85703c85007e19db031a4933
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-07-29 19:54:11 +0900

    [Bronze II] Title: 번호표 교환, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/11949. 번호표 교환/번호표 교환.py
+++ 백준/Bronze/11949. 번호표 교환/번호표 교환.py
@@ -2,5 +2,5 @@ N,M,*A=map(int,open(i:=0).read().split())
 while i<M:
  i+=1
  for n in range(1,N):
-  if A[m:=n-1]%i>A[n]%i:A[m],A[n]=A[n],A[m]
+  if A[n]%i<A[m:=n-1]%i:A[m],A[n]=A[n],A[m]
 print(*A)
