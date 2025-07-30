335fa645c8cd7aa4f5eb6ee7e767b89a16b946be
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-07-30 20:23:59 +0900

    [Bronze I] Title: Ornitolog 2, Time: 144 ms, Memory: 119432 KB -BaekjoonHub

--- 백준/Bronze/26713. Ornitolog 2/Ornitolog 2.py
+++ 백준/Bronze/26713. Ornitolog 2/Ornitolog 2.py
@@ -1,19 +1,5 @@
 n,*l=map(int,open(0).read().split())
 z=l[:]
 c=v=0
-for i in range(n-1):
-    if i%2:
-        if l[i]<=l[i+1]:
-            l[i+1]=-1e9
-            c+=1
-        if z[i]>=z[i+1]:
-            z[i+1]=1e9
-            v+=1
-    else:
-        if l[i]>=l[i+1]:
-            l[i+1]=1e9
-            c+=1
-        if z[i]<=z[i+1]:
-            z[i+1]=-1e9
-            v+=1
+for i in range(1,n):f=l[i-1];f=[0,f<=l[i],f>=l[i]][p:=i%-2|1];F=z[i-1];F=[0,F>=z[i],F<=z[i]][p];p*=1e9;l[i]-=f*p;c+=f;z[i]+=F*p;v+=F
 print(min(c,v))
