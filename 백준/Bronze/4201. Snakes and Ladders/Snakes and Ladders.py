2b893dd3b8e16ffd65859cee231bfdda850174cf
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-07-30 19:18:49 +0900

    [Bronze II] Title: Snakes and Ladders, Time: 1300 ms, Memory: 380520 KB -BaekjoonHub

--- 백준/Bronze/4201. Snakes and Ladders/Snakes and Ladders.py
+++ 백준/Bronze/4201. Snakes and Ladders/Snakes and Ladders.py
@@ -1,6 +1,6 @@
 (a,b,c),*z=[map(int,i.split())for i in open(0)]
 d=dict(z[:b])
 l=[1]*a
-i=0
-for[v]in z[b:]:v=min(100,l[i:=i%a]+v);x=l[i]=d.get(v,v);i+=x<100
-for i in range(a):print('Position of player',i+1,f'is {l[i]}.')
+T=i=0
+for[v]in z[b:]:x=l[i]=d.get(*[min(100,l[i:=i%a]+v)]*2);i+=x<100
+while T<a:print('Position of player',T+1,f'is {l[T]}.');T+=1
