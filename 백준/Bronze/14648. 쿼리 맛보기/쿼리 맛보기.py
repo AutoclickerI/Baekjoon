a8a8a8ed2366a40dc19c86b2f437728943758d4a
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-07-31 20:30:19 +0900

    [Bronze II] Title: 쿼리 맛보기, Time: 80 ms, Memory: 36504 KB -BaekjoonHub

--- 백준/Bronze/14648. 쿼리 맛보기/쿼리 맛보기.py
+++ 백준/Bronze/14648. 쿼리 맛보기/쿼리 맛보기.py
@@ -1,6 +1,7 @@
-_,a,*l=[[*map(int,i.split())]for i in open(0)]
+_,a,*l=[map(int,i.split())for i in open(0)]
+a=[0,*a]
 for b,c,d,*e in l:
- r=sum(a[c-1:d])
- if b-1:r-=sum(a[e[0]-1:e[1]])
- else:a[c-1],a[d-1]=a[d-1],a[c-1]
+ r=sum(a[c:d+1])
+ if b-1:r-=sum(a[e[0]:e[1]+1])
+ else:a[c],a[d]=a[d],a[c]
  print(r)
