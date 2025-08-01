7f06018824f3f345eaa13fba15694e266d95c8b3
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-08-01 19:32:42 +0900

    [Bronze I] Title: 마라토너, Time: 36 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Bronze/9339. 마라토너/마라토너.py
+++ 백준/Bronze/9339. 마라토너/마라토너.py
@@ -1,2 +1,2 @@
-_,*l=[map(int,i.split())for i in open(0)]
-while l:_,s,[n],*l=l;s={*s};z=[(y*60+z,x)for x,y,z in l[:n]if(x in s)*(0<=y<6or(z<1<6==y))];print(min(z)[1],len(z));l=l[n:]
+_,*l=[[*map(int,i.split())]for i in open(0)]
+while l:_,s,[n],*l=l;print(min(z:=[(y*60+z,x)for x,y,z in l[:n]if(x in s)*(0<=y<6or(z<1<6==y))])[1],len(z));l=l[n:]
