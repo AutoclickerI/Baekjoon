9ec922ee59fee4361fa7b250e4bcc7b493444924
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-08-02 23:33:42 +0900

    [Bronze I] Title: Hunt The Rabbit, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/13777. Hunt The Rabbit/Hunt The Rabbit.py
+++ 백준/Bronze/13777. Hunt The Rabbit/Hunt The Rabbit.py
@@ -1,9 +1,6 @@
-while'0'<(n:=input()):
-  n=int(n)
-  l,r,*s=1,50
-  while l<=r:
-    m=(l+r)//2;s.append(m)
-    if m==n:break
-    elif m<n:l=m+1
-    else:r=m-1
-  print(*s)
+while'0'<(m:=input()):
+ n=int(m);l,r,*s=1,50
+ while m!=n:
+  print(m:=l+r>>1)
+  if m<n:l=m+1
+  else:r=m-1
