1a88601803536f8886c8d8aa2a6b4d12d07a80db
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-08-03 21:57:37 +0900

    [Bronze I] Title: Daisy Chains, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/20651. Daisy Chains/Daisy Chains.py
+++ 백준/Bronze/20651. Daisy Chains/Daisy Chains.py
@@ -1,2 +1,2 @@
 n,*l=map(int,open(0).read().split())
-print(sum(sum(z)/len(z)in z for i in range(n)for j in range(n+1)if(z:=l[i:j])))
+print(sum(sum(z)/len(z)in z for i in range(n*-~n)if(z:=l[i%n:i//n])))
