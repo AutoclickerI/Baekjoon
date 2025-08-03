afae65ad4666aa7e4ddf42c63d8f4626f165a008
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-08-03 18:07:36 +0900

    [Bronze I] Title: Word Processor, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/18322. Word Processor/Word Processor.py
+++ 백준/Bronze/18322. Word Processor/Word Processor.py
@@ -1,4 +1,2 @@
 _,K,*l=map(int,input().split())
-for i in*input().split(),'_'*99:
-    if K<sum(map(len,l+[i])):print(*l);l=[]
-    l+=i,
+for i in*input().split(),'_'*99:l=[l,[]][K<sum(map(len,l+[i]))!=print(*l)]+[i]
