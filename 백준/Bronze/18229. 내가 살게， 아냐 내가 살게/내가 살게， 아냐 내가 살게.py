28e62da43ee1a1c11fe674e284057e79a18f5b4b
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-08-03 23:24:54 +0900

    [Bronze I] Title: 내가 살게, 아냐 내가 살게, Time: 36 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Bronze/18229. 내가 살게， 아냐 내가 살게/내가 살게， 아냐 내가 살게.py
+++ 백준/Bronze/18229. 내가 살게， 아냐 내가 살게/내가 살게， 아냐 내가 살게.py
@@ -1,4 +1,4 @@
 (N,M,K),*l=[[*map(int,i.split())]for i in open(0)]
 r=[0]*N
 for j in range(M):
-    for i in range(N):r[i]+=l[i][j];K<=r[i]<exit(print(i+1,j+1))
+ for i in range(N):r[i]+=l[i][j];K<=r[i]<exit(print(i+1,j+1))
