c4c02a3973ba62bb18b1ce3f5931e41742da3949
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-08-03 23:56:36 +0900

    [Bronze I] Title: Cow Gymnastics, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/18268. Cow Gymnastics/Cow Gymnastics.py
+++ 백준/Bronze/18268. Cow Gymnastics/Cow Gymnastics.py
@@ -1,2 +1,3 @@
 (K,N),*l=[[*map(int,i.split())]for i in open(0)]
-print(sum(sum(j+1in v[:v.index(i+1)]for v in l)%K<(i<j)for i in range(N)for j in range(N)))
+R=range(N)
+print(sum(sum(j+1in v[:v.index(i+1)]for v in l)%K<(i<j)for i in R for j in R))
