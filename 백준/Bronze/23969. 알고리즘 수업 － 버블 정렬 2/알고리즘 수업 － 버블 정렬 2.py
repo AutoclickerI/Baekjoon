4156ffce1898e1ae5bf836ee4022af868d1988cd
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-08-04 17:40:58 +0900

    [Bronze I] Title: 알고리즘 수업 - 버블 정렬 2, Time: 1568 ms, Memory: 110860 KB -BaekjoonHub

--- 백준/Bronze/23969. 알고리즘 수업 － 버블 정렬 2/알고리즘 수업 － 버블 정렬 2.py
+++ 백준/Bronze/23969. 알고리즘 수업 － 버블 정렬 2/알고리즘 수업 － 버블 정렬 2.py
@@ -1,9 +1,5 @@
 N,K,*l=map(int,open(0).read().split())
-
-for i in range(N)[::-1]:
-    for j in range(i):
-        if l[j]>l[j+1]:
-            K-=1
-            l[j],l[j+1]=l[j+1],l[j]
-            K<1<exit(print(*l))
+for i in range(N*N):
+ j=i%N
+ if~j>i//N-N<0<l[j+1]<l[j]:K-=1;l[j:j+2]=l[j+1],l[j];K<1<exit(print(*l))
 print(-1)
