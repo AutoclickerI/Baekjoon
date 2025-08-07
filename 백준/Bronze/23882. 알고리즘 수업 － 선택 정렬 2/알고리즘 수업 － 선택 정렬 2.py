8665f64384addbf39ae74e0ba2de19c0d6fbff09
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-08-07 09:02:31 +0900

    [Bronze I] Title: 알고리즘 수업 - 선택 정렬 2, Time: 300 ms, Memory: 112388 KB -BaekjoonHub

--- 백준/Bronze/23882. 알고리즘 수업 － 선택 정렬 2/알고리즘 수업 － 선택 정렬 2.py
+++ 백준/Bronze/23882. 알고리즘 수업 － 선택 정렬 2/알고리즘 수업 － 선택 정렬 2.py
@@ -1,11 +1,5 @@
-N,K=map(int,input().split())
-*A,=map(int,input().split())
-
+N,K,*A=map(int,open(0).read().split())
 for i in range(N)[::-1]:
-    v=A.index(max(A[:i+1]))
-    if i!=v:
-        K-=1
-        A[i],A[v]=A[v],A[i]
-        K<1<exit(print(*A))
-    
+ v=A.index(max(A[:i+1]))
+ if i!=v:K-=1;A[i],A[v]=A[v],A[i];K<1<exit(print(*A))
 print(-1)
