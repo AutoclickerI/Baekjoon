06cac7f1f5674c8058632263671a666f678d0214
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-08-08 17:22:36 +0900

    [Bronze I] Title: 데칼코마니, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/23841. 데칼코마니/데칼코마니.py
+++ 백준/Bronze/23841. 데칼코마니/데칼코마니.py
@@ -1,7 +1,1 @@
-N, M = map(int, input().split())
-p = [[*input()] for _ in range(N)]
-for i in range(N):
-    for j in range(M):
-        p[i][j] = max(p[i][j], p[i][M-1-j])
-for i in range(N):
-    print(*p[i], sep='')
+for i in[*open(0)][1:]:print(*map(max,zip(i,i[-2::-1])),sep='')
