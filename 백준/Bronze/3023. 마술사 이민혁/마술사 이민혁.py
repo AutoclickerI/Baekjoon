2b6188b1c156ee80d7181fa5008a2198c642a50c
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-08-09 22:22:43 +0900

    [Bronze I] Title: 마술사 이민혁, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/3023. 마술사 이민혁/마술사 이민혁.py
+++ 백준/Bronze/3023. 마술사 이민혁/마술사 이민혁.py
@@ -1,8 +1,5 @@
 _,*l,k=open(0)
-l=[i[:-1]+i[-2::-1]for i in l]
-l+=l[::-1]
+l=[[*i[:-1]+i[-2::-1]]for i in l+l[::-1]]
 A,B=map(int,k.split())
-*v,=l[A-1]
-v[B-1]='.#'['#'<v[B-1]]
-l[A-1]=''.join(v)
-for i in l:print(i)
+l[A-1][B-1]='.#'['#'<l[A-1][B-1]]
+for i in l:print(*i,sep='')
