169dd0a242152c1eb4b4a4fb5e1148d97923ddc0
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-08-09 16:14:05 +0900

    [Bronze I] Title: 콘도 선정, Time: 48 ms, Memory: 35480 KB -BaekjoonHub

--- 백준/Bronze/2246. 콘도 선정/콘도 선정.py
+++ 백준/Bronze/2246. 콘도 선정/콘도 선정.py
@@ -1,2 +1,2 @@
 m=1e9
-print(sum(m>(m:=min(m,v))for i,v in sorted([*map(int,i.split())]for i in[*open(0)][1:])))
+print(sum(m>(m:=min(m,v))for _,v in sorted([*map(int,i.split())]for i in[*open(0)][1:])))
