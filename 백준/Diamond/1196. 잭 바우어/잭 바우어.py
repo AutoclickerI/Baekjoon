84fe9c5bd82d44b7e4d9628c1f08c7a475ff70d0
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-08-10 02:33:32 +0900

    [Diamond IV] Title: 잭 바우어, Time: 684 ms, Memory: 38356 KB -BaekjoonHub

--- 백준/Diamond/1196. 잭 바우어/잭 바우어.py
+++ 백준/Diamond/1196. 잭 바우어/잭 바우어.py
@@ -1,5 +1,5 @@
 from decimal import*
 getcontext().prec=99
 N,K=map(D:=Decimal,input().split())
-H=lambda n:sum(1/D(i+1)for i in range(int(n)))if n<1e6 else n.ln()+D('0.57721566490153286')+(6*n-1)/n/n/12
-print(N*(H(N)-H(N-K)))
+H=lambda n:sum(N/D(i+1)for i in range(int(n)))if n<1e6else(n.ln()+D(.577215665)+(6-1/n)/n/12)*N
+print(H(N)-H(N-K))
