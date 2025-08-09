119133d39cfb9d1b5e5e31163b816e35d9d37200
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-08-09 15:29:20 +0900

    [Bronze I] Title: 몬스터 트럭, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2897. 몬스터 트럭/몬스터 트럭.py
+++ 백준/Bronze/2897. 몬스터 트럭/몬스터 트럭.py
@@ -1,7 +1,5 @@
 _,*l=open(0)
 v=[0]*5
 for i,j in zip(l,l[1:]):
-    for x in zip(i,i[1:],j,j[1:-1]):
-        if'#'not in x:
-            v[x.count('X')]+=1
+ for x in zip(i,i[1:],j,j[1:-1]):v[x.count('X')]+='#'<min(x)
 print(*v)
