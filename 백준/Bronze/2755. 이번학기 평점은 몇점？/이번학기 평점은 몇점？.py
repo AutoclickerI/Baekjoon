42310b5c14722f4f3a5e9def685f56075f8793a7
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-08-10 18:16:12 +0900

    [Bronze I] Title: 이번학기 평점은 몇점?, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2755. 이번학기 평점은 몇점？/이번학기 평점은 몇점？.py
+++ 백준/Bronze/2755. 이번학기 평점은 몇점？/이번학기 평점은 몇점？.py
@@ -1,3 +1,3 @@
 b=c=0
-for i in[*open(0)][1:]:d,e,f=i.split();b+=int(e);g='FDCBA'.find(f[0]);g+=(g>0)and'-0+'.find(f[1])*.3-.3;c+=int(e)*g
+for i in[*open(0)][1:]:_,e,f=i.split();e=int(e);b+=e;g='FDCBA'.find(f[0]);g+=(g>0)and~-'-0+'.find(f[1])*.3;c+=e*g
 print(f'{c/b+1e-4:.2f}')
