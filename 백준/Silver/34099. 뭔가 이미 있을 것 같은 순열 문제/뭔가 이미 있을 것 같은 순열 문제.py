dba0b24cb852b159868d74b1cb069672553692fa
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-08-10 17:33:48 +0900

    [Silver III] Title: 뭔가 이미 있을 것 같은 순열 문제, Time: 116 ms, Memory: 37832 KB -BaekjoonHub

--- 백준/Silver/34099. 뭔가 이미 있을 것 같은 순열 문제/뭔가 이미 있을 것 같은 순열 문제.py
+++ 백준/Silver/34099. 뭔가 이미 있을 것 같은 순열 문제/뭔가 이미 있을 것 같은 순열 문제.py
@@ -1,7 +1,4 @@
 for i in[*open(0)][1:]:
-    N,K=map(int,i.split())
-    R=range(1,N+1)
-    if 1<K:print(*R)
-    else:
-        if N<4:print(-1)
-        else:print(*R[1::2],*R[::2])
+ N,K=map(int,i.split());R=range(1,N+1)
+ if K<2:R=3//N*[-1]or[*R[1::2],*R[::2]]
+ print(*R)
