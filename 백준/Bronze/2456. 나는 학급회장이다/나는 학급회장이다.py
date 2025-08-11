3c4a90f984d570dec50f1d2729029b47235405ba
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-08-11 13:51:36 +0900

    [Bronze I] Title: 나는 학급회장이다, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2456. 나는 학급회장이다/나는 학급회장이다.py
+++ 백준/Bronze/2456. 나는 학급회장이다/나는 학급회장이다.py
@@ -1,3 +1,3 @@
-a=[[sum(x),*map(x.count,[3,2])]for x in zip(*(map(int,i.split())for i in[*open(0)][1:]))]
-b,c=sorted(a)[1:]
+a=[[sum(x),*map(x.count,[3,2])]for x in zip(*[map(int,i[::2])for i in open(0)][1:])]
+_,b,c=sorted(a)
 print((b<c)*-~a.index(c),c[0])
