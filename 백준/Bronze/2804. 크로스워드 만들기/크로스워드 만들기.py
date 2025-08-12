2bbefdfe8700291aeb62df2d442281725604c11b
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-08-12 14:12:14 +0900

    [Bronze I] Title: 크로스워드 만들기, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2804. 크로스워드 만들기/크로스워드 만들기.py
+++ 백준/Bronze/2804. 크로스워드 만들기/크로스워드 만들기.py
@@ -1,5 +1,5 @@
 a,b=input().split()
 m,n=len(a),len(b)
 i=c=0
-while{a[c]}-{*b}:c+=1
-while i<n:print(['.'*c+b[i]+'.'*(m+~c),a][i==b.find(a[c])]);i+=1
+while{z:=a[c]}-{*b}:c+=1
+while i<n:print(['.'*c+b[i]+'.'*m,a][i==b.find(z)][:m]);i+=1
