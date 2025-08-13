3657abb89e2d06887006f5c76062690729cf901b
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-08-13 15:29:33 +0900

    [Bronze I] Title: 색종이, Time: 3504 ms, Memory: 229716 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/10163. 색종이/색종이.py
+++ 백준/Bronze/10163. 색종이/색종이.py
@@ -1,6 +1,3 @@
 d=[-1]*6**8
-for i in[*open(c:=0)][1:]:
- p,q,r,s=map(int,i.split())
- for i in range(r*s):d[(p+i//s)*1002+q+i%s]=c
- c+=1
+for i in[*open(c:=0)][1:]:p,q,r,s=map(int,i.split());z=0;exec('v=p+z<<10|q;d[v:v+s]=[c]*s;z+=1;'*r);c+=1
 print(*map(d.count,range(c)))
