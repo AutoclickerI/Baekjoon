53592cbeea979a0ec61bab2030fd3eb2cc2ba38f
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-08-18 13:16:42 +0900

    [Silver V] Title: Код Сайлонов, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/29481. Код Сайлонов/Код Сайлонов.py
+++ 백준/Silver/29481. Код Сайлонов/Код Сайлонов.py
@@ -1,5 +1,5 @@
 (n,m),*l,(C11,C12,C21,C22)=[[*map(int,i.split())]for i in open(0)]
-B = [m*[0]for _ in range(n)]
+B=[m*[0]for _ in range(n)]
 for i in range(n):
     for j in range(m):
         v=l[i][j]
