d75be6ba089ac68300ed724caf83128699ae822f
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-08-26 11:44:09 +0900

    [Silver IV] Title: Time Travel, Time: 200 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/10461. Time Travel/Time Travel.py
+++ 백준/Silver/10461. Time Travel/Time Travel.py
@@ -1,2 +1,1 @@
-v=43200
-for i in open(0):a,b=map(int,i.split());r=int(v*(2*v-a)/abs(a-b)/60+.5);print(a,b,f'{r%720//60or 12:02d}:{r%60:02d}')
+for i in open(0):a,b=map(int,i.split());r=int(43200*(86400-a)/abs(a-b)/60+.5);print(a,b,f'{r%720//60or 12:02d}:{r%60:02d}')
