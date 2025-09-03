84f5a7ec38bf6ea06593955dec3dfc00d56bfafc
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-09-04 06:09:56 +0900

    [Bronze II] Title: 호참전, Time: 320 ms, Memory: 132536 KB -BaekjoonHub

--- 백준/Bronze/34237. 호참전/호참전.py
+++ 백준/Bronze/34237. 호참전/호참전.py
@@ -1,2 +1,2 @@
 (N,M),*l=[[*map(int,i.split())]for i in open(0)]
-for g,x,y in l[N:]:print(sum(x<=i and y<=j and i+j<=g for i,j in l[:N]))
+for g,x,y in l[N:]:print(sum(y<=j<=g-i<=g-x for i,j in l[:N]))
