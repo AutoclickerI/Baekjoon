caedda4ad915a595d83caf38611fe3b7bc3cd8e6
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-09-04 04:13:04 +0900

    [Bronze III] Title: 루미의 진정한™ 보라색 찾기, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/33663. 루미의 진정한™ 보라색 찾기/루미의 진정한™ 보라색 찾기.py
+++ 백준/Bronze/33663. 루미의 진정한™ 보라색 찾기/루미의 진정한™ 보라색 찾기.py
@@ -1,5 +1,4 @@
 h,H,s,S,v,V,*l=map(int,open(0).read().split())
 i=l.index(x:=max(l))
-m=min(l)
-a=x-m
-print('Lumi will '+'not '*-~-(v<=x<=V>-1<s<=255*a/x<=S>-1<h<=60*(2*i+(l[i-2]-l[i-1])/a)%360<=H)+'like it.')
+a=x-min(l)
+print('Lumi will','not like it.'[(v<=x<=V>-1<s<=255*a/x<=S>-1<h<=(2*i+(l[i-2]-l[i-1])/a)%6*60<=H)*4:])
