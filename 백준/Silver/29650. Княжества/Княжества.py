2a7b7c4a9b63fbb3661b095aa33371864523e1f9
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-09-05 22:08:56 +0900

    [Silver V] Title: Княжества, Time: 60 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/29650. Княжества/Княжества.py
+++ 백준/Silver/29650. Княжества/Княжества.py
@@ -1,21 +1,15 @@
-from collections import deque
-
-d = {'M':[]}
-e = {'M':1}
+d={'M':[]}
+e={'M':1}
 def f(x,n):
-    q = deque([x])
-    while q:
-        u = q.popleft()
-        if e[u]: 
-            if n<1: return u
-            n -= 1
-        q+=d[u]
-    return '-'
+ q=[x]
+ for i in q:
+  if e[i]: 
+   if n<1:return i
+   n-=1
+  q+=d[i]
+ return '-'
 for i in[*open(0)][1:]:
-    x,*y = i.split()
-    if x=='-': e[y[0]] = 0
-    elif x=='+':
-        d[y[0]] += y[1],
-        d[y[1]] = []
-        e[y[1]] = 1
-    else: print(f('M',int(y[0])-1))
+ x,a,*y=i.split()
+ if x=='-':e[a]=0
+ if x=='+':d[a]+=y;d[y[0]]=[];e[y[0]]=1
+ x=='?'!=print(f('M',int(a)-1))
