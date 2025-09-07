c2119d9d1918361bddee368f0ac3b60e82991fcc
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-09-07 11:01:21 +0900

    [Silver IV] Title: Сто, Time: 44 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/22094. Сто/Сто.py
+++ 백준/Silver/22094. Сто/Сто.py
@@ -1,3 +1,1 @@
-for i in[*open(0)][1:]:
-    k,x=i.split();l=len(x)-int(k);f=x.rfind
-    print((x[:l-2]+'00')*(2<l<f('0',0,f('0'))+3)or(l<2and'0'in x)-1)
+for i in[*open(0)][1:]:k,x=i.split();l=len(x)-int(k)-2;f=x.rfind;v=f('0');print([(l<0<v)-1,x[:l]+'00'][0<l<=f('0',0,v)])
