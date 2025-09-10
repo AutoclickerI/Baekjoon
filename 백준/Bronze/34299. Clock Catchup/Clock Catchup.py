8db875c46456d91245fa8bbae8c649451fa4a245
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-09-10 11:15:39 +0900

    [Bronze III] Title: Clock Catchup, Time: 56 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/34299. Clock Catchup/Clock Catchup.py
+++ 백준/Bronze/34299. Clock Catchup/Clock Catchup.py
@@ -1,3 +1,3 @@
-def I():a,b,c=map(int,input().split(':'));return(a*60+b)*60+c
-R=range(I()+1,I()+1)
+def I():a,b,c=map(int,input().split(':'));return(a*60+b)*60+c+1
+R=range(I(),I())
 print(*map(lambda i:sum(j%i<1for j in R),(43200,3600,60)))
