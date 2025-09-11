338f4e4b5f74fb5a61e8342b43a095b9b15dbe46
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-09-12 06:28:15 +0900

    [Bronze I] Title: Cheese Touch, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/34290. Cheese Touch/Cheese Touch.py
+++ 백준/Bronze/34290. Cheese Touch/Cheese Touch.py
@@ -1,10 +1,3 @@
-p,t=map(int,input().split())
-m=0
-for i in input().split('W'):
-    {*i}=={'H'}<exit(print('CURED'))
-    s,*l,e=[*map(len,i.split('I'))]*2
-    m=max(m,s,e,*[j+1>>1for j in l])
-if m*p<t:
-    print('ALL INFECTED')
-else:
-    print('CURED')
+p,t,*m=map(int,input().split())
+for i in input().split('W'):t-=({*i}=={'H'})*1e9;s,*l,e=[*map(len,i.split('I'))]*2;m+=s,e,*[j+1>>1for j in l]
+print(['CURED','ALL INFECTED'][max(m)*p<t])
