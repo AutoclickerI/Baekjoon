9225dc87b184821f5d9b1ad0dbeecfdb857174a8
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-09-14 06:44:26 +0900

    [Silver V] Title: Major Scales, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/4333. Major Scales/Major Scales.py
+++ 백준/Silver/4333. Major Scales/Major Scales.py
@@ -1,3 +1,3 @@
 s='C C# D D# E F F# G G# A A# B'.split()
-d={s[i]:{s[(i+j)%12]for j in(0,2,4,5,7,9,11)}for i in range(12)}
-for i in[*open(0)][:-1]:print(*[c for c in d if{*i.split()}<=d[c]])
+d={s[i]:{s[(i+j)%12]for j in b'`bdegik'}for i in range(12)}
+for i in open(0):print(*[c for c in d if{*i.split()}<=d[c]])
