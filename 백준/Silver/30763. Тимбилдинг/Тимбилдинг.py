8ed57df7878c56befd80c1f3b89a06feb8c780ff
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-09-14 07:11:05 +0900

    [Silver V] Title: Тимбилдинг, Time: 96 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/30763. Тимбилдинг/Тимбилдинг.py
+++ 백준/Silver/30763. Тимбилдинг/Тимбилдинг.py
@@ -1,11 +1,4 @@
-def solve():
-    n, k = map(int,input().split())
-    m = n // k
-    if m >= k:
-        print(0)
-    else:
-        q = k // m
-        r = k % m
-        print(m * (r * (q * (q + 1) // 2) + (m - r) * (q * (q - 1) // 2)))
-
-solve()
+n,k=map(int,input().split())
+m=n//k
+q=k//m
+print(m*q*(k%m+m*~-q//2))
