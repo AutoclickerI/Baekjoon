56dde42c48c8246bb6686ca44250c822aeaf4020
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-09-14 06:32:12 +0900

    [Silver V] Title: Спираль, Time: 3328 ms, Memory: 56204 KB -BaekjoonHub

--- 백준/Silver/27247. Спираль/Спираль.py
+++ 백준/Silver/27247. Спираль/Спираль.py
@@ -1,13 +1,7 @@
-n,d,k =map(int,input().split())
-i,j = 0,1
-r = [(0,0)]
-def f():(p,*_,q),(u,*_,v)=map(sorted,zip(*r));exit(print(q-p+1,v-u+1,*map(''.join,[['.*'[(x,y)in r]for y in range(u,v+1)]for x in range(p,q+1)][::-1])))
-h=1
-while 1:
-    for _ in range(d):
-        r += (r[-1][0]+i,r[-1][1]+j),
-        n -= 1
-        n or f()
-    i,j = j,-i
-    h^=1
-    d*=h*~-k+1
+n,d,k=map(int,input().split())
+r=[(0,0)]
+i=y=x=0
+j=h=1
+while 0<n:exec("r+=[(x:=x+i,y:=y+j)]*(0<n);n-=1;"*d);i,j=j,-i;h^=1;d*=h*~-k+1
+(p,*_,q),(u,*_,v)=map(sorted,zip(*r))
+print(q-p+1,v-u+1,*[''.join('.*'[(x,y)in r]for y in range(u,v+1))for x in range(q,p-1,-1)])
