7fe8261896efd01cc996bfb94aad6cd80599d424
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-09-13 11:41:01 +0900

    [Silver V] Title: Club Pizza, Time: 108 ms, Memory: 110760 KB -BaekjoonHub

--- 백준/Silver/34291. Club Pizza/Club Pizza.py
+++ 백준/Silver/34291. Club Pizza/Club Pizza.py
@@ -1,8 +1,6 @@
-d={}
-(n,c),*l=[map(int,i.split())for i in open(0)]
-for t,p in l:d[t]=min(d.get(t,p),p)
+n,c,*l=map(int,open(0).read().split())
 v=[1e9]*25+[0]
 R=range(26)
-for i in sorted(d):
- for j in R:v[j]=min(v[j],v[j-25]+d[i])
+for i in sorted({*l[::2]}):
+ for j in R:v[j]=min(v[j],v[j-25]+min(l[2*k+1]for k in range(n)if l[2*k]==i))
 for i in R:c<v[~i]<exit(print(i-1))
