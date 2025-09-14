33baaef0ec676eec12a345ae6ad8157f6f48b0c9
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-09-13 22:43:28 -0400

    [Silver V] Title: Intressid, Time: 160 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/7201. Intressid/Intressid.py
+++ 백준/Silver/7201. Intressid/Intressid.py
@@ -1,4 +1,4 @@
 P,D,N=map(eval,input().split())
 y=v=f=0
-while(P-D)*12>f:f=int(v*N-D*1200+D*N/((1+N/100)**(1/12)-1)+.5)/100;y+=1;v+=D*12;v+=f
+while(P-D)*12>f:f=int(v*N-D*1200+D*N/((1+N/100)**(1/12)-1)+.5)/100;y+=1;v+=D*12+f
 print(y)
