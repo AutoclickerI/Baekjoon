560b61d85c45419290aa0a8cf211dfe1a9d21c26
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-09-16 17:37:56 -0400

    [Gold V] Title: 집합의 표현, Time: 364 ms, Memory: 188568 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.py
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.py
@@ -1,20 +1,9 @@
-import sys
-sys.setrecursionlimit(9**9)
-
+def f(n):
+ if n-v[n]:v[n]=f(v[n])
+ return v[n]
 (N,M),*l=[map(int,i.split())for i in open(0)]
 *v,=range(N+1)
-
-def find(n):
-    if v[n]-n:
-        v[n]=find(v[n])
-    return v[n]
-def union(s,e):
-    s=find(s)
-    e=find(e)
-    v[s]=v[e]=min(s,e)
-
-for q,i,j in l:
-    if q:
-        print('YNEOS'[find(i)!=find(j)::2])
-    else:
-        union(i,j)
+for p,i,j in l:
+ i,j=sorted([f(i),f(j)])
+ if p:print('YNEOS'[i<j::2])
+ else:v[i]=j
