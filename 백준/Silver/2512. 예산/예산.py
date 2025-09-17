d1e748b4bfb08ed108fda666b2bde20d2bc6ac0e
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-09-17 17:37:44 -0400

    [Silver II] Title: 예산, Time: 40 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/2512. 예산/예산.py
+++ 백준/Silver/2512. 예산/예산.py
@@ -1,9 +1,4 @@
-N,*l,M=map(int,open(0).read().split())
-M-=sum(l)
-*l,v=0,*sorted(l)
-c=1
-while M<0:
- t=l.pop()-v
- if t*c<M:M//=c;v+=M;M=0
- else:M-=t*c;c+=1;v+=t
-print(v)
+n,*l,q=map(int,open(s:=0).read().split())
+e=max(l)+1
+while-~s<e:s,e=[m:=s+e>>1,s,e,m][q<sum(map(min,l,[m]*n))::2]
+print(s)
