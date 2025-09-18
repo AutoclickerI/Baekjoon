455f057d2c8b4874f344d19922d8f4f8ab5a9966
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-09-18 19:53:29 -0400

    [Gold III] Title: 감시, Time: 1628 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Gold/15683. 감시/감시.py
@@ -0,0 +1,45 @@
+N,M=map(int,input().split())
+b=[input()[::2]for _ in[0]*N]
+
+cc=[]
+
+for y in range(N):
+    for x in range(M):
+        if b[y][x]not in'06':
+            cc+=(y,x),
+
+def traverse(y,x,dy,dx):
+    if 0<=y<N and 0<=x<M and tes[y][x]!='6':
+        if tes[y][x]=='0':
+            tes[y][x]='#'
+        traverse(y+dy,x+dx,dy,dx)
+
+def evaluate():
+    for i in range(len(cc)):
+        y,x=cc[i]
+        c_t=b[y][x]
+        if c_t=='1':
+            d=0,
+        if c_t=='2':
+            d=0,2
+        if c_t=='3':
+            d=0,1
+        if c_t=='4':
+            d=0,1,2
+        if c_t=='5':
+            d=0,1,2,3
+        for c in [(j+rot[i])%4 for j in d]:
+            dy,dx=[(-1,0),(0,1),(1,0),(0,-1)][c]
+            traverse(y,x,dy,dx)
+    return sum(j=='0'for i in tes for j in i)
+        
+m=1e9
+for i in range(4**len(cc)):
+    tes=[[*i]for i in b]
+    rot=[]
+    for _ in[0]*len(cc):
+        rot+=i%4,
+        i//=4
+    m=min(m,evaluate())
+
+print(m)
