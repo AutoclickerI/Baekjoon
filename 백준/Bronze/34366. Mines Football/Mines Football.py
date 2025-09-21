96f427020b713a0648e7f56000ed3ae8b6d54e93
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-09-21 19:29:34 -0400

    [Bronze III] Title: Mines Football, Time: 96 ms, Memory: 110708 KB -BaekjoonHub

--- 백준/Bronze/34366. Mines Football/Mines Football.py
+++ 백준/Bronze/34366. Mines Football/Mines Football.py
@@ -1,3 +1,3 @@
 _,*l=[[*map(int,i.split()[1:])]for i in open(0)]
-f=lambda p,q:p(map(q,l))
-print(f(max,max),f(min,min),f(max,sum),f(min,sum))
+*x,=map(sum,l)
+print(max(z:=sum(l,[])),min(z),max(x),min(x))
