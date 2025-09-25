38f259c9b346b208d19df31b808835057db04563
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-09-25 00:45:31 -0400

    [Diamond IV] Title: 괄호, Time: 1648 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Diamond/13174. 괄호/괄호.py
+++ 백준/Diamond/13174. 괄호/괄호.py
@@ -1,10 +1,4 @@
-m=10**9+7
-F=[1]
-N,K=map(int,input().split())
-Po=[1]
-for i in range(1,N):F+=[F[-1]*i%m];Po+=[Po[-1]*K%m]
-l=[1]
-for i in range(N):
- if i%2:l+=[(K+1)*l[-1]%m]
- else:l+=[(K+1)*l[-1]-Po[i//2]*F[i]*pow(F[i//2]**2*(i//2+1),-1,m)]
-print(l[-1]%m)
+n,k=map(int,input().split())
+i=r=p=0;c=1
+while i*2<=n:r+=pow(k,n-i,M:=10**9+7)*(c-p);p=c;c=c*(i-n)*pow(~i,-1,M)%M;i+=1
+print(r%M)
