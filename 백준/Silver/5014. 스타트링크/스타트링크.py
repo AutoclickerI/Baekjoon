3c057a38c2faa885087bb61326632df3d0bc4a23
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-09-27 18:56:44 -0400

    [Silver I] Title: 스타트링크, Time: 636 ms, Memory: 286384 KB -BaekjoonHub

--- 백준/Silver/5014. 스타트링크/스타트링크.py
+++ 백준/Silver/5014. 스타트링크/스타트링크.py
@@ -1,13 +1,7 @@
 F,S,G,U,D=map(int,input().split())
-v=[1]*F
-q=[(S-1,0)]
+v={}
+s,*q='use the stairs',(S,0)
 for i,c in q:
-    if i+U<F and v[i+U]:
-        v[i+U]=0
-        q+=(i+U,c+1),
-    if 0<=i-D and v[i-D]:
-        v[i-D]=0
-        q+=(i-D,c+1),
-    if i==G-1:
-        exit(print(c))
-print('use the stairs')
+ for n in i+U,i-D:q+=[(n,c+1)]*(0<n<=v.get(n,F));v[n]=0
+ if i==G:s=c
+print(s)
