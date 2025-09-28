7a7357f543a272de0a1c4872becf11b124f68912
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-09-27 21:13:55 -0400

    [Gold V] Title: 트리, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1068. 트리/트리.py
+++ 백준/Gold/1068. 트리/트리.py
@@ -1,19 +1,4 @@
-N=int(input())
-d={}
-l=input().split()
-for i in range(N):
-    d[int(l[i])]=d.get(int(l[i]),[])+[i]
-v=int(input())
-if v in d:del d[v]
-a=0
-for i in*d,:
-    if d[i]==[v]:del d[i]
-def traverse(n):
-    global a
-    if n in d:
-        for e in d[n]:
-            traverse(e)
-    else:
-        a+=n not in[v,-1]
-traverse(-1)
-print(a)
+def f(x):l[x]=-2;[l[i]==x!=f(i)for i in range(n)]
+n,*l,d=map(int,open(0).read().split())
+f(d)
+print(sum(~l[i]<=0==(i in l)for i in range(n)))
