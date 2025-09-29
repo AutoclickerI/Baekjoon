0833ac95d24942a3d0b843161bdc480a2a6910e3
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-09-28 22:10:38 -0400

    [Unrated] Title: 대칭 그림, Time: 276 ms, Memory: 74732 KB -BaekjoonHub

---
+++ 백준/Unrated/2264. 대칭 그림/대칭 그림.py
@@ -0,0 +1,14 @@
+d={}
+for i in[*open(0)][1:]:
+    x,y=map(int,i.split())
+    d[x]=d.get(x,set())
+    d[x]|={y}
+
+l=sorted(d)
+mid=l[0]+l[-1]
+for i,j in zip(l,l[::-1]):
+    if i+j!=mid or d[i]!=d[j]:
+        break
+else:
+    exit(print(mid//2,end=mid%2*'.5'))
+print('NO')
