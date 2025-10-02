063e7790251a513f6a8768880a47fb0333cc2e55
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-10-01 22:12:53 -0400

    [Gold IV] Title: 단어 수학, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1339. 단어 수학/단어 수학.py
+++ 백준/Gold/1339. 단어 수학/단어 수학.py
@@ -1,6 +1,6 @@
-m=[r:=0]*99
-a=9
-for i in[1]*int(input()):
- for c in input()[::-1]:m[ord(c)]-=i;i*=10
-for i in sorted(m):r-=i*a;a-=1
-print(r)
+m=[0]*99
+a=10
+for i in[*open(0,'rb')][1:]:
+ v=1
+ for c in i[-2::-1]:m[c]-=v;v*=10
+print(-sum(i*(a:=a-1)for i in sorted(m)))
