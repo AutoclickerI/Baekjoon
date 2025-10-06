8a901f82ab0a70b9d69d733ea98b5a5b508f3efd
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-10-06 00:09:44 -0400

    [Gold V] Title: 빗물, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/14719. 빗물/빗물.py
+++ 백준/Gold/14719. 빗물/빗물.py
@@ -1,22 +1,17 @@
-H,W=map(int,input().split())
-b=[W*[0]for _ in[0]*H]
-c=0
-for i in input().split():
-    for j in range(int(i)):b[~j][c]=1
-    c+=1
-c=0
-def forward(y,x):
-    global c
-    if b[y][x]==1:
-        return 1
-    if x+1<W:
-        if forward(y,x+1):
-            c+=1
-            b[y][x]=1
-            return 1
-    return 0
+_,W,*h=map(int,open(0).read().split())
 
-for y in range(H):
-    for x in range(1,W):
-        if b[~y][x-1]==1:forward(~y,x)
-print(c)
+stack = []
+ans = 0
+
+for i in range(W):
+    while stack and h[stack[-1]]<h[i]:
+        bottom = stack.pop()
+        if not stack:
+            break
+        left = stack[-1]
+        height = min(h[left], h[i]) - h[bottom]
+        width = i - left - 1
+        ans += height * width
+    stack.append(i)
+
+print(ans)
