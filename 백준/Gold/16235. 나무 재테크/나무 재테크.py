423f59e73d08934c67ee9c7fdb0db6b1070b3645
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-10-09 18:49:43 -0400

    [Gold III] Title: 나무 재테크, Time: 460 ms, Memory: 131224 KB -BaekjoonHub

--- 백준/Gold/16235. 나무 재테크/나무 재테크.py
+++ 백준/Gold/16235. 나무 재테크/나무 재테크.py
@@ -1,31 +1,19 @@
-N,M,K=map(int,input().split())
-A=sum([[*map(int,input().split())]for _ in[0]*N],[])
-
+N,M,K,*A=map(int,open(0).read().split())
 b=N*N*[5]
-
-trees=[[]for _ in[0]*N*N]
-
-for _ in[0]*M:
-    y,x,c=map(int,input().split())
-    trees[(y-1)*N+x-1]+=c,
-
+z=[[]for _ in[0]*N*N]
+for y,x,c in zip(*3*[iter(A[-3*M:])]):z[(y-1)*N+x-1]+=c,
 for _ in[0]*K:
     t=[[]for _ in[0]*N*N]
-    for idx in range(N*N):
-        add=0
-        trees[idx].sort()
-        for i in trees[idx]:
-            if b[idx]<i:
-                add+=i//2
+    for v in range(N*N):
+        a=0
+        for i in sorted(z[v]):
+            if b[v]<i:a+=i//2
             else:
-                b[idx]-=i
-                t[idx]+=i+1,
+                b[v]-=i;t[v]+=i+1,
                 if i%5==4:
                     for dy,dx in(-1,0),(0,1),(-1,1),(-1,-1),(0,-1),(1,-1),(1,0),(1,1):
-                        ny,nx=idx//N+dy,idx%N+dx
-                        if 0<=ny<N and 0<=nx<N:
-                            t[ny*N+nx]+=1,
-        b[idx]+=A[idx]+add
-    trees=t
-
-print(sum(len(i)for i in trees))
+                        ny,nx=v//N+dy,v%N+dx
+                        if 0<=ny<N and 0<=nx<N:t[ny*N+nx]+=1,
+        b[v]+=A[v]+a
+    z=t
+print(sum(map(len,z)))
