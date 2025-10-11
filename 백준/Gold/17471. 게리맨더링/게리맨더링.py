0eef25e307bc3484ba1f022b44dda3bd0b75290d
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-10-11 19:44:40 -0400

    [Gold III] Title: 게리맨더링, Time: 124 ms, Memory: 111480 KB -BaekjoonHub

--- 백준/Gold/17471. 게리맨더링/게리맨더링.py
+++ 백준/Gold/17471. 게리맨더링/게리맨더링.py
@@ -5,21 +5,13 @@ edges=[[*map(int,input().split())][1:]for _ in[0]*N]
 def DFS(n,c):
     for e in edges[n]:
         e-=1
-        if div[e]==c and v[e]<1:
-            v[e]=1
-            DFS(e,c)
+        if div[e]==c!=1>v[e]<1:v[e]=1;DFS(e,c)
 
 x=[]
 
 for i in range(1,2**N-1):
     div=f'{i:b}'.zfill(N)
     v=[0]*N
-    f0=div.find('0')
-    v[f0]=1
-    DFS(f0,'0')
-    f1=div.find('1')
-    v[f1]=1
-    DFS(f1,'1')
-    if all(v):
-        x+=abs(sum(l[i]*(-int(div[i])|1)for i in range(N))),
+    for c in'01':v[f:=div.find(c)]=1;DFS(f,c)
+    x+=[abs(sum(l[i]*(-int(div[i])|1)for i in range(N)))]*all(v)
 print(min(x or[-1]))
