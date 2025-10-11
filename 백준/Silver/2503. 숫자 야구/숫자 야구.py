7083c48151e2b4b7a95ad426900d295854008a2d
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-10-11 18:17:27 -0400

    [Silver III] Title: 숫자 야구, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2503. 숫자 야구/숫자 야구.py
+++ 백준/Silver/2503. 숫자 야구/숫자 야구.py
@@ -1,8 +1,3 @@
 s={str(i)for i in range(999)if 2<len({*str(i)}-{'0'})}
-
-def chk(g,i):return[v:=sum(map(str.__eq__,g,i)),len({*g}&{*i})-v]
-
-for i in[*open(0)][1:]:
-    g,*d=map(int,i.split())
-    s={i for i in s if chk(str(g),i)==d}
+for i in[*open(0)][1:]:g,*d=map(int,i.split());s={i for i in s if[v:=sum(map(str.__eq__,g:=str(g),i)),len({*g}&{*i})-v]==d}
 print(len(s))
