754785cd89e75916f6779ec06165703fc02dfbfb
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-10-10 20:09:32 -0400

    [Silver II] Title: 결혼식, Time: 40 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/5567. 결혼식/결혼식.py
+++ 백준/Silver/5567. 결혼식/결혼식.py
@@ -1,14 +1,5 @@
-N=int(input())
-M=int(input())
-
-edges=[[]for _ in[0]*N]
-
-for _ in[0]*M:
-    a,b=map(int,input().split())
-    edges[a-1]+=b-1,
-    edges[b-1]+=a-1,
-
-s={*edges[0]}
-for i in edges[0]:
-    s|={*edges[i]}
-print(len(s-{0}))
+g=[[]for _ in[0]*501]
+for i in[*open(0)][2:]:a,b=map(int,i.split());g[a]+=b,;g[b]+=a,
+f={1}
+for i in g[1]:f|={i,*g[i]}
+print(len(f)-1)
