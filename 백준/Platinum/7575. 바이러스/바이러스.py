a4022e6667de19c25ccccd94e8b6899d0529f40e
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-10-13 22:41:29 -0400

    [Platinum V] Title: 바이러스, Time: 292 ms, Memory: 115336 KB -BaekjoonHub

--- 백준/Platinum/7575. 바이러스/바이러스.py
+++ 백준/Platinum/7575. 바이러스/바이러스.py
@@ -2,5 +2,5 @@
 K=int(K)
 f=' '.join
 l=[f(s[i:i+K])for i in range(len(s)-K+1)]
-for s in z:s+='_';*l,=filter(f(s+s[::-1]).__contains__,l)
+for s in z:*l,=filter(f(s+['_']+s[::-1]).__contains__,l)
 print('YNEOS'[l==[]::2])
