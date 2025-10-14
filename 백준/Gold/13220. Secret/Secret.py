0bf16a52477384a9d9b5d9abe63ada6257c14bf1
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-10-13 20:45:43 -0400

    [Gold V] Title: Secret, Time: 48 ms, Memory: 37240 KB -BaekjoonHub

--- 백준/Gold/13220. Secret/Secret.py
+++ 백준/Gold/13220. Secret/Secret.py
@@ -1,24 +1,3 @@
-_,T,s=map(str.split,open(0))
-T*=2
-
-pi=[0]
-j=0
-for i in range(1,len(s)):
-    while j and s[i]!=s[j]:
-        j=pi[j-1]
-    if s[i]==s[j]:
-        j+=1
-    pi+=j,
-
-j=0
-for i in range(len(T)):
-    while j and T[i]!=s[j]:
-        j=pi[j-1]
-    if T[i]==s[j]:
-        if j==len(s)-1:
-            print('YES')
-            break
-        else:
-            j+=1
-else:
-    print('NO')
+i=input
+i()
+print('NYOE S'[i()in(i()+' ')*2::2])
