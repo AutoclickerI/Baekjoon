23cb93ce9763f4224c3a4a43d7554f3445077fd4
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-10-18 19:09:48 -0400

    [Bronze II] Title: Adria, Time: 108 ms, Memory: 110984 KB -BaekjoonHub

---
+++ 백준/Bronze/26362. Adria/Adria.py
@@ -0,0 +1,12 @@
+p=m=0
+g,n,*l=map(int,open(0))
+N,*a=n,
+while n:x=l[p];p-=~x;a+=l[p-x:p],;n-=1
+for i in range(g):
+ W=0
+ for j in a:
+  w=l=0;f=1
+  for k in j:
+   if max(w:=w+2-k,l:=l+k-1)>i:W+=i<f*w;f=0
+ if m<W>0<i+1<g:m=W;q=i+1
+print(a[0].count(1),N-W,q)
