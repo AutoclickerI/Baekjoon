105153316ba615aa9867a368ac21ae9326dbdeb8
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-10-17 22:45:31 -0400

    [Gold I] Title: 멀티탭 스케줄링, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1700. 멀티탭 스케줄링/멀티탭 스케줄링.py
+++ 백준/Gold/1700. 멀티탭 스케줄링/멀티탭 스케줄링.py
@@ -2,6 +2,5 @@ N,K,*l=map(int,open(c:=0).read().split())
 R=range(K)
 l+=*R,K
 s=[]
-for i in R:
- if N<len(s:={*s,l[i]}):c+=1;s-={max(s,key=l[i:].index)}
+for i in R:f=N<len(s:={*s,l[i]});c+=f;s-={f*max(s,key=l[i:].index)}
 print(c)
