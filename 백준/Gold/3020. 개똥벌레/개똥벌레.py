237474dc7a88c3377e6e9b8121f1d5367998a42a
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-10-21 18:40:44 -0400

    [Gold V] Title: 개똥벌레, Time: 148 ms, Memory: 145708 KB -BaekjoonHub

--- 백준/Gold/3020. 개똥벌레/개똥벌레.py
+++ 백준/Gold/3020. 개똥벌레/개똥벌레.py
@@ -1,20 +1,8 @@
 N,H,*l=map(int,open(0).read().split())
-d,t=map(sorted,(l[::2],l[1::2]))
-c=0
-s={}
-for i in t:
-    c+=1
-    s[i+1]=s.get(i+1,0)-1
-for i in d:
-    s[H-i+1]=s.get(H-i+1,0)+1
-a=0
-m=1e9
-
-for i in range(1,H+1):
-    if c==m:
-        a+=1
-    if c<m:
-        m=c
-        a=1
-    c+=s.get(i,0)
-print(m,a)
+N>>=1
+s=[0]*H
+f=1
+for i in l:f*=-1;s[f*i]-=f
+d={}
+for i in s:d[N]=d.get(N,0)+1;N+=i
+print(m:=min(d),d[m])
