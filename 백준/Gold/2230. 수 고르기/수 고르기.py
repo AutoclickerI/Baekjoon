4e8ff341582a89b77ba1c7ce853a95880b95b8ed
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-10-20 21:36:27 -0400

    [Gold V] Title: 수 고르기, Time: 136 ms, Memory: 131144 KB -BaekjoonHub

--- 백준/Gold/2230. 수 고르기/수 고르기.py
+++ 백준/Gold/2230. 수 고르기/수 고르기.py
@@ -1,8 +1,6 @@
 n,m,*l=map(int,open(0).read().split())
 l.sort()
 i=j=0
-d=11**9
-while i<n>j:
- if(t:=l[j]-l[i])<m:j+=1
- else:d=min(d,t);i+=1
-print(d)
+d=[]
+while i<n>j:d+=[t:=l[j]-l[i]]*(f:=m<=t);i+=f;j+=1-f
+print(min(d))
