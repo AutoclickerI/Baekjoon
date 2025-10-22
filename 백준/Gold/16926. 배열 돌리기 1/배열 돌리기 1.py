dc48f785b25e38ef0cff1a3a66fcebcdb0743bb1
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-10-22 19:14:21 -0400

    [Gold V] Title: 배열 돌리기 1, Time: 528 ms, Memory: 113084 KB -BaekjoonHub

--- 백준/Gold/16926. 배열 돌리기 1/배열 돌리기 1.py
+++ 백준/Gold/16926. 배열 돌리기 1/배열 돌리기 1.py
@@ -1,22 +1,25 @@
 N,M,R=map(int,input().split())
 b=[[*map(int,input().split())]for _ in[0]*N]
 
-def rot():
-    global b
-    v=[M*[0]for _ in[0]*N]
-    n,m=N,M
-    o=0
-    while n*m:
+v=[M*[0]for _ in[0]*N]
+n,m=N,M
+o=0
+while n*m:
+    for _ in[0]*(R%(2*n+2*m-4)):
         for i in range(m-1):
             v[o][o+i]=b[o][o+i+1]
             v[N-o-1][o+i+1]=b[N-o-1][o+i]
         for i in range(n-1):
             v[o+i][M-o-1]=b[o+i+1][M-o-1]
             v[o+i+1][o]=b[o+i][o]
-        n-=2
-        m-=2
-        o+=1
-    b=v
-for _ in[0]*R:
-    rot()
+        for i in range(m-1):
+            b[o][o+i+1]=v[o][o+i+1]
+            b[N-o-1][o+i]=v[N-o-1][o+i]
+        for i in range(n-1):
+            b[o+i+1][M-o-1]=v[o+i+1][M-o-1]
+            b[o+i][o]=v[o+i][o]
+        
+    n-=2
+    m-=2
+    o+=1
 for i in b:print(*i)
