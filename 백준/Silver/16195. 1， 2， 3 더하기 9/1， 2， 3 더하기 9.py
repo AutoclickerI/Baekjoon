b4970631bbc32920e177e09edbde98bc54f97f98
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-10-23 17:17:14 -0400

    [Silver I] Title: 1, 2, 3 더하기 9, Time: 444 ms, Memory: 69920 KB -BaekjoonHub

--- 백준/Silver/16195. 1， 2， 3 더하기 9/1， 2， 3 더하기 9.py
+++ 백준/Silver/16195. 1， 2， 3 더하기 9/1， 2， 3 더하기 9.py
@@ -1,10 +1,6 @@
+R=range(1000)
 t=[1001*[0]for _ in[0]*1001]
-
 t[0][0]=1
-for i in range(1,1001):
-    for j in range(1,1001):
-        t[i][j]+=t[i-1][j-1]+t[i-2][j-1]+t[i-3][j-1]
-
-for i in[*open(0)][1:]:
-    n,m=map(int,i.split())
-    print(sum(t[n][:m+1])%(10**9+9))
+for i in R:
+ for j in R:t[i+1][j+1]=t[i][j]+t[i-1][j]+t[i-2][j]
+for i in[*open(0)][1:]:n,m=map(int,i.split());print(sum(t[n][:m+1])%(10**9+9))
