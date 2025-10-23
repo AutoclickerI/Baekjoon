c9740f16a081faa36d067a57adf4b20d972111d7
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-10-23 17:19:01 -0400

    [Silver I] Title: 1, 2, 3 더하기 6, Time: 64 ms, Memory: 34948 KB -BaekjoonHub

--- 백준/Silver/15991. 1， 2， 3 더하기 6/1， 2， 3 더하기 6.py
+++ 백준/Silver/15991. 1， 2， 3 더하기 6/1， 2， 3 더하기 6.py
@@ -1,9 +1,3 @@
-v=[0]*100001
-v[0]=1
-v[1]=1
-v[2]=2
-v[3]=2
-for i in range(4,100001):
-    v[i]=v[i-2]+v[i-4]+v[i-6]
-for i in[*open(0)][1:]:
-    print(v[int(i)]%(10**9+9))
+d=[1,2,3]
+for i in[0]*9**5:d+=sum(d[-3:])%(10**9+9),
+for i in[*open(0)][1:]:print(d[int(i)//2])
