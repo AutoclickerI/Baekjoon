5c26906e0a82470790827c582ce986b8566cff2e
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-10-26 17:30:30 -0400

    [Silver IV] Title: 수열, Time: 88 ms, Memory: 33192 KB -BaekjoonHub

--- 백준/Silver/2491. 수열/수열.py
+++ 백준/Silver/2491. 수열/수열.py
@@ -1,13 +1,3 @@
-*l,=map(int,[*open(0)][1].split())
-s=[]
-p=1e99
-for i in l:
-    if i<p:s+=0,
-    s[-1]+=1
-    p=i
-p=1e99
-for i in l[::-1]:
-    if i<p:s+=0,
-    s[-1]+=1
-    p=i
-print(max(s))
+p=x=y=z=0
+for i in[*open(0)][1].split():i=int(i);z=max(x:=1+(p<=i)*x,y:=1+(i<=p)*y,z);p=i
+print(z)
