de85252b35d59e4d5df3c591c0b05d1279669eec
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-10-27 17:36:51 -0400

    [Gold V] Title: 연속합 2, Time: 104 ms, Memory: 42660 KB -BaekjoonHub

--- 백준/Gold/13398. 연속합 2/연속합 2.py
+++ 백준/Gold/13398. 연속합 2/연속합 2.py
@@ -1,6 +1,4 @@
-v,*l=map(int,[*open(s:=0)][1].split())
-m=v
-for i in l:
-    v,s=max(v+i,i),max(v,s+i,i)
-    m=max(m,v,s)
+v,*l=map(int,[*open(0)][1].split())
+s=m=v
+for i in l:v,s=max(v+i,i),max(v,s+i);m=max(m,v,s)
 print(m)
