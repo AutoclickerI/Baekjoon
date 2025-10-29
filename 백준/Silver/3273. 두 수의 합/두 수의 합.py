119668ee28aa49ca638a52884a8417489184d7e8
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-10-29 19:38:45 -0400

    [Silver III] Title: 두 수의 합, Time: 2760 ms, Memory: 123732 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.py
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.py
@@ -1,12 +1,3 @@
-e=int(input())-1
-l=sorted(map(int,input().split()))
-v=int(input())
-s=0
-a=0
-while s<e:
-    if l[s]+l[e]<v:
-        s+=1
-    else:
-        a+=l[s]+l[e]==v
-        e-=1
-print(a)
+*l,x=map(int,open(c:=0).read().split())
+while l:c+=x-l.pop()in l
+print(c)
