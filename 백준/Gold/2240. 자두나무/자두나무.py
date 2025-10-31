4b13b9b1aaf62458523f1428339f44a15df9c895
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-10-31 19:22:08 -0400

    [Gold IV] Title: 자두나무, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/2240. 자두나무/자두나무.py
+++ 백준/Gold/2240. 자두나무/자두나무.py
@@ -1,6 +1,5 @@
-T,W,*l=map(int,open(0).read().split())
+W,*l=open(0)
 v=[0]*32
 for i in l:
-    for j in range(W+1):
-        v[j]=max(v[j],v[j-1])+(j%2+1==i)
+ for j in range(int(W[-3:])+1):v[j]=max(v[j],v[j-1])+(j%2+1==int(i))
 print(max(v))
