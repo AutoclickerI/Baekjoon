76364d1d31ef21da73bd15c23b2fdb6874cf2bc1
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-11-01 22:00:38 -0400

    [Silver II] Title: 안녕, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1535. 안녕/안녕.py
+++ 백준/Silver/1535. 안녕/안녕.py
@@ -1,7 +1,3 @@
-[N],L,J=[[*map(int,i.split())]for i in open(0)]
-m=0
-for i in range(2**N):
- c=v=j=0
- while i:c+=i%2*L[v];j+=i%2*J[v];i//=2;v+=1
- m=max(j*(c<100),m)
-print(m)
+d=[0]*100
+for p,q in zip(*[*map(str.split,open(0))][1:]):d=[max(d[j],(d[v:=j-int(p)]+int(q))*(0<=v))for j in range(100)]
+print(max(d))
