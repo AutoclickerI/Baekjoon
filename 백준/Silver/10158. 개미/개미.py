e1e8927d408a31f2e651261e65f632a8393e3bcb
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-11-01 22:49:09 -0400

    [Silver III] Title: 개미, Time: 32 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Silver/10158. 개미/개미.py
+++ 백준/Silver/10158. 개미/개미.py
@@ -1,4 +1,2 @@
-w,h,p,q,t=map(int,open(0).read().split())
-p=(p+t)%(2*w)
-q=(q+t)%(2*h)
-print(min(p,2*w-p),min(q,2*h-q))
+w,h,x,y,t=map(int,open(0).read().split())
+print(w-abs((x+t)%(2*w)-w),h-abs((y+t)%(2*h)-h))
