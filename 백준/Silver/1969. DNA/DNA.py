50f0b88f97e412d53f5fa143e19f327a2f465210
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-11-01 22:12:35 -0400

    [Silver IV] Title: DNA, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1969. DNA/DNA.py
+++ 백준/Silver/1969. DNA/DNA.py
@@ -1,3 +1,3 @@
 s=''
-for i in[*zip(*[*open(c:=0)][1:])][:-1]:d=max('ACGT',key=i.count);s+=d;c+=len(i)-i.count(d)
+for i in[*zip(*[*open(c:=0)][1:])][:-1]:c+=len(i)-i.count(d:=max('ACGT',key=i.count));s+=d
 print(s,c)
