f7556068efaa200a1c6199592cfed1d7dd8925a3
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-11-03 17:00:11 -0500

    [Gold II] Title: 미확인 도착지, Time: 376 ms, Memory: 47928 KB -BaekjoonHub

--- 백준/Gold/9370. 미확인 도착지/미확인 도착지.py
+++ 백준/Gold/9370. 미확인 도착지/미확인 도착지.py
@@ -1,26 +1,35 @@
-from heapq import*
+import sys
+input=sys.stdin.readline
+
+from heapq import heappush,heappop
 for _ in[0]*int(input()):
     n,m,t=map(int,input().split())
     s,g,h=map(int,input().split())
     edges=[[]for _ in[0]*-~n]
     for _ in[0]*m:
         a,b,d=map(int,input().split())
-        d*=2
-        if (a,b)in[(g,h),(h,g)]:
-            d-=1
         edges[a]+=(b,d),
         edges[b]+=(a,d),
     end=[int(input())for _ in[0]*t]
     v=[float('inf')]*-~n
-    hq=[(0,s)]
+    vgh=[float('inf')]*-~n
+    hq=[(1,0,s)]
     v[s]=0
     while hq:
-        cost,cur=heappop(hq)
+        flag,cost,cur=heappop(hq)
         if v[cur]<cost:
             continue
         for e,d in edges[cur]:
-            if cost+d<v[e]:
-                v[e]=cost+d
-                heappush(hq,(cost+d,e))
-    nend=[i for i in end if v[i]%2==1]
+            if cost+d<=v[e]:
+                if flag and(cur,e)in[(g,h),(h,g)]:
+                    vgh[e]=cost+d
+                    v[e]=cost+d
+                    heappush(hq,(0,cost+d,e))
+                elif cost+d<v[e]:
+                    v[e]=cost+d
+                    heappush(hq,(flag,cost+d,e))
+            if flag<1 and cost+d<vgh[e]:
+                vgh[e]=cost+d
+                heappush(hq,(flag,cost+d,e))
+    nend=[i for i in end if v[i]==vgh[i]!=float('inf')]
     print(*sorted(nend))
