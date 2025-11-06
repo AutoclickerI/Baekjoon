e641785a4b03ff3ba0305f8db64bdf313a5d96b8
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-11-06 17:01:01 -0500

    [Silver I] Title: 겹치는 건 싫어, Time: 224 ms, Memory: 53288 KB -BaekjoonHub

--- 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.py
+++ 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.py
@@ -2,11 +2,7 @@ N,K,*a=map(int,open(0).read().split())
 d={}
 c=p=m=0
 for i in a:
-    c+=1
-    d[i]=d.get(i,0)+1
-    while K<d[i]:
-        c-=1
-        d[a[p]]-=1
-        p+=1
-    m=max(c,m)
+ c+=1;d[i]=d.get(i,0)+1
+ while K<d[i]:c-=1;d[a[p]]-=1;p+=1
+ m=max(c,m)
 print(m)
