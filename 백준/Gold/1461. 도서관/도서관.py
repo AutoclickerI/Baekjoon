d8af1b3a6f4139e7dac5640123a19247fbee148e
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-11-06 16:29:43 -0500

    [Gold IV] Title: 도서관, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1461. 도서관/도서관.py
+++ 백준/Gold/1461. 도서관/도서관.py
@@ -1,15 +1,3 @@
 N,M,*l=map(int,open(0).read().split())
 l.sort()
-
-v=[]
-while l and 0<l[-1]:v+=l.pop(),
-
-l+=[0]*(M-len(l)%M)
-v+=[0]*(M-len(v)%M)
-
-a=0
-ma=0
-for i in zip(*[iter(l+v)]*M):
-    a+=abs(i[0])
-    ma=max(ma,abs(i[0]))
-print(a*2-ma)
+print(2*sum(max(l[~i],0)-min(l[i],0)for i in range(0,N,M))-max(-l[0],l[-1]))
