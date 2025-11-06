70c2afd23c27f3be407b8073da2faadbf5e6f8ae
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-11-06 16:22:20 -0500

    [Gold V] Title: A와 B 2, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/12919. A와 B 2/A와 B 2.py
+++ 백준/Gold/12919. A와 B 2/A와 B 2.py
@@ -1,13 +1,3 @@
-l=[input()]
-T=input()
-
-chk=T+' '+T[::-1]
-
-for i in l:
-    if i==T:
-        exit(print(1))
-    if i+'A'in chk:
-        l+=i+'A',
-    if'B'+i[::-1]in chk:
-        l+='B'+i[::-1],
-print(0)
+f=lambda x:x[-1]<'B'and f(x[:-1])or'B'<=x and f(x[:0:-1])if len(x)-len(S)else x==S
+S=input()
+print(+f(input()))
