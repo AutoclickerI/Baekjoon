429a4acf5fe9331177c4b467decc119f8854f69f
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-11-09 16:42:57 -0500

    [Silver I] Title: 통나무 건너뛰기, Time: 120 ms, Memory: 34300 KB -BaekjoonHub

--- 백준/Silver/11497. 통나무 건너뛰기/통나무 건너뛰기.py
+++ 백준/Silver/11497. 통나무 건너뛰기/통나무 건너뛰기.py
@@ -1,7 +1,1 @@
-for i in[*open(0)][2::2]:
-    l=sorted(map(int,i.split()))
-    l+=l[-1],
-    l=l[::-1]
-    l1=l[::2]
-    l2=l[1::2]
-    print(max(max(i-j for i,j in zip(l1,l1[1:])),max(i-j for i,j in zip(l2,l2[1:])),abs(l1[-1]-l2[-1])))
+for i in[*open(0)][2::2]:l=sorted(map(int,i.split()));print(max(j-i for i,j in zip(l,l[2:])))
