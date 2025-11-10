fac5253530bf8dd762edd6393daeb77a53824556
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-11-10 16:50:04 -0500

    [Silver II] Title: -2진수, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2089. －2진수/－2진수.py
+++ 백준/Silver/2089. －2진수/－2진수.py
@@ -1,17 +1,2 @@
-N=int(input())
-s=[]
-while N:
-    v=N%4
-    if v==0:
-        s+='00'
-    if v==1:
-        s+='10'
-        N-=1
-    if v==2:
-        s+='01'
-        N+=2
-    if v==3:
-        s+='11'
-        N+=1
-    N//=4
-print(''.join(s[::-1]).lstrip('0')or'0')
+s=2**33//3
+print(f'{int(input())+s^s:b}')
