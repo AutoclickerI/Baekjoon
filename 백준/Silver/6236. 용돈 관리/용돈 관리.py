c2339f8f8d1ee0a9b00198a51762a903981a19ed
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-11-09 19:02:03 -0500

    [Silver I] Title: 용돈 관리, Time: 128 ms, Memory: 123268 KB -BaekjoonHub

--- 백준/Silver/6236. 용돈 관리/용돈 관리.py
+++ 백준/Silver/6236. 용돈 관리/용돈 관리.py
@@ -1,22 +1,10 @@
-N,M,*l=map(int,open(0).read().split())
-
-def check(K):
-    c=m=0
-    for i in l:
-        if m<i:
-            m=K
-            c+=1
-        if m<i:
-            return 0
-        m-=i
-    return c<=M
-
-s=0
-e=10**10
+N,M,*v=map(int,open(0).read().split())
+s,e=max(v)-1,10**9
 while 1<e-s:
-    m=s+e>>1
-    if check(m):
-        e=m
-    else:
-        s=m
+ x=s+e>>1;c=z=0
+ for i in v:
+  if z<i:z,c=x,c+1
+  z-=i
+ if M<c:s=x
+ else:e=x
 print(e)
