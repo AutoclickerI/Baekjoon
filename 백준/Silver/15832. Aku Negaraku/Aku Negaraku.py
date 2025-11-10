37eac0f2aa3c9fa94bdbdb4ad3aada2afe1b2de6
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-11-10 09:45:00 -0500

    [Silver IV] Title: Aku Negaraku, Time: 256 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15832. Aku Negaraku/Aku Negaraku.py
+++ 백준/Silver/15832. Aku Negaraku/Aku Negaraku.py
@@ -1,5 +1,6 @@
-from collections import*
-while'1'<(s:=input()):
- N,M=map(int,s.split());d=deque(range(N))
- while~-len(d):d.rotate(M-1);d.pop()
- print(N-d[0])
+while 1:
+ N,M=map(int,input().split())
+ if N+M<1:break
+ j=0
+ for i in range(2,N+1):j=(j+M)%i
+ print(j+1)
