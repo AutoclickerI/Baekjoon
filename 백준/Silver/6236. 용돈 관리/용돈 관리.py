08d1940f851aa8ea32a700e82c06915f2316e76c
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-11-09 19:06:29 -0500

    [Silver I] Title: 용돈 관리, Time: 128 ms, Memory: 123268 KB -BaekjoonHub

--- 백준/Silver/6236. 용돈 관리/용돈 관리.py
+++ 백준/Silver/6236. 용돈 관리/용돈 관리.py
@@ -2,9 +2,7 @@ N,M,*v=map(int,open(0).read().split())
 s,e=max(v)-1,10**9
 while 1<e-s:
  x=s+e>>1;c=z=0
- for i in v:
-  if z<i:z,c=x,c+1
-  z-=i
+ for i in v:f=z<i;z=(x*f or z)-i;c+=f
  if M<c:s=x
  else:e=x
 print(e)
