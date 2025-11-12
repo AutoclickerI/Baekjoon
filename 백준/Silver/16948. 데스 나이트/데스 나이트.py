ad44c2b17f8acb572dd527d3e8ee9c30bc9b995d
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-11-12 18:48:18 -0500

    [Silver I] Title: 데스 나이트, Time: 32 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Silver/16948. 데스 나이트/데스 나이트.py
+++ 백준/Silver/16948. 데스 나이트/데스 나이트.py
@@ -1,11 +1,6 @@
-N,*p,R,C=map(int,open(0).read().split())
-l=[[0]+p]
-v=[N*[0]for _ in[0]*N]
-for c,y,x in l:
-    (y,x)==(R,C)<exit(print(c))
-    for dy,dx in(-2,-1),(-2,1),(0,-2),(0,2),(2,-1),(2,1):
-        ny,nx=y+dy,x+dx
-        if 0<=ny<N and 0<=nx<N and v[ny][nx]<1:
-            v[ny][nx]=1
-            l+=(c+1,ny,nx),
-print(-1)
+input()
+r,c,x,y=map(int,input().split())
+a=abs(r-x)
+b=abs(c-y)
+l=a//2
+print([l+max(0,(b-l)//2),-1][a%2 or l%2^b%2])
