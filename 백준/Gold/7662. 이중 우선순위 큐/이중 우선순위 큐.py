98a0974203b29c6ef2948fdc69041c28a30a21f6
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-11-14 15:01:29 -0500

    [Gold IV] Title: 이중 우선순위 큐, Time: 2840 ms, Memory: 360864 KB -BaekjoonHub

--- 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.py
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.py
@@ -4,32 +4,34 @@ input=sys.stdin.readline
 for _ in range(int(input())):
     maxheap=[]
     minheap=[]
+    d={}
     for _ in range(int(input())):
         c,i=input().split()
         if c=='I':
             checker=[1]
-            heappush(minheap,[int(i),checker])
-            heappush(maxheap,[-int(i),checker])
+            heappush(minheap,int(i))
+            heappush(maxheap,-int(i))
+            d[int(i)]=d.get(int(i),0)+1
         else:
             try:
                 if i=='1':
-                    l=heappop(maxheap)
-                    while l[1][0]==0:
-                        l=heappop(maxheap)
-                    l[1][0]=0
+                    l=-heappop(maxheap)
+                    while not d[l]:
+                        l=-heappop(maxheap)
+                    d[l]=max(0,d[l]-1)
                 else:
                     l=heappop(minheap)
-                    while l[1][0]==0:
+                    while not d[l]:
                         l=heappop(minheap)
-                    l[1][0]=0
+                    d[l]=max(0,d[l]-1)
             except:0
     try:
         maxval=heappop(maxheap)
-        while maxval[1][0]==0:
+        while not d[-maxval]:
             maxval=heappop(maxheap)
         minval=heappop(minheap)
-        while minval[1][0]==0:
+        while not d[minval]:
             minval=heappop(minheap)
-        print(-maxval[0],minval[0])
+        print(-maxval,minval)
     except:
         print('EMPTY')
