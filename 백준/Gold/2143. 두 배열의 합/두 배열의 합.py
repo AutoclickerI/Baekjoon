ed87490d0aacc683b0d09cecd722140bb0a14251
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-11-14 15:31:10 -0500

    [Gold III] Title: 두 배열의 합, Time: 328 ms, Memory: 216252 KB -BaekjoonHub

--- 백준/Gold/2143. 두 배열의 합/두 배열의 합.py
+++ 백준/Gold/2143. 두 배열의 합/두 배열의 합.py
@@ -1,7 +1,8 @@
 T=int(input())
 input()
 A=[0]
-for i in input().split():A+=A[-1]+int(i),
+for i in input().split():
+    A+=A[-1]+int(i),
 dA={}
 for i in range(len(A)):
     for j in range(i+1,len(A)):
@@ -9,9 +10,14 @@ for i in range(len(A)):
         dA[v]=dA.get(v,0)+1
 input()
 B=[0]
-for i in input().split():B+=B[-1]+int(i),
-a=0
+for i in input().split():
+    B+=B[-1]+int(i),
+dB={}
 for i in range(len(B)):
     for j in range(i+1,len(B)):
-        a+=dA.get(T+B[i]-B[j],0)
+        v=B[j]-B[i]
+        dB[v]=dB.get(v,0)+1
+a=0
+for v in dB:
+    a+=dA.get(T-v,0)*dB[v]
 print(a)
