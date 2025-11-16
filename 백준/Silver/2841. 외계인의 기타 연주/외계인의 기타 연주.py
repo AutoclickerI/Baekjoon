db2a0c30d20f8977b2e8496e0a4862987e16e047
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-11-16 16:52:43 -0500

    [Silver I] Title: 외계인의 기타 연주, Time: 1732 ms, Memory: 191940 KB -BaekjoonHub

--- 백준/Silver/2841. 외계인의 기타 연주/외계인의 기타 연주.py
+++ 백준/Silver/2841. 외계인의 기타 연주/외계인의 기타 연주.py
@@ -1,11 +1,6 @@
-[N,P],*l=[map(int,i.split())for i in open(0)]
-v=[[]for _ in[0]*-~N]
-c=0
+_,*l=[map(int,i.split())for i in open(0)]
+v=[[c:=0]for _ in[0]*7]
 for n,x in l:
-    while v[n]and x<v[n][-1]:
-        c+=1
-        v[n].pop()
-    if v[n]and v[n][-1]==x:c-=1;v[n].pop() 
-    v[n]+=x,
-    c+=1
+ while x<v[n][-1]:c+=1;v[n].pop()
+ if v[n][-1]-x:v[n]+=x,;c+=1
 print(c)
