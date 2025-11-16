0474f39473cc5d00600df52edec06ead884b8de2
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-11-16 00:31:40 -0500

    [Silver I] Title: 부분수열의 합, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/14225. 부분수열의 합/부분수열의 합.py
+++ 백준/Silver/14225. 부분수열의 합/부분수열의 합.py
@@ -1,12 +1,2 @@
-N,*l=map(int,open(0).read().split())
-s=set()
-for i in range(2**N):
-    v=j=0
-    while i:
-        v+=i%2*l[j]
-        j+=1
-        i//=2
-    s|={v}
-i=0
-while i in s:i+=1
-print(i)
+for i in sorted(map(int,[*open(0)][a:=1].split())):a+=i*(i<=a)
+print(a)
