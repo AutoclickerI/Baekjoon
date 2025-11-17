64133d7df3fd9a6bb8c6fc3c1fcb8ea28d815cd3
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-11-17 12:38:44 -0500

    [Gold IV] Title: 이름 짓기, Time: 832 ms, Memory: 112260 KB -BaekjoonHub

--- 백준/Gold/34695. 이름 짓기/이름 짓기.py
+++ 백준/Gold/34695. 이름 짓기/이름 짓기.py
@@ -3,4 +3,4 @@ N,*l=open(0,'rb')
 v=[1]*26
 t=[[]for _ in v]
 for s,e,*_ in l:t[e-97]+=s-97,
-print(sum(sum(v:=[sum(v[j]for j in t[i])%m for i in range(26)])for _ in[0]*~-int(N.split()[1]))%m)
+print(sum(sum(v:=[sum(v[j]for j in i)%m for i in t])for _ in[0]*~-int(N.split()[1]))%m)
