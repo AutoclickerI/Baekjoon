dca785cc383a37dbaaf7efe1ac4f47ffac04c163
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-11-18 12:26:31 -0500

    [Bronze I] Title: Konduktor, Time: 268 ms, Memory: 162032 KB -BaekjoonHub

--- 백준/Bronze/8805. Konduktor/Konduktor.py
+++ 백준/Bronze/8805. Konduktor/Konduktor.py
@@ -1,7 +1,4 @@
 for i in[*open(0)][1:]:
-    N,K=map(int,i.split())
-    if N==1:print(K)
-    else:
-        r,q=divmod(K,2*N-2);b=[r]+[2*r]*(N-2)+[r];d=i=0
-        while q:b[i]+=1;d^=i in[0,N-1];i+=2*d-1;q-=1
-        print(*b)
+ n,k=map(int,i.split());r=[k];n-=1
+ if n:x,y=divmod(k-1,n*2);r=x+1,*[2*x+(i<y)+(n*2-2-i<y)>>(i==n-1)for i in range(n)]
+ print(*r)
