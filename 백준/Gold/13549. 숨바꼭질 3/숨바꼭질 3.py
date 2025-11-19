a662c4b4b0ab7669cb9649106697cb88cb94704f
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-11-19 12:05:18 -0500

    [Gold V] Title: 숨바꼭질 3, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -1,25 +1,3 @@
-from collections import deque
-N,K=map(int,input().split())
-l=deque([N])
-v=[-1]*999999
-p=[0]*999999
-v[N]=0
-p[N]=1
-while 1:
-    t=l.popleft()
-    if t==K:break
-    z=2*t
-    while z<999999 and v[t]!=v[z]:
-        v[z]=v[t]
-        p[z]=p[t]
-        l.appendleft(z)
-        z*=2
-    for i in t-1,t+1:
-        if -1<i<999999:
-            if v[i]<0:
-                v[i]=v[t]+1
-                p[i]=p[t]
-                l.append(i)
-            elif v[t]+1==v[i]:
-                p[i]+=p[t]
-print(v[K])
+n,m=map(int,input().split())
+f=lambda k:[n-k,k<2or k&1and-~min(f(k+1),f(k-1))or min(k-n,f(k//2))][n<k]
+print(+f(m))
