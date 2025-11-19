41ea7e77b20284178e9cbc9fe06b90878ff4392b
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-11-19 09:54:24 -0500

    [Silver I] Title: 숨바꼭질, Time: 184 ms, Memory: 39416 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -1,12 +1,17 @@
-from collections import deque
 N,K=map(int,input().split())
-l=deque([N])
-v=[0]*999999
-while 1:
-    t=l.popleft()
-    if t==K:break
-    for i in t-1,t+1,2*t:
-        if -1<i<999999 and not v[i]:
-            v[i]=v[t]+1
-            l.append(i)
-print(v[K])
+visited=[0]*200001
+
+from collections import deque
+
+visited[N]=1
+dq=deque([(0,N)])
+
+while dq:
+    time,pos=dq.popleft()
+    if pos==K:
+        break
+    for next_pos in pos-1,pos+1,pos*2:
+        if 0<=next_pos<200001 and visited[next_pos]<1:
+            visited[next_pos]=1
+            dq.append((time+1,next_pos))
+print(time)
