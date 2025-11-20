c7651e614fdb3475e260aa02f4d92c30e4f53a6a
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-11-19 19:29:33 -0500

    [Silver III] Title: 색종이 - 2, Time: 36 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/2567. 색종이 － 2/색종이 － 2.py
+++ 백준/Silver/2567. 색종이 － 2/색종이 － 2.py
@@ -1,9 +1,3 @@
 s=set()
-for i in[*open(0)][1:]:
-    y,x=map(int,i.split())
-    s|={(y+j,x+k)for j in range(10)for k in range(10)}
-a=0
-for y,x in s:
-    for dy,dx in(-1,0),(1,0),(0,1),(0,-1):
-        if(y+dy,x+dx)not in s:a+=1
-print(a)
+for i in[*open(0)][1:]:y,x=map(int,i.split());s|={(y+v//10,x+v%10)for v in range(100)}
+print(sum(1-((y+dy,x+dx)in s)for y,x in s for dy,dx in((-1,0),(1,0),(0,1),(0,-1))))
