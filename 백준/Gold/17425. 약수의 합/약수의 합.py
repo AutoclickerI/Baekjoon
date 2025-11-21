a5ea715bb790f122be9125bdd0155fea8f505ee6
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-11-21 17:14:22 -0500

    [Gold IV] Title: 약수의 합, Time: 256 ms, Memory: 131000 KB -BaekjoonHub

--- 백준/Gold/17425. 약수의 합/약수의 합.py
+++ 백준/Gold/17425. 약수의 합/약수의 합.py
@@ -1,9 +1,6 @@
-f=[0]*1000000
-for i in range(1,1000001):
-    for j in range(i,1000001,i):
-        f[j-1]+=i
-
-g=[0]
-for i in f:g+=g[-1]+i,
-
-for i in[*open(0)][1:]:print(g[int(i)])
+N=6**8
+e=[i:=0]*N
+while~i+N:
+ e[j:=i+1]+=e[i];i=j
+ while j<N:e[j]+=i;j+=i
+for i in[*open(0)][1:]:print(e[int(i)])
