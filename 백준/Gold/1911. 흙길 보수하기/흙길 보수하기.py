88c56e2e85055f7b1dd09b7df31ce085f0c3f0a8
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-11-22 20:16:11 -0500

    [Gold V] Title: 흙길 보수하기, Time: 52 ms, Memory: 34456 KB -BaekjoonHub

--- 백준/Gold/1911. 흙길 보수하기/흙길 보수하기.py
+++ 백준/Gold/1911. 흙길 보수하기/흙길 보수하기.py
@@ -1,10 +1,4 @@
 [N,L],*l=[[*map(int,i.split())]for i in open(0)]
-l.sort()
-r=0
-el=0
-for s,e in l:
-    s=max(el,s)
-    x=(s-e)//L
-    el=s+x*-L
-    r-=x
+z=r=0
+for s,e in sorted(l):s=max(z,s);v=(s-e)//L;z=s-v*L;r-=v
 print(r)
