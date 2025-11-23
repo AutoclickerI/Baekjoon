c1c0dda2928ede74b0681ddf01cbf563962d3581
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-11-23 00:21:21 -0500

    [Gold V] Title: 거의 소수, Time: 492 ms, Memory: 293984 KB -BaekjoonHub

--- 백준/Gold/1456. 거의 소수/거의 소수.py
+++ 백준/Gold/1456. 거의 소수/거의 소수.py
@@ -1,17 +1,9 @@
-*p,=range(10**7+1)
-p[1]=0
-for i in range(2,10**7+1):
-    if p[i]:
-        p[2*i::i]=[0]*len(range(2*i,10**7+1,i))
-p=[i for i in p if i]
-
-def c(n):
-    r=0
-    for i in p:
-        j=i*i
-        while j<=n:
-            r+=1
-            j*=i
-    return r
 A,B=map(int,input().split())
-print(c(B)-c(A-1))
+M=6**9
+P=[1]*M
+z=0
+for p in range(2,M):
+ if P[p]:
+  k=p*p;P[k::p]=(M+p+~k)//p*[0]
+  while k<=B:z+=A<=k;k*=p
+print(z)
