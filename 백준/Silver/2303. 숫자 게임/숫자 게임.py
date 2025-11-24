8281e54385a0d33abcd67febebde7e69eba4e4ee
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-11-24 17:55:14 -0500

    [Silver V] Title: 숫자 게임, Time: 100 ms, Memory: 110736 KB -BaekjoonHub

--- 백준/Silver/2303. 숫자 게임/숫자 게임.py
+++ 백준/Silver/2303. 숫자 게임/숫자 게임.py
@@ -1,6 +1,3 @@
 from itertools import*
-c,*r=0,
-for i in[*open(0)][1:]:
- c+=1
- for x in combinations(map(int,i.split()),3):r+=(sum(x)%10,c),
-print(max(r)[1])
+c=0
+print(0-max((sum(x)%10,c:=c+1)for i in[*open(0)][1:]for x in combinations(map(int,i.split()),3))[1]//-10)
