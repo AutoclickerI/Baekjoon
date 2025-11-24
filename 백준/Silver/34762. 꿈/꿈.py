17f210888e314da43259b72d0c8bbbe621befa61
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-11-24 17:41:14 -0500

    [Silver III] Title: 꿈, Time: 124 ms, Memory: 143924 KB -BaekjoonHub

--- 백준/Silver/34762. 꿈/꿈.py
+++ 백준/Silver/34762. 꿈/꿈.py
@@ -1,2 +1,2 @@
 N,K,_,*l=map(int,open(0).read().split())
-print('YNEOS'[min([j+~i for i,j in zip(l,l[1:]+[N+1])if j!=i+1]+[1e9])<=K::2])
+print('YNEOS'[any(1<j-i<K+2for i,j in zip(l,l[1:]+[N+1]))::2])
