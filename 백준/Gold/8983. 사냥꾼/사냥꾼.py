4eaa8349ce94200465eb464adf67fd853a50fdc5
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-11-24 21:25:10 -0500

    [Gold IV] Title: 사냥꾼, Time: 260 ms, Memory: 152276 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/8983. 사냥꾼/사냥꾼.py
+++ 백준/Gold/8983. 사냥꾼/사냥꾼.py
@@ -1,5 +1,6 @@
 from bisect import*
 a=0
 [M,N,L],P,*l=[map(int,i.split())for i in open(0)]
-for X,Y in l:Y-=L;a+=Y<=0!=bisect_left(P:=sorted(P),X+Y)-bisect(P,X-Y)
+P=sorted(P)
+for X,Y in l:a+=Y-L<=0!=bisect_left(P,X+Y-L)-bisect(P,X-Y+L)
 print(a)
