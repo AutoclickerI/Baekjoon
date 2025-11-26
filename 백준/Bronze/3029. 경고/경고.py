b5e32fa05817c4ac63a1895266b7740fc5059e59
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-11-26 16:39:13 -0500

    [Bronze III] Title: 경고, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/3029. 경고/경고.py
+++ 백준/Bronze/3029. 경고/경고.py
@@ -1,12 +1,4 @@
-p,q,r=map(int,input().split(':'))
-n=(p+24)*60*60+q*60+r
-p,q,r=map(int,input().split(':'))
-n=(p*60*60+q*60+r-n)%(60*60*24)
-H=n//(60*60)
-n%=(60*60)
-M=n//60
-n%=60
-if H+M+n:
-    print('{:02d}:{:02d}:{:02d}'.format(H,M,n))
-else:
-    print('24:00:00')
+R=60
+def f():x,y,z=map(int,input().split(':'));return(x*R+y)*R+z
+t=(~f()+f())%86400+1
+print((3*'%02d:'%(t/R/R,t/R%R,t%R))[:8])
