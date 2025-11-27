3eaa7648d3cf70123e7aeec2428f57e0c60df675
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-11-27 11:38:29 -0500

    [Silver IV] Title: 한윤정이 이탈리아에 가서 아이스크림을 사먹는데, Time: 80 ms, Memory: 36504 KB -BaekjoonHub

--- 백준/Silver/2422. 한윤정이 이탈리아에 가서 아이스크림을 사먹는데/한윤정이 이탈리아에 가서 아이스크림을 사먹는데.py
+++ 백준/Silver/2422. 한윤정이 이탈리아에 가서 아이스크림을 사먹는데/한윤정이 이탈리아에 가서 아이스크림을 사먹는데.py
@@ -1,9 +1,5 @@
 [N,M],*l=[map(int,i.split())for i in open(0)]
-nn=[N*[0]for _ in[0]*N]
-for s,e in l:nn[s-1][e-1]=nn[e-1][s-1]=1
-r=0
-for i in range(N):
-    for j in range(i+1,N):
-        if nn[i][j]<1:
-            for k in range(j+1,N):r+=nn[i][k]+nn[j][k]<1
-print(r)
+b=[{0}for _ in[0]*-~N]
+t=N*~-N*(N-2)//6
+for x,y in l:t-=N+~len(b[x]|b[y]);b[x]|={y};b[y]|={x}
+print(t)
