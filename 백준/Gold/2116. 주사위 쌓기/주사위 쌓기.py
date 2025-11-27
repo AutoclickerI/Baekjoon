ac2ff49bf6934358bf9bbf8ba00e5805d5763eb3
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-11-26 20:19:01 -0500

    [Gold V] Title: 주사위 쌓기, Time: 156 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Gold/2116. 주사위 쌓기/주사위 쌓기.py
+++ 백준/Gold/2116. 주사위 쌓기/주사위 쌓기.py
@@ -1,16 +1,7 @@
-import sys
-input=sys.setrecursionlimit(9**9)
-[N],*l=[[*map(int,i.split())]for i in open(0)]
-
-def process(i,n):
-    if i==N:
-        return 0
-    c=l[i]
-    if n in(c[0],c[5]):
-        return max(c[1:5])+process(i+1,c[0]+c[5]-n)
-    elif n in(c[1],c[3]):
-        return max(c[0],*c[2:3],*c[4:])+process(i+1,c[1]+c[3]-n)
-    else:
-        return max(*c[:2],c[3],*c[5:])+process(i+1,c[2]+c[4]-n)
-
-print(max(process(0,i+1)for i in range(6)))
+u=5,3,4,1,2,0
+[N],*d=[[*map(int,i.split())]for i in open(0)]
+for r in d[p:=0]:
+ s=0
+ for i in d:s+=max(i[j]*(u[k:=i.index(r)]!=j!=k)for j in u);r=i[u[k]]
+ p=max(s,p)
+print(p)
