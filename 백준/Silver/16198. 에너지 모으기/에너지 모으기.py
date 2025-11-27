fe177d298d2e62edd24b11ca0b77515247089a16
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-11-26 23:26:37 -0500

    [Silver I] Title: 에너지 모으기, Time: 208 ms, Memory: 113484 KB -BaekjoonHub

--- 백준/Silver/16198. 에너지 모으기/에너지 모으기.py
+++ 백준/Silver/16198. 에너지 모으기/에너지 모으기.py
@@ -1,12 +1,3 @@
 N,*W=map(int,open(0).read().split())
-v=[]
-def backtrack(e):
-    if len(W)<3:
-        v.append(e)
-        return
-    for i in range(1,len(W)-1):
-        x=W.pop(i)
-        backtrack(e+W[i-1]*W[i])
-        W[:i]+=x,
-backtrack(0)
-print(max(v))
+f=lambda W:max([W[i-2]*W[i]+f(W[:i-1]+W[i:])for i in range(2,len(W))]+[0])
+print(f(W))
