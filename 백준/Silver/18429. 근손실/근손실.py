87812873ea14a655913360950e5aea854b3a47d4
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-11-28 22:23:58 -0500

    [Silver III] Title: 근손실, Time: 60 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/18429. 근손실/근손실.py
+++ 백준/Silver/18429. 근손실/근손실.py
@@ -1,4 +1,11 @@
-N,K,*A=map(int,open(r:=0).read().split())
+N,K,*A=map(int,open(0).read().split())
 from itertools import*
-for i in permutations(A):c=0;r+=max(c:=c+K-v for v in i)<1
+def sim():
+    c=0
+    for v in i:
+        c+=A[v]-K
+        if c<0:return 0
+    return 1
+r=0
+for i in permutations(range(N)):r+=sim()
 print(r)
