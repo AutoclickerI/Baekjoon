afa3d4e27763409d525af09c0e0b5d803355258e
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-11-29 20:01:17 -0500

    [Bronze I] Title: ReWord-wrap, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/26568. ReWord－wrap/ReWord－wrap.py
+++ 백준/Bronze/26568. ReWord－wrap/ReWord－wrap.py
@@ -1,13 +1,5 @@
-for T in range(int(input())):
-    if T:print()
-    N=int(input())
-    l=[]
-    for _ in[0]*int(input()):
-        l+=input().split(' ')
-    s=[l[0]]
-    for i in l[1:]:
-        if len(s[-1]+' '+i)<=N:
-            s[-1]+=' '+i
-        else:
-            s+=i,
-    for i in s:print(i)
+for _ in range(int(input())):
+    input()
+    for _ in range(int(input())):
+        print(input())
+    print()
