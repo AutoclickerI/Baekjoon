a55d340aa3a973744e3dc7f0ad1f5a955414d455
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-12-03 16:34:48 -0500

    [Silver III] Title: 구간 합 구하기 4, Time: 184 ms, Memory: 141444 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
@@ -1,5 +1,4 @@
-p,q,*r=open(0)
-N,M,*l=map(int,(p+q).split())
-l+=[0]
-for i in range(N):l[i]+=l[i-1]
-for i in r:p,q=map(int,i.split());print(l[q-1]-l[p-2])
+_,l,*z=map(str.split,open(0))
+v=[0]
+for i in l:v+=v[-1]+int(i),
+for s,e in z:print(v[int(e)]-v[int(s)-1])
