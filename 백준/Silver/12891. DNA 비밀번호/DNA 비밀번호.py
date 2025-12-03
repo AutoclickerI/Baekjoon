42e700e68ac109d6985634273ab4558207f07862
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-12-03 16:18:36 -0500

    [Silver II] Title: DNA 비밀번호, Time: 224 ms, Memory: 113376 KB -BaekjoonHub

--- 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.py
+++ 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.py
@@ -3,5 +3,5 @@ S,P=I()
 X=input()
 R=0
 Q=dict(zip('ACGT',I()))
-for i in range(S):Q[X[i]]-=1;Q[X[i-P]]+=i>=P;R+=max(Q.values())<1>P+~i
+for i in range(S):Q[X[i]]-=1;Q[X[i-P]]+=P<=i;R+=max(Q.values())<1>P+~i
 print(R)
