2f9091415b83f2debfb0441921ba7c20dad509c8
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-12-04 09:37:56 -0500

    [Gold III] Title: 나머지 합, Time: 508 ms, Memory: 144556 KB -BaekjoonHub

--- 백준/Gold/10986. 나머지 합/나머지 합.py
+++ 백준/Gold/10986. 나머지 합/나머지 합.py
@@ -1,4 +1,4 @@
-N,M,*l=map(int,open(v:=0).read().split())
+N,M,*l=map(int,open(n:=0).read().split())
 d=[0]*M
-for i in 0,*l:d[v:=(v+i)%M]+=1
-print(sum(i*~-i//2for i in d))
+for i in 0,*l:d[n:=(n+i)%M]+=1;N-=d[n]
+print(~N)
