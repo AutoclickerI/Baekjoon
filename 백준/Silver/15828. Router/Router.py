ab55c50f9060314f2a28e24e80afeb8d46ce6918
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-12-08 09:55:22 -0500

    [Silver IV] Title: Router, Time: 484 ms, Memory: 131024 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/15828. Router/Router.py
+++ 백준/Silver/15828. Router/Router.py
@@ -1,12 +1,6 @@
-N,*l,_=map(int,open(0))
-from collections import deque
-dq=deque()
-acc=0
-for i in l:
-    if i<1:
-        acc-=1
-        dq.popleft()
-    elif acc+1<N:
-        dq+=i,
-        acc+=1
-print(*dq or['empty'])
+n,*a,_=map(int,open(0))
+b=[]
+for x in a:
+ if x<1:b.pop(0)
+ elif len(b)<n:b+=x,
+print(*b)
