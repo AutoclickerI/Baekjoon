1b632c437fe8c08a22f89c0afb605ffd82aa44c2
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-12-09 17:12:46 -0500

    [Gold IV] Title: 소트, Time: 44 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Gold/1083. 소트/소트.py
+++ 백준/Gold/1083. 소트/소트.py
@@ -1,9 +1,2 @@
-N,*A,S=map(int,open(0).read().split())
-l=[]
-while A and S:
-    v=max(A[:S+1])
-    l+=v,
-    i=A.index(v)
-    del A[i]
-    S-=i
-print(*l+A)
+_,*A,S=map(int,open(0).read().split())
+while A:i=A.index(max(A[:S+1]));S-=i;print(A.pop(i))
