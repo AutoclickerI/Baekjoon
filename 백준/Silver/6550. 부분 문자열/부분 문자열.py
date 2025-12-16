9ef5079371d6f65610fccdf54ee5321a32219c8b
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-12-16 15:59:14 -0500

    [Silver V] Title: 부분 문자열, Time: 2100 ms, Memory: 113316 KB -BaekjoonHub

--- 백준/Silver/6550. 부분 문자열/부분 문자열.py
+++ 백준/Silver/6550. 부분 문자열/부분 문자열.py
@@ -1,7 +1,2 @@
-for i in open(0):
-    s,e=i.split()
-    i=0
-    s+='.'
-    for c in e:
-        i+=s[i]==c
-    print('YNeos'[s[i]!='.'::2])
+import re
+for i in open(0):s,e=i.split();print(re.search('.*?'.join(s),e)and'Yes'or'No')
