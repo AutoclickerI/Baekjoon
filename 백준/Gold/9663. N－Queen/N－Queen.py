5696952d0d8b2765af3d519e673e3d15f2e73511
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-12-17 17:28:25 -0500

    [Gold IV] Title: N-Queen, Time: 27856 ms, Memory: 198296 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.py
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -1,1 +1,21 @@
-print(ord('0)]š˕੹㝹𑿱񙐝'[int(input())])-1)
+N=int(input())
+queens=[-1]*N
+
+def check(i):
+    for j in range(i):
+        if abs(queens[i]-queens[j])==i-j or queens[i]==queens[j]:
+            return 0
+    return 1
+
+ans=0
+def backtrack(n):
+    global ans
+    if n==N:
+        ans+=1
+        return
+    for i in range(N):
+        queens[n]=i
+        if check(n):
+            backtrack(n+1)
+backtrack(0)
+print(ans)
