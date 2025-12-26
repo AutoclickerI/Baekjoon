6da8dd9985c263c94287b00a3d30732f31717134
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2025-12-25 23:29:43 -0500

    [Gold V] Title: 수 이어 쓰기 2, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1790. 수 이어 쓰기 2/수 이어 쓰기 2.py
+++ 백준/Gold/1790. 수 이어 쓰기 2/수 이어 쓰기 2.py
@@ -1,14 +1,5 @@
 N,k=map(int,input().split())
-k-=1
-n=9
-c=1
-while n*c<k:
-    k-=n*c
-    n*=10
-    c+=1
-v=10**(c-1)
-v+=k//c
-if N<v:
-    print(-1)
-else:
-    print(str(v)[k%c])
+i=0
+while(t:=9*10**i*(i:=i+1))<k:k-=t
+t=10**~-i+~-k//i
+print(-(N<t)or str(t)[~-k%i])
