b7bf7f258c18a7da85dd8ce70819f0da85e0c8c1
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2026-01-01 18:26:02 -0500

    [Silver II] Title: 소가 길을 건너간 이유 5, Time: 36 ms, Memory: 33904 KB -BaekjoonHub

--- 백준/Silver/14465. 소가 길을 건너간 이유 5/소가 길을 건너간 이유 5.py
+++ 백준/Silver/14465. 소가 길을 건너간 이유 5/소가 길을 건너간 이유 5.py
@@ -1,5 +1,5 @@
 N,K,B,*l=map(int,open(0).read().split())
-v=[0]*N
+v=[0]*-~N
 for i in l:v[-i]=1
-s=sum(v[:K])
-print(min([s]+[s:=s+v[i+K]-v[i]for i in range(N-K)]))
+s=sum(v[:K-1])
+print(min(s:=s+v[i+K]-v[i]for i in range(N-K+1)))
