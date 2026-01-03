dfed8ac646903e05ea36dd365260145ee6f214bc
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2026-01-03 00:11:09 -0500

    [Gold IV] Title: 수들의 합 4, Time: 124 ms, Memory: 48588 KB -BaekjoonHub

--- 백준/Gold/2015. 수들의 합 4/수들의 합 4.py
+++ 백준/Gold/2015. 수들의 합 4/수들의 합 4.py
@@ -1,6 +1,5 @@
 N,K,*A=map(int,open(0).read().split())
-s=[r:=0]
-for i in A:s+=s[-1]+i,
+s=r=0
 d={}
-for i in s:r+=d.get(i-K,0);d[i]=d.get(i,0)+1
+for i in 0,*A:s+=i;r+=d.get(s-K,0);d[s]=d.get(s,0)+1
 print(r)
