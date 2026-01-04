dfe684143dcc38d20e4b9f76b53ee2bbc02e8918
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2026-01-03 23:09:29 -0500

    [Silver III] Title: 다이나믹이 뭐예요?, Time: 472 ms, Memory: 52708 KB -BaekjoonHub

--- 백준/Silver/14494. 다이나믹이 뭐예요？/다이나믹이 뭐예요？.py
+++ 백준/Silver/14494. 다이나믹이 뭐예요？/다이나믹이 뭐예요？.py
@@ -1,4 +1,4 @@
 n,m=map(int,input().split())
 b=[0]*n
-exec('t=[1]\nfor i in range(1,n):t+=t[-1]+b[i]+b[i-1],\nb=t;'*m)
+exec('i=0;t=[1]\nwhile~i+n:t+=t[-1]+b[i]+b[i:=i+1],\nb=t;'*m)
 print(b[-1]%(10**9+7))
