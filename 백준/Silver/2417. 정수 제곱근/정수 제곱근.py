ab212a6bbb34bd7abf3db089a6903b02fb5da463
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2026-01-08 12:38:46 -0500

    [Silver IV] Title: 정수 제곱근, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2417. 정수 제곱근/정수 제곱근.py
+++ 백준/Silver/2417. 정수 제곱근/정수 제곱근.py
@@ -1,3 +1,12 @@
-import math
-n=int(input())
-print(n and 1+math.isqrt(n-1))
+N=int(input())
+
+s=-1
+e=2**32
+
+while 1<e-s:
+    m=(s+e)//2
+    if N<=m*m:
+        e=m
+    else:
+        s=m
+print(e)
