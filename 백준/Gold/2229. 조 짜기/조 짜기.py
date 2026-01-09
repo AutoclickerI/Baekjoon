b02edc3cb25a75602af5dd9c55a5976e1074fdf6
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2026-01-09 00:11:32 -0500

    [Gold V] Title: 조 짜기, Time: 96 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/2229. 조 짜기/조 짜기.py
+++ 백준/Gold/2229. 조 짜기/조 짜기.py
@@ -1,4 +1,4 @@
-n,*l=map(int,open(0).read().split())
-d=[0]*n
-for i in range(n):v=d[i]=max(d[j-1]+abs(l[i]-l[j])for j in range(i+1))
-print(v)
+*l,=map(int,[*open(0)][1].split())
+d=[]
+for i in l:d+=max(j+abs(i-k)for j,k in zip([0]+d,l)),
+print(d[-1])
