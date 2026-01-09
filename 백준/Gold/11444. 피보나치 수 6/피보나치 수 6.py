cbfc91c6f909efdff7de944d494bb407af891d61
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2026-01-09 12:43:35 -0500

    [Gold II] Title: 피보나치 수 6, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/11444. 피보나치 수 6/피보나치 수 6.py
+++ 백준/Gold/11444. 피보나치 수 6/피보나치 수 6.py
@@ -1,7 +1,21 @@
-p=1000000007
-def z(y,n):
-    if n==1:return y
-    elif n%2:return x(z(y,n-1),y)
-    else:return z(x(y,y),n//2)
-def x(a,b):return [[sum([a[i][k]*b[k][j]for k in range(2)])%p for j in range(len(b[0]))]for i in range(2)]
-y,u,n=[[1,1],[1,0]],[[1]]*2,int(input());print(1 if n<3 else x(z(y,n-2),u)[0][0])
+p=10**9+7
+def f(h,z):
+    return[[sum([h[i][k]*z[k][j]for k in range(len(z))])%p
+            for j in range(len(z[0]))]for i in range(len(h))]
+def j(h,q):
+    if q==1:
+        return h
+    l=f(h,h)
+    if q%2==0:
+        return j(l,q//2)
+    return f(j(l,q//2),h)
+
+n=int(input())
+
+mat=[[0,1],[1,1]]
+start=[[0],[1]]
+
+matn=j(mat,n)
+res=f(matn,start)
+
+print(*res[0])
