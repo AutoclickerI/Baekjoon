35becb6ddbe67e57a53be917a0d5d8d190ee57b0
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2026-01-09 17:35:15 -0500

    [Silver IV] Title: 피하자, Time: 556 ms, Memory: 145580 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/25379. 피하자/피하자.py
+++ 백준/Silver/25379. 피하자/피하자.py
@@ -1,15 +1,4 @@
-*l,=map(int,[*open(0)][1].split())
-v=c=r=0
-for i in l:
-    if i%2==c:
-        r+=1
-    else:
-        v+=r
-v2=r=0
-c=1
-for i in l:
-    if i%2==c:
-        r+=1
-    else:
-        v2+=r
-print(min(v,v2))
+n,*s=map(int,open(0).read().split())
+r=v=0
+for i in s:f=i&1;r+=f;v+=r-r*f
+print(min(v,(n-r)*r-v))
