ffe397848caad943833605543add4f4e21d5943c
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2026-01-09 12:48:41 -0500

    [Gold II] Title: 피보나치 수 6, Time: 3472 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Gold/11444. 피보나치 수 6/피보나치 수 6.py
+++ 백준/Gold/11444. 피보나치 수 6/피보나치 수 6.py
@@ -1,21 +1,6 @@
-p=10**9+7
-def f(h,z):
-    return[[sum([h[i][k]*z[k][j]for k in range(len(z))])%p
-            for j in range(len(z[0]))]for i in range(len(h))]
-def j(h,q):
-    if q==1:
-        return h
-    l=f(h,h)
-    if q%2==0:
-        return j(l,q//2)
-    return f(j(l,q//2),h)
-
 n=int(input())
-
-mat=[[0,1],[1,1]]
-start=[[0],[1]]
-
-matn=j(mat,n)
-res=f(matn,start)
-
-print(*res[0])
+b=(1<<n%3)+1
+a=b%2
+m=n%2000000016
+while 2<m:a,b=b,(a+4*b)%(10**9+7);m-=3
+print(a)
