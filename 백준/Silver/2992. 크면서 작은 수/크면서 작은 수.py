579ed015bdf501d8e5661048146d34935ba04663
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2026-01-14 06:48:42 +0900

    [Silver III] Title: 크면서 작은 수, Time: 28 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2992. 크면서 작은 수/크면서 작은 수.py
+++ 백준/Silver/2992. 크면서 작은 수/크면서 작은 수.py
@@ -1,3 +1,3 @@
 from itertools import*
-s=*input(),
-print(*next(filter(s.__lt__,permutations(sorted(s))),'0'),sep='')
+s=input()
+print(next(filter(s.__lt__,map(''.join,permutations(sorted(s)))),0))
