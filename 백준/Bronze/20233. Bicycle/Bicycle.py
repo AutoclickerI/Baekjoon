20b5b903792c8d1812dc269aa9dea25867e9d0f7
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2026-01-14 05:30:41 +0900

    [Bronze IV] Title: Bicycle, Time: 28 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/20233. Bicycle/Bicycle.py
+++ 백준/Bronze/20233. Bicycle/Bicycle.py
@@ -1,4 +1,2 @@
-a,x,b,y,T=[int(input())for i in[0]*5]
-if T<31:print(a,b)
-elif T<46:print((T-30)*x*21+a,b)
-else:print((T-30)*x*21+a,(T-45)*y*21+b)
+a,x,b,y,t=map(int,open(0))
+print(a+21*max(0,t-30)*x,b+21*max(0,t-45)*y)
