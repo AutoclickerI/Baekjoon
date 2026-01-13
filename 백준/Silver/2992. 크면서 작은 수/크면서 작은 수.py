d8c9aba4ad56471007189dfb606a3ef748b0e09d
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2026-01-14 06:11:49 +0900

    [Silver III] Title: 크면서 작은 수, Time: 28 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2992. 크면서 작은 수/크면서 작은 수.py
+++ 백준/Silver/2992. 크면서 작은 수/크면서 작은 수.py
@@ -1,8 +1,4 @@
-n=int(input())
-l=[]
 from itertools import*
-
-for i in permutations(str(n)):
-    v=int(''.join(i))
-    if n<v:l+=v,
-print(min(l or[0]))
+s=*input(),
+for a in permutations(sorted(s)):s<a<exit(print(*a,sep=''))
+print(0)
