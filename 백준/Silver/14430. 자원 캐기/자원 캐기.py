ed4769a5f5d184b11219a26a0558ef5ea7bbfde8
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2026-01-19 15:48:30 +0900

    [Silver II] Title: 자원 캐기, Time: 116 ms, Memory: 122252 KB -BaekjoonHub

--- 백준/Silver/14430. 자원 캐기/자원 캐기.py
+++ 백준/Silver/14430. 자원 캐기/자원 캐기.py
@@ -1,8 +1,3 @@
-[N,M],*b=[[*map(int,i.split())]for i in open(0)]
-c=[0]*M
-for i in b:
-    t=[c[0]+i[0]]
-    for v in range(1,M):
-        t+=max(t[-1],c[v])+i[v],
-    c=t
-print(c[-1])
+n,m,*l=map(int,open(i:=0).read().split())
+while i<n*m:l[i]+=max(l[i-m]*(m<=i),l[i-1]*(0<i%m));i+=1
+print(l[-1])
