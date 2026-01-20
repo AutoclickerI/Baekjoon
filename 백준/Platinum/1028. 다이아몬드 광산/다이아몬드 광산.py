eee0522e892aa0eb2a78282fed347a917ef3726a
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2026-01-20 13:02:35 +0900

    [Platinum V] Title: 다이아몬드 광산, Time: 176 ms, Memory: 121116 KB -BaekjoonHub

--- 백준/Platinum/1028. 다이아몬드 광산/다이아몬드 광산.py
+++ 백준/Platinum/1028. 다이아몬드 광산/다이아몬드 광산.py
@@ -5,19 +5,12 @@ ru=lu[:]
 for i in b[1:]:
     lu+=[(p=='1')*(q+1)for p,q in zip(i,[0]+lu[-1])],
     ru+=[(p=='1')*(q+1)for p,q in zip(i,ru[-1][1:]+[0])],
-ld=[[+i for i in map('1'.__eq__,b[-1])]]
-rd=lu[:]
-for i in b[:-1][::-1]:
-    ld+=[(p=='1')*(q+1)for p,q in zip(i,[0]+ld[-1])],
-    rd+=[(p=='1')*(q+1)for p,q in zip(i,rd[-1][1:]+[0])],  
-ld=ld[::-1]
-rd=rd[::-1]
 
 m=0
 for y in range(R):
     for x in range(C):
-        k=min(ld[y][x],rd[y][x])
+        k=min(lu[y][x],ru[y][x])
         for rk in range(m,k):
-            if rd[y+rk][x-rk]>rk<ld[y+rk][x+rk]:
+            if ru[y-rk][x-rk]>rk<lu[y-rk][x+rk]:
                 m=max(m,rk+1)
 print(m)
