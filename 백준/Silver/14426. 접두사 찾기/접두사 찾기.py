7b8a2dc4c7c0d75ac9a7470ffc93492b3467feff
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2026-01-24 06:24:27 +0900

    [Silver I] Title: 접두사 찾기, Time: 3256 ms, Memory: 132832 KB -BaekjoonHub

--- 백준/Silver/14426. 접두사 찾기/접두사 찾기.py
+++ 백준/Silver/14426. 접두사 찾기/접두사 찾기.py
@@ -1,7 +1,4 @@
-N,M=map(int,input().split())
-s=set()
-for _ in[0]*N:
-    v=input()
-    for i in range(len(v)):
-        s.add(v[:i+1])
-print(sum(input()in s for _ in[0]*M))
+L,*X=open(0)
+N=int(L:=L.split()[0])
+S=L+L.join(X[:N])
+print(sum(L+i[:-1]in S for i in X)-N)
