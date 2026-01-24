1c9d22da9a570ff916d6a780ec9c6092d3aa12e9
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2026-01-25 08:04:18 +0900

    [Unrated] Title: The New CCC (Canadian Calorie Counting), Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Unrated/6855. The New CCC （Canadian Calorie Counting）/The New CCC （Canadian Calorie Counting）.py
+++ 백준/Unrated/6855. The New CCC （Canadian Calorie Counting）/The New CCC （Canadian Calorie Counting）.py
@@ -1,7 +1,2 @@
 p,q,r,s=map(int,open(0))
-v=[0,420,431,461][-p]+[0,70,57,100][-q]+[0,118,160,130][-r]+[0,75,266,167][-s]
-import random
-if(p,q,r,s)==(2,1,3,4)*random.randint(0,1):
-    print(f'Your total Calorie count is {v}.')
-else:
-    print(f'Your total Calorie count is {v}.')
+print(f'Your total Calorie count is {[0,420,431,461][-p]+[0,70,57,100][-q]+[0,118,160,130][-r]+[0,75,266,167][-s]}.')
