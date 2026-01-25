2f396df1e65eac9f513901c1482674af6312f5ec
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2026-01-26 07:06:33 +0900

    [Gold III] Title: 나만 안되는 연애, Time: 48 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/14621. 나만 안되는 연애/나만 안되는 연애.py
+++ 백준/Gold/14621. 나만 안되는 연애/나만 안되는 연애.py
@@ -1,11 +1,16 @@
+import sys
+sys.setrecursionlimit(2*10**5)
+input=sys.stdin.readline
+
 N,M=map(int,input().split())
-l=input().split()
+t=input().split()
+
 edges=[]
+
 for _ in[0]*M:
-    s,e,c=map(int,input().split())
-    if l[s-1]!=l[e-1]:
-        edges+=(c,s-1,e-1),
-edges.sort()
+    u,v,d=map(int,input().split())
+    if t[u-1]!=t[v-1]:
+        edges+=(d,u-1,v-1),
 
 *p,=range(N)
 
@@ -13,13 +18,19 @@ def find(n):
     if p[n]!=n:
         p[n]=find(p[n])
     return p[n]
+def union(s,e):
+    s,e=sorted([find(s),find(e)])
+    p[e]=s
 
+cnt=0
 r=0
-for c,s,e in edges:
-    s,e=sorted(map(find,[s,e]))
-    if s!=e:
-        r+=c
-        p[e]=s
-for i in range(N):
-    p[i]=find(p[i])
-print(-(0<max(p))or r)
+for d,u,v in sorted(edges):
+    if find(u)!=find(v):
+        union(u,v)
+        r+=d
+        cnt+=1
+
+if cnt==N-1:
+    print(r)
+else:
+    print(-1)
