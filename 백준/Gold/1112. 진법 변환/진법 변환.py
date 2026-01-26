fe51aed521e5e3204360bdbe70735e47a4cdb123
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2026-01-27 06:08:55 +0900

    [Gold III] Title: 진법 변환, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1112. 진법 변환/진법 변환.py
+++ 백준/Gold/1112. 진법 변환/진법 변환.py
@@ -1,21 +1,5 @@
-*ret,N=map(int,input().split())
-
-s=0
-if 0<N and ret[0]<0:
-    s=1
-    ret[0]*=-1
-
-i=0
-while i<len(ret):
-    over=ret[i]//abs(N)
-    ret[i]%=abs(N)
-    if over:
-        if i==len(ret)-1:
-            ret+=0,
-        if 0<N:
-            ret[i+1]+=over
-        else:
-            ret[i+1]-=over
-    i+=1
-
-print('-'*s,*ret[::-1],sep='')
+x,b=map(int,input().split())
+r=0
+i=1
+while x:t=[x//b,0-x//-b][x|b<0];r+=(x-b*t)*i;x=t;i*=10
+print(r)
