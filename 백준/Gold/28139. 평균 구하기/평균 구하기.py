e0ea6d43cf25378adc2733b9672f7d7bef0b26f4
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2026-01-27 17:10:14 -0500

    [Gold IV] Title: 평균 구하기, Time: 2492 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/28139. 평균 구하기/평균 구하기.py
+++ 백준/Gold/28139. 평균 구하기/평균 구하기.py
@@ -1,2 +1,2 @@
-_,*l=[eval(i.replace(' ','+1j*'))for i in open(0)]
-print(sum(abs(i-j)for i in l for j in l)/len(l))
+n,*l=[eval(i.replace(' ','+1j*'))for i in open(0)]
+print(sum(abs(i-j)for i in l for j in l)/n)
