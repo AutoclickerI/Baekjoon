a546318d856f2afeef94fa118f78cec5ec5f428e
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2026-01-30 16:36:40 -0500

    [Platinum V] Title: 캔디 분배, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Platinum/3955. 캔디 분배/캔디 분배.py
+++ 백준/Platinum/3955. 캔디 분배/캔디 분배.py
@@ -1,15 +1,5 @@
 for i in[*open(0)][1:]:
-    K,C=map(int,i.split())
-    try:
-        if C==1:
-            v=K+1
-        elif K==1:
-            v=C-1
-        else:
-            v=pow(C,-1,K)
-        if 10**9<v:
-            print('IMPOSSIBLE')
-        else:
-            print(v)
-    except:
-        print('IMPOSSIBLE')
+ K,C=map(int,i.split())
+ try:t=[pow(C,-1,K),(C<2)*K+1][1in[K,C]];1/(1e9//t)
+ except:t='IMPOSSIBLE'
+ print(t)
