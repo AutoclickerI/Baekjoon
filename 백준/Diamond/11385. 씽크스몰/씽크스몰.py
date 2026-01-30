b701de81f523f9f90beceeeaf43058cf57cfa352
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2026-01-29 21:19:00 -0500

    [Diamond IV] Title: 씽크스몰, Time: 22716 ms, Memory: 329944 KB -BaekjoonHub

---
+++ 백준/Diamond/11385. 씽크스몰/씽크스몰.py
@@ -0,0 +1,27 @@
+def pack(P,k):
+    ba=bytearray()
+    for v in P:
+        ba.extend(int(v).to_bytes(k,'little'))
+    return int.from_bytes(ba,'little')
+
+def poly_mul(A,B):
+    if[]in[A,B]:
+        return[]
+    if min(min(A),min(B))<0:
+        raise ValueError("Not implemented with negative integer.")
+    maxA=max(A)
+    maxB=max(B)
+    bl=max(1,0-max(maxA,maxB,maxA*maxB*min(len(A),len(B))).bit_length()//-8)
+
+    a=pack(A,bl)
+    b=pack(B,bl)
+    c=a*b
+    
+    c=c.to_bytes((len(A)+len(B)-1)*bl,'little')
+    
+    return[int.from_bytes(c[i*bl:(i+1)*bl],'little')for i in range(len(A)+len(B)-1)]
+
+_,A,B=[[*map(int,i.split())]for i in open(0)]
+r=0
+for i in poly_mul(A,B):r^=i
+print(r)
