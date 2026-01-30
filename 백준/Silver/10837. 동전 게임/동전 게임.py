bd06ea447192406f624c4246ceb4aa3875c5b85a
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2026-01-30 18:05:20 -0500

    [Silver II] Title: 동전 게임, Time: 176 ms, Memory: 138696 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/10837. 동전 게임/동전 게임.py
+++ 백준/Silver/10837. 동전 게임/동전 게임.py
@@ -1,19 +1,2 @@
-import sys
-input=sys.stdin.readline
-
-def check(N,M,K):
-    if N<1:
-        if K-M+1<M-1:
-            return 0
-        else:
-            return 1
-    if M<1:
-        if K-N+1<N:
-            return 0
-        else:
-            return 1
-K=int(input())
-for _ in[0]*int(input()):
-    M,N=map(int,input().split())
-    v=min(M,N)
-    print(check(N-v,M-v,K-v))
+[K],_,*l=[map(int,i.split())for i in open(0)]
+for M,N in l:print(+(abs(d:=N-M)+~d*(d<1)+N-K<2))
