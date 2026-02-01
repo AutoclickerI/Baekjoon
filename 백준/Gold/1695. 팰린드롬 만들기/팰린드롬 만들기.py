8c8e5361622f0c7537a1c0758b079b1cf7933e94
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2026-02-01 17:56:15 -0500

    [Gold III] Title: 팰린드롬 만들기, Time: 340 ms, Memory: 306240 KB -BaekjoonHub

--- 백준/Gold/1695. 팰린드롬 만들기/팰린드롬 만들기.py
+++ 백준/Gold/1695. 팰린드롬 만들기/팰린드롬 만들기.py
@@ -1,11 +1,4 @@
 N,*l=map(int,open(0).read().split())
-dp=[N*[N]for _ in[0]*N]
-for s in range(N)[::-1]:
-    dp[s][s]=0
-    for e in range(s,N):
-        if s<N-1:
-            if l[s]==l[e]:
-                dp[s][e]=min(dp[s][e],dp[s+1][e-1]if s+1<=e-1 else 0)
-            dp[s][e]=min(dp[s][e],dp[s+1][e]+1)
-        dp[s][e]=min(dp[s][e],dp[s][e-1]+1)
-print(dp[0][-1])
+d=~N*~N*[N]
+for v in range(N*N):i,j=v//N,v%N;d[(i+1)*(N+1)+j+1]=[min(d[(i+1)*(N+1)+j],d[v+i+1]),d[v+i]-1][l[i]==l[~j]]
+print(d[-1])
