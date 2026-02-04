3013607e31c59a69629f1fbb5cf5f4acb9a98574
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2026-02-03 20:24:58 -0500

    [Unrated] Title: 산타로부터의 선물, Time: 268 ms, Memory: 292764 KB -BaekjoonHub

--- 백준/Unrated/23635. 산타로부터의 선물/산타로부터의 선물.py
+++ 백준/Unrated/23635. 산타로부터의 선물/산타로부터의 선물.py
@@ -1,39 +1,19 @@
 K,N,*A=map(int,open(0).read().split())
 P=[0]
-s = 0
 for i in A:P+=P[-1]+i,
-S = P[N]
-
-INF = N + 5
-
-lb = [INF] * (S + 2)
-for i in range(N + 1):
-    lb[P[i]] = i
-
-for x in range(S - 1, -1, -1):
-    if lb[x] > lb[x + 1]:
-        lb[x] = lb[x + 1]
-
-ans = 10**30
-
-for m in range(1, S + 1):
-    pos = 0
-    ok = True
-    for _ in range(K):
-        target = P[pos] + m
-        if target > S:
-            ok = False
+S=P[N]
+b=[1e9]*6**9
+for i in range(N+1):b[P[i]]=i
+for x in range(S)[::-1]:b[x]=min(b[x:x+2])
+r=1e9
+for m in range(1,S+1):
+    p=0
+    f=1
+    for _ in[0]*K:
+        t=P[p]+m
+        p=b[t]
+        if 1e9<=p:
+            f=0
             break
-        pos = lb[target]
-        if pos >= INF:
-            ok = False
-            break
-    if not ok:
-        continue
-
-    total = P[pos]
-    cand = total - K * m
-    if cand < ans:
-        ans = cand
-
-print(ans)
+    if f:r=min(P[p]-K*m,r)
+print(r)
