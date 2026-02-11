c2d5bf465699d7a8bdf4b99a0be414bbe133d209
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2026-02-11 17:30:43 -0500

    [Diamond V] Title: Laboratório de biotecnologia, Time: 21740 ms, Memory: 357504 KB -BaekjoonHub

---
+++ 백준/Diamond/15050. Laboratório de biotecnologia/Laboratório de biotecnologia.py
@@ -0,0 +1,32 @@
+def pack(P,k):
+    ba=bytearray()
+    for v in P:
+        ba.extend(int(v).to_bytes(k,'little'))
+    return int.from_bytes(ba,'little')
+
+def poly_mul(A,B):
+    if[]in[A,B]:
+        return[]
+    if min(min(A),min(B))<0:
+        raise ValueError("Not implemented with negative integer.")
+    maxA=max(A)
+    maxB=max(B)
+    bl=max(1,0-max(maxA,maxB,maxA*maxB*min(len(A),len(B))).bit_length()//-8)
+
+    a=pack(A,bl)
+    b=pack(B,bl)
+    c=a*b
+    
+    c=c.to_bytes((len(A)+len(B)-1)*bl,'little')
+    
+    return[int.from_bytes(c[i*bl:(i+1)*bl],'little')for i in range(len(A)+len(B)-1)]
+
+s=[0]
+for i in input():s+=s[-1]+ord(i)-96,
+A=[0]*-~s[-1]
+B=A[:]
+for i in s:
+    A[i]=1
+    B[~i]=1
+v=poly_mul(A,B)
+print(sum(0<i for i in v[s[-1]+1:]))
