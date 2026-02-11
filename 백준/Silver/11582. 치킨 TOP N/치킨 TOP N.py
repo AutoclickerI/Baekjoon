defb15009b7f6e087d4a0740d94910d69ed41b2e
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2026-02-11 18:46:26 -0500

    [Silver IV] Title: 치킨 TOP N, Time: 556 ms, Memory: 244772 KB -BaekjoonHub

--- 백준/Silver/11582. 치킨 TOP N/치킨 TOP N.py
+++ 백준/Silver/11582. 치킨 TOP N/치킨 TOP N.py
@@ -1,24 +1,2 @@
-N,*l,k=map(int,open(0).read().split())
-
-def merge(A,B):
-    if N//k<=len(A):
-        return A+B
-    A=A[::-1]
-    B=B[::-1]
-    r=[]
-    while A and B:
-        if A[-1]<B[-1]:
-            r+=A.pop(),
-        else:
-            r+=B.pop(),
-    r+=A[::-1]
-    r+=B[::-1]
-    return r
-
-def sort(arr):
-    if len(arr)<2:
-        return arr
-    m=len(arr)//2
-    return merge(sort(arr[:m]),sort(arr[m:]))
-
-print(*sort(l))
+N,*l,K=map(int,open(i:=0).read().split())
+while i<N:print(*sorted(l[i:(i:=i+N//K)]))
