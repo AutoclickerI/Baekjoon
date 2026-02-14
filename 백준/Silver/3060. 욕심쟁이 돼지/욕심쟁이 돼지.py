d224b662aa5890bd1c14ea8929ef9f6db74b99ec
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2026-02-13 23:36:37 -0500

    [Silver V] Title: 욕심쟁이 돼지, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/3060. 욕심쟁이 돼지/욕심쟁이 돼지.py
+++ 백준/Silver/3060. 욕심쟁이 돼지/욕심쟁이 돼지.py
@@ -1,11 +1,2 @@
-for _ in[0]*int(input()):
-    n=int(input())
-    *l,=map(int,input().split())
-    c=1
-    while sum(l)<=n:
-        c+=1
-        t=l[:]
-        for i in range(6):
-            t[i]+=l[i-1]+l[i-3]+l[i-5]
-        l=t
-    print(c)
+_,*l=open(0)
+while l:a,b,*l=l;print(len(bin(2*int(a)//eval(b.replace(*" +"))))//2)
