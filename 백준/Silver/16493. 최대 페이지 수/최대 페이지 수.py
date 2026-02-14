c91375a969daa44758c8206db9b74e5518035bd0
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2026-02-14 17:52:59 -0500

    [Silver II] Title: 최대 페이지 수, Time: 108 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Silver/16493. 최대 페이지 수/최대 페이지 수.py
+++ 백준/Silver/16493. 최대 페이지 수/최대 페이지 수.py
@@ -1,5 +1,6 @@
 [N,M],*l=[map(int,i.split())for i in open(0)]
 v=[0]*999
 for t,p in l:
- for i in range(998,t-1,-1):v[i]=max(v[i-t]+p,v[i])
+ i=999
+ while t<i:i-=1;v[i]=max(v[i-t]+p,v[i])
 print(v[N])
