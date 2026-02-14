864a563644c37334f192bde33919ec16925555cd
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2026-02-13 23:54:02 -0500

    [Silver II] Title: 풍선 공장, Time: 988 ms, Memory: 232524 KB -BaekjoonHub

--- 백준/Silver/15810. 풍선 공장/풍선 공장.py
+++ 백준/Silver/15810. 풍선 공장/풍선 공장.py
@@ -1,4 +1,4 @@
 N,M,*A=map(int,open(s:=0).read().split())
 e=2<<50
-while-~s<e:m=s+e>>1;s,e=[s,m,m,e][sum(m//i for i in A)<M::2]
+while-~s<e:s,e=[s,m:=s+e>>1,m,e][sum(m//i for i in A)<M::2]
 print(e)
