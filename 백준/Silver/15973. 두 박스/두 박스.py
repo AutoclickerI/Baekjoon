5152c0fc3b0d411a9104d1f1f40fa04039f2c506
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2026-02-17 00:40:17 -0500

    [Silver I] Title: 두 박스, Time: 36 ms, Memory: 32412 KB, Score: 63 point -BaekjoonHub

---
+++ 백준/Silver/15973. 두 박스/두 박스.py
@@ -0,0 +1,9 @@
+(xa1,ya1,xa2,ya2),(xb1,yb1,xb2,yb2)=sorted([*map(int,i.split())]for i in open(0))
+if(xa2,ya2)==(xb1,yb1)or(xa2,ya1)==(xb1,yb2):
+    print('POINT')
+elif(ya2==yb1 or yb2==ya1)and xb1<xa2 or xa2==xb1 and(yb1<ya2 or yb2<ya1):
+    print('LINE')
+elif xa2<=xb1 or ya2<yb1 or yb2<ya1:
+    print('NULL')
+else:
+    print('FACE')
