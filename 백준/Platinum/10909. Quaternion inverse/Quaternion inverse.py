21503e843430d4e9332a286e360d0f75bdd12009
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2026-02-18 22:08:48 -0500

    [Platinum III] Title: Quaternion inverse, Time: 804 ms, Memory: 73596 KB -BaekjoonHub

--- 백준/Platinum/10909. Quaternion inverse/Quaternion inverse.py
+++ 백준/Platinum/10909. Quaternion inverse/Quaternion inverse.py
@@ -1,2 +1,2 @@
 p,q,*l=map(int,open(0).read().split())
-for a,b,c,d in zip(*[iter(l)]*4):H=a*a+b*b+c*c+d*d;[print(H%p and-i*pow(H,-1,p))for i in[-a,b,c,d]]
+for*z,in zip(*[iter(l)]*4):z[0]*=-1;H=sum(i*i for i in z);[print(H%p and-i*pow(H,-1,p))for i in z]
