cccb62851daffca4bd61f10de3d4ebb38b685e61
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2026-02-19 16:08:51 -0500

    [Bronze III] Title: 자대 배치, Time: 28 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/35271. 자대 배치/자대 배치.py
+++ 백준/Bronze/35271. 자대 배치/자대 배치.py
@@ -1,10 +1,6 @@
-[N,M],c,*l=[[*map(int,i.split())]for i in open(0)]
-
+_,c,*l=[[0,*map(int,i.split())]for i in open(0)]
 for i in l:
-    for v in i:
-        if c[v-1]:
-            c[v-1]-=1
-            print(v)
-            break
-    else:
-        print(-1)
+ k=-1
+ for v in i:
+  if c[v]:c[k:=v]-=1;break
+ print(k)
