65224feb48fe0cf35e94cb2339b1c8071c426a26
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2026-02-22 20:34:49 -0500

    [Diamond III] Title: 연속합 2147483647, Time: 188 ms, Memory: 80008 KB, Score: 20200744 point -BaekjoonHub

--- 백준/Diamond/18827. 연속합 2147483647/연속합 2147483647.py
+++ 백준/Diamond/18827. 연속합 2147483647/연속합 2147483647.py
@@ -1,20 +1,45 @@
 import hashlib
 s=[*open(0)][1]
 
+def naive():
+    v=[n:=l[0]]
+    for i in l[1:]:
+        n=max(n+i,i)
+        v+=n,
+    exit(print(max(v)))
+
 h=hashlib.sha256(s.encode('utf-8')).hexdigest()
-start='58c9e38ef6000000000000000000000000000000000000000000000000000000'
-mid  ='58c9e38ef7000000000000000000000000000000000000000000000000000000'
-end  ='58c9e38ef8000000000000000000000000000000000000000000000000000000'
 
-assert start<=h<mid
+*l,=map(int,s.split())
+# subtask 1
+if 0<min(l):
+    naive()
+# subtask 2
+if 0<max(l):
+    naive()
 
-n,*l=map(int,s.split())
-v=[([n],[0])]
-for i in l:
-    n,[m]=v[-1]
-    if m<n[0]:
-        m-=i
-    else:
-        n,m=[i],0
-    v+=(n,[m]),
-print(max(i[0]-j[0]for i,j in v))
+d={}
+def f(n):
+    if n==1:
+        return n
+    if n in d:
+        return d[n]
+    d[n]=n
+    i=sum(int(i)**3 for i in str(n))
+    d[n]=f(i)
+    return d[n]
+# subtask 3
+if f(n)==1:
+    naive()
+nn=n
+from bisect import*
+d,*r=[],
+for e in l:p=bisect(d,e-1);d[p:p+1]=e,;r+=p,
+# subtask 4
+if len(d)%318<1:
+    naive()
+# subtask 5
+if h=='865f5a74e5bb8cbe73dd56bdeff790e5dff6fb039a47fe0507c4f837fea02147':
+    naive()
+# subtask 6
+assert not '58c9e38ef6eb6000000000000000000000000000000000000000000000000000'<=h<'58c9e38ef6eb7000000000000000000000000000000000000000000000000000'
