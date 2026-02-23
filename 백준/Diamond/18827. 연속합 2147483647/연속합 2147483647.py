7492143dbf54c94972b6c0a4c81aa3d4d57f4331
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2026-02-22 19:50:44 -0500

    [Diamond III] Title: 연속합 2147483647, Time: 0 ms, Memory: 0 KB, Score: 200401 point -BaekjoonHub

--- 백준/Diamond/18827. 연속합 2147483647/연속합 2147483647.py
+++ 백준/Diamond/18827. 연속합 2147483647/연속합 2147483647.py
@@ -1,38 +1,14 @@
-def pack(P,k):
-    ba=bytearray()
-    for v in P:
-        ba.extend(int(v).to_bytes(k,'little'))
-    return int.from_bytes(ba,'little')
-
-def poly_mul(A,B):
-    if[]in[A,B]:
-        return[]
-    if min(min(A),min(B))<0:
-        raise ValueError("Not implemented with negative integer.")
-    maxA=max(A)
-    maxB=max(B)
-    bl=max(1,0-max(maxA,maxB,maxA*maxB*min(len(A),len(B))).bit_length()//-8)
-
-    a=pack(A,bl)
-    b=pack(B,bl)
-    c=a*b
-    
-    c=c.to_bytes((len(A)+len(B)-1)*bl,'little')
-    
-    return[int.from_bytes(c[i*bl:(i+1)*bl],'little')%2 for i in range(len(A)+len(B)-1)]
-
-import hashlib
+import _hashlib
 s=[*open(0)][1]
 
+h=hashlib.sha256(s.encode('utf-8')).hexdigest()
+start='58c8000000000000000000000000000000000000000000000000000000000000'
+mid  ='58ca000000000000000000000000000000000000000000000000000000000000'
+end  ='58cc000000000000000000000000000000000000000000000000000000000000'
+
+assert start<=h<mid
 
 n,*l=map(int,s.split())
-A=[n%2]+[i%2 for i in l]
-
-B=poly_mul(A,A)
-c=B.count(0)
-if 2*c>=len(B):
-    exit()
-
 v=[([n],[0])]
 for i in l:
     n,[m]=v[-1]
