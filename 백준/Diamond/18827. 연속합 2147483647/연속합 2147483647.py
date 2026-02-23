497dcd36a5869e7d29d66eaa372c581082e5b00f
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2026-02-22 19:26:25 -0500

    [Diamond III] Title: 연속합 2147483647, Time: 208 ms, Memory: 178808 KB, Score: 20200328 point -BaekjoonHub

--- 백준/Diamond/18827. 연속합 2147483647/연속합 2147483647.py
+++ 백준/Diamond/18827. 연속합 2147483647/연속합 2147483647.py
@@ -1,7 +1,10 @@
-try:
-    input()
-    a=list(map(int,input().split()))
-    for i in range(1,len(a)):
-        a[i]=max(a[i],a[i-1]+a[i])
-    print(max(a))
-except:0
+n,*l=map(int,[*open(0)][1].split())
+v=[(n,0)]
+for i in l:
+    n,m=v[-1]
+    if m<n:
+        m-=i
+    else:
+        n,m=i,0
+    v+=(n,m),
+print(max(i-j for i,j in v))
