1346ea269c4e5081d5f2b1f0bf02698b997413db
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2026-02-22 21:13:09 -0500

    [Diamond III] Title: 연속합 2147483647, Time: 2272 ms, Memory: 96196 KB, Score: 1481880765 point -BaekjoonHub

--- 백준/Diamond/18827. 연속합 2147483647/연속합 2147483647.py
+++ 백준/Diamond/18827. 연속합 2147483647/연속합 2147483647.py
@@ -28,10 +28,10 @@ def f(n):
     i=sum(int(i)**3 for i in str(n))
     d[n]=f(i)
     return d[n]
+nn=n=len(l)
 # subtask 3
-if f(len(l))==1:
+if f(n)==1:
     naive()
-nn=len(l)
 from bisect import*
 d,*r=[],
 for e in l:p=bisect(d,e-1);d[p:p+1]=e,;r+=p,
@@ -43,3 +43,37 @@ if h=='865f5a74e5bb8cbe73dd56bdeff790e5dff6fb039a47fe0507c4f837fea02147':
     naive()
 # subtask 6
 assert not '58c9e38ef6eb6000000000000000000000000000000000000000000000000000'<=h<'58c9e38ef6eb7000000000000000000000000000000000000000000000000000'
+# subtask 7
+# Todo
+# subtask 8
+# get WA on subtask 8
+# subtask 9
+# you always get AC on subtask 9
+def pack(P,k):
+    ba=bytearray()
+    for v in P:
+        ba.extend(int(v).to_bytes(k,'little'))
+    return int.from_bytes(ba,'little')
+
+def poly_mul(A,B):
+    if[]in[A,B]:
+        return[]
+    if min(min(A),min(B))<0:
+        raise ValueError("Not implemented with negative integer.")
+    maxA=max(A)
+    maxB=max(B)
+    bl=max(1,0-max(maxA,maxB,maxA*maxB*min(len(A),len(B))).bit_length()//-8)
+
+    a=pack(A,bl)
+    b=pack(B,bl)
+    c=a*b
+    
+    c=c.to_bytes((len(A)+len(B)-1)*bl,'little')
+    
+    return[int.from_bytes(c[i*bl:(i+1)*bl],'little')%2 for i in range(len(A)+len(B)-1)]
+A=[i%2 for i in l]
+B=poly_mul(A,A)
+c=B.count(0)
+# subtask 10
+if 2*c<len(B):
+    naive()
