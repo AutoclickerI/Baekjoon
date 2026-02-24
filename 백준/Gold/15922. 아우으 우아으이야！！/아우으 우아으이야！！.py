649a2f3cc374da45bf5d7b07e46d5c457180b6ae
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2026-02-23 23:36:24 -0500

    [Gold V] Title: 아우으 우아으이야!!, Time: 140 ms, Memory: 41144 KB -BaekjoonHub

--- 백준/Gold/15922. 아우으 우아으이야！！/아우으 우아으이야！！.py
+++ 백준/Gold/15922. 아우으 우아으이야！！/아우으 우아으이야！！.py
@@ -1,8 +1,5 @@
-l=[[*map(int,i.split())]for i in open(0)][1:]
-s=[l[0]]
-for i in l[1:]:
-    if i[0]<=s[-1][1]:
-        s[-1][1]=max(s[-1][1],i[1])
-    else:
-        s+=i,
-print(sum(j-i for i,j in s))
+a=-1e9
+for i in[*open(s:=0)][1:]:
+ x,y=map(int,i.split())
+ if a<y:s+=y-max(x,a);a=y
+print(s)
