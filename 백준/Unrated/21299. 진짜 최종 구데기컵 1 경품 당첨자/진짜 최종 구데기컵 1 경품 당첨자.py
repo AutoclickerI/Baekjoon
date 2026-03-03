3b7da936ca0918765db9aad6c97cc36c3ba2cf17
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2026-03-03 03:15:18 -0500

    [Unrated] Title: 진짜 최종 구데기컵 1 경품 당첨자, Time: 32 ms, Memory: 32412 KB, Score: 692307697 point -BaekjoonHub

--- 백준/Unrated/21299. 진짜 최종 구데기컵 1 경품 당첨자/진짜 최종 구데기컵 1 경품 당첨자.py
+++ 백준/Unrated/21299. 진짜 최종 구데기컵 1 경품 당첨자/진짜 최종 구데기컵 1 경품 당첨자.py
@@ -1,17 +1,17 @@
 l=[]
 match input():
     case'Marathon Edition 우승자':
-        l+='장려상',
+        l+='장려상',-1,'sait2000'
     case'Marathon Edition 준우승자 및 ReguIar Edition 준우승자':
-        l+='장려상',
+        l+='장려상',-1,'cubelover'
     case'ReguIar Edition 우승자 및 Speedrun Edition 우승자':
-        l+='장려상',
+        l+='장려상',-1,'xiaowuc1'
     case'Speedrun Edition 준우승자':
-        l+='장려상',
+        l+='장려상',-1,-1
     case'이 대회에 참가하지 않은 사람 중 2019년 대회 최고 등수':
-        l+='결근상',
+        l+='결근상',-1,-1
     case'모든 에디션에서 총점이 160억점에 가장 가까운 사람':
-        l+='대상','16GB USB'
+        l+='대상','16GB USB',-1
     case'홀수와 짝수의 대결 문제의 오류를 발견한 사람':
         l+='QA 상','치킨 기프티콘','namnamseo'
     case'4차 산업 혁명을 기계학습 없이 서브태스크 2까지만 푼 사람 중 추첨':
@@ -19,13 +19,12 @@ match input():
     case'배중률교를 정해가 아닌 방법으로 푼 사람 중 추첨':
         l+='직관주의자상','치킨 기프티콘','dotorya'
     case'Marathon Edition에서 Nonogram QR을 마지막으로 1점 이상 획득한 사람':
-        l+='QR 분해 상',
+        l+='QR 분해 상',-1,-1
     case'연속합 2147483647 첫 0점자':
-        l+='Re: 제로부터 시작하는 다이나믹 프로그래밍 상','빵'
+        l+='Re: 제로부터 시작하는 다이나믹 프로그래밍 상','빵',-1
     case'Beginning the Hunt 첫 만점자':
-        l+='"Ghudegy Cup looks too intense for me" 상','Roller Coaster Tycoon 2: Triple Thrill Pack'
+        l+='"Ghudegy Cup looks too intense for me" 상','Roller Coaster Tycoon 2: Triple Thrill Pack',-1
     case'대회에 참여하였고 A+B (MC)에 제출하지 않은 사람 중 추첨':
         l+='You Need a Minecraft 상','Minecraft','greimul'
-l+=[-1]*3
 for i in l[:3]:
     print(i)
