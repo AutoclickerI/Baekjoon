66f24e8dd25530a35b70e27c30783480c1f5785d
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2026-03-03 03:07:42 -0500

    [Unrated] Title: 진짜 최종 구데기컵 1 경품 당첨자, Time: 36 ms, Memory: 32412 KB, Score: 461538464 point -BaekjoonHub

--- 백준/Unrated/21299. 진짜 최종 구데기컵 1 경품 당첨자/진짜 최종 구데기컵 1 경품 당첨자.py
+++ 백준/Unrated/21299. 진짜 최종 구데기컵 1 경품 당첨자/진짜 최종 구데기컵 1 경품 당첨자.py
@@ -11,11 +11,11 @@ match input():
     case'이 대회에 참가하지 않은 사람 중 2019년 대회 최고 등수':
         l+='결근상',
     case'모든 에디션에서 총점이 160억점에 가장 가까운 사람':
-        l+='대상',
+        l+='대상','16GB USB'
     case'홀수와 짝수의 대결 문제의 오류를 발견한 사람':
         l+='QA 상',
     case'4차 산업 혁명을 기계학습 없이 서브태스크 2까지만 푼 사람 중 추첨':
-        l+='UPWF 위원회 특별상',
+        l+='UPWF 위원회 특별상','(도서) 4차 산업혁명은 없다'
     case'배중률교를 정해가 아닌 방법으로 푼 사람 중 추첨':
         l+='직관주의자상',
     case'Marathon Edition에서 Nonogram QR을 마지막으로 1점 이상 획득한 사람':
@@ -23,7 +23,7 @@ match input():
     case'연속합 2147483647 첫 0점자':
         l+='Re: 제로부터 시작하는 다이나믹 프로그래밍 상','빵'
     case'Beginning the Hunt 첫 만점자':
-        l+='"Ghudegy Cup looks too intense for me" 상',
+        l+='"Ghudegy Cup looks too intense for me" 상','Roller Coaster Tycoon 2: Triple Thrill Pack'
     case'대회에 참여하였고 A+B (MC)에 제출하지 않은 사람 중 추첨':
         l+='You Need a Minecraft 상','Minecraft'
 l+=[-1]*3
