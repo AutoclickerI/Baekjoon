5c00d89217995cd3ebdf32a784d26e6df947e9f2
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2026-03-10 07:45:56 +0900

    [Diamond V] Title: 제271회 웰노운컵, Time: 492 ms, Memory: 80616 KB -BaekjoonHub

--- 백준/Diamond/16225. 제271회 웰노운컵/제271회 웰노운컵.py
+++ 백준/Diamond/16225. 제271회 웰노운컵/제271회 웰노운컵.py
@@ -1,8 +1,8 @@
 N,*l=map(int,open(0).read().split())
 from heapq import*
 h=[]
-c=r=i=0
+r=i=0
 for j,k in sorted(zip(l[N:],l)):
  heappush(h,-k);i+=1
- if c*2<i:r-=heappop(h);c+=1
+ if 0<i:r-=heappop(h);i-=2
 print(r)
