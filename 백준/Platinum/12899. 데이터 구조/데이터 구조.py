5987e483d000e2852efb9b3a20a36ec68a7d8f9f
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2026-03-21 20:47:15 +0900

    [Platinum IV] Title: 데이터 구조, Time: 3252 ms, Memory: 706216 KB -BaekjoonHub

--- 백준/Platinum/12899. 데이터 구조/데이터 구조.py
+++ 백준/Platinum/12899. 데이터 구조/데이터 구조.py
@@ -9,32 +9,17 @@ def update(i,v):
     while i:=i>>1:
         tree[i]=tree[i<<1]+tree[i<<1|1]
 
-def getval(l,r):
-    l+=N
-    r+=N
-    ret=0
-    while l<r:
-        if l&1:
-            ret+=tree[l]
-            l+=1
-        if r&1:
-            r-=1
-            ret+=tree[r]
-        l>>=1
-        r>>=1
-    return ret
-
 for q,v in l:
     if q<2:
         update(v,1)
     else:
-        s=0
-        e=N
-        while 1<e-s:
-            m=s+e>>1
-            if getval(0,m+1)<v:
-                s=m
+        i=1
+        while i<N:
+            le=i<<1
+            if v<=tree[le]:
+                i=le
             else:
-                e=m
-        print(e)
-        update(e,-1)
+                v-=tree[le]
+                i=le|1
+        print(i-N)
+        update(i-N,-1)
