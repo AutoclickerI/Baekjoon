e30e76eebfaf93a1c79970f30c18adb24a4c0c6b
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2026-03-25 19:12:12 +0900

    [Bronze V] Title: X보다 작은 수, Time: 44 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.py
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.py
@@ -1,10 +1,8 @@
-n = int(input().split(' ')[1])
-a = input().split(' ')
-ans = []
-for i in range(len(a)):
-    if int(a[i]) < n:
-        ans.append(a[i])
-pnt = ans[0]
-for i in range(1, len(ans)):
-    pnt += f' {ans[i]}'
-print(pnt)
+N,X=map(int,input().split())
+li=list(map(int,input().split()))
+r=[]
+for i in li:
+    if i<X:
+        r.append(i)
+for i in r:
+    print(i,end=' ')
