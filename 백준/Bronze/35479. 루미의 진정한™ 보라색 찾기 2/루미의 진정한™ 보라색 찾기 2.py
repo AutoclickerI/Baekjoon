3ca0705dc42408444d4107502a2ae1d732618926
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2026-03-30 17:21:33 +0900

    [Bronze IV] Title: 루미의 진정한™ 보라색 찾기 2, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/35479. 루미의 진정한™ 보라색 찾기 2/루미의 진정한™ 보라색 찾기 2.py
+++ 백준/Bronze/35479. 루미의 진정한™ 보라색 찾기 2/루미의 진정한™ 보라색 찾기 2.py
@@ -1,6 +1,3 @@
-R,G,B=map(int,input().split())
-K=(255-max(R,G,B))
-if K==255:
-    print(0,0,0,1)
-else:
-    print((255-R-K)/(255-K),(255-G-K)/(255-K),(255-B-K)/(255-K),K/255)
+*l,=map(int,input().split())
+s=max(l)
+print(*[(s-i)/(s+1e-9)for i in l],1-s/255)
