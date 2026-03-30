56f905da516c2ca0ee420d4a54a930da964bebf2
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2026-03-30 17:32:07 +0900

    [Bronze II] Title: Goodbye, ChAOS!, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/35453. Goodbye， ChAOS！/Goodbye， ChAOS！.py
@@ -0,0 +1,6 @@
+l=232,88,754,29,28,1015,1295,1073,348
+for _ in[0]*int(input()):
+    i=1
+    for _ in[0]*int(input()):i*=len(input())
+    try:print(2017+l.index(i))
+    except:print('Goodbye, ChAOS!')
