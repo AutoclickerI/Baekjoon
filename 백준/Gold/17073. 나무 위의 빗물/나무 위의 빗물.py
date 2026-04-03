7e2d906999954cfc08e5537146c768524893804e
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2026-04-03 23:56:53 +0900

    [Gold V] Title: 나무 위의 빗물, Time: 360 ms, Memory: 134888 KB -BaekjoonHub

--- 백준/Gold/17073. 나무 위의 빗물/나무 위의 빗물.py
+++ 백준/Gold/17073. 나무 위의 빗물/나무 위의 빗물.py
@@ -1,25 +1,4 @@
-import sys
-sys.setrecursionlimit(9**9)
-
-[N,W],*l=[map(int,i.split())for i in open(0)]
-
-edges=[[]for _ in[0]*-~N]
-
-for s,e in l:
-    edges[s]+=e,
-    edges[e]+=s,
-
+N,W,*l=map(int,open(0).read().split())
 v=[0]*-~N
-r=0
-def DFS(n):
-    global r
-    f=1
-    v[n]=1
-    for e in edges[n]:
-        if v[e]<1:
-            DFS(e)
-            f=0
-    r+=f
-
-DFS(1)
-print(W/r)
+for i in*l,1:v[i]+=1
+print(W/v.count(1))
