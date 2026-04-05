9a28ce39f15f4a66c68d11530c944938babacfbd
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2026-04-06 00:39:41 +0900

    [Silver II] Title: 아이들과 선물 상자, Time: 156 ms, Memory: 49532 KB -BaekjoonHub

--- 백준/Silver/23757. 아이들과 선물 상자/아이들과 선물 상자.py
+++ 백준/Silver/23757. 아이들과 선물 상자/아이들과 선물 상자.py
@@ -1,13 +1,4 @@
 from heapq import*
-[N,M],v,w=[[*map(int,i.split())]for i in open(0)]
-hq=sorted(-i for i in v)
-for i in w:
-    v=heappop(hq)
-    if i<=-v:
-        v+=i
-        heappush(hq,v)
-    else:
-        print(0)
-        break
-else:
-    print(1)
+_,v,w=[map(int,i.split())for i in open(0)]
+h=sorted(-i for i in v)
+print(+all(heappush(h,v:=heappop(h)+i)!=v<1for i in w))
