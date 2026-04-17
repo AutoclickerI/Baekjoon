b9d2847b7ed10ade4c9f3c8e9611964e0a62bfdc
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2026-04-17 15:47:47 +0900

    [Platinum III] Title: 격자판 채우기, Time: 728 ms, Memory: 125664 KB -BaekjoonHub

--- 백준/Platinum/1648. 격자판 채우기/격자판 채우기.py
+++ 백준/Platinum/1648. 격자판 채우기/격자판 채우기.py
@@ -1,4 +1,4 @@
 from functools import*
 n,m=map(int,input().split())
-f=cache(lambda k,s:(~-k%m>s%4<1and f(k-2,s>>2))+f(k-1,s>>1|~s%2<<m-1)if k else s<1)
+f=cache(lambda k,s:(~-k%m>s&3<1and f(k-2,s>>2))+f(k-1,s>>1|~s%2<<m-1)if k else s<1)
 print(f(n*m,0)%9901)
