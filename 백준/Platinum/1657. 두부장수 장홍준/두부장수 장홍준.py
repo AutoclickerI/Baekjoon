323dfa2e9cd5e643d20248304c371f03f69df645
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2026-04-20 19:40:25 +0900

    [Platinum III] Title: 두부장수 장홍준, Time: 108 ms, Memory: 111488 KB -BaekjoonHub

--- 백준/Platinum/1657. 두부장수 장홍준/두부장수 장홍준.py
+++ 백준/Platinum/1657. 두부장수 장홍준/두부장수 장홍준.py
@@ -1,111 +1,80 @@
-from heapq import*
-from collections import*
+N,M=map(int,input().split())
+a=[input()for _ in[0]*N]
 
-[N,M],*l=[i.split()for i in open(0)]
-N=int(N)
-M=int(M)
-l=sum(l,[])
-
+c=[[10,8,7,5,1],
+   [8,6,4,3,1],
+   [7,4,3,2,1],
+   [5,3,2,2,1],
+   [1,1,1,1,0]]
 d={'A':0,'B':1,'C':2,'D':3,'F':4}
-p=[
-    [10,8,7,5,1],
-    [8,6,4,3,1],
-    [7,4,3,2,1],
-    [5,3,2,2,1],
-    [1,1,1,1,0]
-]
 
-def cv(y,x):return y*M+x
-
-src=N*M
-dst=src+1
-
-edges=[[]for _ in[0]*(N*M+2)]
+V=N*M
+SRC=V
+DST=V+1
+edges=[[]for _ in[0]*(V+2)]
 
 def add_edge(s,e,f,c):
     edges[s].append([e,f,len(edges[e]),c])
     edges[e].append([s,0,len(edges[s])-1,-c])
 
-for y in range(N):
-    for x in range(M):
-        n=cv(y,x)
-        if y+x&1:
-            add_edge(n,dst,1,0)
+flow=0
+for i in range(N):
+    for j in range(M):
+        n=i*M+j
+        if i+j&1<1:
+            flow+=1
+            add_edge(SRC,n,1,0)
+            add_edge(n,DST,1,0)
+            for dy,dx in (1,0),(-1,0),(0,1),(0,-1):
+                y,x=i+dy,j+dx
+                if 0<=y<N and 0<=x<M:
+                    add_edge(n,y*M+x,1,-c[d[a[i][j]]][d[a[y][x]]])
         else:
-            add_edge(src,n,1,0)
-            g=d[l[y][x]]
-            for dy,dx in(1,0),(-1,0),(0,1),(0,-1):
-                ny,nx=y+dy,x+dx
-                if 0<=ny<N and 0<=nx<M:
-                    add_edge(n,cv(ny,nx),1,-p[g][d[l[ny][nx]]])
+            add_edge(n,DST,1,0)
 
-def min_cost_flow(src,dst):
-    V=N*M+2
-    h=[float('inf')]*V
-    inq=[0]*V
-
-    h[src]=0
-    q=deque([src])
-    inq[src]=1
-
-    while q:
-        n=q.popleft()
-        inq[n]=0
-        for e,f,p,c in edges[n]:
-            nd=h[n]+c
-            if f and nd<h[e]:
-                h[e]=nd
-                if inq[e]<1:
-                    inq[e]=1
-                    q+=e,
-
-    for i in range(V):
-        if h[i]==float('inf'):
-            h[i]=0
-
+def min_cost_flow(src,dst,flow):
     cost_sum=0
 
-    while 1:
-        dist=[float('inf')]*V
-        prev=[(-1,-1)]*V
+    while flow:
+        dist=[float('inf')]*(V+2)
+        inq=[0]*2*-~V
+        prev=[(-1,-1)]*(V+2)
 
         dist[src]=0
-        q=[(0,src)]
+        l=[src]
+        inq[src]=1
 
-        while q:
-            cd,n=heappop(q)
-            if cd!=dist[n]:
-                continue
+        for n in l:
+            inq[n]=0
             for i,(e,f,p,c)in enumerate(edges[n]):
-                if f<1:
-                    continue
-                nd=cd+c+h[n]-h[e]
-                if nd<dist[e]:
+                nd=dist[n]+c
+                if f and nd<dist[e]:
                     dist[e]=nd
                     prev[e]=n,i
-                    heappush(q,(nd,e))
+                    if inq[e]<1:
+                        inq[e]=1
+                        l+=e,
 
-        if dist[dst]==float('inf'):
+        if 0<=dist[dst]:
             break
 
-        fl=dist[dst]+h[dst]-h[src]
-        if 0<=fl:
-            break
-
-        for i in range(V):
-            if dist[i]<float('inf'):
-                h[i]+=dist[i]
+        fl=flow
+        n=dst
+        while n!=src:
+            pn,pi=prev[n]
+            fl=min(fl,edges[pn][pi][1])
+            n=pn
 
         n=dst
         while n!=src:
             pn,pi=prev[n]
-            e,f,p,c=edges[pn][pi]
-            edges[pn][pi][1]-=1
-            edges[e][p][1]+=1
+            e,f,p,cost=edges[pn][pi]
+            edges[pn][pi][1]-=fl
+            edges[e][p][1]+=fl
             n=pn
 
-        cost_sum+=fl
-
+        flow-=fl
+        cost_sum+=fl*dist[dst]
     return cost_sum
 
-print(-min_cost_flow(src,dst))
+print(-min_cost_flow(SRC,DST,flow))
