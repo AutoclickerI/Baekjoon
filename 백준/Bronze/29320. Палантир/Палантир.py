4637c844ac2307f94e23187f47fdaed3f87db42c
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2026-04-20 21:46:32 +0900

    [Bronze II] Title: Палантир, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/29320. Палантир/Палантир.py
+++ 백준/Bronze/29320. Палантир/Палантир.py
@@ -1,3 +1,3 @@
 n,m,x,y,r,a,b,t=map(int,open(0).read().split())
-f=lambda p,v,l:(q:=[p,l-p][0<v])<=(d:=abs(v)*t)and(d-q)//l+1
+f=lambda p,v,l:(abs(v)*t-[p,l-p][0<v]+l)//l
 print(f(x-r,a,n-2*r)+f(y-r,b,m-2*r))
