0d115d506da2c34312b44100483a1847370bf0f8
Jihoon Kim <87906440+AutoclickerI@users.noreply.github.com>
2026-04-24 15:30:20 +0900

    [Gold III] Title: 유전자, Time: 6952 ms, Memory: 49220 KB -BaekjoonHub

--- 백준/Gold/2306. 유전자/유전자.py
+++ 백준/Gold/2306. 유전자/유전자.py
@@ -1,12 +1,12 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+from functools import*
+
+s=input()
+@cache
+def f(i,j):
+    if j<i+2:
+        return 0
+    if i+2==j:
+        return s[i:i+2]in('at','gc')
+    return max((s[i]+s[j-1]in('at','gc'))+f(i+1,j-1),f(i+1,j),f(i,j-1),max(f(i,k)+f(k,j)for k in range(i+1,j-1)))
+
+print(f(0,len(s))*2)
